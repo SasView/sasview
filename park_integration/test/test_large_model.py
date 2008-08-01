cfe97ea0b428b0187f4d80a432800b8dea085a37
Gervaise Alina <gervyh@hotmail.com>
2008-08-01 18:51:22 +0000

    add more tests

--- park_integration/test/test_large_model.py
+++ park_integration/test/test_large_model.py
@@ -13,7 +13,7 @@ class testFitModule(unittest.TestCase):
         #load data
         from sans.fit.Loader import Load
         load= Load()
-        load.set_filename("testdata_line.txt")
+        load.set_filename("cyl_testdata.txt")
         load.set_values()
         data1 = Data1D(x=[], y=[],dx=None, dy=None)
         load.load_data(data1)
@@ -30,14 +30,17 @@ class testFitModule(unittest.TestCase):
         # Receives the type of model for the fitting
         from sans.models.CylinderModel import CylinderModel
         model1  = CylinderModel()
-        model2  = CylinderModel()
+        #model2  = CylinderModel()
         
         #Do the fit SCIPY
         fitter.set_data(data1,1)
-        fitter.set_model(model1,"M1",1,)
+        import math
+        pars1={'background':0,'contrast':3*math.pow(10, -6),\
+                'cyl_phi':1,'cyl_theta':1,'length':400,'radius':20,'scale':1}
+        fitter.set_model(model1,"M1",1,pars1)
         
-        fitter.set_data(data2,2)
-        fitter.set_model(model1,"M1",2,None)
+        #fitter.set_data(data2,2)
+        #fitter.set_model(model1,"M1",2,pars1)
         
         chisqr1, out1, cov1=fitter.fit()
         print "park",chisqr1, out1, cov1
