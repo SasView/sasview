b146d0680b586f347d53dd2e6f3c9261007c7e60
Gervaise Alina <gervyh@hotmail.com>
2009-01-13 21:00:08 +0000

    comment new_plot.info in post_data. info unknown for model2D plot

--- guiframe/local_perspectives/plotting/SectorSlicer.py
+++ guiframe/local_perspectives/plotting/SectorSlicer.py
@@ -174,7 +174,7 @@ class SectorInteractor(_BaseInteractor):
        
 
         new_plot.source=self.base.data2D.source
-        new_plot.info=self.base.data2D.info
+        #new_plot.info=self.base.data2D.info
         new_plot.interactive = True
         #print "loader output.detector",output.source
         new_plot.detector =self.base.data2D.detector
