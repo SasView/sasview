1b6bb09972e2bb836f6d485078d82c6ac75e71b8
Gervaise Alina <gervyh@hotmail.com>
2009-01-15 13:48:51 +0000

    slicer event added to load panel

--- guicomm/events.py
+++ guicomm/events.py
@@ -1,3 +1,4 @@
 import wx.lib
 (NewPlotEvent, EVT_NEW_PLOT) = wx.lib.newevent.NewEvent()
 (StatusEvent,  EVT_STATUS)   = wx.lib.newevent.NewEvent()
+(SlicerParameterEvent, EVT_SLICER_PARS)   = wx.lib.newevent.NewEvent()
