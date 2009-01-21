fe060f7f0582c36481365539884cb99634fe17a9
Gervaise Alina <gervyh@hotmail.com>
2009-01-21 23:44:23 +0000

    added new event--> need to remove some

--- guicomm/events.py
+++ guicomm/events.py
@@ -1,4 +1,6 @@
 import wx.lib
 (NewPlotEvent, EVT_NEW_PLOT) = wx.lib.newevent.NewEvent()
 (StatusEvent,  EVT_STATUS)   = wx.lib.newevent.NewEvent()
-(SlicerParameterEvent, EVT_SLICER_PARS)   = wx.lib.newevent.NewEvent()
+#(SlicerParameterEvent, EVT_SLICER_PARS)   = wx.lib.newevent.NewEvent()
+(SlicerPanelEvent, EVT_SLICER_PANEL)   = wx.lib.newevent.NewEvent()
+(SlicerParamUpdateEvent, EVT_SLICER_PARS_UPDATE)   = wx.lib.newevent.NewEvent()
