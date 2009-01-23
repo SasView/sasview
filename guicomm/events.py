21e0ec415c3f43f49fd006799da1adccbb746e81
Gervaise Alina <gervyh@hotmail.com>
2009-01-23 18:28:00 +0000

    one event added and comment added

--- guicomm/events.py
+++ guicomm/events.py
@@ -1,6 +1,11 @@
 import wx.lib
+# plot data
 (NewPlotEvent, EVT_NEW_PLOT) = wx.lib.newevent.NewEvent()
+# print the messages on statusbar
 (StatusEvent,  EVT_STATUS)   = wx.lib.newevent.NewEvent()
-#(SlicerParameterEvent, EVT_SLICER_PARS)   = wx.lib.newevent.NewEvent()
+#event to get id for model view 2D panel
+(Model2DPanelEvent, EVT_MODEL2D_PANEL)   = wx.lib.newevent.NewEvent()
+#create a panel slicer 
 (SlicerPanelEvent, EVT_SLICER_PANEL)   = wx.lib.newevent.NewEvent()
+#print update paramaters for panel slicer 
 (SlicerParamUpdateEvent, EVT_SLICER_PARS_UPDATE)   = wx.lib.newevent.NewEvent()
