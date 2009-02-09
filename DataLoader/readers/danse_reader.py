eeaabb151a5384921a045bd185de7be6d62ff365
Jae Cho <jhjcho@gmail.com>
2009-02-09 17:51:36 +0000

    The titles and units on 2d plots are corrected.

--- DataLoader/readers/danse_reader.py
+++ DataLoader/readers/danse_reader.py
@@ -86,13 +86,13 @@ class Reader:
             data_conv_q = None
             data_conv_i = None
             
-            if has_converter == True and output.Q_unit != '1/A':
-                data_conv_q = Converter('1/A')
+            if has_converter == True and output.Q_unit != 'A^{-1}':
+                data_conv_q = Converter('A^{-1}')
                 # Test it
                 data_conv_q(1.0, output.Q_unit)
                 
-            if has_converter == True and output.I_unit != '1/cm':
-                data_conv_i = Converter('1/cm')
+            if has_converter == True and output.I_unit != 'cm^{-1}':
+                data_conv_i = Converter('cm^{-1}')
                 # Test it
                 data_conv_i(1.0, output.I_unit)            
         
@@ -160,13 +160,13 @@ class Reader:
                 theta = (i_x-center_x+1)*pixel / distance / 100.0
                 qx = 4.0*math.pi/wavelength * math.sin(theta/2.0)
                 
-                if has_converter == True and output.Q_unit != '1/A':
+                if has_converter == True and output.Q_unit != 'A^{-1}':
                     qx = data_conv_q(qx, units=output.Q_unit)
                 
                 x_vals.append(qx)
-                if xmin==None or qx<xmin:
+                if xmin==None or qx<=xmin:
                     xmin = qx
-                if xmax==None or qx>xmax:
+                if xmax==None or qx>=xmax:
                     xmax = qx
             
             ymin = None
@@ -175,7 +175,7 @@ class Reader:
                 theta = (i_y-center_y+1)*pixel / distance / 100.0
                 qy = 4.0*math.pi/wavelength * math.sin(theta/2.0)
                 
-                if has_converter == True and output.Q_unit != '1/A':
+                if has_converter == True and output.Q_unit != 'A^{-1}':
                     qy = data_conv_q(qy, units=output.Q_unit)
                 
                 y_vals.append(qy)
@@ -241,7 +241,7 @@ class Reader:
             ymin    =ymin-stepq/2.0
             ymax    =ymax+stepq/2.0
             
-            if has_converter == True and output.Q_unit != '1/A':
+            if has_converter == True and output.Q_unit != 'A^{-1}':
                 xmin = data_conv_q(xmin, units=output.Q_unit)
                 xmax = data_conv_q(xmax, units=output.Q_unit)
                 ymin = data_conv_q(ymin, units=output.Q_unit)
@@ -257,11 +257,11 @@ class Reader:
            
             # Units
             if data_conv_q is not None:
-                output.xaxis("\\rm{Q}", output.Q_unit)
-                output.yaxis("\\rm{Q}", output.Q_unit)
+                output.xaxis("\\rm{Q_{x}}", output.Q_unit)
+                output.yaxis("\\rm{Q_{y}}", output.Q_unit)
             else:
-                output.xaxis("\\rm{Q}", 'A^{-1}')
-                output.yaxis("\\rm{Q}", 'A^{-1}')
+                output.xaxis("\\rm{Q_{x}}", 'A^{-1}')
+                output.yaxis("\\rm{Q_{y}}", 'A^{-1}')
                 
             if data_conv_i is not None:
                 output.zaxis("\\{I(Q)}", output.I_unit)
