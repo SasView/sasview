13e120a3730ef9c58d1866954242b56e3beee246
Gervaise Alina <gervyh@hotmail.com>
2009-02-09 20:22:03 +0000

    adding x_bins and y_bins to model after plotting

--- sansview/perspectives/fitting/fitting.py
+++ sansview/perspectives/fitting/fitting.py
@@ -766,9 +766,10 @@ class Plugin:
         complete in %g sec" % elapsed))
         #print "complete",output, model,qmin, qmax
         data = output
-        theory= Data2D(data)
-        #print data.detector
-        #theory.detector= data.detector
+        temp= numpy.zeros(numpy.shape(data))
+        temp[temp==0]=1
+        theory= Data2D(image=data, err_image=temp)
+    
         from DataLoader.data_info import Detector, Source
         
         detector = Detector()
@@ -782,7 +783,33 @@ class Plugin:
         theory.detector[0].distance= 1e+12#13705.0
         theory.source= Source()
         theory.source.wavelength= 2*math.pi/1e+12#8.4
+        theory.x_bins =[]
+        theory.y_bins =[]
+        # compute x_bins and y_bins
+        # Qx and Qy vectors
+    
+        distance   = theory.detector[0].distance
+        pixel      = qstep/2-1
+        theta      = pixel / distance / 100.0
+        wavelength = theory.source.wavelength
+        pixel_width_x = theory.detector[0].pixel_size.x
+        pixel_width_y = theory.detector[0].pixel_size.y
+        center_x      = theory.detector[0].beam_center.x/pixel_width_x
+        center_y      = theory.detector[0].beam_center.y/pixel_width_y
         
+        
+        size_x, size_y= numpy.shape(theory.data)
+        #print "size_x,size_y",size_x, size_y
+        for i_x in range(size_x):
+            theta = (i_x-center_x+1)*pixel_width_x / distance / size_x#100.0
+            qx = 4.0*math.pi/wavelength * math.sin(theta/2.0)
+            theory.x_bins.append(qx)    
+        for i_y in range(size_y):
+            theta = (i_y-center_y+1)*pixel_width_y / distance / size_y#100.0
+            qy = 4.0*math.pi/wavelength * math.sin(theta/2.0)
+            theory.y_bins.append(qy)
+           
+        #theory.err_data= numpy.    
         theory.name= model.name
         theory.group_id ="Model"
         theory.id ="Model"
