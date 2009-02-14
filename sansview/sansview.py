365a17f615526a6b3e91f8933f2e763e83125eef
Mathieu Doucet <doucetm@ornl.gov>
2009-02-14 00:55:35 +0000

    Preparing for executable build

--- sansview/sansview.py
+++ sansview/sansview.py
@@ -18,6 +18,12 @@ class SansView():
         # Add perspectives to the basic application
         # Additional perspectives can still be loaded
         # dynamically
+        # Note: py2exe can't find dynamically loaded
+        # modules. We load the fitting module here
+        # to ensure a complete Windows executable build.
+        import perspectives.fitting as module    
+        fitting_plug = module.Plugin()
+        self.gui.add_perspective(fitting_plug)
             
         # Build the GUI
         self.gui.build_gui()
