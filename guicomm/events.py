8dddbd40dc9dbb7cacc39ee67383776859fdb409
Gervaise Alina <gervyh@hotmail.com>
2009-02-28 01:52:00 +0000

    event removed

--- guicomm/events.py
+++ guicomm/events.py
@@ -3,8 +3,6 @@ import wx.lib
 (NewPlotEvent, EVT_NEW_PLOT) = wx.lib.newevent.NewEvent()
 # print the messages on statusbar
 (StatusEvent,  EVT_STATUS)   = wx.lib.newevent.NewEvent()
-#event to get id for model view 2D panel
-(Model2DPanelEvent, EVT_MODEL2D_PANEL)   = wx.lib.newevent.NewEvent()
 #create a panel slicer 
 (SlicerPanelEvent, EVT_SLICER_PANEL)   = wx.lib.newevent.NewEvent()
 #print update paramaters for panel slicer 
