de1da345776b44ad0d3bb2f5e75ed2230de5ae09
Jae Cho <jhjcho@gmail.com>
2009-03-10 13:47:19 +0000

    Added load-ability to get dQ (for smear) if exists, and capability to sort the data by Q as a key

--- DataLoader/readers/ascii_reader.py
+++ DataLoader/readers/ascii_reader.py
@@ -51,7 +51,15 @@ class Reader:
                 x  = numpy.zeros(0)
                 y  = numpy.zeros(0)
                 dy = numpy.zeros(0)
-                output = Data1D(x, y, dy=dy)
+                dx = numpy.zeros(0)
+                
+               #temp. space to sort data
+                tx  = numpy.zeros(0)
+                ty  = numpy.zeros(0)
+                tdy = numpy.zeros(0)
+                tdx = numpy.zeros(0)
+                
+                output = Data1D(x, y, dy=dy, dx=dx)
                 self.filename = output.filename = basename
            
                 data_conv_q = None
@@ -70,7 +78,8 @@ class Reader:
                 
                 # The first good line of data will define whether
                 # we have 2-column or 3-column ascii
-                has_error = None
+                has_error_dx = None
+                has_error_dy = None
                 
                 for line in lines:
                     toks = line.split()
@@ -101,13 +110,41 @@ class Reader:
                             
                         # If we haven't set the 3rd column
                         # flag, set it now.
-                        if has_error == None:
-                            has_error = False if _dy == None else True
+                        if has_error_dy == None:
+                            has_error_dy = False if _dy == None else True
+                            
+                        #Check for dx
+                        _dx = None
+                        if len(toks)>3:
+                            try:
+                                _dx = float(toks[3])
+                                
+                                if data_conv_i is not None:
+                                    _dx = data_conv_i(_dx, units=output.x_unit)
+                                
+                            except:
+                                # The 4th column is not a float, skip it.
+                                pass
+                            
+                        # If we haven't set the 3rd column
+                        # flag, set it now.
+                        if has_error_dx == None:
+                            has_error_dx = False if _dx == None else True
 
                         x  = numpy.append(x,   _x) 
                         y  = numpy.append(y,   _y)
-                        if has_error == True:
+                        if has_error_dy == True:
                             dy = numpy.append(dy, _dy)
+                        if has_error_dx == True:
+                            dx = numpy.append(dx, _dx)
+                            
+                        #Same for temp.
+                        tx  = numpy.append(tx,   _x) 
+                        ty  = numpy.append(ty,   _y)
+                        if has_error_dy == True:
+                            tdy = numpy.append(tdy, _dy)
+                        if has_error_dx == True:
+                            tdx = numpy.append(tdx, _dx)
                         
                     except:
                         # Couldn't parse this line, skip it 
@@ -115,17 +152,31 @@ class Reader:
                          
                      
                 # Sanity check
-                if has_error == True and not len(y) == len(dy):
+                if has_error_dy == True and not len(y) == len(dy):
+                    raise RuntimeError, "ascii_reader: y and dy have different length"
+                if has_error_dx == True and not len(x) == len(dx):
                     raise RuntimeError, "ascii_reader: y and dy have different length"
 
                 # If the data length is zero, consider this as
                 # though we were not able to read the file.
                 if len(x)==0:
                     raise RuntimeError, "ascii_reader: could not load file"
-               
+                
+                #Let's reoder the data
+                ind = numpy.lexsort((ty,tx))
+                for i in ind:
+                    x[i] = tx[ind[i]]
+                    y[i] = ty[ind[i]]
+                    if has_error_dy == True:
+                        dy[i] = tdy[ind[i]]
+                    if has_error_dx == True:
+                        dx[i] = tdx[ind[i]]
+                    
                 output.x = x
                 output.y = y
-                output.dy = dy if has_error == True else None
+                output.dy = dy if has_error_dy == True else None
+                output.dx = dx if has_error_dx == True else None
+                
                 if data_conv_q is not None:
                     output.xaxis("\\rm{Q}", output.x_unit)
                 else:
