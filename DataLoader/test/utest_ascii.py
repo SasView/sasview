7a4401e0c18c7c9cbb0db42feb36e31baf484a58
Mathieu Doucet <doucetm@ornl.gov>
2009-04-09 23:24:23 +0000

    Unit test for new ascii reader implementation

--- DataLoader/test/utest_ascii.py
+++ DataLoader/test/utest_ascii.py
@@ -15,10 +15,39 @@ class abs_reader(unittest.TestCase):
         self.loader = Loader()
         
     def test_checkdata(self):
-        # Test .ABS file loaded as ascii
+        """
+            Test .ABS file loaded as ascii
+        """
         f = self.loader.load("ascii_test_1.txt")
         # The length of the data is 10
         self.assertEqual(len(f.x), 10)
+        
+    def test_truncated_1(self):
+        """
+            Test an ascii file with header and a 
+            comment line in the middle of the data section.
+            The business rule says that we should stop
+            reading at the first comment once the data
+            section has started (and treat the comment
+            as though it were the start of a footer).
+        """
+        # Test .ABS file loaded as ascii
+        f = self.loader.load("ascii_test_2.txt")
+        # The length of the data is 10
+        self.assertEqual(len(f.x), 5)
+        
+    def test_truncated_2(self):
+        """
+            Test a 6-col ascii file with header and a 
+            line with only 2 columns in the middle of the data section.
+            The business rule says that we should stop
+            reading at the first inconsitent line.
+        """
+        # Test .ABS file loaded as ascii
+        f = self.loader.load("ascii_test_3.txt")
+        # The length of the data is 10
+        self.assertEqual(len(f.x), 5)
+        
         
         
 if __name__ == '__main__':
