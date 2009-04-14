7b78bb63ccb70269d0a5ae6ef8c62431581d7756
Gervaise Alina <gervyh@hotmail.com>
2009-04-14 19:50:45 +0000

    compuite chiqsr for 2 d

--- sansview/perspectives/fitting/fitpage.py
+++ sansview/perspectives/fitting/fitpage.py
@@ -494,7 +494,11 @@ class FitPage(BasicPage):
         """
         from sans.guiframe.utils import check_value
         flag = check_value( self.qmin, self.qmax)
-       
+        err_image = self.data.err_image
+        if err_image==[] or err_image==None:
+            err_image= numpy.zeros(len(self.data.x_bins),len(self.data.y_bins))
+                       
+        err_image[err_image==0]=1
         res=[]
         if flag== True:
             try:
@@ -503,13 +507,13 @@ class FitPage(BasicPage):
                 for i in range(len(self.data.x_bins)):
                     for j in range(len(self.data.y_bins)):
                         #Check the range containing data between self.qmin_x and self.qmax_x
-                        if math.pow(self.data.x_bins[i],2)+math.pow(self.data.y_bins[j],2)>=math.pow(self.qmin_x,2):
-                            if math.pow(self.data.x_bins[i],2)+math.pow(self.data.y_bins[j],2)<=math.pow(self.qmax_x,2):
-                                chisqrji=(self.data.data[j][i]- self.model.runXY(\
-                                                                                    [self.data.x_bins[i],self.data.y_bins[j]]))\
-                                                                                    /self.data.err_data[j][i]
-                                #Vector containing residuals
-                                res.append( math.pow(chisqrji,2) )
+                        value =  math.pow(self.data.x_bins[i],2)+ math.pow(self.data.y_bins[j],2)
+                        if value >= math.pow(self.qmin_x,2) and value <= math.pow(self.qmax_x,2):
+                            temp = [self.data.x_bins[i],self.data.y_bins[j]]
+                            error= self.data.err_data[j][i]
+                            chisqrji = (self.data.data[j][i]- self.model.runXY(temp ))/error
+                            #Vector containing residuals
+                            res.append( math.pow(chisqrji,2) )
                 # compute sum of residual
                 sum=0
                 for item in res:
