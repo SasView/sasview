fcad8e45396f4f16e1f777a8d678500f1ce2d1b4
Jae Cho <jhjcho@gmail.com>
2009-04-21 00:59:26 +0000

    updated year and version number

--- sansview/setup_exe.py
+++ sansview/setup_exe.py
@@ -50,7 +50,7 @@ class Target:
     def __init__(self, **kw):
         self.__dict__.update(kw)
         # for the versioninfo resources
-        self.version = "0.1"
+        self.version = "0.9"
         self.company_name = "U Tennessee"
         self.copyright = "copyright 2009"
         self.name = "SansView"
