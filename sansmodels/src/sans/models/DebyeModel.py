d37ba31f90770e95e2b80c5999a7c2c4b4b82acd
Jae Cho <jhjcho@gmail.com>
2009-04-29 14:59:39 +0000

    removed singular

--- sansmodels/src/sans/models/DebyeModel.py
+++ sansmodels/src/sans/models/DebyeModel.py
@@ -63,8 +63,11 @@ class DebyeModel(BaseComponent):
         # Note that a zero denominator value will raise
         # an exception
         y = (x * self.params['rg'])**2.0
-        D = 2.0*( math.exp(-y) + y -1.0 )/y**2.0
-        return self.params['scale']* D + self.params['background']
+        if y == 0:
+            return 1e+16
+        else:
+            D = 2.0*( math.exp(-y) + y -1.0 )/y**2.0
+            return self.params['scale']* D + self.params['background']
    
     def run(self, x = 0.0):
         """ Evaluate the model
