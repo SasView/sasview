bd66af349e57f79c3014909ed224fd0e68ee8971
Gervaise Alina <gervyh@hotmail.com>
2009-06-01 21:18:18 +0000

    add a plugin for plash page

--- sansview/sansview.py
+++ sansview/sansview.py
@@ -4,7 +4,6 @@ from sans.guiframe import gui_manager
 
 # For py2exe, import config here
 import local_config
-import logging
 
 
 class SansView():
@@ -22,7 +21,7 @@ class SansView():
         # Note: py2exe can't find dynamically loaded
         # modules. We load the fitting module here
         # to ensure a complete Windows executable build.
-
+        
         # P(r) perspective
         try:
             import sans.perspectives.pr as module    
@@ -31,9 +30,14 @@ class SansView():
         except:
             logging.error("SansView: could not find P(r) plug-in module") 
 
-        # Fitting perspective
+		# Fitting perspective
         import perspectives.fitting as module    
         fitting_plug = module.Plugin()
+        self.gui.add_perspective(fitting_plug)
+        
+        #plash page perspective
+        import perspectives.splashinfo as module
+        fitting_plug =module.Plugin()
         self.gui.add_perspective(fitting_plug)
         
         # Build the GUI
