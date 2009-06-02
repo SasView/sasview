2d39535e87cdea8d0e921f044b9b5182eae0728a
Gervaise Alina <gervyh@hotmail.com>
2009-06-02 12:46:14 +0000

    restore from previous version

--- sansview/sansview.py
+++ sansview/sansview.py
@@ -6,6 +6,7 @@ from sans.guiframe import gui_manager
 import local_config
 import logging
 
+
 class SansView():
     
     def __init__(self):
@@ -21,7 +22,7 @@ class SansView():
         # Note: py2exe can't find dynamically loaded
         # modules. We load the fitting module here
         # to ensure a complete Windows executable build.
-        
+
         # P(r) perspective
         try:
             import sans.perspectives.pr as module    
@@ -30,14 +31,9 @@ class SansView():
         except:
             logging.error("SansView: could not find P(r) plug-in module") 
 
-		# Fitting perspective
+        # Fitting perspective
         import perspectives.fitting as module    
         fitting_plug = module.Plugin()
-        self.gui.add_perspective(fitting_plug)
-        
-        #plash page perspective
-        import perspectives.splashinfo as module
-        fitting_plug =module.Plugin()
         self.gui.add_perspective(fitting_plug)
         
         # Build the GUI
