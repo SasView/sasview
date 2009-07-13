770e4c9e7d8f55e328b706d080c8771925265c1e
Gervaise Alina <gervyh@hotmail.com>
2009-07-13 13:53:21 +0000

    reove unused model module

--- sansmodels/src/sans/models/c_models/c_models.cpp
+++ sansmodels/src/sans/models/c_models/c_models.cpp
@@ -34,7 +34,6 @@ void addCHollowCylinderModel(PyObject *module);
 void addCMultiShellModel(PyObject *module);
 void addCVesicleModel(PyObject *module);
 void addCBinaryHSModel(PyObject *module);
-void addCBinaryHSPSF11Model(PyObject *module);
 
 
 extern "C" {
@@ -188,7 +187,6 @@ initc_models(void)
 	addCHollowCylinderModel(m);
 	addCMultiShellModel(m);
 	addCBinaryHSModel(m);
-	addCBinaryHSPSF11Model(m);
 	addDisperser(m);
 	addCGaussian(m);
 	addCLorentzian(m);
