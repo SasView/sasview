7d8c41fa51c3af95e21170fb08d72a053fbb1601
Mathieu Doucet <doucetm@ornl.gov>
2009-09-06 02:43:21 +0000

    pr_inversion: added d_max explorer as independent computation module [should make similar code in PrView obsolete]

---
+++ pr_inversion/test/utest_explorer.py
@@ -0,0 +1,34 @@
+"""
+    Unit tests for DistExplorer class
+"""
+
+import unittest, math, numpy
+from utest_invertor import load
+from sans.pr.invertor import Invertor
+from sans.pr.distance_explorer import DistExplorer
+        
+class TestExplorer(unittest.TestCase):
+            
+    def setUp(self):
+        self.invertor = Invertor()
+        x, y, err = load('sphere_80.txt')
+        
+        # Choose the right d_max...
+        self.invertor.d_max = 160.0
+        # Set a small alpha
+        self.invertor.alpha = .0007
+        # Set data
+        self.invertor.x   = x
+        self.invertor.y   = y
+        self.invertor.err = err        
+        self.invertor.nfunc = 15
+        
+        self.explo = DistExplorer(self.invertor)
+        
+    def test_exploration(self):
+        results = self.explo(120, 200, 25)
+        self.assertEqual(len(results.errors), 0)
+        self.assertEqual(len(results.chi2), 25)
+        
+if __name__ == '__main__':
+    unittest.main()
