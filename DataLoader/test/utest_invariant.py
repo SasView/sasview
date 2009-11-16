d23528ee8d8b2c9bc69e68c4120d7d76d1da0d18
Gervaise Alina <gervyh@hotmail.com>
2009-11-16 23:47:47 +0000

    working on invariant, must the test are not passing yet.need more testing.incompleted

--- DataLoader/test/utest_invariant.py
+++ DataLoader/test/utest_invariant.py
@@ -2,8 +2,6 @@
     Unit tests for data manipulations
     @author Gervaise Alina: unittest imcoplete so far 
 """
-
-
 import unittest
 import numpy, math
 from DataLoader.loader import  Loader
@@ -11,52 +9,185 @@ from DataLoader.data_info import Data1D, Data2D
 from DataLoader.invariant import InvariantCalculator
 
 
-class InvariantTest(unittest.TestCase):
+class TestInvPolySphere(unittest.TestCase):
+    """
+        Test unsmeared data for invariant computation
+    """
+    def setUp(self):
+        #Data with no slit smear information
+        data= Loader().load("PolySpheres.txt")
+        self.I = InvariantCalculator( data= data)
+        
+     
+    def testWrongData(self):
+        """ test receiving Data1D not of type loader"""
+        #compute invariant with smear information
+        from danse.common.plottools.plottables import Data1D
+        data= Data1D(x=[1,2],y=[2,3] )
+        try:
+            self.assertRaises(ValueError,InvariantCalculator(data))
+        except ValueError, msg:
+            print "test pass "+ str(msg)
+        else: raise ValueError, "fail to raise exception when expected"
+    
+    def testInvariant(self):
+        """ test the invariant value for data without smearing"""
+        self.assertAlmostEquals(self.I.q_star, 7.48959e-5,2)
+        
+    def testVolume(self):
+        """ test volume fraction for polysphere"""
+        volume = self.I.get_volume_fraction(contrast=2.6e-6)
+        self.assert_(volume >= 0 and volume <=1)
+        self.assertAlmostEqual(volume ,0.01)
+        
+    def testSurface(self):
+        """ test surface for polysphere """
+        surface = self.I.get_surface(contrast=2.6e-6, porod_const=20)
+        self.assertAlmostEqual(surface,0.01)
+      
+      
+class TestInvariantSmear(unittest.TestCase):
+    """
+        Test smeared data for invariant computation
+    """
+    def setUp(self):
+        # data with smear info
+        list = Loader().load("latex_smeared.xml")
+        self.data_q_smear = list[0]
+        self.I_q_smear = InvariantCalculator( data= self.data_q_smear)
+        
+        self.data_slit_smear = list[1]
+        self.I_slit_smear = InvariantCalculator( data= self.data_slit_smear)
+        
+        from sans.models.PowerLawModel import PowerLawModel
+        self.power_law = PowerLawModel()
+        from sans.models.GuinierModel import GuinierModel
+        self.guinier = GuinierModel()
+     
+     
+    def test_invariant_slit(self):
+        """ test the invariant value for data with slit smear"""
+        self.assertTrue(self.I_slit_smear.q_star>0)
+        self.assertAlmostEquals(self.I_slit_smear.q_star, 4.1539e-4)
+   
+    def test_volume_slit_smear(self):
+        """ test volume fraction for data with slit smear"""
+        volume = self.I_slit_smear.get_volume_fraction(contrast=2.6e-6)
+        self.assert_(volume >= 0 and volume <=1)
+        self.assertAlmostEqual(volume ,0.01)
+        
+    def test_invariant_slit_low_guinier(self):
+        """ test the invariant with slit smear for data extrapolated low q range using guinier"""
+        q_star_min = self.I_slit_smear.get_qstar_min( data= self.data_slit_smear,
+                                                       model= self.guinier, npts=10)
+        self.assertAlmostEquals(q_star_min, 4.89783e-8)
+        self.assertAlmostEquals(self.I.q_star_min, q_star_min)
+          
+    def test_invariant_slit_low_power(self):
+        """ test the invariant with slit smear for data extrapolated low q range using pow_law"""
+        q_star_min = self.I_slit_smear.get_qstar_min( data=self.data_slit_smear,
+                                                       model= self.power_law, npts=10)
+        self.assertFalse(numpy.isfinite(q_star_min))
+        self.assertEquals(self.I.q_star_min, q_star_min)
+        
+    def test_invariant_slit_high_power(self):
+        """ test the invariant with slit smear for data extrapolated high q range using pow_law"""
+        q_star_max = self.I_slit_smear.get_qstar_max( data= self.data_slit_smear,
+                                                       model= self.power_law, npts=10)
+        self.assertFalse(numpy.isfinite(q_star_max))
+        self.assertEquals(self.I.q_star_max, q_star_max)
+        
+    def testSurface(self):
+        """ test volume fraction for data with slit smear"""
+        surface = self.I_q_smear.get_surface(contrast=2.6e-6, porod_const=20)
+        self.assertAlmostEqual(surface,0.01)
+        
+    def test_invariant_q(self):
+        """ test the invariant value for data with q smear"""
+        self.assertTrue(self.I_q_smear.q_star>0)
+        self.assertAlmostEquals(self.I_q_smear.q_star, 1.361677e-3)
+        
+    def test_invariant_q_low_guinier(self):
+        """ test the invariant with q smear for data extrapolated low q range using guinier"""
+        q_star_min = self.I_slit_smear.get_qstar_min( data= self.data_q_smear,
+                                                       model= self.guinier, npts=10)
+        self.assertAlmostEquals(q_star_min,6.76e-4)
+        self.assertAlmostEquals(self.I.q_star_min, q_star_min)
+          
+    def test_invariant_q_low_power(self):
+        """ test the invariant with q smear for data extrapolated low q range using pow_law"""
+        q_star_min = self.I_slit_smear.get_qstar_min( data= self.data_q_smear,
+                                                       model= self.power_law, npts=10)
+        self.assertFalse(numpy.isfinite(q_star_min))
+        self.assertEquals(self.I.q_star_min, q_star_min)
+        
+    def test_invariant_q_high_power(self):
+        """ test the invariant with q smear for data extrapolated high q range using pow_law"""
+        q_star_max = self.I_slit_smear.get_qstar_max( data= self.data_q_smear,
+                                                       model= self.power_law, npts=10)
+        self.assertAlmostEquals(q_star_max,1.2e-4)
+        self.assertEquals(self.I.q_star_max, q_star_max)
+      
+    def test_volume_q_smear(self):
+        """ test volume fraction for data with q smear"""
+        volume = self.I_slit_smear.get_volume_fraction(contrast=2.6e-6)
+        self.assert_(volume > 0 and volume <=1)
+        self.assertAlmostEqual(volume ,0.01)
+        
+    def testSurface_q_smear(self):
+        """ test surface for data with q smear"""
+        surface = self.I_q_smear.get_surface(contrast=2.6e-6, porod_const=20)
+        self.assertAlmostEqual(surface,0.01)
+      
+      
+class ExtrapolationTest(unittest.TestCase):
     
     def setUp(self):
         #Data with no slit smear information
-        data0 = Loader().load("PolySpheres.txt")
-        self.I0 = InvariantCalculator( data=data0)
+        self.data= Loader().load("PolySpheres.txt")
+        from sans.models.PowerLawModel import PowerLawModel
+        self.power_law = PowerLawModel()
+        from sans.models.GuinierModel import GuinierModel
+        self.guinier = GuinierModel()
+        self.I = InvariantCalculator( data= self.data)
+       
         
-        # data with smear info
-        list = Loader().load("latex_smeared.xml")
-        data1= list[0]
-        self.I1= InvariantCalculator( data= data1)
+    def testInvariant(self):
+        """ test the invariant value for data extrapolated"""
+        self.assertAlmostEquals(self.I.q_star, 7.48959e-5)
         
-        data2= list[1]
-        self.I2= InvariantCalculator( data= data2)
-     
-    def test_invariant(self):
-        """ test the invariant value for data with no slit smear"""
-        self.assertAlmostEquals(self.I0.q_star, 7.48959e-5)
-        self.assertTrue(self.I1.q_star>0)
-        self.assertTrue(self.I2.q_star>0)
+    def testInvariantLowGuinier(self):
+        """ test the invariant value for data extrapolated low range using guinier"""
+        q_star_min = self.I.get_qstar_min( data= self.data, model= self.guinier, npts=10)
+        self.assertAlmostEquals(q_star_min, 4.89783e-8)
+        self.assertAlmostEquals(self.I.q_star_min, q_star_min)
+          
+    def testInvariantLowPower(self):
+        """ test the invariant value for data extrapolated low range using pow_law"""
+        q_star_min = self.I.get_qstar_min( data= self.data, model= self.power_law, npts=10)
+        self.assertFalse(numpy.isfinite(q_star_min))
+        self.assertEquals(self.I.q_star_min, q_star_min)
+          
+    def testInvariantHigh(self):
+        """ test the invariant value for data extrapolated high range"""
+        q_star_max = self.I.get_qstar_max( self.data, model=self.power_law, npts=10)
+        self.assertAlmostEquals(q_star_max, 4.066202e-6)
+        self.assertAlmostEquals(self.I0.q_star_max, q_star_max)
         
-    def test_computation(self):
-        """
-            Test identity smearing
-        """
-        vol = self.I0.get_volume_fraction(contrast=2.6e-6)
-        surface = self.I0.get_surface(contrast=2.6e-6, porod_const=20)
+    def testInvariantTotal(self):
+        """ test the total invariant value for data extrapolated"""
+        self.assertAlmostEquals(self.I.q_star_total, 7.88981e-5)  
         
-        # TODO: Need to test output values
-        #self.assertAlmostEquals(vol, 0)
-        #self.assertAlmostEquals(surface, 0)
-        vol = self.I1.get_volume_fraction(contrast=5.3e-6)
-        surface = self.I1.get_surface(contrast=5.3e-6, porod_const=20)
-        
-        # TODO: Need to test output values
-        #self.assertAlmostEquals(vol, 0)
-        #self.assertAlmostEquals(surface, 0)
-        
-        vol = self.I2.get_volume_fraction(contrast=5.3e-6)
-        surface = self.I2.get_surface(contrast=5.3e-6, porod_const=20)
-        
-        # TODO: Need to test output values
-        self.assertAlmostEquals(vol, 0)
-        self.assertAlmostEquals(surface, 0)
-        
-      
-        
+    def testVolume(self):
+        """ test volume fraction for data extrapolated"""
+        volume = self.I.get_volume_fraction(contrast=2.6e-6)
+        self.assert_(volume > 0 and volume <=1)
+        self.assertAlmostEqual(volume ,0.01)
+    
+    def testSurface(self):
+        """ test surface for data extrapolated"""
+        surface = self.I.get_surface(contrast=2.6e-6, porod_const=20)
+        self.assertAlmostEqual(surface,0.01)
+            
 if __name__ == '__main__':
     unittest.main()
