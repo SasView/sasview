5d91e9c1987a6f48956d9c1e6266a3a3cb8487b3
Mathieu Doucet <doucetm@ornl.gov>
2009-11-24 17:58:42 +0000

    invariant: modify setup to comply to our usual standard

--- Invariant/setup.py
+++ Invariant/setup.py
@@ -1,24 +1,18 @@
 """
     Setup for invariant
 """
-
-import sys
-if len(sys.argv) == 1:
-    sys.argv.append('install')
-# Then build and install the modules
 from distutils.core import setup
 
 setup(
-    name="sans.Invariant",
+    name="sans.invariant",
     version = "0.1",
-    description = "Python module for fitting",
+    description = "Python module for invariant calculation",
     author = "University of Tennessee",
-    #author_email = "",
     url = "http://danse.chem.utk.edu",
     
     # Use the pure python modules
-    package_dir = {"sans.Invariant":"."},
+    package_dir = {"sans.invariant":"."},
     
-    packages = ["sans.Invariant"]
+    packages = ["sans.invariant"]
     )
         
