cfaf6d908f1f90339c5384c230411b256bc6c414
Gervaise Alina <gervyh@hotmail.com>
2009-12-15 01:26:25 +0000

    calculator module created

---
+++ calculator/setup.py
@@ -0,0 +1,18 @@
+"""
+    Setup for Calculator plug-in
+"""
+from distutils.core import setup
+
+setup(
+    name="sans.calculator",
+    version = "0.1",
+    description = "Python module for sld  calculation",
+    author = "University of Tennessee",
+    url = "http://danse.chem.utk.edu",
+    
+    # Use the pure python modules
+    package_dir = {"sans.calculator":"."},
+    
+    packages = ["sans.calculator"]
+    )
+        
