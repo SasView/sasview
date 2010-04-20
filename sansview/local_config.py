9ce70c2124c942ee40c3b7a91d63a465c9578ba9
Jae Cho <jhjcho@gmail.com>
2010-04-20 13:38:45 +0000

    updated the version number

--- sansview/local_config.py
+++ sansview/local_config.py
@@ -5,7 +5,7 @@ import time
 
 # Version of the application
 __appname__ = "SansView"
-__version__ = '1.0.0'
+__version__ = '1.3.0'
 __download_page__ = 'http://danse.chem.utk.edu'
 __update_URL__ = 'http://danse.chem.utk.edu/sansview_version.php'
 
