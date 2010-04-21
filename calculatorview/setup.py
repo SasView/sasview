2d3e12384322733fd5b9ceebef7af31844b65eae
Gervaise Alina <gervyh@hotmail.com>
2010-04-21 18:22:48 +0000

    setup for calculator

--- calculatorview/setup.py
+++ calculatorview/setup.py
@@ -2,14 +2,25 @@
      Installation script for DANSE calculator perspective for SansView
 """
 
-from distutils.core import setup
+import os
 
+    
+from distutils.core import setup
+from distutils.sysconfig import get_python_lib
+from distutils.filelist import findall
+new_path = os.path.join(get_python_lib(),"sans","perspectives","calculator","media")
+path = "media"
+data_files = []
+for f in findall(path):
+    if os.path.split(f)[0].count('.svn')==0:
+        data_files.append((new_path, [f]))
 setup(
     name="calculator",
-    description = "calculator perspective for SansView",
-    package_dir = {"sans.perspectives":"perspectives",
+    description="calculator perspective for SansView",
+    package_dir={"sans.perspectives":"perspectives",
                    "sans.perspectives.calculator":"perspectives/calculator"},
-    packages = ["sans.perspectives",
+    data_files=data_files,
+    packages=["sans.perspectives",
                 "sans.perspectives.calculator"],
     )
 
