481ad379d030a725114cdda76265727806c19c3f
Mathieu Doucet <doucetm@ornl.gov>
2010-05-03 13:04:29 +0000

    DataLoader: update setup.py to allow install with --prefix

--- DataLoader/setup.py
+++ DataLoader/setup.py
@@ -5,7 +5,6 @@ import os, sys
     
 # Then build and install the modules
 from distutils.core import setup, Extension
-from distutils.sysconfig import get_python_lib
 from numpy.distutils.misc_util import get_numpy_include_dirs
 numpy_incl_path = os.path.join(get_numpy_include_dirs()[0], "numpy")
 setup(
@@ -17,7 +16,7 @@ setup(
     
     package_dir = {"DataLoader":".",
                    "DataLoader.extensions":"extensions"},
-    data_files=[(os.path.join(get_python_lib(),"DataLoader","readers"), ["readers/defaults.xml"])],
+    package_data={"DataLoader.readers": ['defaults.xml']},
     packages = ["DataLoader","DataLoader.readers","DataLoader.extensions"],
     
     ext_modules = [ Extension("DataLoader.extensions.smearer",
