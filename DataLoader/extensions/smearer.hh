a378756bc9e0bad1707ae73172ade1321b362a08
Jae Cho <jhjcho@gmail.com>
2010-05-14 20:41:07 +0000

    changed the default value (10000 => 1000) of the number of points used in the smearing computation; It still seems too large(?).

--- DataLoader/extensions/smearer.hh
+++ DataLoader/extensions/smearer.hh
@@ -60,7 +60,7 @@ class SlitSmearer : public BaseSmearer {
 
 protected:
     // Number of points used in the smearing computation
-    static const int npts   = 10000;
+    static const int npts   = 1000;
 
 public:
     // Slit width in Q units
