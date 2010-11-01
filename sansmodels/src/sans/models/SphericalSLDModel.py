4b3d25baa17f0bb0a51e61b205a2b0b0c2548415
Jae Cho <jhjcho@gmail.com>
2010-11-01 20:24:58 +0000

    new model and some bug fixes

--- sansmodels/src/sans/models/SphericalSLDModel.py
+++ sansmodels/src/sans/models/SphericalSLDModel.py
@@ -1,12 +1,13 @@
    
 from sans.models.BaseComponent import BaseComponent
-from sans.models.ReflModel import ReflModel
+from sans.models.SphereSLDModel import SphereSLDModel
 from copy import deepcopy
 from math import floor
-from scipy.special import erf
-func_list = {'Erf':0, 'Linear':1}
+#from scipy.special import erf
+func_list = {'Erf(|nu|*z)':0, 'RPower(z^|nu|)':1, 'LPower(z^|nu|)':2, \
+                     'RExp(-|nu|*z)':3, 'LExp(-|nu|*z)':4}
 max_nshells = 10
-class ReflectivityModel(BaseComponent):
+class SphericalSLDModel(BaseComponent):
     """
     This multi-model is based on Parratt formalism and provides the capability
     of changing the number of layers between 0 and 10.
@@ -20,14 +21,14 @@ class ReflectivityModel(BaseComponent):
 
         ## Setting  model name model description
         self.description=""
-        model = ReflModel()
+        model = SphereSLDModel()
         self.model = model
-        self.name = "ReflectivityModel"
+        self.name = "SphericalSLDModel"
         self.description=model.description
-        self.n_layers = multfactor
+        self.n_shells = multfactor
         ## Define parameters
         self.params = {}
-
+        
         ## Parameter details [units, min, max]
         self.details = {}
         
@@ -46,13 +47,13 @@ class ReflectivityModel(BaseComponent):
         
         #list of parameter that can be fitted
         self._set_fixed_params()  
-        self.model.params['n_layers'] = self.n_layers
+        self.model.params['n_shells'] = self.n_shells
         
         ## functional multiplicity info of the model
         # [int(maximum no. of functionality),"str(Titl),
         # [str(name of function0),...], [str(x-asix name of sld),...]]
-        self.multiplicity_info = [max_nshells,"No. of Layers:",[],['Depth']]
-
+        self.multiplicity_info = [max_nshells,"No. of Shells:",[],['Radius']]
+        
     
     def _clone(self, obj):
         """
@@ -86,36 +87,38 @@ class ReflectivityModel(BaseComponent):
         for name , value in self.model.params.iteritems():
             n = 0
             pos = len(name.split('_'))-1
-            if name.split('_')[0] == 'sldIM':
+            first_name = name.split('_')[0]
+            last_name = name.split('_')[pos]
+            if first_name == 'npts':
+                self.params[name]=value
                 continue
-            elif name.split('_')[0] == 'func':
+            elif first_name == 'func':
                 n= -1
-                while n<self.n_layers:
+                while n<self.n_shells:
                     n += 1
-                    if name.split('_')[pos] == 'inter%s' % str(n):
+                    if last_name == 'inter%s' % str(n): 
                         self.params[name]=value
                         continue
-                #continue
-            elif name.split('_')[pos][0:5] == 'inter':
+            elif last_name[0:5] == 'inter':
                 n= -1
-                while n<self.n_layers:
+                while n<self.n_shells:
                     n += 1
-                    if name.split('_')[pos] == 'inter%s' % str(n):
+                    if last_name == 'inter%s' % str(n):
                         self.params[name]= value
                         continue
-            elif name.split('_')[pos][0:4] == 'flat':
-                while n<self.n_layers:
+            elif last_name[0:4] == 'flat':
+                while n<self.n_shells:
                     n += 1
-                    if name.split('_')[pos] == 'flat%s' % str(n):
+                    if last_name == 'flat%s' % str(n):
                         self.params[name]= value
                         continue
-            elif name == 'n_layers':
+            elif name == 'n_shells':
                 continue
             else:
                 self.params[name]= value
-               
-        self.model.params['n_layers'] = self.n_layers    
-
+  
+        self.model.params['n_shells'] = self.n_shells    
+  
         # set constrained values for the original model params
         self._set_xtra_model_param()       
 
@@ -139,16 +142,15 @@ class ReflectivityModel(BaseComponent):
                 if  key.split('_')[0] == 'thick':
                     self.model.setParam(key, 0)
                     continue
-                if  key.split('_')[0] == 'func':
-                    self.model.setParam(key, 0)
-                    continue
-                for nshell in range(self.n_layers,max_nshells):
+                if  key.split('_')[0] == 'func': 
+                        self.model.setParam(key, 0)
+                        continue
+
+                for nshell in range(self.n_shells,max_nshells):
                     if key.split('_')[1] == 'flat%s' % str(nshell+1):
                         try:
                             if key.split('_')[0] == 'sld':
-                                value = self.model.params['sld_medium']
-                            elif key.split('_')[0] == 'sldIM':
-                                value = self.model.params['sldIM_medium']
+                                value = self.model.params['sld_solv']
                             self.model.setParam(key, value)
                         except: pass
     
@@ -167,119 +169,90 @@ class ReflectivityModel(BaseComponent):
                 beta is a list of the corresponding SLD values 
         """
         # max_pts for each layers
-        n_sub = 21
+        n_sub = self.params['npts_inter']
         z = []
         beta = []
-        sub_range = floor(n_sub/2.0)
-        z.append(0)
-        beta.append(self.params['sld_bottom0']) 
-       
         z0 = 0
-        # for layers from the top
-        for n in range(1,self.n_layers+2):
+        sub_range = floor(n_sub/2.0)
+        # two sld points for core
+        z.append(0)
+        beta.append(self.params['sld_core0']) 
+        z.append(self.params['rad_core0']) 
+        beta.append(self.params['sld_core0']) 
+        z0 += self.params['rad_core0']
+        # for layers from the core
+        for n in range(1,self.n_shells+2):
             i = n
-
+            # j=0 for interface, j=1 for flat layer 
             for j in range(0,2):
-                for n_s in range(-sub_range,sub_range+1):
+                # interation for sub-layers
+                for n_s in range(0,n_sub+1):
                     if j==1:
-                        if i==self.n_layers+1:
+                        if i==self.n_shells+1:
                             break
                         # shift half sub thickness for the first point
-                        z0 += dz/2.0
+                        z0 -= dz#/2.0
                         z.append(z0)
                         #z0 -= dz/2.0
                         z0 += self.params['thick_flat%s'% str(i)]
                         
                         sld_i = self.params['sld_flat%s'% str(i)]
                         beta.append(self.params['sld_flat%s'% str(i)])
+                        dz = 0
                     else:
-                        
                         dz = self.params['thick_inter%s'% str(i-1)]/n_sub
-                        
-                        if n_s == -sub_range:
-                            # shift half sub thickness for the first point
-                            z0 -= dz/2.0
-                        #exec "dz = self.params['thick_inter[%s-1]'% str(i)]/9"
-                        #print "%d = %g \n"% (i,self.params['thick_inter3'])
-                        z0 += dz
-
+                        nu = self.params['nu_inter%s'% str(i-1)]
+                        # decide which sld is which, sld_r or sld_l
                         if i == 1:
-                            sld_l = self.params['sld_bottom0']
+                            sld_l = self.params['sld_core0']
                         else:
                             sld_l = self.params['sld_flat%s'% str(i-1)]
-                        if i == self.n_layers+1:
-                            sld_r = self.params['sld_medium']
+                        if i == self.n_shells+1:
+                            sld_r = self.params['sld_solv']
                         else:
                             sld_r = self.params['sld_flat%s'% str(i)]
+                        # get function type
                         func_idx = self.params['func_inter%s'% str(i-1)]
-                        func = self._get_func(n_s, n_sub, func_idx)
-                        if sld_r>sld_l:
-                            sld_i = (sld_r-sld_l)*func+sld_l
-                        elif sld_r<sld_l:
-                            sld_i = (sld_l-sld_r)*(1-func)+sld_r
-                        else:
-                             sld_i = sld_r
+                        # calculate the sld
+                        sld_i = self._get_sld(func_idx, n_sub, n_s, nu,
+                                              sld_l, sld_r)
+                    # append to the list
                     z.append(z0)
                     beta.append(sld_i)
+                    z0 += dz
                     if j==1: break
-        # put substrate and superstrate profile
-        # shift half sub thickness for the first point
-        z0 += dz/2.0
+        # put sld of solvent
         z.append(z0)
-        beta.append(self.params['sld_medium'])  
-        z_ext = z0/6.0
-        
-        # put the extra points for the substrate 
-        # and superstrate
+        beta.append(self.params['sld_solv'])  
+        z_ext = z0/5.0
         z.append(z0+z_ext)
-        beta.append(self.params['sld_medium']) 
-        z.insert(0,-z_ext)
-        beta.insert(0,self.params['sld_bottom0']) 
-        z = [z0 - x for x in z]
-        z.reverse()
-        beta.reverse()  
+        beta.append(self.params['sld_solv']) 
+        # return sld profile (r, beta)
         return z, beta
     
-    def _get_func(self, index, n_sub, func_idx):
+    def _get_sld(self, func_idx, n_sub, n_s, nu, sld_l, sld_r):
         """
-        Get the function asked to buil sld profile
-        : param index: index of sub_layer
+        Get the function asked to build sld profile
+        : param func_idx: func type number
         : param n_sub: total number of sub_layer
-        : param func_idx: an integer to identify a function
-        
-        : return out: the output from the function, float
+        : param n_s: index of sub_layer
+        : param nu: coefficient of the function
+        : param sld_l: sld on the left side
+        : param sld_r: sld on the right side
+        : return: sld value, float
         """
-        # cal bin_size
-        bin_size = 1.0/n_sub
-        # erf
-        if func_idx == 0:
-            out = erf(index/(n_sub/5.0))/2.0 + 0.5
-            return out
-        else:
-            index += 0.5
-        # linear
-        if func_idx == 1:
-            out = ((index + floor(n_sub/2.0))*bin_size)
-        # r_parabolic
-        elif func_idx == 2:
-            out = ((index + floor(n_sub/2.0))*bin_size)* \
-                    ((index + floor(n_sub/2.0))*bin_size)
-        # l_parabolic
-        elif func_idx == 3:
-            out = 1.0-(((index + floor(n_sub/2.0))*bin_size) - 1.0) *\
-                    (((index + floor(n_sub/2.0))*bin_size) - 1.0)
-        # r_cubic
-        elif func_idx == 4:
-            out = ((index + floor(n_sub/2.0))*bin_size)* \
-                    ((index + floor(n_sub/2.0))*bin_size)* \
-                    ((index + floor(n_sub/2.0))*bin_size)
-        # l_cubic
-        elif func_idx == 5:
-            out = 1.0+(((index + floor(n_sub/2.0)))*bin_size - 1.0) *\
-                    (((index + floor(n_sub/2.0)))*bin_size - 1.0) *\
-                    (((index + floor(n_sub/2.0)))*bin_size - 1.0)
-        # return output
-        return out
+        from sans.models.SLDCalFunc import SLDCalFunc
+        # sld_cal init
+        sld_cal = SLDCalFunc()
+        # set params
+        sld_cal.setParam('fun_type',func_idx)
+        sld_cal.setParam('npts_inter',n_sub)
+        sld_cal.setParam('shell_num',n_s)
+        sld_cal.setParam('nu_inter',nu)
+        sld_cal.setParam('sld_left',sld_l)
+        sld_cal.setParam('sld_right',sld_r)
+        # return sld value
+        return sld_cal.run()
     
     def setParam(self, name, value):
         """ 
@@ -292,9 +265,9 @@ class ReflectivityModel(BaseComponent):
         self._setParamHelper( name, value)
         
         ## setParam to model 
-        if name=='sld_medium':
-            # the sld_*** model.params not in params must set 
-            # to value of sld_solv
+        if name=='sld_solv':
+            # the sld_*** model.params not in params must set to 
+            # value of sld_solv
             for key in self.model.params.iterkeys():
                 if key not in self.params.keys()and key.split('_')[0] == 'sld':
                         self.model.setParam(key, value)
