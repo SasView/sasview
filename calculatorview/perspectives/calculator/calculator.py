a0ac888595771c57496f83015dc8a21526e3b0f8
Gervaise Alina <gervyh@hotmail.com>
2011-01-10 17:40:16 +0000

    make calculator plugin inheriting brom pluginbase

--- calculatorview/perspectives/calculator/calculator.py
+++ calculatorview/perspectives/calculator/calculator.py
@@ -11,64 +11,20 @@ Calculator Module
 #copyright 2010, University of Tennessee
 ################################################################################
 
-#import wx
+
+from sans.guiframe.plugin_base import PluginBase
 import logging
 
-class Plugin:
+class Plugin(PluginBase):
     """
     This class defines the interface for a Plugin class
     for calculator perspective
     """
     def __init__(self, standalone=True):
-        """
-        Abstract class for gui_manager Plugins.
-        """
-        ## Plug-in name. It will appear on the application menu.
-        self.sub_menu = "Calculator"        
-        #standalone flag
-        self.standalone = standalone
-        ## Reference to the parent window. Filled by get_panels() below.
-        self.parent = None
-        
-        ## List of panels that you would like to open in AUI windows
-        #  for your plug-in. This defines your plug-in "perspective"
-        self.perspective = []
+        PluginBase.__init__(self, name="Calculator", standalone=standalone)
         # Log startup
         logging.info("Calculator plug-in started")   
-        
-    def populate_menu(self, id, owner):
-        """
-        Create and return the list of application menu
-        items for the plug-in. 
-        
-        :param id: deprecated. Un-used.
-        :param parent: parent window
-        
-        :return: plug-in menu
-        
-        """
-        return []
-      
-    def get_panels(self, parent):
-        """
-        Create and return the list of wx.Panels for your plug-in.
-        Define the plug-in perspective.
-        
-        Panels should inherit from DefaultPanel defined below,
-        or should present the same interface. They must define
-        "window_caption" and "window_name".
-        
-        :param parent: parent window
-        
-        :return: list of panels
-        
-        """
-        ## Save a reference to the parent
-        self.parent = parent
-       
-        return []
-       
-    
+  
     def help(self, evt):
         """
         Show a general help dialog. 
@@ -79,37 +35,7 @@ class Plugin:
         from help_panel import  HelpWindow
         frame = HelpWindow(None, -1)    
         frame.Show(True)
-        #evt.Skip()
-        
-    def get_context_menu(self, graph=None):
-        """
-        This method is optional.
-    
-        When the context menu of a plot is rendered, the 
-        get_context_menu method will be called to give you a 
-        chance to add a menu item to the context menu.
-        
-        A ref to a Graph object is passed so that you can
-        investigate the plot content and decide whether you
-        need to add items to the context menu.  
-        
-        This method returns a list of menu items.
-        Each item is itself a list defining the text to 
-        appear in the menu, a tool-tip help text, and a
-        call-back method.
-            
-            @param graph: the Graph object to which we attach the context menu
-            @return: a list of menu items with call-back function
-        """
-        return []    
-    
-    def get_perspective(self):
-        """
-        Get the list of panel names for this perspective
-        """
-        return self.perspective
-        
-    
+
     def get_tools(self):
         """
         Returns a set of menu entries for tools
@@ -185,12 +111,7 @@ class Plugin:
         """
         self.parent.set_perspective(self.perspective)
         event.Skip()
-        
-    def post_init(self):
-        """
-        Post initialization call back to close the loose ends
-        """
-        pass
+
     
   
     
