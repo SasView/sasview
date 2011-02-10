cbb8bb614f58694bab8aca4e822ed4116a035298
Jae Cho <jhjcho@gmail.com>
2011-02-10 16:47:30 +0000

    moving smearer to c_models

--- DataLoader/__init__.py
+++ DataLoader/__init__.py
@@ -1,4 +1,3 @@
 from data_info import *
 from manipulations import *
-from qsmearing import *
 from readers import *
--- DataLoader/setup.py
+++ DataLoader/setup.py
@@ -1,12 +1,13 @@
 """
      Installation script for SANS DataLoader
 """
-import os, sys
-    
+import os
+import sys
+
 # Then build and install the modules
-from distutils.core import setup, Extension
-from numpy.distutils.misc_util import get_numpy_include_dirs
-numpy_incl_path = os.path.join(get_numpy_include_dirs()[0], "numpy")
+from distutils.core import setup#, Extension
+#from numpy.distutils.misc_util import get_numpy_include_dirs
+#numpy_incl_path = os.path.join(get_numpy_include_dirs()[0], "numpy")
 setup(
     name="DataLoader",
     version = "0.2",
@@ -14,18 +15,8 @@ setup(
     author = "University of Tennessee",
     url = "http://danse.chem.utk.edu",
     
-    package_dir = {"DataLoader":".",
-                   "DataLoader.extensions":"extensions"},
+    package_dir = {"DataLoader":"."},
     package_data={"DataLoader.readers": ['defaults.xml']},
-    packages = ["DataLoader","DataLoader.readers","DataLoader.extensions"],
-    
-    ext_modules = [ Extension("DataLoader.extensions.smearer",
-     sources = [
-        "extensions/smearer_module.cpp",
-        "extensions/smearer.cpp",
-            ],
-         include_dirs=["extensions", numpy_incl_path]
-     
-     
-     )])
+    packages = ["DataLoader","DataLoader.readers"]
+    )
         
