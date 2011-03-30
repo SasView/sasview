73c200a74d12e4687dd3503b12ba8635d7a197d1
Gervaise Alina <gervyh@hotmail.com>
2011-03-30 17:29:11 +0000

    remove uunused import

--- sansview/sansview.py
+++ sansview/sansview.py
@@ -27,10 +27,6 @@ from welcome_panel import WelcomePanel
 import local_config
 import logging
 
-# Application dimensions
-APP_HEIGHT = 820
-APP_WIDTH  = 1050
-GSTYLE = GUIFRAME.MULTIPLE_APPLICATIONS|GUIFRAME.TOOL_ON
 
 class SansViewApp(gui_manager.ViewApp):
     """
@@ -75,9 +71,8 @@ class SansView():
             invariant_plug = module.Plugin(standalone=False)
             self.gui.add_perspective(invariant_plug)
         except:
-            raise
-            #logging.error("SansView: could not find Invariant plug-in module") 
-            #logging.error(sys.exc_value)  
+            logging.error("SansView: could not find Invariant plug-in module") 
+            logging.error(sys.exc_value)  
         
         #Calculator perspective   
         try:
@@ -87,23 +82,7 @@ class SansView():
         except:
             logging.error("SansView: could not find Calculator plug-in module")
             logging.error(sys.exc_value)  
-            
-        """ 
-        # theory perspective
-        try:
-            import sans.perspectives.theory as module    
-            theory_plug = module.Plugin(standalone=False)
-            self.gui.add_perspective(theory_plug)
-        except:
-            logging.error("SansView: could not find theory plug-in module")
-            logging.error(sys.exc_value) 
-        """ 
-            
-        # Fitting perspective
-        import perspectives.fitting as module    
-        fitting_plug = module.Plugin()
-        self.gui.add_perspective(fitting_plug)
-       
+   
         # Add welcome page
         self.gui.set_welcome_panel(WelcomePanel)
       
