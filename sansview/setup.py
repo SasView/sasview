e1422082016cbc6e1d04bf1afae8c9d6354c414f
Gervaise Alina <gervyh@hotmail.com>
2011-05-24 15:25:39 +0000

    change version to 1.9

--- sansview/setup.py
+++ sansview/setup.py
@@ -1,12 +1,11 @@
 """
      Installation script for DANSE P(r) inversion perspective for SansView
 """
-import sys
-if len(sys.argv) == 1:
-    sys.argv.append('install')
+
 from distutils.core import setup
 
 setup(
+    version = "1.9",
     name="SansView",
     description = "SansView description",
     package_dir = {"sans.perspectives":"perspectives",
