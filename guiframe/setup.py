3c47277f2d70c04df3f9344c2a2acb81e336bddc
Gervaise Alina <gervyh@hotmail.com>
2011-06-10 21:24:57 +0000

    working easy

--- guiframe/setup.py
+++ guiframe/setup.py
@@ -9,7 +9,7 @@ from distutils.core import setup, Extension
 from distutils.sysconfig import get_python_lib
 import os
 import sys
-import glob
+
 
     
 path = os.path.join(os.getcwd(), 'local_perspectives')
@@ -29,22 +29,7 @@ for dir in os.listdir(path):
         package_name = "sans.guiframe.local_perspectives." + dir
         packages.append(package_name)
         package_dir[package_name] = "local_perspectives/" + dir
-               
-# Check whether the sans module exists,
-# if not, make sure a default __init__ is created
-if 'install' in sys.argv:
-    try:
-        lib_dir = get_python_lib()
-        danse_init = os.path.join(lib_dir, 'sans', '__init__.py')
-        if not os.path.isfile(danse_init):
-            if not os.path.isdir("tmp_sans"):
-                os.mkdir("tmp_sans")
-            f = open("tmp_sans/__init__.py",'w')
-            f.close()
-            package_dir['sans'] = "tmp_sans"
-            packages.append("sans")
-    except:
-        print "Couldn't create sans/__init__.py\n  %s" % sys.exc_value
+
 
 setup(
     name="guiframe",
@@ -59,21 +44,3 @@ setup(
     package_data = package_data,
 
     )
-if 'sdist' in sys.argv:
-    import zipfile
-    from distutils.filelist import findall
-    filename = "guiframe-0.9"
-    cwd = os.getcwd()
-    for f in findall(cwd):
-        if os.path.basename(f).count(filename) == 1:
-            if zipfile.is_zipfile(f):
-                zfile = zipfile.ZipFile(f, "a")
-                for image_file in glob.glob(os.path.join("images","*")):
-                    print "Adding %s to %s\n" % (image_file, os.path.basename(f))
-                    zfile.write(image_file, os.path.join(filename, image_file))
-                for media_file in glob.glob(os.path.join("media","*")):
-                    print "Adding %s to %s\n" % (media_file, os.path.basename(f))
-                    zfile.write(media_file, os.path.join(filename, media_file))
-                zfile.close()
-                break
-                    
