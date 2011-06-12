84021e1654334dc04278da063da7977365a8b4cf
Jae Cho <jhjcho@gmail.com>
2011-06-12 19:23:16 +0000

    adde success in printing

--- park_integration/ScipyFitting.py
+++ park_integration/ScipyFitting.py
@@ -179,7 +179,7 @@ class ScipyFit(FitEngine):
             result.stderr  = stderr
             result.pvec = out
             result.success = success
-            print "result1",result.pvec
+            print "result1",success, result.pvec
             if q is not None:
                 q.put(result)
                 return q
