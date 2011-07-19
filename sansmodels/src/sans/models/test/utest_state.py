5f43ff9563cb01f72a92d4da0d4e3ede009bec6b
Jae Cho <jhjcho@gmail.com>
2011-07-19 17:00:27 +0000

    update model io test

--- sansmodels/src/sans/models/test/utest_state.py
+++ sansmodels/src/sans/models/test/utest_state.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python
+""" 
+    Test for setstate and reduce_ex operations for models
+"""
 import unittest
 
 from sans.models.SphereModel import SphereModel
@@ -12,16 +16,16 @@ class TestSphere(unittest.TestCase):
             was generated with
         """
         self.sphere.setParam('radius', 44.0)
-        state = self.sphere.__getstate__()
+        _, _, state, _, _ = self.sphere.__reduce_ex__(0)
         
         sphere_copy = SphereModel()
         sphere_copy.__setstate__(state)
-        
+        sphere_clone = sphere_copy.clone()
         self.assertEqual(sphere_copy.getParam('radius'), 44)
         
         self.sphere.setParam('radius', 33.0)
         
-        self.assertEqual(sphere_copy.getParam('radius'), 44)
+        self.assertEqual(sphere_clone.getParam('radius'), 44)
         
     
 if __name__ == '__main__':
