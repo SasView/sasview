f4c72c299ab7754bd247b69d96e694aa48983b8b
Jae Cho <jhjcho@gmail.com>
2011-08-09 13:32:29 +0000

    updated name etc.

--- sansdataloader/src/setup.py
+++ sansdataloader/src/setup.py
@@ -3,20 +3,21 @@
 """
 import os
 import sys
-
+if len(sys.argv) == 1:
+    sys.argv.append('install')
 # Then build and install the modules
 from distutils.core import setup#, Extension
 #from numpy.distutils.misc_util import get_numpy_include_dirs
 #numpy_incl_path = os.path.join(get_numpy_include_dirs()[0], "numpy")
 setup(
-    name="DataLoader",
-    version = "0.9",
+    name="sans.dataloader",
+    version = "0.9.1",
     description = "Python module for loading",
     author = "University of Tennessee",
     url = "http://danse.chem.utk.edu",
     
-    package_dir = {"DataLoader":"."},
-    package_data={"DataLoader.readers": ['defaults.xml']},
-    packages = ["DataLoader","DataLoader.readers"]
+    package_dir = {"sans.dataloader":"sans/dataloader"},
+    package_data={"sans.dataloader.readers": ['defaults.xml']},
+    packages = ["sans.dataloader","sans.dataloader.readers"]
     )
         
