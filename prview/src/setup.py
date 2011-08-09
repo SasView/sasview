592cf5755b1c4fad2e1c47ce98bdbc09727cb1f3
Jessica Tumarkin <jtumarki@andrew.cmu.edu>
2011-08-09 17:21:53 +0000

    updated setup.py for prview

--- prview/src/setup.py
+++ prview/src/setup.py
@@ -3,13 +3,12 @@
 """
 
 from distutils.core import setup
-
 setup(
     version = "0.9.1",
     name="prview",
     description = "P(r) inversion perspective for SansView",
-    package_dir = {"sans.perspectives":"perspectives",
-                   "sans.perspectives.pr":"perspectives/pr"},
+    package_dir = {"sans.perspectives":"sans/perspectives",
+                   "sans.perspectives.pr":"sans/perspectives/pr"},
     packages = ["sans.perspectives",
                 "sans.perspectives.pr"],
     )
