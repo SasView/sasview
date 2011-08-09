032d585f51ee9a5c27c579f84f0b626eb8758bec
Jessica Tumarkin <jtumarki@andrew.cmu.edu>
2011-08-09 17:28:24 +0000

    Updated setup.py for invariantview

--- invariantview/src/setup.py
+++ invariantview/src/setup.py
@@ -3,14 +3,14 @@
 """
 import os
 from distutils.core import setup
-
 setup(
     version = "0.9",
     name="invariant",
     description = "Invariant perspective for SansView",
-    package_dir = {"sans.perspectives":"perspectives",
-                   "sans.perspectives.invariant":"perspectives/invariant",
-                   "sans.perspectives.invariant.media":"media"},
+    package_dir = {"sans.perspectives":"sans/perspectives",
+                   "sans.perspectives.invariant":"sans/perspectives/invariant",
+                   "sans.perspectives.invariant.media":
+                   "sans/perspectives/invariant/media"},
     package_data={'sans.perspectives.invariant.media': ['*']},
     packages = ["sans.perspectives",
                 "sans.perspectives.invariant",
