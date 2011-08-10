7bb7a33353f81ef23fde0b1bd4b683d7959fe622
Gervaise Alina <gervyh@hotmail.com>
2011-08-10 20:07:48 +0000

    fix setup.py fitting

--- fittingview/setup.py
+++ fittingview/setup.py
@@ -1,15 +1,20 @@
 """
      Installation script for DANSE P(r) inversion perspective for SansView
 """
-
+import os
 from distutils.core import setup
 setup(
     version = "0.9.1",
     name="fittingview",
     description = "Fitting module  for SansView",
-    package_dir = {"sans.perspectives":"src/sans/perspectives",
-                   "sans.perspectives.pr":"src/sans/perspectives/fitting"},
-    packages = ["sans.perspectives", "src/sans",
+    package_dir = {"sans":os.path.join("src", "sans"),
+                   "sans.perspectives":os.path.join("src",
+                                                    "sans", "perspectives"),
+                   "sans.perspectives.pr":os.path.join("src",
+                                                       "sans",
+                                                       "perspectives",
+                                                       "fitting")},
+    packages = ["sans.perspectives", 'sans',
                 "sans.perspectives.fitting"],
     )
 
