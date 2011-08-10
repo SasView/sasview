3f0771e5dad1b0020d0b13b479f9a3a45b68c615
Gervaise Alina <gervyh@hotmail.com>
2011-08-10 21:06:12 +0000

    refactore setup.py

--- invariantview/setup.py
+++ invariantview/setup.py
@@ -2,23 +2,24 @@
      Installation script for DANSE calculator perspective for SansView
 """
 import os
-import sys
-if len(sys.argv) == 1:
-    sys.argv.append('install')
+
 from distutils.core import setup
 setup(
     version = "0.9",
     name="invariant",
     description = "Invariant perspective for SansView",
-    package_dir = {"sans":"src/sans",
-                   "sans.perspectives":"src/sans/perspectives",
-                   "sans.perspectives.invariant":"src/sans/perspectives/invariant",
-                   "sans.perspectives.invariant.media":
-                   "src/sans/perspectives/invariant/media"},
-    package_data={'sans.perspectives.invariant.media': ['*']},
+    package_dir = {"sans":os.path.join("src", "sans"),
+                   "sans.perspectives":os.path.join("src", "sans",
+                                                    "perspectives"),
+                   "sans.perspectives.invariant":os.path.join("src",
+                                                            "sans",
+                                                            "perspectives",
+                                                            "invariant"),
+                   },
+    package_data={'sans.perspectives.invariant': [os.path.join("media",
+                                                               '*')]},
     packages = ["sans.perspectives",
                 "sans",
-                "sans.perspectives.invariant",
-                "sans.perspectives.invariant.media"],
+                "sans.perspectives.invariant",],
     )
 
