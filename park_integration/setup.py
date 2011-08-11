e26f184d75577e56cd1fbcce6359d0761bf9286f
Gervaise Alina <gervyh@hotmail.com>
2011-08-11 20:25:04 +0000

    change packages name

--- park_integration/setup.py
+++ park_integration/setup.py
@@ -13,10 +13,9 @@ setup(
     author = "University of Tennessee",
     #author_email = "",
     url = "http://danse.chem.utk.edu",
-    
     # Use the pure python modules
-    package_dir = {"sans.fit":"src/sans/fit"},
-    
-    packages = ["sans.fit", "src/sans"]
+    package_dir = {"sans":"src/sans",
+                   "sans.fit":"src/sans/fit"},
+    packages = ["sans.fit", "sans"]
     )
         
