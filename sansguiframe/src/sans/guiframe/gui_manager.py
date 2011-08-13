cdda5524592047d2ac93cdca2f0dad7a3e8c0d66
Jae Cho <jhjcho@gmail.com>
2011-08-13 19:37:08 +0000

    changed exit mesg.

--- sansguiframe/src/sans/guiframe/gui_manager.py
+++ sansguiframe/src/sans/guiframe/gui_manager.py
@@ -1708,9 +1708,8 @@ class ViewerFrame(wx.Frame):
         """
         Pop up message to make sure the user wants to quit the application
         """
-        message = "Do you really want to quit \n"
-        message += "this application?"
-        dial = wx.MessageDialog(self, message, 'Question',
+        message = "\nDo you really want to exit this application?        \n\n"
+        dial = wx.MessageDialog(self, message, 'Confirm Exit',
                            wx.YES_NO|wx.YES_DEFAULT|wx.ICON_QUESTION)
         if dial.ShowModal() == wx.ID_YES:
             return True
