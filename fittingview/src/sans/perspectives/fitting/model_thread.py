7e7e806d47f3dbc7bcfbd81a9b77a10dcf58eccb
Gervaise Alina <gervyh@hotmail.com>
2011-08-26 14:22:08 +0000

    modify model thread to make data as a required input parameter

--- fittingview/src/sans/perspectives/fitting/model_thread.py
+++ fittingview/src/sans/perspectives/fitting/model_thread.py
@@ -1,10 +1,11 @@
 
 
 import time
-from data_util.calcthread import CalcThread
 import sys
-import numpy,math
+import numpy
+import math
 from sans.models.smearing_2d import Smearer2D
+from data_util.calcthread import CalcThread
 
 class Calc2D(CalcThread):
     """
@@ -13,32 +14,29 @@ class Calc2D(CalcThread):
     where points are computed for one half of the detector
     and I(qx, qy) = I(-qx, -qy) is assumed.
     """
-    def __init__(self, x, y, data,model,smearer,qmin, qmax,qstep,
-                 page_id ,
+    def __init__(self, data, model, smearer, qmin, qmax,  page_id,
                  state=None,
                  toggle_mode_on=False,
-                 completefn = None,
-                 updatefn   = None,
+                 completefn=None,
+                 updatefn=None,
                  update_chisqr=True,
-                 yieldtime  = 0.04,
-                 worktime   = 0.04
+                 yieldtime=0.04,
+                 worktime=0.04
                  ):
         CalcThread.__init__(self,completefn,
                  updatefn,
                  yieldtime,
                  worktime)
-        self.qmin= qmin
-        self.qmax= qmax
-        self.qstep= qstep
+        self.qmin = qmin
+        self.qmax = qmax
+        #self.qstep = qstep
         self.toggle_mode_on = toggle_mode_on
-        self.x = x
-        self.y = y
-        self.data= data
+        self.data = data
         self.page_id = page_id
         self.state = None
         # the model on to calculate
         self.model = model
-        self.smearer = smearer#(data=self.data,model=self.model)
+        self.smearer = smearer
         self.starttime = 0  
         self.update_chisqr = update_chisqr
         
@@ -48,78 +46,52 @@ class Calc2D(CalcThread):
         """
         self.starttime = time.time()
         # Determine appropriate q range
-        if self.qmin==None:
+        if self.qmin == None:
             self.qmin = 0
-        if self.qmax== None:
-            if self.data !=None:
-                newx= math.pow(max(math.fabs(self.data.xmax),math.fabs(self.data.xmin)),2)
-                newy= math.pow(max(math.fabs(self.data.ymax),math.fabs(self.data.ymin)),2)
-                self.qmax=math.sqrt( newx + newy )
+        if self.qmax == None:
+            if self.data != None:
+                newx = math.pow(max(math.fabs(self.data.xmax),
+                                   math.fabs(self.data.xmin)), 2)
+                newy = math.pow(max(math.fabs(self.data.ymax),
+                                   math.fabs(self.data.ymin)), 2)
+                self.qmax = math.sqrt(newx + newy)
         
-        if self.data != None:
-            self.I_data = self.data.data
-            self.qx_data = self.data.qx_data
-            self.qy_data = self.data.qy_data
-            self.dqx_data = self.data.dqx_data
-            self.dqy_data = self.data.dqy_data
-            self.mask    = self.data.mask
-        else:          
-            xbin =  numpy.linspace(start= -1*self.qmax,
-                                   stop= self.qmax,
-                                   num= self.qstep,
-                                   endpoint=True )  
-            ybin = numpy.linspace(start= -1*self.qmax,
-                                   stop= self.qmax,
-                                   num= self.qstep,
-                                   endpoint=True )            
+        if self.data is None:
+            msg = "Compute Calc2D receive data = %s.\n" % str(self.data)    
+            raise ValueError, msg
             
-            new_xbin = numpy.tile(xbin, (len(ybin),1))
-            new_ybin = numpy.tile(ybin, (len(xbin),1))
-            new_ybin = new_ybin.swapaxes(0,1)
-            new_xbin = new_xbin.flatten()
-            new_ybin = new_ybin.flatten()
-            self.qy_data = new_ybin
-            self.qx_data = new_xbin
-            # fake data
-            self.I_data = numpy.ones(len(self.qx_data))
-           
-            self.mask = numpy.ones(len(self.qx_data),dtype=bool)
-            
-        # Define matrix where data will be plotted    
-        radius= numpy.sqrt( self.qx_data*self.qx_data + self.qy_data*self.qy_data )
-        index_data= (self.qmin<= radius)&(self.mask)
+        # Define matrix where data will be plotted 
+        radius= numpy.sqrt((self.data.qx_data * self.data.qx_data) + \
+                    (self.data.qy_data * self.data.qy_data))
+        index_data = (self.qmin <= radius) & self.data.mask
 
         # For theory, qmax is based on 1d qmax 
         # so that must be mulitified by sqrt(2) to get actual max for 2d
-        index_model = ((self.qmin <= radius)&(radius<= self.qmax))
-        index_model = (index_model)&(self.mask)
-        index_model = (index_model)&(numpy.isfinite(self.I_data))
-        if self.data ==None:
-            # Only qmin value will be consider for the detector
-            index_model = index_data  
-
-        if self.smearer != None:
+        index_model = (self.qmin <= radius) & (radius <= self.qmax)
+        index_model = index_model & self.data.mask
+        index_model = index_model & numpy.isfinite(self.data.data)
+      
+        if self.smearer is not None:
             # Set smearer w/ data, model and index.
             fn = self.smearer
             fn.set_model(self.model)
             fn.set_index(index_model)
             # Get necessary data from self.data and set the data for smearing
             fn.get_data()
-            # Calculate smeared Intensity (by Gaussian averaging): DataLoader/smearing2d/Smearer2D()
+            # Calculate smeared Intensity 
+            #(by Gaussian averaging): DataLoader/smearing2d/Smearer2D()
             value = fn.get_value()
-
         else:    
             # calculation w/o smearing
-            value =  self.model.evalDistribution([self.qx_data[index_model],self.qy_data[index_model]])
-
-        output = numpy.zeros(len(self.qx_data))
-        
+            value =  self.model.evalDistribution([self.data.qx_data[index_model],
+                                                self.data.qy_data[index_model]])
+        output = numpy.zeros(len(self.data.qx_data))
         # output default is None
-        # This method is to distinguish between masked point(nan) and data point = 0.
+        # This method is to distinguish between masked 
+        #point(nan) and data point = 0.
         output = output/output
         # set value for self.mask==True, else still None to Plottools
         output[index_model] = value 
-
         elapsed = time.time()-self.starttime
         self.complete(image=output,
                        data=self.data, 
@@ -131,17 +103,17 @@ class Calc2D(CalcThread):
                        index=index_model,
                        qmin=self.qmin,
                        qmax=self.qmax,
-                       qstep=self.qstep,
-                       update_chisqr = self.update_chisqr )
+                       #qstep=self.qstep,
+                       update_chisqr = self.update_chisqr)
         
 
 class Calc1D(CalcThread):
     """
     Compute 1D data
     """
-    def __init__(self, x, model,
+    def __init__(self, model,
                  page_id,
-                 data=None,
+                 data,
                  qmin=None,
                  qmax=None,
                  smearer=None,
@@ -149,9 +121,9 @@ class Calc1D(CalcThread):
                  state=None,
                  completefn = None,
                  update_chisqr=True,
-                 updatefn   = None,
-                 yieldtime  = 0.01,
-                 worktime   = 0.01
+                 updatefn=None,
+                 yieldtime=0.01,
+                 worktime=0.01
                  ):
         """
         """
@@ -159,7 +131,6 @@ class Calc1D(CalcThread):
                  updatefn,
                  yieldtime,
                  worktime)
-        self.x = numpy.array(x)
         self.data = data
         self.qmin = qmin
         self.qmax = qmax
@@ -176,26 +147,28 @@ class Calc1D(CalcThread):
         Compute model 1d value given qmin , qmax , x value 
         """
         self.starttime = time.time()
-        output = numpy.zeros((len(self.x)))
-        index= (self.qmin <= self.x)& (self.x <= self.qmax)
+        output = numpy.zeros((len(self.data.x)))
+        index= (self.qmin <= self.data.x)& (self.data.x <= self.qmax)
      
         ##smearer the ouput of the plot    
-        if self.smearer!=None:
-            first_bin, last_bin = self.smearer.get_bin_range(self.qmin, self.qmax)
-            output[first_bin:last_bin] = self.model.evalDistribution(self.x[first_bin:last_bin])
+        if self.smearer is not None:
+            first_bin, last_bin = self.smearer.get_bin_range(self.qmin, 
+                                                             self.qmax)
+            mask = self.data.x[first_bin:last_bin]
+            output[first_bin:last_bin] = self.model.evalDistribution(mask)
             output = self.smearer(output, first_bin, last_bin) 
         else:
-            output[index] = self.model.evalDistribution(self.x[index])
+            output[index] = self.model.evalDistribution(self.data.x[index])
          
         elapsed = time.time() - self.starttime
        
-        self.complete(x=self.x[index], y=output[index], 
+        self.complete(x=self.data.x[index], y=output[index], 
                       page_id=self.page_id,
                       state=self.state,
                       toggle_mode_on=self.toggle_mode_on,
                       elapsed=elapsed,index=index, model=self.model,
                       data=self.data, 
-                      update_chisqr = self.update_chisqr )
+                      update_chisqr = self.update_chisqr)
         
     def results(self):
         """
