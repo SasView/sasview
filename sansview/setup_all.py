df83aa414c75e6d669a0b7af357b3a7dd1b918a6
Jae Cho <jhjcho@gmail.com>
2011-09-14 16:53:25 +0000

    simplified the code

--- sansview/setup_all.py
+++ sansview/setup_all.py
@@ -23,71 +23,72 @@ else:
     PYTHON = 'python'
 
 
-# Release version 1.9
-SANSMODELS = "0.9.1"
-DATALOADER = "0.9"
-GUIFRAME   = "0.9.1"
-SANSVIEW   = "1.9.1"
-PLOTTOOLS  = "0.9.1"
-UTIL       = "0.9.1"
-PARK       = "1.2.1"
-PARK_INTEG = "0.9.1"
-PRVIEW     = "0.9.1"
-PR_INV     = "0.9"
-CALCULATOR = "0.9.1"
-CALC_VIEW  = "0.9"
-INVARIANT  = "0.9.1"
-INV_VIEW   = "0.9"
-FIT_VIEW   = "0.9.1"
-
-
 def check_system():
     """
         Checks that the system has the necessary modules.
     """
     is_ok = True
+    msg = ''
     try:
         import wx
         if not wx.__version__.count('2.8.11') and \
              not wx.__version__.count('2.8.12'):
-            print "wx: Recommending version 2.8.11 or 2.8.12"
+            mesg = "wx: Recommending version 2.8.11 or 2.8.12"
+            msg += mesg + "\n"
+            print mesg
     except:
         is_ok = False
-        print "Error: wxpython 2.8.11 (12) missing"
+        mesg = "Error: wxpython 2.8.11 (12) missing"
+        msg += mesg + "\n"
+        print mesg
         logging.error("wxpython missing")
     
     try:
         import matplotlib
         if not matplotlib.__version__.count('0.99.0') and \
              not matplotlib.__version__.count('0.99.1'):
-            print "matplotlib: Recommending version 0.99.0 or 0.99.1"
+            mesg = "matplotlib: Recommending version 0.99.0 or 0.99.1"
+            msg += mesg + "\n"
+            print mesg
     except:
         is_ok = False
-        print "Error: matplotlib 0.99.0 (1) missing"
+        mesg = "Error: matplotlib 0.99.0 (1) missing"
+        msg += mesg + "\n"
+        print mesg
         logging.error("matplotlib missing")
         
     try:
         import numpy 
         if not numpy.__version__.count('1.4.1'):
-            print "numpy: Recommending version 1.4.1"
+            mesg = "numpy: Recommending version 1.4.1"
+            msg += mesg + "\n"
+            print mesg
     except:
         is_ok = False
-        print "Error: numpy 1.4.1 missing"
+        mesg = "Error: numpy 1.4.1 missing"
+        msg += mesg + "\n"
+        print mesg
         logging.error("numpy missing")
         
     try:
         import scipy 
         if not scipy.__version__.count('0.7.2'):
-            print "scipy: Recommending version 0.7.2"
+            mesg = "scipy: Recommending version 0.7.2"
+            msg += mesg + "\n"
+            print mesg
     except:
         is_ok = False
-        print "Error: scipy 0.7.2 missing"
+        mesg = "Error: scipy 0.7.2 missing"
+        msg += mesg + "\n"
+        print mesg
         logging.error("scipy missing")
 
     try:
         import periodictable
         if not periodictable.__version__.count('1.3.0'):
-            print "periodictable: Recommending version 1.3.0"
+            mesg = "periodictable: Recommending version 1.3.0"
+            msg += mesg + "\n"
+            print mesg
     except:
         print "Trying to install perodic table..."
         try:
@@ -95,6 +96,8 @@ def check_system():
             print "installed periodictable"
         except:
             is_ok = False
+            mesg = "Error: periodictable missing"
+            msg += mesg + "\n"
             print "easy_install periodictable failed"
             logging.error("periodictable missing")
     
@@ -104,7 +107,9 @@ def check_system():
             from wx.lib.pdfwin import PDFWindow   
     except:
         is_ok = False
-        print "comtypes missing"
+        mesg = "comtypes missing"
+        msg += mesg + "\n"
+        print mesg
         logging.error("comtypes missing")
         
     try:
@@ -112,7 +117,9 @@ def check_system():
             import win32com
     except:
         is_ok = False
-        print "pywin32 missing"
+        mesg = "pywin32 missing"
+        msg += mesg + "\n"
+        print mesg
         logging.error("pywin32 missing")
             
     try:
@@ -123,14 +130,20 @@ def check_system():
             print "installed pyparsing"
         except:
             is_ok = False
+            mesg = "pyparsing missing"
+            msg += mesg + "\n"
+            print mesg
             print "easy_install pyparsing failed"
             logging.error("pyparsing missing")
               
     if os.system("gcc -dumpversion")==1:
         is_ok = False
-        logging.error("missing mingw")
+        mesg = "missing mingw/gcc"
+        msg += mesg + "\n"
+        print mesg
+        logging.error("missing mingw/gcc")
     
-    return is_ok
+    return is_ok, msg
 
 
 def install_pkg(setup_dir):
@@ -145,9 +158,10 @@ def install_pkg(setup_dir):
     #logging.info("Installing Pakages" )
     try:   
         os.chdir(setup_dir) 
-        os.system("%s setup.py install" % PYTHON)    
+        os.system("%s setup.py install" % PYTHON)  
+        print "Installation %s successful.." % setup_dir  
     except:
-        logging.error("Install failed on %s"% setup_dir)
+        logging.error("Installation failed on %s"% setup_dir)
         logging.error(sys.exc_value)
         raw_input("Press enter to continue\n")
         sys.exit()
@@ -158,96 +172,16 @@ def install(release=False):
     """
     wd = os.getcwd()
     os.chdir(wd)
-    
-    if release:
-        install_pkg("sansdataloader-%s" % DATALOADER)
-    else:
-        install_pkg("sansdataloader")
-    
-    os.chdir(wd)
-    if release:
-        install_pkg("sansmodels-%s" % SANSMODELS)
-    else:
-        install_pkg("sansmodels")
-    
-    os.chdir(wd)
-    if release:
-        install_pkg("sansguiframe-%s" % GUIFRAME)
-    else:
-        install_pkg("sansguiframe")
-    
-    os.chdir(wd)
-    if release:
-        install_pkg("plottools-%s" % PLOTTOOLS)
-    else:
-        install_pkg("plottools")
-    
-    os.chdir(wd)
-    if release:
-        install_pkg("util-%s" % UTIL)
-    else:
-        install_pkg("util")
-    
-    os.chdir(wd)
-    if release:
-        install_pkg("park_integration-%s" % PARK_INTEG)
-    else:
-        install_pkg("park_integration")
-    
-    os.chdir(wd)
-    if release:
-        install_pkg("inversionview-%s" % PRVIEW)
-    else:
-        install_pkg("inversionview")
-    
-    os.chdir(wd)
-    if release:
-        install_pkg("pr_inversion-%s" % PR_INV)
-    else:
-        install_pkg("pr_inversion")
-    
-    os.chdir(wd)
-    if release:
-        install_pkg("sansinvariant-%s" % INVARIANT)
-    else:
-        install_pkg("sansinvariant")
-    
-    os.chdir(wd)
-    if release:
-        install_pkg("invariantview-%s" % INV_VIEW)
-    else:
-        install_pkg("invariantview")
-    
-    os.chdir(wd)
-    if release:
-        install_pkg("calculatorview-%s" % CALC_VIEW)
-    else:
-        install_pkg("calculatorview")
-    
-    os.chdir(wd)
-    if release:
-        install_pkg("sanscalculator-%s" % CALCULATOR)
-    else:
-        install_pkg("sanscalculator")
+    for folder in os.listdir(wd):
+        if os.path.isdir(os.path.join(wd, folder)):
+            if folder.count('sansview') > 0:
+                setup_dir = folder
+            try:
+                install_pkg(folder)
+            except:
+                pass
+        os.chdir(wd)
 
-
-    os.chdir(wd)
-    if release:
-        install_pkg("park-%s" % PARK)
-    else:
-        install_pkg("park-1.2")
-        
-    os.chdir(wd)
-    if release:
-        install_pkg("fittingview-%s" % FIT_VIEW)
-    else:
-        install_pkg("fittingview")
-            
-    os.chdir(wd)
-    if release:
-        setup_dir = "sansview-%s" % SANSVIEW
-    else:
-        setup_dir = "sansview" 
         
     # run sansview
     try:
@@ -289,7 +223,8 @@ def prepare(wipeout = True):
         
 if __name__ == "__main__": 
     
-    if check_system():    
+    is_ok, _msg = check_system()
+    if is_ok:    
         if len(sys.argv)==1:
             # If there is no argument, build the installer
             sys.argv.append("-t")
@@ -304,21 +239,8 @@ if __name__ == "__main__":
                 print "    -r: Builds a SansView using the released modules"
                 print "    -t: Builds SansView from the trunk"       
 
-            elif sys.argv[1]=="-n":
-                # Print out release URLs
-                print SANSMODELS_URL 
-                print DATALOADER_URL 
-                print GUIFRAME_URL 
-                print PLOTTOOLS_URL 
-                print UTIL_URL 
-                print SANSVIEW_URL
-                print PARK_INTEG_URL 
-                print PARK_URL 
-                print PRVIEW 
-                print PR_INV 
-                print FIT_URL
             else:
-                logging.info("Build script for SansView %s" % SANSVIEW)
+                logging.info("Build script for SansView")
                 print "Warning: This script will delete the previous SansView packages..."
                 prepare()
                 # Check the command line argument
@@ -330,8 +252,9 @@ if __name__ == "__main__":
                     install(True)
    
     
-
+        print _msg
         raw_input("Press enter to continue\n")
     
     else:
+        print _msg
         raw_input("Press enter and install the missing packages before re-run this scripts.\n")
