930f559ed803ab8451e23e7e22ee7514a3fa55ed
Gervaise Alina <gervyh@hotmail.com>
2011-11-18 19:30:52 +0000

    use freeze_support for py2exe and process use

--- sansview/sansview.py
+++ sansview/sansview.py
@@ -104,5 +104,10 @@ class SansView():
         self.gui.MainLoop()  
        
 
+
 if __name__ == "__main__": 
+    from multiprocessing import freeze_support
+    freeze_support()
     sansview = SansView()
+
+   
