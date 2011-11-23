26da93f875ddfe3c299c17093d1d6251a22c1a79
Mathieu Doucet <doucetm@ornl.gov>
2011-11-23 20:20:10 +0000

    Re #5 adding config to build

--- setup.py
+++ setup.py
@@ -191,7 +191,7 @@ packages.append("sans.sansview")
 
 #required = ['lxml>=2.2.2', 'numpy>=1.4.1', 'matplotlib>=0.99.1.1', 'wxPython>=2.8.11',
 #            'pil','periodictable>=1.3.0', 'scipy>=0.7.2']
-required = ['lxml', 'numpy>=1.3.0', 'matplotlib>=0.99.1.1',
+required = ['lxml', 'numpy>=1.3.0', 'matplotlib',
             'pil','periodictable>=1.3.0']
 if sys.platform=='win32':
     required.extend(['comtypes','pywin32', 'pisa', 'html5lib', 'reportlab'])
