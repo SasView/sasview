618e438ee2c7a4e3c8c065df556d9043225e7112
Mathieu Doucet <doucetm@ornl.gov>
2011-11-23 23:23:47 +0000

    Re #5 Fix setup.py

--- sansdataloader/src/sans/dataloader/readers/associations.py
+++ sansdataloader/src/sans/dataloader/readers/associations.py
@@ -45,6 +45,8 @@ def read_associations(loader, settings='defaults.xml'):
         path = os.path.join(os.getcwd(), settings)
     if not os.path.isfile(path):
         path = os.path.join(sys.path[0], settings)
+    if not os.path.isfile(path):
+        path = settings
     if os.path.isfile(path):
         tree = etree.parse(path, parser=etree.ETCompatXMLParser())
         
