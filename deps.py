9e338cafec0a46d3dab47bda28e30539cd52947b
Mathieu Doucet <doucetm@ornl.gov>
2011-11-23 20:41:11 +0000

    Re #5 adding script to check dependencies

---
+++ deps.py
@@ -0,0 +1,34 @@
+"""
+    Check dependencies
+"""
+try:
+    import matplotlib
+    print "matplotlib ", matplotlib.__version__
+except:
+    print "No matplotlib"
+    
+try:
+    import wx
+    print "wx ", wx.__version__
+except:
+    print "No wx"
+    
+try:
+    import scipy
+    print "scipy ", scipy.__version__
+except:
+    print "No scipy"
+    
+try:
+    import numpy
+    print "numpy ", numpy.__version__
+except:
+    print "No numpy"
+    
+try:
+    import lxml.etree
+    print "lxml ", lxml.etree.__version__
+except:
+    print "No lxml"
+    
+    
