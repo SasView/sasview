7a7dbf43a77239eb011f86d44a1d40ee6096d7a4
Jae Cho <jhjcho@gmail.com>
2011-12-02 17:55:59 +0000

    removed unfinished process & freeze

--- sansview/sansview.py
+++ sansview/sansview.py
@@ -106,8 +106,9 @@ class SansView():
 
 
 if __name__ == "__main__": 
-    from multiprocessing import freeze_support
-    freeze_support()
+    #from multiprocessing import Process, freeze_support
+    #freeze_support()
+    #Process(target=SansView).start()
     sansview = SansView()
 
    
