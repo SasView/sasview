ee1e4b20dd07d6aa74556ad5bff1e1fa64124dc8
Mathieu Doucet <doucetm@ornl.gov>
2011-12-16 13:44:55 +0000

    Re #3 Refactoring version handling

--- build_tools/get_version.py
+++ build_tools/get_version.py
@@ -1,2 +1,7 @@
 __revision__ = filter(str.isdigit, "$Revision$")
-__version__ = "2.0.1"
+__version__ = "0.0.0"
+try:
+    import sans.sansview
+    __version__ = sans.sansview.__version__
+except:
+    print "Could not load sansview module"
