8bea12ddb76b18cae350684c1423711b58948114
Mathieu Doucet <doucetm@ornl.gov>
2012-01-04 19:20:35 +0000

    Fix warning

--- sansmodels/src/sans/models/libigor/libTwoPhase.h
+++ sansmodels/src/sans/models/libigor/libTwoPhase.h
@@ -21,23 +21,3 @@ double PolyGaussCoil(double dp[], double q);
 double GaussLorentzGel(double dp[], double q);
 double GaussianShell(double dp[], double q);
 
-
-/* internal functions */
-static double
-gammln(double xx) {
-
-    double x,y,tmp,ser;
-    static double cof[6]={76.18009172947146,-86.50532032941677,
-		24.01409824083091,-1.231739572450155,
-		0.1208650973866179e-2,-0.5395239384953e-5};
-    int j;
-
-    y=x=xx;
-    tmp=x+5.5;
-    tmp -= (x+0.5)*log(tmp);
-    ser=1.000000000190015;
-    for (j=0;j<=5;j++) ser += cof[j]/++y;
-    return -tmp+log(2.5066282746310005*ser/x);
-}
-
-
