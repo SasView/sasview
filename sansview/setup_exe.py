cc37badc8ffa8f57558979ff9a26ab75eadc2ef7
Mathieu Doucet <doucetm@ornl.gov>
2012-03-06 15:55:31 +0000

    fix win32com problem

--- sansview/setup_exe.py
+++ sansview/setup_exe.py
@@ -35,6 +35,34 @@ except:
 from distutils.core import setup
 from distutils.filelist import findall
 import matplotlib
+
+# Solution taken from here: http://www.py2exe.org/index.cgi/win32com.shell
+# ModuleFinder can't handle runtime changes to __path__, but win32com uses them
+try:
+    # py2exe 0.6.4 introduced a replacement modulefinder.
+    # This means we have to add package paths there, not to the built-in
+    # one.  If this new modulefinder gets integrated into Python, then
+    # we might be able to revert this some day.
+    # if this doesn't work, try import modulefinder
+    try:
+        import py2exe.mf as modulefinder
+    except ImportError:
+        import modulefinder
+    import win32com, sys
+    for p in win32com.__path__[1:]:
+        modulefinder.AddPackagePath("win32com", p)
+    for extra in ["win32com.shell", "win32com.adsi", "win32com.axcontrol",
+                    "win32com.axscript", "win32com.bits", "win32com.ifilter",
+                    "win32com.internet", "win32com.mapi", "win32com.propsys",
+                    "win32com.taskscheduler"]:
+        __import__(extra)
+        m = sys.modules[extra]
+        for p in m.__path__[1:]:
+            modulefinder.AddPackagePath(extra, p)
+except ImportError:
+    # no build path setup, no worries.
+    pass
+
 import py2exe
 import shutil
 # Remove the build folder
