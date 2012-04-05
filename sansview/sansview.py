b53414b4326c1db9bb861fd9e36f9e8e70622bf2
Jae Cho <jhjcho@gmail.com>
2012-04-05 19:56:06 +0000

    fix for non-ascii file (<python2.7)

--- sansview/sansview.py
+++ sansview/sansview.py
@@ -23,6 +23,7 @@ if hasattr(sys, 'frozen'):
     if not os.path.exists(mplconfigdir):
         os.mkdir(mplconfigdir)
     os.environ['MPLCONFIGDIR'] = mplconfigdir
+    reload(sys) #for python<2.7
     sys.setdefaultencoding("iso-8859-1")
 from sans.guiframe import gui_manager
 from sans.guiframe.gui_style import GUIFRAME
