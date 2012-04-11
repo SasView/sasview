38c05cbfe5edbab7771b25b238943c0e3e433af6
Mathieu Doucet <doucetm@ornl.gov>
2012-04-11 20:24:31 +0000

    Fixing sphinx docs

--- sphinx-docs/generate_docs.py
+++ sphinx-docs/generate_docs.py
@@ -1,12 +1,5 @@
 import os
 cwd = os.getcwd()
-for d in os.listdir('.'):
-    sphinx_dir = os.path.join(d, 'docs', 'sphinx')
-    if os.path.isdir(sphinx_dir) and sphinx_dir.find("calculator")<0:
-        print sphinx_dir
-        #os.chdir(sphinx_dir)
-        #os.system("cd %s; make stubs; cd %s" % (sphinx_dir, cwd))
-        #print "DONE"
         
 module_list = ["fittingview", "invariantview", "inversionview",
                "park_integration", "calculatorview", "plottools",
