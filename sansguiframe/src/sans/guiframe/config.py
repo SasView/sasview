8182fbb1d0d2a0a159ac57e1cdb4308a4fc89ad7
Jae Cho <jhjcho@gmail.com>
2012-05-29 23:54:15 +0000

    working on pylint score

--- sansguiframe/src/sans/guiframe/config.py
+++ sansguiframe/src/sans/guiframe/config.py
@@ -6,8 +6,9 @@ import time
 from sans.guiframe.gui_style import GUIFRAME
 # Version of the application
 __appname__ = "DummyView"
-__version__ = '0.1.0'
-__download_page__ = 'http://danse.chem.utk.edu'
+__version__ = '0.0.0'
+__download_page__ = 'https://sourceforge.net/projects/sansviewproject/files/'
+__update_URL__ = 'http://sansviewproject.svn.sourceforge.net/viewvc/sansviewproject/trunk/sansview.latestversion'
 
 
 # Debug message flag
