56393871f470cb6a19cdb48fff911794a691a156
Tobias Richter <tobias.richter@esss.se>
2014-04-04 09:01:18 +0000

    attempt to have a non-version numbered egg so trends in pylint output are meaningful (otherwise pylint/jenkins treats each file in the versioned egg as new)

--- build_tools/jenkins_linux_pylint.sh
+++ build_tools/jenkins_linux_pylint.sh
@@ -10,10 +10,13 @@ WORKSPACE=${WORKSPACE:-`readlink -f $BUILD_TOOLS_DIR/..`}
 
 cd $WORKSPACE/sasview-install
 export SASVIEWMODULE=`echo sasview-*`
+ln -s $SASVIEWMODULE forpylint.egg
 
 export PYTHONPATH=$PYTHONPATH:$WORKSPACE/sasview-install:$WORKSPACE/utils
 
 cd $WORKSPACE
 rm -f test/*.txt
 
-$PYLINT --rcfile $WORKSPACE/build_tools/pylint.rc -f parseable sasview-install/$SASVIEWMODULE/sans sansview > test/sansview.txt || exit 0
+$PYLINT --rcfile $WORKSPACE/build_tools/pylint.rc -f parseable sasview-install/forpylint.egg/sans sansview > test/sansview.txt || true
+
+exit 0
