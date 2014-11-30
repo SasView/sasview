143c80106aea1df4017545873545381c4dd70e63
Peter Parker <peter.parker@stfc.ac.uk>
2014-11-30 12:46:22 +0000

    Refs #202 - Add sphinx to check_packages.py.

--- check_packages.py
+++ check_packages.py
@@ -19,6 +19,7 @@ common_required_package_list = {
     'wx':{'version':'2.8.12.1','import_name':'wx','test':'__version__'},
     'matplotlib':{'version':'1.1.0','import_name':'matplotlib','test':'__version__'},
     'pisa':{'version':'3.0.28','import_name':'ho.pisa','test':'__version__'},
+    'sphinx':{'version':'1.2.1','import_name':'sphinx','test':'__version__'},
 }
 win_required_package_list = {
     'comtypes':{'version':'0.6.2','import_name':'comtypes','test':'__version__'},
