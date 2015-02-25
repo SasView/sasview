d85973795b991750c70dcd083e11f39a88ed81d5
Doucet, Mathieu <doucetm@ornl.gov>
2015-02-25 09:53:21 -0500

    Remove svn rev code.

--- build_tools/get_version.py
+++ build_tools/get_version.py
@@ -4,8 +4,8 @@ __version__ = "0.0.0"
     
 import subprocess
 
-def get_svn_revision():
-    p = subprocess.Popen("svnversion", shell=True, \
+def get_latest_git_commit():
+    p = subprocess.Popen("git rev-parse --short HEAD", shell=True, \
        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     (stdout, stderr) = p.communicate()
     try:
@@ -13,7 +13,7 @@ def get_svn_revision():
         return str(rev)
     except:
         return __revision__
-
+    
 # Get the version number from the sasview module
 # Check to see if we are dealing with a release
 try:
@@ -25,7 +25,7 @@ except:
 
 # Get actual revision number if possible
 try:
-    rev = get_svn_revision()
+    rev = get_latest_git_commit()
     if rev is not None:
         __revision__ = rev
 except:
--- sasview/__init__.py
+++ sasview/__init__.py
@@ -5,16 +5,6 @@ try:
     d = pkg_resources.get_distribution("sasview")
     __build__ = str(d.version)
 except:
-    try:
-        import os
-        dir = os.path.dirname(__file__)
-        filepath = os.path.join(dir, "BUILD_NUMBER")
-        if os.path.isfile(filepath):
-            f=open(filepath, 'r')
-            buff = f.read().strip()
-            if len(buff)<50:
-                __build__ = buff
-    except:
-        import logging
-        import sys
-        logging.warning("Error while determining build number\n  %s" % sys.exc_value)
+    import logging
+    import sys
+    logging.warning("Error while determining build number\n  %s" % sys.exc_value)
