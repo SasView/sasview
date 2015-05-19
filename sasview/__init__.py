bc84fa7ee20ffc3ec40239d645a48f79863db33d
Mathieu Doucet <matd10@yahoo.com>
2015-05-19 09:25:01 -0400

    Remove access git revision

--- sasview/__init__.py
+++ sasview/__init__.py
@@ -1,10 +1,2 @@
 __version__ = "3.1.0"
 __build__ = "1"
-try:
-    import subprocess
-    d = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD'])
-    __build__ = str(d).strip()
-except:
-    import logging
-    import sys
-    logging.warning("Error while determining build number\n  %s" % sys.exc_value)
