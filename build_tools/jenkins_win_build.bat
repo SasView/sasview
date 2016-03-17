c6ea8e9c0c4969e3ab6da3aea22ff081bde820b5
trnielsen <torben.nielsen@esss.se>
2016-03-17 14:22:45 +0100

    Add new windows build script

---
+++ build_tools/jenkins_win_build.bat
@@ -0,0 +1,49 @@
+
+set PYTHON=C:\Python27\python
+set EASY_INSTALL=c:\python27\scripts\easy_install.exe
+set NEXUSDIR="C:\Program Files (x86)\NeXus Data Format\"
+set PATH=C:\Python27;C:\Python27\Scripts;C:\mingw\bin;%PATH%
+
+
+
+cd %WORKSPACE%
+%PYTHON% check_packages.py
+
+
+cd %WORKSPACE%
+python setup.py build -cmingw32
+
+
+cd %WORKSPACE%
+python setup.py docs
+
+
+cd %WORKSPACE%
+python setup.py bdist_egg --skip-build
+
+
+cd %WORKSPACE%\test
+%PYTHON% utest_sasview.py
+
+
+cd %WORKSPACE%
+mkdir sasview-install
+set PYTHONPATH=%WORKSPACE%\sasview-install;%PYTHONPATH%
+
+
+cd %WORKSPACE%
+cd dist
+%EASY_INSTALL% -d ..\sasview-install sasview-3.1.2-py2.7-win32.egg
+
+
+cd %WORKSPACE%\sasview
+python setup_exe.py py2exe
+python installer_generator.py
+"C:\Program Files (x86)\Inno Setup 5\ISCC.exe" installer.iss 
+
+
+cd Output
+xcopy setupSasView.exe %WORKSPACE%\dist
+
+
+cd  %WORKSPACE%
