ef908db37c9eb600c4448e9e4633b500a40c6361
Jeff Krzywon <krzywon@yahoo.com>
2016-03-19 15:42:09 +0100

    Remove all tests referring to saview Models and delete unused test files.

--- test/sasmodels/test/test_pickle_cyl.py
+++
@@ -1,9 +0,0 @@
-import pickle
-from sas.models.CylinderModel import CylinderModel
-import copy
-
-model = CylinderModel()
-model.setParam('cyl_theta', 1.234)
-p = copy.deepcopy(model)
-
-assert p.getParam('cyl_theta') == model.getParam('cyl_theta')
