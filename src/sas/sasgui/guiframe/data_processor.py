dc2da3d94e998f662862f44953b4ac6dd8bedeab
paul butler <butlerpd@udel.edu>
2016-03-21 05:36:48 -0400

    Link new documentation to data_processor.py (batch fit page).  This completes linking of help to guiframe elements

--- src/sas/sasgui/guiframe/data_processor.py
+++ src/sas/sasgui/guiframe/data_processor.py
@@ -1423,7 +1423,7 @@ class GridPanel(SPanel):
         #if put at top of file with rest of imports.
         from documentation_window import DocumentationWindow
 
-        _TreeLocation = "user/perspectives/fitting/fitting_help.html"
+        _TreeLocation = "user/sasgui/perspectives/fitting/fitting_help.html"
         _PageAnchor = "#batch-fit-mode"
         _doc_viewer = DocumentationWindow(self, -1, _TreeLocation, _PageAnchor,
                                           "Batch Mode Help")
