52b7fd91951e7f237a09e95996a842162b51d9c1
wpotrzebowski <Wojciech.Potrzebowski@esss.se>
2016-05-23 22:16:41 +0200

    PYTHONPATH modified in jenkins osx script, so proper version of six is bundeled

--- build_tools/jenkins_osx_build.sh
+++ build_tools/jenkins_osx_build.sh
@@ -6,6 +6,8 @@ PYLINT=${PYLINT:-`which pylint`}
 
 export PYTHONPATH=$PYTHONPATH:$WORKSPACE/sasview/utils
 export PYTHONPATH=$PYTHONPATH:$WORKSPACE/sasview/sasview-install
+export PYTHONPATH=$PYTHONPATH:/Library/Python/2.7/site-packages
+
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 
