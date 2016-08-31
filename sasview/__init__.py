a3f1c11e89e334f2707e2834709485c8107d1e72
Andrew Jackson <andrew.jackson@esss.se>
2016-08-31 17:46:14 +0200

    Fixing problem with getting git revision

--- sasview/__init__.py
+++ sasview/__init__.py
@@ -1,14 +1,14 @@
 __version__ = "4.0b1"
 __build__ = "GIT_COMMIT"
 try:
+    import logging
     import subprocess
     import os
     FNULL = open(os.devnull, 'w')
-    git_revision = subprocess.check_output(['git', 'rev-parse', 'HEAD'],
+    git_revision = subprocess.check_output(['git describe --tags'],
+    #git_revision = subprocess.check_output(['pwd'],
                     stderr=FNULL,
                     shell=True)
     __build__ = str(git_revision).strip()
-except:
-    import logging
-    import sys
-    logging.warning("Error while determining build number\n  %s" % sys.exc_value)
+except subprocess.CalledProcessError as cpe:
+    logging.warning("Error while determining build number\n  Using command:\n %s \n Output:\n %s"% (cpe.cmd,cpe.output))
