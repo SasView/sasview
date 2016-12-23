2d09bc223867dbeb2d4b3aa663ef3408dab3ea70
Jeff Krzywon <krzywon@yahoo.com>
2016-12-23 10:16:21 -0500

    Trying to remove hidden characters in pagestate.

--- src/sas/sasgui/perspectives/fitting/pagestate.py
+++ src/sas/sasgui/perspectives/fitting/pagestate.py
@@ -395,7 +395,6 @@ class PageState(object):
             elif self.categorycombobox == 'Structure Factor':
                 self.formfactorcombobox = 'hardsphere'
 
-    @staticmethod
     def param_remap_to_sasmodels_convert(params, is_string=False):
         """
         Remaps the parameters for sasmodels conversion
@@ -427,7 +426,6 @@ class PageState(object):
             p[name + ".units"] = units
         return p
 
-    @staticmethod
     def param_remap_from_sasmodels_convert(params):
         """
         Converts {name : value} map back to [] param list
