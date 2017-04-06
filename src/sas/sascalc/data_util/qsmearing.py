d01b55cc7875304ca63dedcfabab1bae5bf2b87a
Adam Washington <adam.washington@stfc.ac.uk>
2017-04-06 10:56:51 +0100

    Include wavelength information in the SesansTransform

--- src/sas/sascalc/data_util/qsmearing.py
+++ src/sas/sascalc/data_util/qsmearing.py
@@ -73,7 +73,7 @@ def smear_selection(data, model = None):
         zaccept = Converter("1/A")(q_max, "1/" + data.source.wavelength_unit),
 
         Rmax = 10000000
-        hankel = SesansTransform(data.x, SElength, zaccept, Rmax)
+        hankel = SesansTransform(data.x, SElength, data.source.wavelength, zaccept, Rmax)
         # Then return the actual transform, as if it were a smearing function
         return PySmear(hankel, model, offset=0)
 
