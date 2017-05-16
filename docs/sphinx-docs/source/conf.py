17aff6f46ed0e38a2827427b91671c63c3ed9f38
Andrew Jackson <andrew.jackson@esss.se>
2017-05-16 15:33:02 +0200

    Changing URL for mathjax CDN as per ticket #948.
    This is an interim solution until the future of mathjax CDN is clear or an alternative solution is deployed in SasView

--- docs/sphinx-docs/source/conf.py
+++ docs/sphinx-docs/source/conf.py
@@ -38,6 +38,9 @@ extensions = ['sphinx.ext.autodoc',
               'dollarmath',
               'sphinx.ext.viewcode']
 
+#set mathjax path
+mathjax_path="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js"
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
