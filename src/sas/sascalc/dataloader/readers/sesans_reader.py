be434489a51d7ec8b03d82a7e2d53cb8baa37c83
Lewis O'Driscoll <lewis.o'driscoll@stfc.ac.uk>
2017-08-02 10:09:59 +0100

    Fix sesans reader

--- src/sas/sascalc/dataloader/readers/sesans_reader.py
+++ src/sas/sascalc/dataloader/readers/sesans_reader.py
@@ -74,74 +74,74 @@ class Reader(FileReader):
         self._insist_header(headers, "Depolarisation_error")
         self._insist_header(headers, "Wavelength")
 
-            data = np.loadtxt(self.f_open)
+        data = np.loadtxt(self.f_open)
 
-            if data.shape[1] != len(headers):
-                raise FileContentsException(
-                    "File has {} headers, but {} columns".format(
-                        len(headers),
-                        data.shape[1]))
+        if data.shape[1] != len(headers):
+            raise FileContentsException(
+                "File has {} headers, but {} columns".format(
+                    len(headers),
+                    data.shape[1]))
 
-            if not data.size:
-                raise FileContentsException("{} is empty".format(path))
-            x = data[:, headers.index("SpinEchoLength")]
-            if "SpinEchoLength_error" in headers:
-                dx = data[:, headers.index("SpinEchoLength_error")]
-            else:
-                dx = x * 0.05
-            lam = data[:, headers.index("Wavelength")]
-            if "Wavelength_error" in headers:
-                dlam = data[:, headers.index("Wavelength_error")]
-            else:
-                dlam = lam * 0.05
-            y = data[:, headers.index("Depolarisation")]
-            dy = data[:, headers.index("Depolarisation_error")]
+        if not data.size:
+            raise FileContentsException("{} is empty".format(path))
+        x = data[:, headers.index("SpinEchoLength")]
+        if "SpinEchoLength_error" in headers:
+            dx = data[:, headers.index("SpinEchoLength_error")]
+        else:
+            dx = x * 0.05
+        lam = data[:, headers.index("Wavelength")]
+        if "Wavelength_error" in headers:
+            dlam = data[:, headers.index("Wavelength_error")]
+        else:
+            dlam = lam * 0.05
+        y = data[:, headers.index("Depolarisation")]
+        dy = data[:, headers.index("Depolarisation_error")]
 
-            lam_unit = self._unit_fetch("Wavelength")
-            x, x_unit = self._unit_conversion(x, "A",
-                                              self._unit_fetch(
-                                                  "SpinEchoLength"))
-            dx, dx_unit = self._unit_conversion(
-                dx, lam_unit,
-                self._unit_fetch("SpinEchoLength"))
-            dlam, dlam_unit = self._unit_conversion(
-                dlam, lam_unit,
-                self._unit_fetch("Wavelength"))
-            y_unit = self._unit_fetch("Depolarisation")
+        lam_unit = self._unit_fetch("Wavelength")
+        x, x_unit = self._unit_conversion(x, "A",
+                                          self._unit_fetch(
+                                              "SpinEchoLength"))
+        dx, dx_unit = self._unit_conversion(
+            dx, lam_unit,
+            self._unit_fetch("SpinEchoLength"))
+        dlam, dlam_unit = self._unit_conversion(
+            dlam, lam_unit,
+            self._unit_fetch("Wavelength"))
+        y_unit = self._unit_fetch("Depolarisation")
 
-            self.current_dataset.x = x
-            self.current_dataset.y = y
-            self.current_dataset.lam = lam
-            self.current_dataset.dy = dy
-            self.current_dataset.dx = dx
-            self.current_dataset.dlam = dlam
-            self.current_datainfo.isSesans = True
+        self.current_dataset.x = x
+        self.current_dataset.y = y
+        self.current_dataset.lam = lam
+        self.current_dataset.dy = dy
+        self.current_dataset.dx = dx
+        self.current_dataset.dlam = dlam
+        self.current_datainfo.isSesans = True
 
-            self.current_datainfo._yunit = y_unit
-            self.current_datainfo._xunit = x_unit
-            self.current_datainfo.source.wavelength_unit = lam_unit
-            self.current_datainfo.source.wavelength = lam
-            self.current_datainfo.filename = os.basename(self.f_open.name)
-            self.current_dataset.xaxis(r"\rm{z}", x_unit)
-            # Adjust label to ln P/(lam^2 t), remove lam column refs
-            self.current_dataset.yaxis(r"\rm{ln(P)/(t \lambda^2)}", y_unit)
-            # Store loading process information
-            self.current_datainfo.meta_data['loader'] = self.type_name
-            self.current_datainfo.sample.name = params["Sample"]
-            self.current_datainfo.sample.ID = params["DataFileTitle"]
-            self.current_datainfo.sample.thickness = self._unit_conversion(
-                float(params["Thickness"]), "cm",
-                self._unit_fetch("Thickness"))[0]
+        self.current_datainfo._yunit = y_unit
+        self.current_datainfo._xunit = x_unit
+        self.current_datainfo.source.wavelength_unit = lam_unit
+        self.current_datainfo.source.wavelength = lam
+        self.current_datainfo.filename = os.path.basename(self.f_open.name)
+        self.current_dataset.xaxis(r"\rm{z}", x_unit)
+        # Adjust label to ln P/(lam^2 t), remove lam column refs
+        self.current_dataset.yaxis(r"\rm{ln(P)/(t \lambda^2)}", y_unit)
+        # Store loading process information
+        self.current_datainfo.meta_data['loader'] = self.type_name
+        self.current_datainfo.sample.name = params["Sample"]
+        self.current_datainfo.sample.ID = params["DataFileTitle"]
+        self.current_datainfo.sample.thickness = self._unit_conversion(
+            float(params["Thickness"]), "cm",
+            self._unit_fetch("Thickness"))[0]
 
-            self.current_datainfo.sample.zacceptance = (
-                float(params["Theta_zmax"]),
-                self._unit_fetch("Theta_zmax"))
+        self.current_datainfo.sample.zacceptance = (
+            float(params["Theta_zmax"]),
+            self._unit_fetch("Theta_zmax"))
 
-            self.current_datainfo.sample.yacceptance = (
-                float(params["Theta_ymax"]),
-                self._unit_fetch("Theta_ymax"))
+        self.current_datainfo.sample.yacceptance = (
+            float(params["Theta_ymax"]),
+            self._unit_fetch("Theta_ymax"))
 
-            self.send_to_output()
+        self.send_to_output()
 
     @staticmethod
     def _insist_header(headers, name):
