1f21a4324ca47f4d2cb52e146b0a47cf9767fad9
Paul Kienzle <pkienzle@nist.gov>
2017-09-19 13:02:20 -0400

    more complete corfunc test

--- test/corfunc/test/utest_corfunc.py
+++ test/corfunc/test/utest_corfunc.py
@@ -13,18 +13,29 @@ class TestCalculator(unittest.TestCase):
 
     def setUp(self):
         self.data = load_data()
+        # Note: to generate target values from the GUI:
+        # * load the data from test/corfunc/test/98929.txt
+        # * set qrange to (0, 0.013), (0.15, 0.24)
+        # * select fourier transform type
+        # * click Calculate Bg
+        # * click Extrapolate
+        # * click Compute Parameters
+        # * copy the Guinier and Porod values to the extrapolate function
+        # * for each graph, grab the data from DataInfo and store it in _out.txt
         self.calculator = CorfuncCalculator(data=self.data, lowerq=0.013,
             upperq=(0.15, 0.24))
+        self.calculator.background = 0.3
         self.extrapolation = None
+        self.results = [np.loadtxt(filename+"_out.txt").T[2]
+                        for filename in ("gamma1", "gamma3", "idf")]
 
     def extrapolate(self):
         params, extrapolation, s2 = self.calculator.compute_extrapolation()
-
         # Check the extrapolation parameters
-        self.assertAlmostEqual(params['A'], 4.19, places=2)
-        self.assertAlmostEqual(params['B'], -25470, places=0)
-        self.assertAlmostEqual(params['K'], 4.5e-5, places=2)
-        self.assertAlmostEqual(params['sigma'], 2.2e-10, places=2)
+        self.assertAlmostEqual(params['A'], 4.18970, places=5)
+        self.assertAlmostEqual(params['B'], -25469.9, places=1)
+        self.assertAlmostEqual(params['K'], 4.44660e-5, places=10)
+        self.assertAlmostEqual(params['sigma'], 1.70181e-10, places=15)
 
         # Ensure the extraplation tends to the background value
         self.assertAlmostEqual(extrapolation.y[-1], self.calculator.background)
@@ -61,18 +72,28 @@ class TestCalculator(unittest.TestCase):
         self.assertIsNotNone(transform1)
         self.assertAlmostEqual(transform1.y[0], 1)
         self.assertAlmostEqual(transform1.y[-1], 0, 5)
-        self.transformation = transform1
+        self.transformation = transforms
 
     def extract_params(self):
-        params = self.calculator.extract_parameters(self.transformation)
+        params = self.calculator.extract_parameters(self.transformation[0])
         self.assertIsNotNone(params)
         self.assertEqual(len(params), 6)
         self.assertLess(abs(params['max']-75), 2.5) # L_p ~= 75
 
+    def check_transforms(self):
+        gamma1, gamma3, idf = self.transformation
+        gamma1_out, gamma3_out, idf_out = self.results
+        def compare(a, b):
+            return max(abs((a-b)/b))
+        #np.savetxt("gamma1_test.txt", np.vstack((gamma1.x[gamma1.x<=200.], gamma1.y[gamma1.x<=200.])).T)
+        self.assertLess(compare(gamma1.y[gamma1.x<=200.], gamma1_out), 1e-10)
+        self.assertLess(compare(gamma3.y[gamma3.x<=200.], gamma3_out), 1e-10)
+        self.assertLess(compare(idf.y[idf.x<=200.], idf_out), 1e-10)
+
     # Ensure tests are ran in correct order;
     # Each test depends on the one before it
     def test_calculator(self):
-        steps = [self.extrapolate, self.transform, self.extract_params]
+        steps = [self.extrapolate, self.transform, self.extract_params, self.check_transforms]
         for test in steps:
             try:
                 test()
@@ -82,7 +103,7 @@ class TestCalculator(unittest.TestCase):
 
 
 def load_data(filename="98929.txt"):
-    data = np.loadtxt(filename, dtype=np.float32)
+    data = np.loadtxt(filename, dtype=np.float64)
     q = data[:,0]
     iq = data[:,1]
     return Data1D(x=q, y=iq)
