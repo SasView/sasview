6a88ad9d0769c1221502eb3945898208b681c8be
Paul Kienzle <pkienzle@nist.gov>
2017-09-21 18:02:02 -0400

    git build tag moved to run.py

--- src/sas/sasview/__init__.py
+++ src/sas/sasview/__init__.py
@@ -1,20 +1,2 @@
 __version__ = "4.1"
 __build__ = "GIT_COMMIT"
-
-try:
-    import subprocess
-    import os
-    import platform
-    FNULL = open(os.devnull, 'w')
-    if platform.system() == "Windows":
-        args = ['git', 'describe', '--tags']
-    else:
-        args = ['git describe --tags']
-    git_revision = subprocess.check_output(args,
-                    stderr=FNULL,
-                    shell=True)
-    __build__ = str(git_revision).strip()
-except subprocess.CalledProcessError as cpe:
-    import logging
-    logger = logging.getLogger(__name__)
-    logger.warning("Error while determining build number\n  Using command:\n %s \n Output:\n %s"% (cpe.cmd,cpe.output))
