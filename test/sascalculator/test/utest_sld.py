a50da8251d0d92f4c04979ecac4b78ac2c43b925
Paul Kienzle <pkienzle@nist.gov>
2017-09-23 11:24:22 -0400

    enable periodictable tests

--- test/sascalculator/test/utest_sld.py
+++ test/sascalculator/test/utest_sld.py
@@ -9,7 +9,7 @@ from periodictable import formula
 from periodictable.xsf import xray_energy, xray_sld_from_atoms
 from periodictable.constants import avogadro_number
 from  periodictable.nsf import neutron_scattering, neutron_sld
-       
+
 
 def calculate_xray_sld(element, density, molecule_formula):
     """
@@ -18,8 +18,8 @@ def calculate_xray_sld(element, density, molecule_formula):
     """
     element_formula = formula(str(element))
     if len(element_formula.atoms) != 1:
-        return 
-    element = element_formula.atoms.keys()[0] 
+        return
+    element = next(iter(element_formula.atoms))  # only one element...
     energy = xray_energy(element.K_alpha)
     atom = molecule_formula.atoms
     return xray_sld_from_atoms(atom, density=density, energy=energy)
@@ -29,27 +29,27 @@ class TestH2O(unittest.TestCase):
     """
     Sld calculator test for H2O
     """
-    
+
     def setUp(self):
         """Inititialze variables"""
-        
+
         self.compound = "H2O"
         self.density = 1.0
         self.wavelength = 6.0
         self.sld_formula = formula(self.compound, density=self.density)
-    
+
     def test_neutron_sld(self):
         """
-        test sld 
+        test sld
         """
         #Compute incoherence , absorption, and incoherence
         (sld_real,sld_im,sld_inc), (coh,abs,incoh), length = neutron_scattering(self.compound,
-                                       density=self.density, wavelength=self.wavelength) 
+                                       density=self.density, wavelength=self.wavelength)
         cu_real, cu_im = calculate_xray_sld(element="Cu", density=self.density,
                                   molecule_formula=self.sld_formula)
         mo_real, mo_im = calculate_xray_sld(element="Mo", density=self.density,
                                   molecule_formula=self.sld_formula)
-        #test sld 
+        #test sld
         self.assertAlmostEquals(sld_real * _SCALE, -5.6e-7, 1)
         self.assertAlmostEquals(sld_im * _SCALE, 0)
         #test absorption value
@@ -63,33 +63,33 @@ class TestH2O(unittest.TestCase):
         # test Mo sld
         self.assertAlmostEquals(mo_real * _SCALE, 9.43e-6)
         self.assertAlmostEquals(mo_im * _SCALE, 5.65e-7,1)
-    
-    
+
+
 class TestD2O(unittest.TestCase):
     """
     Sld calculator test for D2O
     """
-    
+
     def setUp(self):
         """Inititialze variables"""
-        
+
         self.compound = "D2O"
         self.density = 1.1
         self.wavelength = 6.0
         self.sld_formula = formula(self.compound, density=self.density)
-    
+
     def test_neutron_sld(self):
         """
-        test sld 
+        test sld
         """
         #Compute incoherence , absorption, and incoherence
         (sld_real,sld_im,sld_inc), (coh,abs,incoh), length = neutron_scattering(self.compound,
-                                       density=self.density, wavelength=self.wavelength) 
+                                       density=self.density, wavelength=self.wavelength)
         cu_real, cu_im = calculate_xray_sld(element="Cu", density=self.density,
                                   molecule_formula=self.sld_formula)
         mo_real, mo_im = calculate_xray_sld(element="Mo", density=self.density,
                                   molecule_formula=self.sld_formula)
-        #test sld 
+        #test sld
         self.assertAlmostEquals(sld_real * _SCALE, 6.33e-6, 1)
         self.assertAlmostEquals(sld_im * _SCALE, 0)
         #test absorption value
@@ -103,13 +103,13 @@ class TestD2O(unittest.TestCase):
         # test Mo sld
         self.assertAlmostEquals(mo_real * _SCALE, 9.33e-6)
         self.assertAlmostEquals(mo_im * _SCALE, 5.59e-9,1)
-    
-        
+
+
 class TestCd(unittest.TestCase):
     """
     Sld calculator test for Cd
     """
-    
+
     def setUp(self):
         """Inititialze variables"""
         # the calculator default value for wavelength is 6
@@ -117,19 +117,19 @@ class TestCd(unittest.TestCase):
         self.density = 4.0
         self.wavelength = 6.0
         self.sld_formula = formula(self.compound, density=self.density)
-       
+
     def test_neutron_sld(self):
         """
-        test sld 
+        test sld
         """
         #Compute incoherence , absorption, and incoherence
         (sld_real,sld_im,sld_inc), (coh,abs,incoh), length = neutron_scattering(self.compound,
-                                density=self.density, wavelength=self.wavelength) 
+                                density=self.density, wavelength=self.wavelength)
         cu_real, cu_im = calculate_xray_sld(element="Cu", density=self.density,
                                   molecule_formula=self.sld_formula)
         mo_real, mo_im = calculate_xray_sld(element="Mo", density=self.density,
                                   molecule_formula=self.sld_formula)
-        #test sld 
+        #test sld
         self.assertAlmostEquals(sld_real * _SCALE, 1.04e-6, 1)
         self.assertAlmostEquals(sld_im * _SCALE, -1.5e-7, 1)
         #test absorption value
@@ -143,6 +143,6 @@ class TestCd(unittest.TestCase):
         # test Mo sld
         self.assertAlmostEquals(mo_real * _SCALE, 2.84e-5, 1)
         self.assertAlmostEquals(mo_im * _SCALE, 7.26e-7,1)
-  
+
 if __name__ == '__main__':
     unittest.main()
