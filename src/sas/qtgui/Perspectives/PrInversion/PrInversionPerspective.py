d2d0441e1ba7062f8a3cdd048cc39e4b8273d7a9
Jeff Krzywon <krzywon@yahoo.com>
2017-10-26 11:55:59 +0200

    Finished P(r) GUI. Added basic methods for closing window.

--- src/sas/qtgui/Perspectives/PrInversion/PrInversionPerspective.py
+++ src/sas/qtgui/Perspectives/PrInversion/PrInversionPerspective.py
@@ -1,12 +1,11 @@
-from PyQt4 import QtGui
+from PyQt4 import QtGui, QtCore, QtWebKit
 
 # sas-global
-from sas.sascalc.invariant import invariant
-from sas.qtgui.Plotting.PlotterData import Data1D
-import sas.qtgui.Utilities.GuiUtils as GuiUtils
+#import sas.qtgui.Utilities.GuiUtils as GuiUtils
 
 # pr inversion gui elements
-from sas.qtgui.Perspectives.PrInversion.UI.TabbedPrInversionUI import Ui_PrInversion
+from sas.qtgui.Perspectives.PrInversion.UI.TabbedPrInversionUI import \
+    Ui_PrInversion
 
 class PrInversionWindow(QtGui.QTabWidget, Ui_PrInversion):
     """
@@ -20,10 +19,37 @@ class PrInversionWindow(QtGui.QTabWidget, Ui_PrInversion):
 
         self.setWindowTitle("P(r) Inversion Perspective")
 
-        self.parent = parent
+        self._manager = parent
+
+        self._model_item = QtGui.QStandardItem()
+        self._helpView = QtWebKit.QWebView()
         self._data = data
 
         # The tabs need to be closeable
         self._allow_close = False
 
-        self.communicate = self.parent.communicator()
+        # Let's choose the Standard Item Model.
+        self.model = QtGui.QStandardItemModel(self)
+
+    def allowBatch(self):
+        return False
+
+    def setClosable(self, value=True):
+        """
+        Allow outsiders close this widget
+        """
+        assert isinstance(value, bool)
+        self._allow_close = value
+
+    def closeEvent(self, event):
+        """
+        Overwrite QDialog close method to allow for custom widget close
+        """
+        if self._allow_close:
+            # reset the closability flag
+            self.setClosable(value=False)
+            event.accept()
+        else:
+            event.ignore()
+            # Maybe we should just minimize
+            self.setWindowState(QtCore.Qt.WindowMinimized)
