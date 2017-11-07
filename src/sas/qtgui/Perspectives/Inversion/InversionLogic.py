5fba4c48e5252f419671a379e799a5c662c5829f
krzywon <krzywon@yahoo.com>
2017-11-07 11:15:20 -0500

    Update plots if they exist.

--- src/sas/qtgui/Perspectives/Inversion/InversionLogic.py
+++ src/sas/qtgui/Perspectives/Inversion/InversionLogic.py
@@ -112,6 +112,60 @@ class InversionLogic(object):
 
         return new_plot
 
+    def update1DPlot(self, plot, out, pr, q=None):
+        """
+        Create a new 1D data instance based on fitting results
+        """
+
+        qtemp = pr.x
+        if q is not None:
+            qtemp = q
+
+        # Make a plot
+        maxq = max(qtemp)
+
+        minq = min(qtemp)
+
+        # Check for user min/max
+        if pr.q_min is not None and maxq >= pr.q_min >= minq:
+            minq = pr.q_min
+        if pr.q_max is not None and maxq >= pr.q_max >= minq:
+            maxq = pr.q_max
+
+        x = pylab.arange(minq, maxq, maxq / 301.0)
+        y = np.zeros(len(x))
+        err = np.zeros(len(x))
+        for i in range(len(x)):
+            value = pr.iq(out, x[i])
+            y[i] = value
+            try:
+                err[i] = math.sqrt(math.fabs(value))
+            except:
+                err[i] = 1.0
+                print("Error getting error", value, x[i])
+
+        plot.x = x
+        plot.y = y
+
+        # If we have used slit smearing, plot the smeared I(q) too
+        if pr.slit_width > 0 or pr.slit_height > 0:
+            x = pylab.arange(minq, maxq, maxq / 301.0)
+            y = np.zeros(len(x))
+            err = np.zeros(len(x))
+            for i in range(len(x)):
+                value = pr.iq_smeared(pr.out, x[i])
+                y[i] = value
+                try:
+                    err[i] = math.sqrt(math.fabs(value))
+                except:
+                    err[i] = 1.0
+                    print("Error getting error", value, x[i])
+
+            plot.x = x
+            plot.y = y
+
+        return plot
+
     def newPRPlot(self, out, pr, cov=None):
         """
         """
@@ -120,7 +174,6 @@ class InversionLogic(object):
 
         y = np.zeros(len(x))
         dy = np.zeros(len(x))
-        y_true = np.zeros(len(x))
 
         total = 0.0
         pmax = 0.0
@@ -161,6 +214,43 @@ class InversionLogic(object):
         new_plot.group_id = GROUP_ID_PR_FIT
 
         return new_plot
+
+    def updatePRPlot(self, plot, out, pr, cov=None):
+        x = pylab.arange(0.0, pr.d_max, pr.d_max / self._pr_n_pts)
+
+        y = np.zeros(len(x))
+        dy = np.zeros(len(x))
+
+        total = 0.0
+        pmax = 0.0
+        cov2 = np.ascontiguousarray(cov)
+
+        for i in range(len(x)):
+            if cov2 is None:
+                value = pr.pr(out, x[i])
+            else:
+                (value, dy[i]) = pr.pr_err(out, cov2, x[i])
+            total += value * pr.d_max / len(x)
+
+            # keep track of the maximum P(r) value
+            if value > pmax:
+                pmax = value
+
+            y[i] = value
+
+        # if self._normalize_output == True:
+        #     y = y / total
+        #     dy = dy / total
+        # elif self._scale_output_unity == True:
+        #     y = y / pmax
+        #     dy = dy / pmax
+        plot.x = x
+        plot.y = y
+
+        if cov2 is not None:
+            plot.dy = dy
+
+        return plot
 
     def computeDataRange(self):
         """
