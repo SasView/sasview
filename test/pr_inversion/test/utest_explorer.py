7c4bb4dbc7861021d98b1ad347e2c9f3ed90b336
Paul Kienzle <pkienzle@nist.gov>
2018-10-11 14:34:40 -0400

    fix pylint relative import error on pr_inversion test

--- test/pr_inversion/test/utest_explorer.py
+++ test/pr_inversion/test/utest_explorer.py
@@ -2,12 +2,11 @@
     Unit tests for DistExplorer class
 """
 
+import sys
 import os.path
 import unittest, math, numpy
 from sas.sascalc.pr.invertor import Invertor
 from sas.sascalc.pr.distance_explorer import DistExplorer
-from .utest_invertor import load
-
 
 def find(filename):
     return os.path.join(os.path.dirname(__file__), filename)
@@ -35,6 +34,41 @@ class TestExplorer(unittest.TestCase):
         results = self.explo(120, 200, 25)
         self.assertEqual(len(results.errors), 0)
         self.assertEqual(len(results.chi2), 25)
+
+
+# Note: duplicated from utest_invertor because the following failed:
+#from .utest_invertor import load
+def load(path = "sphere_60_q0_2.txt"):
+    import numpy as np
+    import math
+    import sys
+    # Read the data from the data file
+    data_x   = np.zeros(0)
+    data_y   = np.zeros(0)
+    data_err = np.zeros(0)
+    scale    = None
+    if path is not None:
+        input_f = open(path,'r')
+        buff    = input_f.read()
+        lines   = buff.split('\n')
+        for line in lines:
+            try:
+                toks = line.split()
+                x = float(toks[0])
+                y = float(toks[1])
+                if len(toks)>2:
+                    err = float(toks[2])
+                else:
+                    if scale==None:
+                        scale = 0.15*math.sqrt(y)
+                    err = scale*math.sqrt(y)
+                data_x = np.append(data_x, x)
+                data_y = np.append(data_y, y)
+                data_err = np.append(data_err, err)
+            except:
+                pass
+
+    return data_x, data_y, data_err
         
 if __name__ == '__main__':
     unittest.main()
