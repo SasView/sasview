5e16b300002805e77419d0b6692a4dbc4c5b04f6
Paul Butler <butlerpd@udel.edu>
2019-02-10 13:13:57 -0500

    Update __init__.py
    Final update to __init__.py with the new DOI from zenodo.  Should now be ready to merge

--- src/sas/sasview/__init__.py
+++ src/sas/sasview/__init__.py
@@ -1,6 +1,6 @@
 from distutils.version import StrictVersion
 __version__ = "4.2.1"
 StrictVersion(__version__)
-__DOI__ = "Zenodo, 10.5281/zenodo.1412041"
+__DOI__ = "Zenodo, 10.5281/zenodo.2561236"
 __release_date__ = "2019"
 __build__ = "GIT_COMMIT"
