c892a50a53199d350380b3bbf245ead395f41ef9
Piotr Rozyczko <piotr.rozyczko@gmail.com>
2019-04-12 10:56:31 +0200

    Added FONTCONFIG paths so fonts on Ubuntu look pretty and consistent. SASVIEW-1136

--- installers/set_sasview_qt5_path.sh
+++ installers/set_sasview_qt5_path.sh
@@ -3,3 +3,9 @@
 PyQt5='/PyQt5/Qt/plugins'
 export QT_PLUGIN_PATH=$PWD$PyQt5
 
+# fontconfig paths
+if [ -d /etc/fonts ]; then
+  export FONTCONFIG_PATH=/etc/fonts/
+  export FONTCONFIG_FILE=/etc/fonts/fonts.conf
+fi
+
