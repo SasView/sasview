ecbfb0883718017b4eaa251fcb5dced708ae7ea6
Paul Butler <butlerpd@udel.edu>
2019-04-27 16:53:42 -0400

    Update DOI for 4.2.2

--- src/sas/sasview/__init__.py
+++ src/sas/sasview/__init__.py
@@ -1,6 +1,6 @@
 from distutils.version import StrictVersion
 __version__ = "4.2.2"
 StrictVersion(__version__)
-__DOI__ = "Zenodo, 10.5281/zenodo.2561236"
+__DOI__ = "Zenodo, :10.5281/zenodo.2652478"
 __release_date__ = "2019"
 __build__ = "GIT_COMMIT"
