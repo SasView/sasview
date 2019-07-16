cf81c4be5e87d18010ac8a09d8a42afbf3b7afb2
piotr.rozyczko@gmail.com <piotr.rozyczko@gmail.com>
2019-07-16 15:31:59 +0200

    Define SAS_MODELPATH for sasmodels to use. Don't add [plug-in] to custom plugin name. Part of SASVIEW-1329

--- run.py
+++ run.py
@@ -20,7 +20,9 @@ import os
 import sys
 from contextlib import contextmanager
 from os.path import join as joinpath
-from os.path import abspath, dirname
+from os.path import abspath, dirname, realpath
+
+PLUGIN_MODEL_DIR = 'plugin_models'
 
 def addpath(path):
     """
@@ -65,6 +67,14 @@ def import_dll(modname, build_path):
     # print "importing", modname, "from", path
     return imp.load_dynamic(modname, path)
 
+def setup_sasmodels():
+    """
+    Prepare sasmodels for running within sasview.
+    """
+    # Set SAS_MODELPATH so sasmodels can find our custom models
+    import sas
+    plugin_dir = os.path.join(sas.get_user_dir(), PLUGIN_MODEL_DIR)
+    os.environ['SAS_MODELPATH'] = plugin_dir
 
 def prepare(rebuild=True):
     # Don't create *.pyc files
@@ -176,6 +186,7 @@ if __name__ == "__main__":
 
     get_logger().debug("Starting SASVIEW in debug mode.")
     prepare()
+    setup_sasmodels()
     from sas.sasview.sasview import run_cli, run_gui
     if len(sys.argv) == 1:
         run_gui()
