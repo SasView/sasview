fc346db501253a765c1e831e5c0191d0799aa1f2
ZiggyAttala <ziggy.attala@stfc.ac.uk>
2019-07-22 15:56:28 +0100

    base methods added

--- src/sas/sascalc/file_converter/py_bsl_loader.py
+++ src/sas/sascalc/file_converter/py_bsl_loader.py
@@ -4,14 +4,6 @@ Python implementation of C extension bsl_loader.c
 import numpy as np
 
 class PLoader():
-    def __init__(self, filename, frames, pixels, rasters, swap_bytes):
-        self.filename = filename
-        self.n_frames = frames
-        self.n_pixels = pixels
-        self.n_rasters = rasters
-        self.swap_bytes = swap_bytes
-        #Placeholder value
-        self.frame = 0
 
     #File to load (string)
     @property
@@ -61,15 +53,38 @@ class PLoader():
     def swap_bytes(self, swap_bytes):
         self._swap_bytes = swap_bytes
 
+    def __init__(self, filename, frames, pixels, rasters, swap_bytes):
+        self.filename = filename
+        self.n_frames = frames
+        self.n_pixels = pixels
+        self.n_rasters = rasters
+        self.swap_bytes = swap_bytes
+        #Placeholder value
+        self.frame = 0
+
+    def __str__(self):
+        """
+        Print the objects params.
+        """
+        desc = "Filename: " + self.filename + "\n"
+        desc += "n_frames: " + str(self.n_frames) + "\n"
+        desc += "frame: " + str(self.frame) + "\n"
+        desc += "n_pixels: " + str(self.n_pixels) + "\n"
+        desc += "n_rasters: " + str(self.n_rasters) + "\n"
+        desc += "swap_bytes: " + str(self.swap_bytes) + "\n"
+        return desc
+
+    def load_data(self):
+        """
+        Load the data into a numpy array.
+        """
+        pass
+
+
 
 if(__name__ == "__main__"):
     test = PLoader("Name", 20, 20, 20, 20)
     test.filename = "ASDF"
     test.frame = test.n_frames * test.n_pixels
 
-    print(test.filename)
-    print(test.frame)
-    print(test.n_frames)
-    print(test.n_pixels)
-    print(test.n_rasters)
-    print(test.swap_bytes)
+    print(test)
