ab7bc166fa64acb1b90b279dfa8a394123bf4f34
ZiggyAttala <ziggy.attala@stfc.ac.uk>
2019-08-13 15:20:30 +0100

    setup.py updated to remove reference to c_extensions.
    code builds properly now, but not sure if more needs to be done in run.py or somewhere else to remove all references to pr c_extensions.

--- setup.py
+++ setup.py
@@ -244,15 +244,8 @@ ext_modules.append(Extension("sas.sascalc.calculator._sld2i",
                              ))
 
 # sas.sascalc.pr
-srcdir = os.path.join("src", "sas", "sascalc", "pr", "c_extensions")
 package_dir["sas.sascalc.pr"] = os.path.join("src", "sas", "sascalc", "pr")
 packages.append("sas.sascalc.pr")
-ext_modules.append(Extension("sas.sascalc.pr._pr_inversion",
-                             sources=[os.path.join(srcdir, "Cinvertor.c"),
-                                      os.path.join(srcdir, "invertor.c"),
-                                      ],
-                             include_dirs=[],
-                             ))
 
 
 # sas.sascalc.file_converter
