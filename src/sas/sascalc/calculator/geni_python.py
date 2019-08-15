19cfcf90440a972a81e31e69a0c5f1f83144026e
ZiggyAttala <ziggy.attala@stfc.ac.uk>
2019-08-15 16:51:17 +0100

    implemented first version of gen_i class

--- src/sas/sascalc/calculator/geni_python.py
+++ src/sas/sascalc/calculator/geni_python.py
@@ -4,35 +4,139 @@ class, defined through structs and global free functions in C.
 
 Depends on lib.py, implementation of libfunc.c and librefl.c
 """
+from math import factorial
+
 import numpy as np
+from scipy.special import sici
 
 from . import lib
 
 class GenI():
-    #Instance variables, no setters/getters in C so public.
+    def __init__(self, is_avg, npix, x, y, sldn, z, mx, my, mz,
+                 voli, in_spin, out_spin, s_theta):
+        """
+        Constructor for GenI
+        :param qx: array of Qx values.
+        :param qy: array of Qy values.
+        :param qz: array of Qz values.
+        :param x: array of x values.
+        :param y: array of y values.
+        :param z: array of z values.
+        :param sldn: array of sld n.
+        :param mx: array of sld mx.
+        :param my: array of sld my.
+        :param mz: array of sld mz.
+        :param in_spin: ratio of up spin in Iin.
+        :param out_spin: ratio of up spin in Iout.
+        :param s_theta: angle (from x-axis) of the up spin in degrees.
+        """
+        self.is_avg = is_avg
+        self.n_pix = npix
+        self.x_val = x
+        self.y_val = y
+        self.z_val = z
+        self.sldn_val = sldn
+        self.mx_val = mx
+        self.my_val = my
+        self.mz_val = mz
+        self.vol_pix = voli
+        self.inspin = in_spin
+        self.outspin = out_spin
+        self.stheta = s_theta
 
-    is_avg = int(0)
-    n_pix = int(0)
-    x_val = np.zeros(0, dtype=float)
-    y_val = np.zeros(0, dtype=float)
-    z_val = np.zeros(0, dtype=float)
-    sldn_val = np.zeros(0, dtype=float)
-    mx_val = np.zeros(0, dtype=float)
-    my_val = np.zeros(0, dtype=float)
-    mz_val = np.zeros(0, dtype=float)
-    vol_pix = np.zeros(0, dtype=float)
+    def genicomXY(self, npoints, qx, qy, I_out):
+        """
+        Compute 2d ansotropic.
 
-    #Spin Ratios.
-    inspin = float(0)
-    outspin = float(0)
-    stheta = float(0)
+        Stores result by reference in vector I_out[]
+        """
+        #npoints is given negative for angular averaging
+        #Assumes that q doesn't have qz component and sld_n is all real
+        b_sld = polar_sld()
+        qr = float()
+        iqr = np.complex()
+        ephase = np.complex()
+        comp_sld = np.complex()
 
-    def __init__(self):
-        pass
+        sumj_uu = np.complex()
+        sumj_ud = np.complex()
+        sumj_du = np.complex()
+        sumj_dd = np.complex()
+        temp_fi = np.complex()
+
+        count = float()
+
+        iqr = np.complex()
+        ephase = np.complex()
+        comp_sld = np.complex()
+	    #Assume that pixel volumes are given in vol_pix in A^3 unit
+        #Loop over q-values and multiply apply matrix
+        for i in range(npoints):
+            sumj_uu = np.complex()
+            sumj_ud = np.complex()
+            sumj_du = np.complex()
+            sumj_dd = np.complex()
+
+            for j in range(self.n_pix):
+                if self.sldn_val[j] != 0.0 | self.mx_val[j] != 0.0 | self.my_val[j] != 0.0 | self.mz_val[j] != 0.0:
+
+				    #anisotropic
+                    temp_fi = np.complex()
+                    b_sld.cal_msld(b_sld, 0, qx[i], qy[i], self.sldn_val[j],
+                                   self.mx_val[j], self.my_val[j], self.mz_val[j],
+                                   self.inspin, self.outspin, self.stheta);
+                    qr = (qx[i]*self.x_val[j] + qy[i]*self.y_val[j]);
+                    iqr = np.complex(0.0, qr)
+
+                    #As in the original code, sets ephase reference to two methods that should
+                    #both reset it, both set the real and imaginary parts.
+                    ephase = np.exp(iqr)
+                    #Let's multiply pixel(atomic) volume here
+                    ephase = self.vol_pix[j] * ephase
+
+				    #up_up
+                    if (self.inspin > 0.0) & (self.outspin > 0.0):
+                        comp_sld = np.complex(b_sld.uu, 0.0)
+                        temp_fi = comp_sld * ephase
+                        sumj_uu = sumj_uu + temp_fi
+
+				    #down_down
+                    if (self.inspin < 1.0) & (self.outspin < 1.0):
+                        comp_sld = np.complex(b_sld.dd, 0.0)
+                        temp_fi = comp_sld * ephase
+                        sumj_dd = sumj_dd + temp_fi
+
+                    #up_down
+                    if (self.inspin > 0.0) & (self.outspin < 1.0):
+                        comp_sld = np.complex(b_sld.re_ud, b_sld.im_ud)
+                        temp_fi = comp_sld * ephase
+                        sumj_ud = sumj_ud + temp_fi
+
+                    #down_up
+                    if (self.inspin < 1.0) & (self.outspin > 0.0):
+                        comp_sld = np.complex(b_sld.re_du, b_sld.im_du)
+                        temp_fi = comp_sld * ephase
+                        sumj_du = sumj_du + temp_fi
+
+                    if i == 0:
+                        count += self.vol_pix[j];
+
+            calc = lambda x: np.square(x.real) + np.square(y.imag)
+
+            I_out[i] = calc(sumj_uu)
+            I_out[i] += calc(sumj_ud)
+            I_out[i] += calc(sumj_du)
+            I_out[i] += calc(sumj_dd)
+
+            I_out[i] *= (1.0E+8 / count) #in cm (unit) / number; to be multiplied by vol_pix
 
     def genicom(self, npoints, q, I_out):
+        """
+        Computes 1D isotropic.
+        Isotropic: Assumes all slds are real (no magnetic)
+        Also assumes there is no polarization: No dependency on spin.
+        """
         #current method returns by reference using I_out, will change to return by value.
-
         #qr = vector norm in 3d.
 	    #make 3d matrix of points for other purposes as well?
 	    #x y and z, all length j, must be same,
@@ -42,7 +146,7 @@ class GenI():
         coords[1, :] = self.y_val
         coords[2, :] = self.z_val
 
-        norm_vals = np.linalg.norm(coords, axis = 1)
+        norm_vals = np.linalg.norm(coords, axis=1)
 
         for i in range(npoints):
             sumj = 0.0
@@ -57,10 +161,10 @@ class GenI():
 
                 if qr > 0.0:
                     qr = np.sin(qr) / qr
-                    sumj += self.sldn_val * this.vol_pix * qr
+                    sumj += self.sldn_val * self.vol_pix * qr
 
                 else:
-                    sumj += this.sldn_val * this.vol_pix
+                    sumj += self.sldn_val * self.vol_pix
             else:
 			    #for k in range(self.n_pix):
 			    #j = k, up to n_pix, if x y and z is len(n_pix) then just all
@@ -77,15 +181,13 @@ class GenI():
                 else:
                     sumj += np.sum(sld_j)
 
-            if(i == 0):
+            if i == 0:
                 count += self.vol_pix
 
             I_out[i] = sumj
 
-            if(self.is_avg == 1):
+            if self.is_avg == 1:
                 I_out[i] *= sumj
 
             I_out[i] *= 1.0E+8 / count
 
-    def genicomXY(self):
-        pass
