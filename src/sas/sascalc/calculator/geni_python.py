cc2c3d079a2901e451201a995575fd3ba8a99ddc
ZiggyAttala <ziggy.attala@stfc.ac.uk>
2019-08-15 16:54:24 +0100

    np.square() instead of **2

--- src/sas/sascalc/calculator/geni_python.py
+++ src/sas/sascalc/calculator/geni_python.py
@@ -172,7 +172,7 @@ class GenI():
 			    #as long as vol_pix and sldn_val shape is also [n_pix] should work.
 
                 sld_j = self.sldn_val * self.sldn_val * self.vol_pix * self.vol_pix
-                qr = (self.x_val - self.x_val)**2 + (self.y_val - self.y_val)**2 + (self.z_val - self.z_val)**2
+                qr = np.square(self.x_val - self.x_val) + np.square(self.y_val - self.y_val) + np.square(self.z_val - self.z_val)
 
                 qr = np.sqrt(qr) * q[i]
 
@@ -190,4 +190,3 @@ class GenI():
                 I_out[i] *= sumj
 
             I_out[i] *= 1.0E+8 / count
-
