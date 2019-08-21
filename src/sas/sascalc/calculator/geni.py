a3e69f41c72e8dabb53a65a41f7c540d50e64008
ZiggyAttala <ziggy.attala@stfc.ac.uk>
2019-08-21 14:06:01 +0100

    changed polar_sld to be array instead of class, first iteration of cal_msld_vec and genicomXY_vec

--- src/sas/sascalc/calculator/geni.py
+++ src/sas/sascalc/calculator/geni.py
@@ -57,7 +57,8 @@ class GenI():
 
         #npoints is given negative for angular averaging
         #Assumes that q doesn't have qz component and sld_n is all real
-        b_sld = lib.polar_sld()
+        print(lib.get_polar_sld_type())
+        b_sld = np.array([(np.float(), np.float(), np.complex(), np.complex())], dtype=lib.get_polar_sld_type())
         qr = float()
         iqr = np.complex()
         ephase = np.complex()
@@ -91,7 +92,7 @@ class GenI():
 				    #anisotropic
                     temp_fi = np.complex()
 
-                    b_sld.cal_msld(0, qx[i], qy[i], self.sldn_val[j],
+                    lib.cal_msld(b_sld, 0, qx[i], qy[i], self.sldn_val[j],
                                    self.mx_val[j], self.my_val[j], self.mz_val[j],
                                    self.inspin, self.outspin, self.stheta);
                     qr = (qx[i]*self.x_val[j] + qy[i]*self.y_val[j]);
@@ -105,30 +106,145 @@ class GenI():
 
 				    #up_up
                     if (self.inspin > 0.0) & (self.outspin > 0.0):
-                        comp_sld = np.complex(b_sld.uu, 0.0)
+                        comp_sld = np.complex(b_sld[0]['uu'], 0.0)
                         temp_fi = comp_sld * ephase
                         sumj_uu = sumj_uu + temp_fi
 
 				    #down_down
                     if (self.inspin < 1.0) & (self.outspin < 1.0):
-                        comp_sld = np.complex(b_sld.dd, 0.0)
+                        comp_sld = np.complex(b_sld[0]['dd'], 0.0)
                         temp_fi = comp_sld * ephase
                         sumj_dd = sumj_dd + temp_fi
 
                     #up_down
                     if (self.inspin > 0.0) & (self.outspin < 1.0):
-                        comp_sld = np.complex(b_sld.re_ud, b_sld.im_ud)
+                        comp_sld = np.complex(b_sld[0]['ud'].real, b_sld[0]['ud'].imag)
                         temp_fi = comp_sld * ephase
                         sumj_ud = sumj_ud + temp_fi
 
                     #down_up
                     if (self.inspin < 1.0) & (self.outspin > 0.0):
-                        comp_sld = np.complex(b_sld.re_du, b_sld.im_du)
+                        comp_sld = np.complex(b_sld[0]['du'].real, b_sld[0]['du'].imag)
                         temp_fi = comp_sld * ephase
                         sumj_du = sumj_du + temp_fi
 
                     if i == 0:
                         count += self.vol_pix[j];
+
+            calc = lambda x: np.square(x.real) + np.square(x.imag)
+            I_out[i] = calc(sumj_uu)
+            I_out[i] += calc(sumj_ud)
+            I_out[i] += calc(sumj_du)
+            I_out[i] += calc(sumj_dd)
+            I_out[i] *= (1.0E+8 / count) #in cm (unit) / number; to be multiplied by vol_pix
+
+        return I_out
+
+    #not producing right answer but producing correct vector size.
+    def genicomXY_vec(self, qx, qy):
+        """
+        Compute 2d ansotropic.
+
+        Returns I_out[] 1d array.
+        """
+
+        npoints = len(qx)
+
+        #npoints is given negative for angular averaging
+        #Assumes that q doesn't have qz component and sld_n is all real
+        print(lib.get_polar_sld_type())
+        b_sld = np.zeros(self.n_pix, dtype=lib.get_polar_sld_type())
+        qr = float()
+        iqr = np.complex()
+        ephase = np.complex()
+        comp_sld = np.complex()
+
+        sumj_uu = np.complex()
+        sumj_ud = np.complex()
+        sumj_du = np.complex()
+        sumj_dd = np.complex()
+        temp_fi = np.complex()
+
+        count = float()
+
+        iqr = np.complex()
+        ephase = np.complex()
+        comp_sld = np.complex()
+
+        I_out = np.zeros(npoints, dtype=np.float)
+
+	    #Assume that pixel volumes are given in vol_pix in A^3 unit
+        #Loop over q-values and multiply apply matrix
+        for i in range(npoints):
+            sumj_uu = np.complex()
+            sumj_ud = np.complex()
+            sumj_du = np.complex()
+            sumj_dd = np.complex()
+
+            #for j in range(self.n_pix):
+            zero_sldn = self.sldn_val == 0
+            zero_mx = self.mx_val == 0
+            zero_my = self.my_val == 0
+            zero_mz = self.mz_val == 0
+            #find index where all are equal to 0
+            index_zero = [s & x & y & z for (s, x, y, z) in zip(zero_sldn, zero_mx, zero_my, zero_mz)]
+
+            #then exclude those from the calculation
+            index_zero = np.array(index_zero, dtype=bool)
+            sldn_val_use = self.sldn_val[~index_zero]
+            mx_val_use = self.mx_val[~index_zero]
+            my_val_use = self.my_val[~index_zero]
+            mz_val_use = self.mz_val[~index_zero]
+
+			#anisotropic
+            temp_fi = np.complex()
+
+            #all passed to cal_msld, not full values but should all be same length.
+            print(~index_zero)
+            lib.cal_msld_vec(b_sld, 0, qx[i], qy[i], sldn_val_use,
+                            mx_val_use, my_val_use, mz_val_use,
+                            self.inspin, self.outspin, self.stheta);
+            qr = (qx[i]*self.x_val + qy[i]*self.y_val);
+            iqr = np.zeros(len(self.x_val), dtype = np.complex_)
+
+            for j in range(len(self.x_val)):
+                _temp = np.complex(0.0, qr[j])
+                iqr[j] = _temp
+
+            #iqr = np.complex(0.0, qr)
+
+            #As in the original code, sets ephase reference to two methods that should
+            #both reset it, both set the real and imaginary parts.
+            ephase = np.exp(iqr)
+            #Let's multiply pixel(atomic) volume here
+            ephase = self.vol_pix * ephase
+
+			#up_up
+            if (self.inspin > 0.0) & (self.outspin > 0.0):
+                comp_sld = b_sld['uu']
+                temp_fi = comp_sld * ephase
+                sumj_uu = np.sum(sumj_uu + temp_fi)
+
+			#down_down
+            if (self.inspin < 1.0) & (self.outspin < 1.0):
+                comp_sld = b_sld['dd']
+                temp_fi = comp_sld * ephase
+                sumj_dd = np.sum(sumj_dd + temp_fi)
+
+            #up_down
+            if (self.inspin > 0.0) & (self.outspin < 1.0):
+                comp_sld = b_sld['ud']
+                temp_fi = comp_sld * ephase
+                sumj_ud = np.sum(sumj_ud + temp_fi)
+
+            #down_up
+            if (self.inspin < 1.0) & (self.outspin > 0.0):
+                comp_sld = b_sld['du']
+                temp_fi = comp_sld * ephase
+                sumj_du = np.sum(sumj_du + temp_fi)
+            print("I: \n\n", i)
+            if i == 0:
+                count = np.sum(self.vol_pix);
 
             calc = lambda x: np.square(x.real) + np.square(x.imag)
             I_out[i] = calc(sumj_uu)
@@ -179,7 +295,7 @@ class GenI():
 
                 sld_j = np.dot(np.square(self.sldn_val).reshape(len(self.sldn_val), 1), np.square(self.vol_pix).reshape(1, len(self.vol_pix)))
 
-                #calc calculates (x[j] - x[:]) * (x[j] - x[:]) where x is a 1d array.
+                #calc calculates (x[:] - x[:]) * (x[:] - x[:]) where x is a 1d array.
                 calc = lambda x: np.square(x.reshape(len(x), 1) - x.reshape(1, len(x)))
 
                 #qr should be result of vector addition of [self.n_pix] + [self.n_pix] + [self.n_pix]
@@ -207,6 +323,7 @@ class GenI():
             I_out[i] *= 1.0E+8 / count
 
         return I_out
+
 def demo():
     is_avg = 0
     npix = 301
@@ -251,7 +368,7 @@ I_out = gen_i.genicomXY(npix, x, y)'''
 
     #print(times)
 
-    I_out = gen_i.genicomXY(x, y)
+    I_out = gen_i.genicomXY_vec(x, y)
 
     print(I_out)
     print(I_out.shape)
@@ -268,4 +385,4 @@ I_out = gen_i.genicomXY(npix, x, y)'''
     #print(test_sld)
 
 if(__name__ == "__main__"):
-    test()
+    demo()
--- src/sas/sascalc/calculator/lib.py
+++ src/sas/sascalc/calculator/lib.py
@@ -13,7 +13,330 @@ logger = logging.getLogger(__name__)
 #defines structure, with a global free, call_msld
 #Doesn't need to be a class? Originally had seperate variables for complex/real component
 #of two numbers re_ud im_ud, assuming.
-class polar_sld():
+#Refractoring polar_sld as structured array, then pass in
+
+#returns dtype of polar_sld_type.
+def get_polar_sld_type():
+    dtype = [('uu', 'f8'), ('dd', 'f8'), ('ud', 'complex_'), ('du', 'complex_')]
+    return dtype
+
+#cal_msld taking in vector of bn, m09, mtheta1, mphi1.
+#also takes in structured array of polar_sld_type, polar_slds, assumes is of right type.
+def cal_msld_vec(polar_slds, isangle, qx, qy, bn, m01, mtheta1, mphi1, spinfraci, spinfracf, spintheta):
+        """
+        calculate magnetic sld and return total sld.
+        Note: all angles are in degrees.
+
+        :param bn: Contrast (not just sld of the layer)
+        :param m01: Max mag of M.
+        :param mtheta1: angle from x-z plane.
+        :param mph1i: Angle (anti-clock-wise)of x-z projection(M) from x axis.
+        :param spinfraci1: The fraction of UP among UP+Down (before sample).
+        :param spinfracf: The fraction of UP among UP+Down (after sample and before detector).
+        :param spintheta: Angle (anti-clock-wise) between neutron spin(up) and x axis.
+        """
+        #Locals
+        q_x = qx
+        q_y = qy
+        sld = bn
+        is_angle = isangle
+        pi = np.pi
+        #s_theta is spintheta in radians.
+        s_theta = np.radians(spintheta)
+        m_max = m01
+        m_phi = mphi1
+        m_theta = mtheta1
+        in_spin = spinfraci
+        out_spin = spinfracf
+
+        m_perp = 0.0
+        m_perp_z = 0.0
+        m_perp_y = 0.0
+        m_perp_x = 0.0
+
+        m_sigma_x = 0.0
+        m_sigma_y = 0.0
+        m_sigma_z = 0.0
+
+        q_angle = 0.0
+        mx = 0.0
+        my = 0.0
+        mz = 0.0
+        uu = sld
+        dd = sld
+        re_ud = sld
+        im_ud = sld
+        re_du = sld
+        im_du = sld
+
+        temp = 1.0e-32
+        temp2 = 1.0e-16
+
+        #values that uu and dd are set to frequently during calculation.
+        #intentionally leaving in_spin and out_spin to late bind because may be modified by method.
+        calc_uu = lambda uu: np.sqrt(np.sqrt(in_spin * out_spin)) * uu
+        calc_dd = lambda dd: np.sqrt(np.sqrt((1.0 - in_spin) * (1.0 - out_spin))) * dd
+
+        #Accepting values, which is values where absolute values of m_max < 1.0, abs(m_phi) < tempand abs(m_theta) < temp
+
+        index_m_max = np.fabs(m_max) < 1.0
+        index_m_phi = np.fabs(m_phi) < temp
+        index_m_theta = np.fabs(m_theta) < temp
+
+        index_accept = np.array([x & y & z for (x, y, z) in zip(index_m_max, index_m_phi, index_m_theta)], dtype = np.bool)
+
+        #only do the computation in elif if there if something in accept matrix otherwise will do regardless of other conditions.
+
+        if isangle > 0:
+            if m_max < temp:
+                uu = calc_uu(uu)
+                dd = calc_dd(dd)
+
+        elif index_accept.any():
+            #if there is any in index_accept, then do this, just so won't do computation if isangle > 0.
+
+            uu[index_accept] = calc_uu
+            dd[index_accept] = calc_dd
+
+            #then need way of computing other indexes.
+
+
+        #if not all were values of uu were covered by uu
+        if ~index_accept.all():
+            in_spin = 0.0 if in_spin < 0.0 else in_spin
+            in_spin = 1.0 if in_spin > 1.0 else in_spin
+            out_spin = 0.0 if out_spin < 0.0 else out_spin
+            out_spin = 1.0 if out_spin > 1.0 else out_spin
+
+            q_angle = pi / 2.0 if q_x == 0.0 else np.arctan(q_y/q_x)
+
+            if (q_y < 0.0) & (q_x < 0.0):
+                q_angle -= pi
+
+            elif (q_y > 0.0) & (q_x < 0.0):
+                q_angle += pi
+
+            q_angle = pi/2.0 - q_angle
+
+            if q_angle > pi:
+                q_angle -= 2.0 * pi
+
+            elif q_angle < -pi:
+                q_angle += 2.0 * pi
+
+            if (np.fabs(q_x) < temp2) & (np.fabs(q_y) < temp2):
+                m_perp = 0.0
+            else:
+                m_perp = m_max
+
+            if is_angle > 0:
+                m_phi = np.radians(m_phi)
+                m_theta = np.radians(m_theta)
+                mx = m_perp * np.cos(m_theta) * np.cos(m_phi)
+                my = m_perp * np.sin(m_theta)
+                mz = -(m_perp * np.cos(m_theta) * np.sin(m_phi))
+            else:
+                mx = m_perp
+                my = m_phi
+                mz = m_theta
+
+            #ToDo: simplify these steps
+            # m_perp1 -m_perp2
+            m_perp_x = (mx) * np.cos(q_angle)
+            m_perp_x -= (my) * np.sin(q_angle)
+            m_perp_y = m_perp_x
+            m_perp_x *= np.cos(-q_angle)
+            m_perp_y *= np.sin(-q_angle)
+            m_perp_z = mz
+
+            m_sigma_x = (m_perp_x * np.cos(-s_theta) - m_perp_y * np.sin(-s_theta))
+            m_sigma_y = (m_perp_x * np.sin(-s_theta) + m_perp_y * np.cos(-s_theta))
+            m_sigma_z = (m_perp_z)
+
+            #Find b
+            uu[~index_accept] -= m_sigma_x
+            dd[~index_accept] += m_sigma_x
+            re_ud[~index_accept] = m_sigma_y
+            re_du[~index_accept] = m_sigma_y
+            im_ud[~index_accept] = m_sigma_z
+            im_du[~index_accept] = -m_sigma_z
+
+            uu[~index_accept] = calc_uu(uu)
+            dd[~index_accept] = calc_dd(dd)
+
+            re_ud[~index_accept] = np.sqrt(np.sqrt(in_spin * (1.0 - out_spin))) * re_ud
+            im_ud[~index_accept] = np.sqrt(np.sqrt(in_spin * (1.0 - out_spin))) * im_ud
+            re_du[~index_accept] = np.sqrt(np.sqrt((1.0 - in_spin) * out_spin)) * re_du
+            im_du[~index_accept] = np.sqrt(np.sqrt((1.0 - in_spin) * out_spin)) * im_du
+
+        print(type(uu))
+
+        polar_slds['uu'] = uu
+        polar_slds['dd'] = dd
+
+        #generate all complex numbers into new array, combining real and imaginary components calculated.
+        #only way can find so far of doing this.
+        _ud = np.zeros(len(re_ud), dtype=np.complex_)
+        _du = np.zeros(len(re_du), dtype=np.complex_)
+
+        for i in range(len(_ud)):
+            _udt = np.complex(re_ud[i], im_ud[i])
+            _dut = np.complex(re_du[i], im_du[i])
+            _ud[i] = _udt
+            _du[i] = _dut
+
+        #new ud.
+        polar_slds['ud'] = _ud
+        #new du.
+        polar_slds['du'] = _du
+
+#cal_msld taking in vector of bn, m09, mtheta1, mphi1.
+#also takes in structured array of polar_sld_type, polar_slds, assumes is of right type.
+def cal_msld(polar_slds, isangle, qx, qy, bn, m01, mtheta1, mphi1, spinfraci, spinfracf, spintheta):
+        """
+        calculate magnetic sld and return total sld.
+        Note: all angles are in degrees.
+
+        :param bn: Contrast (not just sld of the layer)
+        :param m01: Max mag of M.
+        :param mtheta1: angle from x-z plane.
+        :param mph1i: Angle (anti-clock-wise)of x-z projection(M) from x axis.
+        :param spinfraci1: The fraction of UP among UP+Down (before sample).
+        :param spinfracf: The fraction of UP among UP+Down (after sample and before detector).
+        :param spintheta: Angle (anti-clock-wise) between neutron spin(up) and x axis.
+        """
+        #Locals
+        q_x = qx
+        q_y = qy
+        sld = bn
+        is_angle = isangle
+        pi = np.pi
+        #s_theta is spintheta in radians.
+        s_theta = np.radians(spintheta)
+        m_max = m01
+        m_phi = mphi1
+        m_theta = mtheta1
+        in_spin = spinfraci
+        out_spin = spinfracf
+
+        m_perp = 0.0
+        m_perp_z = 0.0
+        m_perp_y = 0.0
+        m_perp_x = 0.0
+
+        m_sigma_x = 0.0
+        m_sigma_y = 0.0
+        m_sigma_z = 0.0
+
+        q_angle = 0.0
+        mx = 0.0
+        my = 0.0
+        mz = 0.0
+        uu = sld
+        dd = sld
+        re_ud = 0.0
+        im_ud = 0.0
+        re_du = 0.0
+        im_du = 0.0
+
+        temp = 1.0e-32
+        temp2 = 1.0e-16
+
+        #values that uu and dd are set to frequently during calculation.
+        #intentionally leaving in_spin and out_spin to late bind because may be modified by method.
+        calc_uu = lambda uu: np.sqrt(np.sqrt(in_spin * out_spin)) * uu
+        calc_dd = lambda dd: np.sqrt(np.sqrt((1.0 - in_spin) * (1.0 - out_spin))) * dd
+
+        if isangle > 0:
+            if m_max < temp:
+                uu = calc_uu(uu)
+                dd = calc_dd(dd)
+
+        elif (np.fabs(m_max) < 1.0) & (np.fabs(m_phi) < temp) & (np.fabs(m_theta) < temp):
+            uu = calc_uu
+            dd = calc_dd
+
+        else:
+            in_spin = 0.0 if in_spin < 0.0 else in_spin
+            in_spin = 1.0 if in_spin > 1.0 else in_spin
+            out_spin = 0.0 if out_spin < 0.0 else out_spin
+            out_spin = 1.0 if out_spin > 1.0 else out_spin
+
+            q_angle = pi / 2.0 if q_x == 0.0 else np.arctan(q_y/q_x)
+
+            if (q_y < 0.0) & (q_x < 0.0):
+                q_angle -= pi
+
+            elif (q_y > 0.0) & (q_x < 0.0):
+                q_angle += pi
+
+            q_angle = pi/2.0 - q_angle
+
+            if q_angle > pi:
+                q_angle -= 2.0 * pi
+
+            elif q_angle < -pi:
+                q_angle += 2.0 * pi
+
+            if (np.fabs(q_x) < temp2) & (np.fabs(q_y) < temp2):
+                m_perp = 0.0
+            else:
+                m_perp = m_max
+
+            if is_angle > 0:
+                m_phi = np.radians(m_phi)
+                m_theta = np.radians(m_theta)
+                mx = m_perp * np.cos(m_theta) * np.cos(m_phi)
+                my = m_perp * np.sin(m_theta)
+                mz = -(m_perp * np.cos(m_theta) * np.sin(m_phi))
+            else:
+                mx = m_perp
+                my = m_phi
+                mz = m_theta
+
+            #ToDo: simplify these steps
+            # m_perp1 -m_perp2
+            m_perp_x = (mx) * np.cos(q_angle)
+            m_perp_x -= (my) * np.sin(q_angle)
+            m_perp_y = m_perp_x
+            m_perp_x *= np.cos(-q_angle)
+            m_perp_y *= np.sin(-q_angle)
+            m_perp_z = mz
+
+            m_sigma_x = (m_perp_x * np.cos(-s_theta) - m_perp_y * np.sin(-s_theta))
+            m_sigma_y = (m_perp_x * np.sin(-s_theta) + m_perp_y * np.cos(-s_theta))
+            m_sigma_z = (m_perp_z)
+
+            #Find b
+            uu -= m_sigma_x
+            dd += m_sigma_x
+            re_ud = m_sigma_y
+            re_du = m_sigma_y
+            im_ud = m_sigma_z
+            im_du = -m_sigma_z
+
+            uu = calc_uu(uu)
+            dd = calc_dd(dd)
+
+            re_ud = np.sqrt(np.sqrt(in_spin * (1.0 - out_spin))) * re_ud
+            im_ud = np.sqrt(np.sqrt(in_spin * (1.0 - out_spin))) * im_ud
+            re_du = np.sqrt(np.sqrt((1.0 - in_spin) * out_spin)) * re_du
+            im_du = np.sqrt(np.sqrt((1.0 - in_spin) * out_spin)) * im_du
+
+        polar_slds[0]['uu'] = uu
+        polar_slds[0]['dd'] = dd
+
+        #new ud.
+        _ud = np.complex(re_ud, im_ud)
+        polar_slds[0]['ud'] = _ud
+
+        #new du.
+        _du = np.complex(re_du, im_du)
+        polar_slds[0]['du'] = _du
+
+
+#OLD_POLAR_SLD
+class polar_sld_old():
     def __init__(self):
         self.uu = float()
         self.dd = float()
