8a8e2ba7490c63f61c10b18216099551491c57a1
Jeff Krzywon <jkrzywon@nist.gov>
2019-10-03 14:43:44 -0400

    Warn the user when data is fully masked on load.

--- src/sas/sascalc/dataloader/readers/red2d_reader.py
+++ src/sas/sascalc/dataloader/readers/red2d_reader.py
@@ -230,11 +230,6 @@ class Reader(FileReader):
             mask = np.invert(np.asarray(data_point[(7 + ver)], dtype=bool))
         q_data = np.sqrt(qx_data*qx_data+qy_data*qy_data+qz_data*qz_data)
 
-        # Extra protection(it is needed for some data files):
-        # If all mask elements are False, put all True
-        if not mask.any():
-            mask[mask == False] = True
-
         # Store limits of the image in q space
         xmin = np.min(qx_data)
         xmax = np.max(qx_data)
