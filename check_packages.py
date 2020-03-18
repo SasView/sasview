7010b58de60e4a4d0d83b07ee8cb9b5337d3b673
Jeff Krzywon <krzywon@yahoo.com>
2020-03-18 17:05:54 -0400

    Code cleanup and the console output is also much clearer. Verified to work in Win64.

--- check_packages.py
+++ check_packages.py
@@ -10,41 +10,48 @@ from colorama import Fore, Back, Style
 
 
 if sys.platform == 'win32':
-    file_location = 'build_tools/conda/ymls/sasview-env-build_win.yml'
+    file_location = './build_tools/conda/ymls/sasview-env-build_win.yml'
 elif sys.platform == 'darwin':
-    file_location = 'build_tools/conda/ymls/sasview-env-build_osx.yml'
+    file_location = './build_tools/conda/ymls/sasview-env-build_osx.yml'
 else:
-    file_location = 'build_tools/conda/ymls/sasview-env-build_linux.yml'
+    file_location = './build_tools/conda/ymls/sasview-env-build_linux.yml'
 
 with open(file_location, 'r') as stream:
     try:
         yaml_dict = yaml.load(stream, Loader=yaml.SafeLoader)
-        print(yaml_dict)
         common_required_package_list = yaml_dict['dependencies']
     except Exception as e:
         print(e)
 
-print(Style.RESET_ALL)
-print("CHECKING REQUIRED PACKAGES....")
+print("")
+print(Style.RESET_ALL +
+      "....COMPARING PACKAGES LISTED IN {0} to INSTALLED PACKAGES....".format(
+          file_location))
+print("")
 
 for yaml_name in common_required_package_list:
-    text_split = yaml_name.split('=')
+    try:
+        text_split = yaml_name.split('=')
+    except AttributeError as e:
+        # Continue for tiered files including pip installs
+        continue
     package_name = text_split[0]
     package_version = text_split[1]
 
-    print(Style.RESET_ALL)
     try:
-        i = __import__(package_name, fromlist=[''])
-        if package_version is None:
-            print("%s Installed (Unknown version)" % package_name)
-        elif package_name == 'lxml':
-            verstring = str(getattr(i, 'LXML_VERSION'))
-            print("%s Version Installed: %s" % (package_name, verstring.replace(', ', '.').lstrip('(').rstrip(')')))
+        if package_name == 'python':
+            full_version = sys.version.split()
+            installed_version = full_version[0]
         else:
-            print("%s Version Installed: %s" % (package_name, getattr(i, '__version__')))
+            i = __import__(package_name, fromlist=[''])
+            installed_version = getattr(i, '__version__')
+        if package_version == installed_version:
+            print(Style.RESET_ALL + "{0} - Expected Version Installed: {1}".format(package_name, installed_version))
+        else:
+            print(Fore.LIGHTYELLOW_EX + "{0} - Version Mismatch - Installed: {1}, Expected: {2}".format(package_name, installed_version, package_version))
     except AttributeError:
-        print("%s Installed (Unknown version)" % package_name)
+        print(Fore.YELLOW + "{0} - Version Cannot Be Determined - Expected: {1}".format(package_name, package_version))
     except ImportError:
-        print(Fore.BLACK + Back.WHITE + '%s NOT INSTALLED' % package_name)
+        print(Fore.LIGHTRED_EX + '{0} NOT INSTALLED'.format(package_name))
 
 print(Style.RESET_ALL)
