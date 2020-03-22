145303697e972e059e661d835a6166cd33120446
Paul Butler <butlerpd@udel.edu>
2020-03-21 23:23:36 -0400

    Fix Sv calculation, error and display
    The Sv was being calculated in a circular manner and provided completely
    bogus uncertainty.  Sv is fundamentally based on the porod constant and
    contrast with are both user inputs with NO uncertainty estimate (would
    be nice to get some?).  Redid the computation to use those values
    directly so that it will display even if vol fraction has an error.
    Also replaced error with NONE if ds ==0. I actually included code do
    properly compute the uncertainty if uncertainties are ever given on
    Porod Constant and contrast..
    
    Also replace NaN with ERROR  both for s and phi and added a logger
    statement that goes to the log file but currently NOTHING is going to
    the console screen as mentioned in another ticket somewhere.

--- src/sas/sascalc/invariant/invariant.py
+++ src/sas/sascalc/invariant/invariant.py
@@ -810,23 +810,39 @@ class InvariantCalculator(object):
         """
         Compute the specific surface from the data.
 
-        Implementation::
+        Historically, Sv was computed with the invariant and the Porod
+        constant so as not to have to know the contrast in order to get the
+        Sv as:
+            surface = (pi * V * (1- V) * porod_const) / q_star
+        However, that turns out to be a pointless exercise since it
+        also requires a knowledge of the volume fractions and from the
+        volumer fraction and the invariant the contrast can be calculated as:
+            contrast**2 = q_star / (2 * pi**2 * V * (1- V))
 
-          V =  self.get_volume_fraction(contrast, extrapolation)
+        Thus either way, mathematically it is always identical to computing
+        with only the contrast and the Porod Constant. up to and including
+        SasView versions 4.2.2 and 5.0.1 the implentation used the traditional
+        circular approach.
 
-          Compute the surface given by:
-            surface = (pi *V(1- V)*porod_const)/ q_star
+        Implementation: ::
+           Given the above, as of SassView 4.3 and 5.0.2 we compute Sv simply
+           from the Poroc Constant and the contrast between the two phases as:
+             surface = porod_const / (2 * pi contrast**2) 
 
-        :param contrast: contrast value to compute the volume
-        :param porod_const: Porod constant to compute the surface
-        :param extrapolation: string to apply optional extrapolation
+
+        :param contrast: contrast between the two phases
+        :param porod_const: Porod constant
+        :param extrapolation: string to apply optional extrapolation. This will
+               only be needed if and when the contrast term is calculated from
+               the invariant.
 
         :return: specific surface
         """
-        # Compute the volume
-        volume = self.get_volume_fraction(contrast, extrapolation)
-        return math.pi * volume * (1 - volume) * \
-            float(porod_const) / self._qstar
+        # convert porod_const to units of A^-5 instead of cm^-1 A^-4 so that
+        # s is returned in units of 1/A.
+        _porod_const = 1.0e-8 * porod_const
+        return float(_porod_const) / (2 * math.pi\
+                                     * math.fabs(float(contrast))**2)
 
     def get_volume_fraction(self, contrast, extrapolation=None):
         """
@@ -934,32 +950,37 @@ class InvariantCalculator(object):
 
     def get_surface_with_error(self, contrast, porod_const, extrapolation=None):
         """
-        Compute uncertainty of the surface value as well as the surface value.
-        The uncertainty is given as follow: ::
+        As of SasView 4.3 and 5.0.2, the specivie surface is computed directly
+        fro the contrast and porod_constant wich are currently user inputs
+        with no optionf for any uncertainty so no uncertainty can be calculated.
+        However we include the uncertainty computation for future use if and
+        when these values get an uncertainty. This is given as:  ::
 
-            dS = porod_const *2*pi[( dV -2*V*dV)/q_star
-                 + dq_star(v-v**2)
+                ds/s = sqrt[(dcp/cp)**2 + 2* (dcontrast/contrast)**2]
 
-            q_star: the invariant value
-            dq_star: the invariant uncertainty
-            V: the volume fraction value
-            dV: the volume uncertainty
+            wich gives (this should be checked before using in anger)
+                ds = sqrt((dporod_const)**2 + 2 * (porod_const *
+                          dcontrast / contrast)**2) / (2 * pi * contrast**2)
 
-        :param contrast: contrast value
-        :param porod_const: porod constant value
-        :param extrapolation: string to apply optional extrapolation
+        :param contrast: contrast value eventually with the error
+        :param porod_const: porod constant value eventually with the error
+        :param extrapolation: string to apply optional extrapolation. This will
+               only be needed if and when the contrast term is calculated from
+               the invariant.
 
-        :return S, dS: the surface, with its uncertainty
+        :return s, ds: the surface, with its uncertainty
         """
-        # We get the volume fraction, with error
-        #   get_volume_fraction_with_error calls get_volume_fraction
-        #   get_volume_fraction calls get_qstar
-        #   which computes Qstar and dQstar
-        v, dv = self.get_volume_fraction_with_error(contrast, extrapolation)
 
-        s = self.get_surface(contrast=contrast, porod_const=porod_const,
-                             extrapolation=extrapolation)
-        ds = porod_const * 2 * math.pi * ((dv - 2 * v * dv) / self._qstar\
-                 + self._qstar_err * (v - v ** 2))
+        # until contrast and porod_constant are given with uncertainties set
+        # them to 0
+        dcontrast = 0
+        # If and when dporod_const exists remember it likely needs unit
+        # conversion from cm^-1 A^-4 to A^-5.
+        dporod_const = 0 * 1e-8
+        _porod_const = porod_const *1e-8
+
+        s = self.get_surface(contrast=contrast, porod_const=porod_const)
+        ds = math.sqrt((dporod_const)**2 + 2 * (_porod_const\
+                 * dcontrast / contrast)**2) / (2 * math.pi * contrast**2)
 
         return s, ds
