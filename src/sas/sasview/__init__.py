68a8d474ea6c0c0dfb025577fa35ca48cda01684
piotr.rozyczko@gmail.com <piotr.rozyczko@gmail.com>
2020-04-14 11:46:07 +0200

    Bumped version number in preparation for the new release

--- src/sas/sasview/__init__.py
+++ src/sas/sasview/__init__.py
@@ -1,5 +1,5 @@
 from distutils.version import StrictVersion
-__version__ = "5.0.1"
+__version__ = "5.0.2"
 StrictVersion(__version__)
 __DOI__ = "Zenodo, DOI:10.5281/zenodo.3653469"
 __release_date__ = "2020"
