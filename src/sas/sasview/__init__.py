7bc0418f01409dc0655b37730eb33c15155068e8
rozyczko <piotr.rozyczko@gmail.com>
2021-02-13 21:54:35 +0100

    Version number bump to 5.0.4

--- src/sas/sasview/__init__.py
+++ src/sas/sasview/__init__.py
@@ -1,6 +1,6 @@
 from distutils.version import StrictVersion
-__version__ = "5.0.3"
+__version__ = "5.0.4"
 StrictVersion(__version__)
 __DOI__ = "Zenodo, DOI:10.5281/zenodo.3930098"
-__release_date__ = "2020"
+__release_date__ = "2021"
 __build__ = "GIT_COMMIT"
