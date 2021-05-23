454820a97903f20b102f87bd58b585dd4d64076a
Paul Butler <butlerpd@udel.edu>
2021-05-23 12:40:48 -0400

    Now actually increment the version number

--- src/sas/sasview/__init__.py
+++ src/sas/sasview/__init__.py
@@ -1,5 +1,5 @@
 from distutils.version import StrictVersion
-__version__ = "5.0.4-alpha.1"
+__version__ = "5.0.5-alpha.1"
 StrictVersion(__version__)
 __DOI__ = "Zenodo, DOI:10.5281/zenodo.4467703"
 __release_date__ = "2021"
