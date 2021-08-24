f0c9e6f2ce3d3fc30fe1476316cf713a35d49871
Wojtek Potrzebowski <wojciech.potrzebowski@esss.se>
2021-08-24 16:08:50 +0200

    added hardtime for sasview

--- build_tools/code_sign_osx.py
+++ build_tools/code_sign_osx.py
@@ -8,13 +8,11 @@ import os
 so_list = glob.glob("SasView5.app/Contents/MacOS/**/*.so", recursive=True)
 dylib_list = glob.glob("SasView5.app/Contents/MacOS/**/*.dylib", recursive=True)
 dylib_list_resources = glob.glob("SasView5.app/Contents/Resources/.dylibs/*.dylib", recursive=True)
-
+sign_command = 'codesign  --timestamp --options=runtime --verify --verbose=4 --force --sign "Developer ID Application: European Spallation Source Eric (W2AG9MPZ43)"'
 for sfile in so_list + dylib_list + dylib_list_resources:
     os.system(
-        'codesign  --timestamp --options runtime --verify --verbose=4 --force --sign "Developer ID Application: European Spallation Source Eric (W2AG9MPZ43)" '
-        + sfile
+        sign_command + sfile
     )
 
-os.system(
-    'codesign --timestamp --options runtime --verify --verbose=4 --force --sign "Developer ID Application: European Spallation Source Eric (W2AG9MPZ43)" SasView5.app'
-)
+os.system( sign_command + 'SasView5.app')
+os.system( sign_command + 'SasView5.dmg/SasView5.app/Contents/MacOS/sasview')
