bf6f805517c089b2d5286d83a63fb49c5c8878b6
Wojtek Potrzebowski <wojciech.potrzebowski@esss.se>
2021-08-24 16:36:00 +0200

    added runtime option

--- build_tools/code_sign_osx.py
+++ build_tools/code_sign_osx.py
@@ -8,7 +8,7 @@ import os
 so_list = glob.glob("SasView5.app/Contents/MacOS/**/*.so", recursive=True)
 dylib_list = glob.glob("SasView5.app/Contents/MacOS/**/*.dylib", recursive=True)
 dylib_list_resources = glob.glob("SasView5.app/Contents/Resources/.dylibs/*.dylib", recursive=True)
-sign_command = 'codesign  --timestamp --options=runtime --verify --verbose=4 --force --sign "Developer ID Application: European Spallation Source Eric (W2AG9MPZ43)"'
+sign_command = 'codesign  --timestamp --options=runtime --verify --verbose=4 --force --sign "Developer ID Application: European Spallation Source Eric (W2AG9MPZ43)" '
 for sfile in so_list + dylib_list + dylib_list_resources:
     os.system(
         sign_command + sfile
