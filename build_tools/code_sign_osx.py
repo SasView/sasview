3438d5ad74a2296ede791f560144746bc3e54e45
Wojtek Potrzebowski <wojciech.potrzebowski@esss.se>
2021-08-24 15:37:19 +0200

    added more paths to signing

--- build_tools/code_sign_osx.py
+++ build_tools/code_sign_osx.py
@@ -7,23 +7,13 @@ import os
 
 so_list = glob.glob("SasView5.app/Contents/MacOS/**/*.so", recursive=True)
 dylib_list = glob.glob("SasView5.app/Contents/MacOS/**/*.dylib", recursive=True)
+dylib_list_resources = glob.glob("SasView5.app/Contents/Resources/.dylibs/*.dylib", recursive=True)
 
-
-for sfile in so_list:
+for sfile in so_list + dylib_list + dylib_list_resources:
     os.system(
         'codesign  --timestamp --options runtime --verify --verbose=4 --force --sign "Developer ID Application: European Spallation Source Eric (W2AG9MPZ43)" '
         + sfile
     )
-
-for dfile in dylib_list:
-    os.system(
-        'codesign  --timestamp --options runtime --verify --verbose=4 --force --sign "Developer ID Application: European Spallation Source Eric (W2AG9MPZ43)" '
-        + dfile
-    )
-
-#os.system(
-#    'codesign --timestamp --options runtime --verify --verbose=4 --force --sign "Developer ID Application: European Spallation Source Eric (W2AG9MPZ43)" SasView5.app/Contents/MacOS/base_library.zip'
-#)
 
 os.system(
     'codesign --timestamp --options runtime --verify --verbose=4 --force --sign "Developer ID Application: European Spallation Source Eric (W2AG9MPZ43)" SasView5.app'
