e381e37c0c0084a3389feaf5149ebf184209973c
dehoni <dirk.honecker@stfc.ac.uk>
2021-10-23 18:05:33 +0100

    revert changes for testing code

--- src/sas/qtgui/Plotting/ColorMap.py
+++ src/sas/qtgui/Plotting/ColorMap.py
@@ -22,7 +22,7 @@ from sas.qtgui.Plotting.UI.ColorMapUI import Ui_ColorMapUI
 
 class ColorMap(QtWidgets.QDialog, Ui_ColorMapUI):
     apply_signal = QtCore.pyqtSignal(tuple, str)
-    def __init__(self, parent=None, cmap=None, xmin=-1,xmax=10, ymin=-1.0, ymax=15.0, vmin=0.0, vmax=100.0, data=None):
+    def __init__(self, parent=None, cmap=None, vmin=0.0, vmax=100.0, data=None):
         super(ColorMap, self).__init__()
 
         self.setupUi(self)
@@ -38,12 +38,6 @@ class ColorMap(QtWidgets.QDialog, Ui_ColorMapUI):
 
         self.vmin = self.vmin_orig = vmin
         self.vmax = self.vmax_orig = vmax
-
-        self.data.xmin = xmin
-        self.data.xmax = xmax
-
-        self.data.ymin = ymin
-        self.data.ymax = ymax
 
         # Initialize detector labels
         self.initDetectorData()
