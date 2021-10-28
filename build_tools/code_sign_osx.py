a1676dc651450fa372ba58fe1e32304f116f99a9
Wojtek Potrzebowski <wojciech.potrzebowski@esss.se>
2021-10-28 22:12:13 +0200

    fixes in code_sign script

--- build_tools/code_sign_osx.py
+++ build_tools/code_sign_osx.py
@@ -3,16 +3,23 @@ Listing all so and dylib files and signs them separetly.
 Required for notarization.
 """
 import glob
-import os
+import subprocess
+import itertools
 
 so_list = glob.glob("SasView5.app/Contents/MacOS/**/*.so", recursive=True)
 dylib_list = glob.glob("SasView5.app/Contents/MacOS/**/*.dylib", recursive=True)
-dylib_list_resources = glob.glob("SasView5.app/Contents/Resources/.dylibs/*.dylib", recursive=True)
-sign_command = 'codesign  --timestamp --options=runtime --verify --verbose=4 --force --sign "Developer ID Application: European Spallation Source Eric (W2AG9MPZ43)" '
-for sfile in so_list + dylib_list + dylib_list_resources:
-    os.system(
-        sign_command + sfile
-    )
+dylib_list_resources = glob.glob(
+    "SasView5.app/Contents/Resources/.dylibs/*.dylib", recursive=True
+)
 
-os.system( sign_command + 'SasView5.app')
-os.system( sign_command + 'SasView5.dmg/SasView5.app/Contents/MacOS/sasview')
+sign_command = ['codesign', '--timestamp', '--options=runtime', '--verify', '--verbose=4', '--force',
+                '--sign \"Developer ID Application: European Spallation Source Eric (W2AG9MPZ43)\"']
+
+for sfile in list(itertools.chain(so_list, dylib_list, dylib_list_resources)):
+    exec_command = sign_command
+    exec_command.append(sfile)
+    subprocess.check_call(exec_command)
+
+subprocess.check_call(sign_command.append("SasView5.app"))
+sign_command.pop()
+subprocess.check_call(sign_command.append("SasView5.dmg/SasView5.app/Contents/MacOS/sasview"))
