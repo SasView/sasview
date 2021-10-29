d75440149e400fc94a9f26c8981bcb09e619938f
Wojtek Potrzebowski <wojciech.potrzebowski@esss.se>
2021-10-29 09:54:10 +0200

    Another fix to code sigining script

--- build_tools/code_sign_osx.py
+++ build_tools/code_sign_osx.py
@@ -11,13 +11,13 @@ dylib_list = glob.glob("SasView5.app/Contents/MacOS/**/*.dylib", recursive=True)
 dylib_list_resources = glob.glob(
     "SasView5.app/Contents/Resources/.dylibs/*.dylib", recursive=True
 )
-top_level_files = ['SasView5.app', 'SasView5.dmg/SasView5.app/Contents/MacOS/sasview']
+
 sign_command = ['codesign', '--timestamp', '--options=runtime', '--verify', '--verbose=4', '--force',
                 '--sign', 'Developer ID Application: European Spallation Source Eric (W2AG9MPZ43)']
 
 
 #TODO: Check if it is necesarry to do it per file (one long list maybe enough)
-for sfile in itertools.chain(so_list, dylib_list, dylib_list_resources, top_level_files):
+for sfile in itertools.chain(so_list, dylib_list, dylib_list_resources):
     sign_command.append(sfile)
     subprocess.check_call(sign_command)
     sign_command.pop()
