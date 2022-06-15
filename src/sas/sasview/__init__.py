32abc9684172d3bff030058d966bb9bb50df1fa5
Wojciech Potrzebowski <Wojciech.Potrzebowski@esss.se>
2022-06-15 17:25:53 +0200

    Update __init__.py
    We are going for 5.1a1

--- src/sas/sasview/__init__.py
+++ src/sas/sasview/__init__.py
@@ -1,7 +1,7 @@
 from distutils.version import StrictVersion
 # The version number must follow StrictVersion rules as outlined
 # in http://epydoc.sourceforge.net/stdlib/distutils.version.StrictVersion-class.html
-__version__ = "5.0.5"
+__version__ = "5.1a1"
 StrictVersion(__version__)
 __DOI__ = "Zenodo, 10.5281/zenodo.6331344"
 __release_date__ = "2022"
