eb1ebb2bc53a3c9b290a61846f59c661f7eaa03e
krzywon <krzywon@yahoo.com>
2022-08-11 15:34:03 -0400

    Ensure the Reports .ui files are included in the installation and include missing dependencies to workflows

--- setup.py
+++ setup.py
@@ -280,6 +280,7 @@ package_data['sas.qtgui'] = ['Calculators/UI/*',
                              'Perspectives/Inversion/UI/*',
                              'Plotting/UI/*',
                              'Utilities/UI/*',
+                             'Utilities/Reports/UI/*',
                              'UI/*',
                              'UI/res/*',
                              ]
