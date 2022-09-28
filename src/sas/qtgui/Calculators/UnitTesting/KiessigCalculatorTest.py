95a901a2320b122f6babc0c1e6d362fbcc08c46a
Stuart Prescott <stuart@debian.org>
2022-09-28 06:06:33 +0000

    Change tests to asserts in KiessigCalculatorTest.py

--- src/sas/qtgui/Calculators/UnitTesting/KiessigCalculatorTest.py
+++ src/sas/qtgui/Calculators/UnitTesting/KiessigCalculatorTest.py
@@ -1,87 +1,84 @@
 import sys
-import unittest
+
+import pytest
+
 from PyQt5 import QtGui, QtWidgets
 from PyQt5.QtTest import QTest
 from PyQt5.QtCore import Qt
 
-import path_prepare
 from unittest.mock import MagicMock
 
 from sas.qtgui.Calculators.KiessigPanel import KiessigPanel
 
-if not QtWidgets.QApplication.instance():
-    app = QtWidgets.QApplication(sys.argv)
-
-
-class KiessigCalculatorTest(unittest.TestCase):
+class KiessigCalculatorTest:
     """Test the KiessigCalculator"""
-    def setUp(self):
-        """Create the KiessigCalculator"""
-        self.widget = KiessigPanel(None)
 
-    def tearDown(self):
-        """Destroy the KiessigCalculator"""
-        self.widget.close()
-        self.widget = None
+    @pytest.fixture(autouse=True)
+    def widget(self, qapp):
+        """Create/Destroy the KiessigPanel"""
+        w = KiessigPanel(None)
 
-    def testDefaults(self):
+        yield w
+
+        w.close()
+        w = None
+
+
+    def testDefaults(self, widget):
         """Test the GUI in its default state"""
-        self.assertIsInstance(self.widget, QtWidgets.QWidget)
-        self.assertEqual(self.widget.windowTitle(), "Kiessig Thickness Calculator")
-        self.assertEqual(self.widget.sizePolicy().Policy(), QtWidgets.QSizePolicy.Fixed)
+        assert isinstance(widget, QtWidgets.QWidget)
+        assert widget.windowTitle() == "Kiessig Thickness Calculator"
+        assert widget.sizePolicy().Policy() == QtWidgets.QSizePolicy.Fixed
 
-    def testHelp(self):
+    def testHelp(self, widget):
         """ Assure help file is shown """
-        self.widget.manager = QtWidgets.QWidget()
-        self.widget.manager.showHelp = MagicMock()
-        self.widget.onHelp()
-        self.assertTrue(self.widget.manager.showHelp.called_once())
-        args = self.widget.manager.showHelp.call_args
-        self.assertIn('kiessig_calculator_help.html', args[0][0])
+        widget.manager = QtWidgets.QWidget()
+        widget.manager.showHelp = MagicMock()
+        widget.onHelp()
+        assert widget.manager.showHelp.called_once()
+        args = widget.manager.showHelp.call_args
+        assert 'kiessig_calculator_help.html' in args[0][0]
 
-    def testComplexEntryNumbers(self):
+    def testComplexEntryNumbers(self, widget):
         """ User entered compound calculations and subsequent reset"""
 
-        self.widget.deltaq_in.clear()
-        self.widget.deltaq_in.insert('0.05')
+        widget.deltaq_in.clear()
+        widget.deltaq_in.insert('0.05')
         #
         # Push Compute with the left mouse button
-        computeButton = self.widget.computeButton
+        computeButton = widget.computeButton
         QTest.mouseClick(computeButton, Qt.LeftButton)
-        self.assertEqual(self.widget.lengthscale_out.text(), '125.664')
+        assert widget.lengthscale_out.text() == '125.664'
 
-    def testComplexEntryNumbers2(self):
+    def testComplexEntryNumbers2(self, widget):
         """ User entered compound calculations and subsequent reset"""
 
-        self.widget.deltaq_in.clear()
-        self.widget.deltaq_in.insert('1.0')
+        widget.deltaq_in.clear()
+        widget.deltaq_in.insert('1.0')
         #
         # Push Compute with the left mouse button
-        computeButton = self.widget.computeButton
+        computeButton = widget.computeButton
         QTest.mouseClick(computeButton, Qt.LeftButton)
-        self.assertEqual(self.widget.lengthscale_out.text(), '6.283')
+        assert widget.lengthscale_out.text() == '6.283'
 
-    def testComplexEntryNumbers3(self):
+    def testComplexEntryNumbers3(self, widget):
         """ User entered compound calculations and subsequent reset"""
 
-        self.widget.deltaq_in.clear()
-        self.widget.deltaq_in.insert('2.0')
+        widget.deltaq_in.clear()
+        widget.deltaq_in.insert('2.0')
         #
         # Push Compute with the left mouse button
-        computeButton = self.widget.computeButton
+        computeButton = widget.computeButton
         QTest.mouseClick(computeButton, Qt.LeftButton)
-        self.assertEqual(self.widget.lengthscale_out.text(), '3.142')
+        assert widget.lengthscale_out.text() == '3.142'
 
-    def testComplexEntryLetters(self):
+    def testComplexEntryLetters(self, widget):
         """ User entered compound calculations and subsequent reset"""
-        self.widget.deltaq_in.clear()
-        self.widget.deltaq_in.insert("xyz")
+        widget.deltaq_in.clear()
+        widget.deltaq_in.insert("xyz")
 
         # Push Compute with the left mouse button
-        computeButton = self.widget.computeButton
+        computeButton = widget.computeButton
         QTest.mouseClick(computeButton, Qt.LeftButton)
-        self.assertEqual(self.widget.deltaq_in.text(), '')
-        self.assertEqual(self.widget.lengthscale_out.text(), '')
-
-if __name__ == "__main__":
-    unittest.main()
+        assert widget.deltaq_in.text() == ''
+        assert widget.lengthscale_out.text() == ''
