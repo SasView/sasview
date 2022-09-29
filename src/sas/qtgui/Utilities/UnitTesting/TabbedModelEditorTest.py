45702a94f8944fe03e56837bdf273b9fc67fce03
Stuart Prescott <stuart@debian.org>
2022-09-29 15:20:49 +0000

    Change to pytest fixtures TabbedModelEditorTest.py

--- src/sas/qtgui/Utilities/UnitTesting/TabbedModelEditorTest.py
+++ src/sas/qtgui/Utilities/UnitTesting/TabbedModelEditorTest.py
@@ -1,6 +1,5 @@
 import os
 import sys
-import unittest
 import logging
 
 import pytest
@@ -11,9 +10,6 @@ from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 
-# set up import paths
-import sas.qtgui.path_prepare
-
 from sas.qtgui.UnitTesting.TestUtils import QtSignalSpy
 
 # Local
@@ -23,198 +19,206 @@ from sas.qtgui.Utilities.PluginDefinition import PluginDefinition
 from sas.qtgui.Utilities.ModelEditor import ModelEditor
 
 
-if not QApplication.instance():
-    app = QApplication(sys.argv)
 
-class TabbedModelEditorTest(unittest.TestCase):
-    def setUp(self):
-        """
-        Prepare the editors
-        """
-        class dummy_manager(object):
+class TabbedModelEditorTest:
+    @pytest.fixture(autouse=True)
+    def widget(self, qapp):
+        '''Create/Destroy the editor'''
+        class dummy_manager:
             _parent = QWidget()
             communicate = GuiUtils.Communicate()
 
-        self.widget = TabbedModelEditor(parent=dummy_manager)
-        self.widget_edit = TabbedModelEditor(parent=dummy_manager, edit_only=True)
+        w = TabbedModelEditor(parent=dummy_manager)
 
-    def tearDown(self):
-        """Destroy the DataOperationUtility"""
+        yield w
+
         QMessageBox.exec = MagicMock(return_value=QMessageBox.Discard)
-        self.widget.close()
-        self.widget = None
-        self.widget_edit.close()
-        self.widget_edit = None
+        w.close()
 
-    def testDefaults(self):
+    @pytest.fixture(autouse=True)
+    def widget_edit(self, qapp):
+        '''Create/Destroy the editor'''
+        class dummy_manager:
+            _parent = QWidget()
+            communicate = GuiUtils.Communicate()
+
+        w = TabbedModelEditor(parent=dummy_manager, edit_only=True)
+
+        yield w
+
+        QMessageBox.exec = MagicMock(return_value=QMessageBox.Discard)
+        w.close()
+        widget_edit = TabbedModelEditor(parent=dummy_manager, edit_only=True)
+
+    def testDefaults(self, widget, widget_edit):
         """Test the GUI in its default state"""
-        assert self.widget.filename == ""
-        assert self.widget.window_title == "Model Editor"
-        assert not self.widget.is_modified
-        assert not self.widget.edit_only
-        assert self.widget_edit.edit_only
+        assert widget.filename == ""
+        assert widget.window_title == "Model Editor"
+        assert not widget.is_modified
+        assert not widget.edit_only
+        assert widget_edit.edit_only
 
         # Add model
-        #self.assertFalse(self.widget.cmdLoad.isVisible())
-        #self.assertTrue(self.widget_edit.cmdLoad.isVisible())
-        assert isinstance(self.widget.plugin_widget, PluginDefinition)
-        assert isinstance(self.widget.editor_widget, ModelEditor)
+        #self.assertFalse(widget.cmdLoad.isVisible())
+        #self.assertTrue(widget_edit.cmdLoad.isVisible())
+        assert isinstance(widget.plugin_widget, PluginDefinition)
+        assert isinstance(widget.editor_widget, ModelEditor)
         # tabs
-        assert self.widget.tabWidget.count() == 2
-        assert not self.widget.editor_widget.isEnabled()
-        assert self.widget_edit.tabWidget.count() == 1
-        assert not self.widget_edit.editor_widget.isEnabled()
+        assert widget.tabWidget.count() == 2
+        assert not widget.editor_widget.isEnabled()
+        assert widget_edit.tabWidget.count() == 1
+        assert not widget_edit.editor_widget.isEnabled()
 
         #buttons
-        assert not self.widget.buttonBox.button(QDialogButtonBox.Apply).isEnabled()
-        assert self.widget.buttonBox.button(QDialogButtonBox.Apply).text() == "Apply"
-        assert self.widget_edit.buttonBox.button(QDialogButtonBox.Apply).text() == "Save"
+        assert not widget.buttonBox.button(QDialogButtonBox.Apply).isEnabled()
+        assert widget.buttonBox.button(QDialogButtonBox.Apply).text() == "Apply"
+        assert widget_edit.buttonBox.button(QDialogButtonBox.Apply).text() == "Save"
 
-    def testSetPluginActive(self):
+    def testSetPluginActive(self, widget):
         """ Enables the plugin editor """
         # by default it is on
-        assert self.widget.plugin_widget.isEnabled()
+        assert widget.plugin_widget.isEnabled()
         # Let's disable it
-        self.widget.setPluginActive(False)
-        assert not self.widget.plugin_widget.isEnabled()
+        widget.setPluginActive(False)
+        assert not widget.plugin_widget.isEnabled()
         # and back to enabled
-        self.widget.setPluginActive(True)
-        assert self.widget.plugin_widget.isEnabled()
+        widget.setPluginActive(True)
+        assert widget.plugin_widget.isEnabled()
 
-    def notestCloseEvent(self):
+    def notestCloseEvent(self, widget):
         """Test the close event wrt. saving info"""
         event = QObject()
         event.accept = MagicMock()
 
         # 1. no changes to document - straightforward exit
-        self.widget.is_modified = False
-        self.widget.closeEvent(event)
+        widget.is_modified = False
+        widget.closeEvent(event)
         assert event.accept.called_once()
 
         # 2. document changed, cancelled
-        self.widget.is_modified = True
+        widget.is_modified = True
         QMessageBox.exec = MagicMock(return_value=QMessageBox.Cancel)
-        self.widget.closeEvent(event)
+        widget.closeEvent(event)
         assert QMessageBox.exec.called_once()
         # no additional calls to event accept
         assert event.accept.called_once()
 
         # 3. document changed, save
         QMessageBox.exec = MagicMock(return_value=QMessageBox.Save)
-        self.widget.filename = "random string #8"
-        self.widget.updateFromEditor = MagicMock()
-        self.widget.closeEvent(event)
+        widget.filename = "random string #8"
+        widget.updateFromEditor = MagicMock()
+        widget.closeEvent(event)
         assert QMessageBox.exec.called_once()
         # no additional calls to event accept
         assert event.accept.called_once()
-        assert self.widget.updateFromEditor.called_once()
+        assert widget.updateFromEditor.called_once()
 
-    def testOnApply(self):
+    def testOnApply(self, widget):
         """Test the Apply/Save event"""
         # name the plugin
-        self.widget.plugin_widget.txtName.setText("Uncharacteristically eloquent filename")
-        self.widget.plugin_widget.txtName.editingFinished.emit()
+        widget.plugin_widget.txtName.setText("Uncharacteristically eloquent filename")
+        widget.plugin_widget.txtName.editingFinished.emit()
 
         # make sure the flag is set correctly
-        assert self.widget.is_modified
+        assert widget.is_modified
 
         # default tab
-        self.widget.updateFromPlugin = MagicMock()
-        self.widget.onApply()
-        assert self.widget.updateFromPlugin.called_once()
+        widget.updateFromPlugin = MagicMock()
+        widget.onApply()
+        assert widget.updateFromPlugin.called_once()
 
         # switch tabs
-        self.widget.tabWidget.setCurrentIndex(1)
-        self.widget.updateFromEditor = MagicMock()
-        self.widget.onApply()
-        assert self.widget.updateFromEditor.called_once()
+        widget.tabWidget.setCurrentIndex(1)
+        widget.updateFromEditor = MagicMock()
+        widget.onApply()
+        assert widget.updateFromEditor.called_once()
 
-    def testEditorModelModified(self):
+    def testEditorModelModified(self, widget):
          """Test reaction to direct edit in the editor """
          # Switch to the Editor tab
-         self.widget.tabWidget.setCurrentIndex(1)
-         assert not self.widget.is_modified
+         widget.tabWidget.setCurrentIndex(1)
+         assert not widget.is_modified
 
          # add some text. This invokes tested method
-         self.widget.editor_widget.txtEditor.setPlainText("Plain Text")
+         widget.editor_widget.txtEditor.setPlainText("Plain Text")
 
          # assure relevant functionality is invoked
-         assert self.widget.buttonBox.button(QDialogButtonBox.Apply).isEnabled()
-         assert self.widget.is_modified
-         assert "*" in self.widget.windowTitle()
+         assert widget.buttonBox.button(QDialogButtonBox.Apply).isEnabled()
+         assert widget.is_modified
+         assert "*" in widget.windowTitle()
 
 
-    def testpluginTitleSet(self):
+    def testpluginTitleSet(self, widget):
         """Test reaction to direct edit in plugin wizard"""
-        assert not self.widget.is_modified
+        assert not widget.is_modified
 
         # Call the tested method with no filename defined
-        self.widget.pluginTitleSet()
+        widget.pluginTitleSet()
 
         # Assure the apply button is disabled
-        assert not self.widget.buttonBox.button(QDialogButtonBox.Apply).isEnabled()
+        assert not widget.buttonBox.button(QDialogButtonBox.Apply).isEnabled()
 
         # Modify plugin name
         new_name = "NAME"
-        self.widget.plugin_widget.txtName.setText(new_name)
-        self.widget.plugin_widget.txtName.editingFinished.emit()
+        widget.plugin_widget.txtName.setText(new_name)
+        widget.plugin_widget.txtName.editingFinished.emit()
 
         # Assure relevant functionality is invoked
-        assert "*" in self.widget.windowTitle()
-        assert new_name in self.widget.windowTitle()
-        assert self.widget.buttonBox.button(QDialogButtonBox.Apply).isEnabled()
-        assert self.widget.is_modified
+        assert "*" in widget.windowTitle()
+        assert new_name in widget.windowTitle()
+        assert widget.buttonBox.button(QDialogButtonBox.Apply).isEnabled()
+        assert widget.is_modified
 
-    def testSetTabEdited(self):
+    def testSetTabEdited(self, widget):
         """
         Test the simple string update method
         """
         title = "title"
         title_star = "title*"
-        self.widget.setWindowTitle(title)
+        widget.setWindowTitle(title)
 
         # 1. -> edited
-        self.widget.setTabEdited(True)
-        assert "*" in self.widget.windowTitle()
+        widget.setTabEdited(True)
+        assert "*" in widget.windowTitle()
         # make sure we don't get another star
-        self.widget.setWindowTitle(title_star)
-        self.widget.setTabEdited(True)
-        assert title_star == self.widget.windowTitle()
+        widget.setWindowTitle(title_star)
+        widget.setTabEdited(True)
+        assert title_star == widget.windowTitle()
 
         # 2. -> not edited
-        self.widget.setWindowTitle(title_star)
-        self.widget.setTabEdited(False)
-        assert title == self.widget.windowTitle()
+        widget.setWindowTitle(title_star)
+        widget.setTabEdited(False)
+        assert title == widget.windowTitle()
         # No changes when no star in title
-        self.widget.setWindowTitle(title)
-        self.widget.setTabEdited(False)
-        assert title == self.widget.windowTitle()
+        widget.setWindowTitle(title)
+        widget.setTabEdited(False)
+        assert title == widget.windowTitle()
 
     @pytest.mark.xfail(reason="2022-09 already broken")
-    def testUpdateFromEditor(self):
+    def testUpdateFromEditor(self, widget):
         """
         Test the behaviour on editor window being updated
         """
         # Assure the test rises when no filename present
-        self.widget.filename = ""
+        widget.filename = ""
         with pytest.raises(Exception):
-            self.widget.updateFromEditor()
+            widget.updateFromEditor()
 
         # change the filename
-        self.widget.filename="testfile.py"
-        self.widget.writeFile = MagicMock()
+        widget.filename="testfile.py"
+        widget.writeFile = MagicMock()
         boring_text = "so bored with unit tests"
-        self.widget.editor_widget.txtEditor.toPlainText = MagicMock(return_value=boring_text)
-        self.widget.writeFile = MagicMock()
-        self.widget.plugin_widget.is_python = MagicMock()
+        widget.editor_widget.txtEditor.toPlainText = MagicMock(return_value=boring_text)
+        widget.writeFile = MagicMock()
+        widget.plugin_widget.is_python = MagicMock()
         #invoke the method
-        self.widget.updateFromEditor()
+        widget.updateFromEditor()
 
         # Test the behaviour
-        assert self.widget.writeFile.called_once()
-        assert self.widget.writeFile.called_with('testfile.py', boring_text)
+        assert widget.writeFile.called_once()
+        assert widget.writeFile.called_with('testfile.py', boring_text)
 
-    def testCanWriteModel(self):
+    def testCanWriteModel(self, widget):
         """
         Test if the model can be written to a file, given initial conditions
         """
@@ -223,19 +227,19 @@ class TabbedModelEditorTest(unittest.TestCase):
         test_path = "test.py"
 
         with pytest.raises(Exception):
-            self.widget.canWriteModel()
+            widget.canWriteModel()
 
         with pytest.raises(Exception):
-            self.widget.canWriteModel(model=test_model)
+            widget.canWriteModel(model=test_model)
 
         with pytest.raises(Exception):
-            self.widget.canWriteModel(full_path=test_path)
+            widget.canWriteModel(full_path=test_path)
 
         # 1. Overwrite box unchecked, file exists
         os.path.isfile = MagicMock(return_value=True)
         QMessageBox.critical = MagicMock()
 
-        ret = self.widget.canWriteModel(model=test_model, full_path=test_path)
+        ret = widget.canWriteModel(model=test_model, full_path=test_path)
         assert not ret
         assert QMessageBox.critical.called_once()
         assert 'Plugin Error' in QMessageBox.critical.call_args[0][1]
@@ -247,7 +251,7 @@ class TabbedModelEditorTest(unittest.TestCase):
         test_model['text'] = ""
         QMessageBox.critical = MagicMock()
 
-        ret = self.widget.canWriteModel(model=test_model, full_path=test_path)
+        ret = widget.canWriteModel(model=test_model, full_path=test_path)
         assert not ret
         assert QMessageBox.critical.called_once()
         assert 'Plugin Error' in QMessageBox.critical.call_args[0][1]
@@ -259,7 +263,7 @@ class TabbedModelEditorTest(unittest.TestCase):
         test_model['text'] = "I am a simple model"
         QMessageBox.critical = MagicMock()
 
-        ret = self.widget.canWriteModel(model=test_model, full_path=test_path)
+        ret = widget.canWriteModel(model=test_model, full_path=test_path)
         assert not ret
         assert QMessageBox.critical.called_once()
         assert 'Plugin Error' in QMessageBox.critical.call_args[0][1]
@@ -268,7 +272,7 @@ class TabbedModelEditorTest(unittest.TestCase):
         # 4. Overwrite box unchecked, file doesnt exist, good model
         os.path.isfile = MagicMock(return_value=False)
         test_model['text'] = "return"
-        ret = self.widget.canWriteModel(model=test_model, full_path=test_path)
+        ret = widget.canWriteModel(model=test_model, full_path=test_path)
         assert ret
 
     def testGenerateModel(self):
@@ -288,7 +292,7 @@ class TabbedModelEditorTest(unittest.TestCase):
         """
         pass
 
-    def testStrFromParamDict(self):
+    def testStrFromParamDict(self, widget):
         """
         Test Conversion from dict to param string
         """
@@ -301,6 +305,5 @@ class TabbedModelEditorTest(unittest.TestCase):
         #{0: ('variable','value'),
         # 1: ('variable','value'),
 
-        test_str = self.widget.strFromParamDict(test_dict)
+        test_str = widget.strFromParamDict(test_dict)
         assert "" in test_str
-
