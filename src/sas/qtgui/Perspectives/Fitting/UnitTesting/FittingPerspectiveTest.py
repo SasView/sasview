e81f1eb44038b07f0f3a97d82dbcd2d030ccc7a2
Stuart Prescott <stuart@debian.org>
2022-09-29 02:37:05 +0000

    Change tests to asserts in FittingPerspectiveTest.py

--- src/sas/qtgui/Perspectives/Fitting/UnitTesting/FittingPerspectiveTest.py
+++ src/sas/qtgui/Perspectives/Fitting/UnitTesting/FittingPerspectiveTest.py
@@ -2,6 +2,8 @@ import sys
 import unittest
 import webbrowser
 
+import pytest
+
 from PyQt5 import QtGui
 from PyQt5 import QtWidgets
 from PyQt5 import QtTest
@@ -39,46 +41,46 @@ class FittingPerspectiveTest(unittest.TestCase):
 
     def testDefaults(self):
         '''Test the GUI in its default state'''
-        self.assertIsInstance(self.widget, QtWidgets.QWidget)
-        self.assertIn("Fit panel", self.widget.windowTitle())
-        self.assertEqual(self.widget.optimizer, "Levenberg-Marquardt")
-        self.assertEqual(len(self.widget.tabs), 1)
-        self.assertEqual(self.widget.maxIndex, 2)
-        self.assertEqual(self.widget.getTabName(), "FitPage2")
+        assert isinstance(self.widget, QtWidgets.QWidget)
+        assert "Fit panel" in self.widget.windowTitle()
+        assert self.widget.optimizer == "Levenberg-Marquardt"
+        assert len(self.widget.tabs) == 1
+        assert self.widget.maxIndex == 2
+        assert self.widget.getTabName() == "FitPage2"
 
     def testAddTab(self):
         '''Add a tab and test it'''
 
         # Add an empty tab
         self.widget.addFit(None)
-        self.assertEqual(len(self.widget.tabs), 2)
-        self.assertEqual(self.widget.getTabName(), "FitPage3")
-        self.assertEqual(self.widget.maxIndex, 3)
+        assert len(self.widget.tabs) == 2
+        assert self.widget.getTabName() == "FitPage3"
+        assert self.widget.maxIndex == 3
         # Add an empty batch tab
         self.widget.addFit(None, is_batch=True)
-        self.assertEqual(len(self.widget.tabs), 3)
-        self.assertEqual(self.widget.getTabName(2), "BatchPage4")
-        self.assertEqual(self.widget.maxIndex, 4)
+        assert len(self.widget.tabs) == 3
+        assert self.widget.getTabName(2) == "BatchPage4"
+        assert self.widget.maxIndex == 4
 
     def testAddCSTab(self):
         ''' Add a constraint/simult tab'''
         self.widget.addConstraintTab()
-        self.assertEqual(len(self.widget.tabs), 2)
-        self.assertEqual(self.widget.getCSTabName(), "Const. & Simul. Fit")
+        assert len(self.widget.tabs) == 2
+        assert self.widget.getCSTabName() == "Const. & Simul. Fit"
 
     def testResetTab(self):
         ''' Remove data from last tab'''
-        self.assertEqual(len(self.widget.tabs), 1)
-        self.assertEqual(self.widget.getTabName(), "FitPage2")
-        self.assertEqual(self.widget.maxIndex, 2)
+        assert len(self.widget.tabs) == 1
+        assert self.widget.getTabName() == "FitPage2"
+        assert self.widget.maxIndex == 2
 
         # Attempt to remove the last tab
         self.widget.resetTab(0)
 
         # see that the tab didn't disappear, just changed the name/id
-        self.assertEqual(len(self.widget.tabs), 1)
-        self.assertEqual(self.widget.getTabName(), "FitPage3")
-        self.assertEqual(self.widget.maxIndex, 3)
+        assert len(self.widget.tabs) == 1
+        assert self.widget.getTabName() == "FitPage3"
+        assert self.widget.maxIndex == 3
 
         # Now, add data
         data = Data1D(x=[1,2], y=[1,2])
@@ -86,11 +88,11 @@ class FittingPerspectiveTest(unittest.TestCase):
         item = QtGui.QStandardItem("test")
         self.widget.setData([item])
         # Assert data is on widget
-        self.assertEqual(len(self.widget.tabs[0].all_data), 1)
+        assert len(self.widget.tabs[0].all_data) == 1
         # Reset the tab
         self.widget.resetTab(0)
         # See that the tab contains data no more
-        self.assertEqual(len(self.widget.tabs[0].all_data), 0)
+        assert len(self.widget.tabs[0].all_data) == 0
 
     def testCloseTab(self):
         '''Delete a tab and test'''
@@ -99,27 +101,27 @@ class FittingPerspectiveTest(unittest.TestCase):
 
         # Remove the original tab
         self.widget.tabCloses(1)
-        self.assertEqual(len(self.widget.tabs), 1)
-        self.assertEqual(self.widget.maxIndex, 3)
-        self.assertEqual(self.widget.getTabName(), "FitPage3")
+        assert len(self.widget.tabs) == 1
+        assert self.widget.maxIndex == 3
+        assert self.widget.getTabName() == "FitPage3"
 
         # Attemtp to remove the last tab
         self.widget.tabCloses(1)
         # The tab should still be there
-        self.assertEqual(len(self.widget.tabs), 1)
-        self.assertEqual(self.widget.maxIndex, 4)
-        self.assertEqual(self.widget.getTabName(), "FitPage4")
+        assert len(self.widget.tabs) == 1
+        assert self.widget.maxIndex == 4
+        assert self.widget.getTabName() == "FitPage4"
 
     def testAllowBatch(self):
         '''Assure the perspective allows multiple datasets'''
-        self.assertTrue(self.widget.allowBatch())
+        assert self.widget.allowBatch()
 
     def testSetData(self):
         ''' Assure that setting data is correct'''
-        with self.assertRaises(AssertionError):
+        with pytest.raises(AssertionError):
             self.widget.setData(None)
 
-        with self.assertRaises(AttributeError):
+        with pytest.raises(AttributeError):
             self.widget.setData("BOOP")
 
         # Mock the datafromitem() call from FittingWidget
@@ -130,19 +132,19 @@ class FittingPerspectiveTest(unittest.TestCase):
         self.widget.setData([item])
 
         # First tab should accept data
-        self.assertEqual(len(self.widget.tabs), 1)
+        assert len(self.widget.tabs) == 1
 
         # Add another set of data
         self.widget.setData([item])
 
         # Now we should have two tabs
-        self.assertEqual(len(self.widget.tabs), 2)
+        assert len(self.widget.tabs) == 2
 
         # Add two more items in a list
         self.widget.setData([item, item])
 
         # Check for 4 tabs
-        self.assertEqual(len(self.widget.tabs), 4)
+        assert len(self.widget.tabs) == 4
 
     def testSwapData(self):
         '''Assure that data swapping is correct'''
@@ -163,29 +165,32 @@ class FittingPerspectiveTest(unittest.TestCase):
         self.widget.swapData(item)
 
         # Check that data has been swapped
-        self.assertEqual(self.widget.tabs[0].data, data2)
+        assert self.widget.tabs[0].data == data2
 
         # We should only have one tab
-        self.assertEqual(len(self.widget.tabs), 1)
+        assert len(self.widget.tabs) == 1
 
         # send something stupid as data
         item = "foo"
 
         # It should raise an AttributeError
-        self.assertRaises(AttributeError, self.widget.swapData, item)
+        with pytest.raises(AttributeError):
+            self.widget.swapData(item)
 
         # Create a batch tab
         item = QtGui.QStandardItem("test")
         self.widget.addFit(None, is_batch=True)
 
         # It should raise an exception
-        self.assertRaises(RuntimeError, self.widget.swapData, item)
+        with pytest.raises(RuntimeError):
+            self.widget.swapData(item)
 
         # Create a non valid tab
         self.widget.addConstraintTab()
 
         # It should raise a TypeError
-        self.assertRaises(TypeError, self.widget.swapData, item)
+        with pytest.raises(TypeError):
+            self.widget.swapData(item)
 
     def testSetBatchData(self):
         ''' Assure that setting batch data is correct'''
@@ -200,17 +205,17 @@ class FittingPerspectiveTest(unittest.TestCase):
         self.widget.setData([item, item], is_batch=True)
 
         # First tab should not accept data
-        self.assertEqual(len(self.widget.tabs), 2)
+        assert len(self.widget.tabs) == 2
 
         # Add another set of data
         self.widget.setData([item, item], is_batch=True)
 
         # Now we should have two batch tabs
-        self.assertEqual(len(self.widget.tabs), 3)
+        assert len(self.widget.tabs) == 3
 
         # Check the names of the new tabs
-        self.assertEqual(self.widget.tabText(1), "BatchPage2")
-        self.assertEqual(self.widget.tabText(2), "BatchPage3")
+        assert self.widget.tabText(1) == "BatchPage2"
+        assert self.widget.tabText(2) == "BatchPage3"
 
     def testGetFitTabs(self):
         '''test the fit tab getter method'''
@@ -218,8 +223,8 @@ class FittingPerspectiveTest(unittest.TestCase):
         self.widget.addFit(None)
         # Get the tabs
         tabs = self.widget.getFitTabs()
-        self.assertTrue(isinstance(tabs, list))
-        self.assertEqual(len(tabs), 2)
+        assert isinstance(tabs, list)
+        assert len(tabs) == 2
 
     def testGetActiveConstraintList(self):
         '''test the active constraint getter'''
@@ -239,9 +244,9 @@ class FittingPerspectiveTest(unittest.TestCase):
         constraints = self.widget.getActiveConstraintList()
 
         # we should have 2 constraints
-        self.assertEqual(len(constraints), 2)
-        self.assertEqual(constraints, [("M1.scale", "M2.scale +2"),
-                                       ('M2.scale', 'M2.background +2')])
+        assert len(constraints) == 2
+        assert constraints == [("M1.scale", "M2.scale +2"),
+                                       ('M2.scale', 'M2.background +2')]
 
     def testGetSymbolDictForConstraints(self):
         '''test the symbol dict getter'''
@@ -256,24 +261,24 @@ class FittingPerspectiveTest(unittest.TestCase):
 
         symbols = self.widget.getSymbolDictForConstraints()
         # we should have 2 symbols
-        self.assertEqual(len(symbols), 2)
-        self.assertEqual(list(symbols.keys()), ["M1.scale", "M2.scale"])
+        assert len(symbols) == 2
+        assert list(symbols.keys()) == ["M1.scale", "M2.scale"]
 
     def testGetConstraintTab(self):
         '''test the constraint tab getter'''
         # no constraint tab is present, should return None
         constraint_tab = self.widget.getConstraintTab()
-        self.assertEqual(constraint_tab, None)
+        assert constraint_tab == None
 
         # add a constraint tab
         self.widget.addConstraintTab()
         constraint_tab = self.widget.getConstraintTab()
-        self.assertEquals(constraint_tab, self.widget.tabs[1])
+        assert constraint_tab == self.widget.tabs[1]
 
     def testSerialization(self):
         ''' Serialize fit pages and check data '''
-        self.assertTrue(hasattr(self.widget, 'isSerializable'))
-        self.assertTrue(self.widget.isSerializable())
+        assert hasattr(self.widget, 'isSerializable')
+        assert self.widget.isSerializable()
         data = Data1D(x=[1,2], y=[1,2])
         GuiUtils.dataFromItem = MagicMock(return_value=data)
         item = QtGui.QStandardItem("test")
@@ -286,7 +291,7 @@ class FittingPerspectiveTest(unittest.TestCase):
         data_id = str(self.widget.currentTabDataId()[0])
         # check values - disabled control, present weights
         rowcount = tab._model_model.rowCount()
-        self.assertEqual(rowcount, 8)
+        assert rowcount == 8
         state_default = self.widget.serializeAll()
         state_all = self.widget.serializeAllFitpage()
         state_cp = self.widget.serializeCurrentPage()
@@ -294,13 +299,13 @@ class FittingPerspectiveTest(unittest.TestCase):
         # Pull out params from state
         params = state_all[data_id]['fit_params'][0]
         # Tests
-        self.assertEqual(len(state_all), len(state_default))
-        self.assertEqual(len(state_cp), len(page))
-        self.assertEqual(len(state_all), 1)
+        assert len(state_all) == len(state_default)
+        assert len(state_cp) == len(page)
+        assert len(state_all) == 1
         # getPage should include an extra param 'data_id' removed by serialize
-        self.assertNotEqual(len(params), len(page))
-        self.assertEqual(len(params), 28)
-        self.assertEqual(page.get('data_id', None), None)
+        assert len(params) != len(page)
+        assert len(params) == 28
+        assert page.get('data_id', None) == None
 
     def testUpdateFromConstraints(self):
         '''tests the method that parses the loaded project dict and retuens a dict with constrains across all fit pages'''
@@ -335,10 +340,10 @@ class FittingPerspectiveTest(unittest.TestCase):
                                  func="M1.background")
         tab1_call_dict = tab1.addConstraintToRow.call_args[1]
         tab2_call_dict = tab2.addConstraintToRow.call_args[1]
-        self.assertEqual(vars(tab1_call_dict['constraint']), vars(constraint1))
-        self.assertEqual(vars(tab2_call_dict['constraint']), vars(constraint2))
-        self.assertEqual(tab1_call_dict['row'], 0)
-        self.assertEqual(tab2_call_dict['row'], 1)
+        assert vars(tab1_call_dict['constraint']) == vars(constraint1)
+        assert vars(tab2_call_dict['constraint']) == vars(constraint2)
+        assert tab1_call_dict['row'] == 0
+        assert tab2_call_dict['row'] == 1
 
     def testGetTabByName(self):
         '''test getting a tab by its name'''
@@ -346,10 +351,10 @@ class FittingPerspectiveTest(unittest.TestCase):
         self.widget.addFit(None)
         # get the second tab
         tab = self.widget.getTabByName('M2')
-        self.assertEqual(tab, self.widget.tabs[1])
+        assert tab == self.widget.tabs[1]
         # get some unexisting tab
         tab = self.widget.getTabByName('foo')
-        self.assertFalse(tab)
+        assert not tab
 
 if __name__ == "__main__":
     unittest.main()
