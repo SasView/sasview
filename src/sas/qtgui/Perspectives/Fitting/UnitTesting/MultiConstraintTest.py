6584e0fcb128999e9869ead2e4f2b3d563845fd4
Stuart Prescott <stuart@debian.org>
2022-09-29 02:30:32 +0000

    Change tests to asserts in MultiConstraintTest.py

--- src/sas/qtgui/Perspectives/Fitting/UnitTesting/MultiConstraintTest.py
+++ src/sas/qtgui/Perspectives/Fitting/UnitTesting/MultiConstraintTest.py
@@ -8,7 +8,7 @@ from unittest.mock import MagicMock
 from PyQt5 import QtGui, QtWidgets
 
 # set up import paths
-import path_prepare
+import sas.qtgui.path_prepare
 
 # Local
 from sas.qtgui.Perspectives.Fitting.MultiConstraint import MultiConstraint
@@ -32,25 +32,25 @@ class MultiConstraintTest(unittest.TestCase):
 
     def testDefaults(self):
         '''Test the GUI in its default state'''
-        self.assertIsInstance(self.widget, QtWidgets.QDialog)
+        assert isinstance(self.widget, QtWidgets.QDialog)
         # Default title
-        self.assertEqual(self.widget.windowTitle(), "2 parameter constraint")
+        assert self.widget.windowTitle() == "2 parameter constraint"
 
         # modal window
-        self.assertTrue(self.widget.isModal())
+        assert self.widget.isModal()
 
     def testLabels(self):
         ''' various labels on the widget '''
         # params related setup
-        self.assertEqual(self.widget.txtParam1.text(), self.p1)
-        self.assertEqual(self.widget.txtParam1_2.text(), self.p1)
-        self.assertEqual(self.widget.txtParam2.text(), self.p2)
+        assert self.widget.txtParam1.text() == self.p1
+        assert self.widget.txtParam1_2.text() == self.p1
+        assert self.widget.txtParam2.text() == self.p2
 
     def testTooltip(self):
         ''' test the tooltip'''
         tooltip = "E.g.\n%s = 2.0 * (%s)\n" %(self.p1, self.p2)
         tooltip += "%s = sqrt(%s) + 5"%(self.p1, self.p2)
-        self.assertEqual(self.widget.txtConstraint.toolTip(), tooltip)
+        assert self.widget.txtConstraint.toolTip() == tooltip
 
     def testValidateFormula(self):
         ''' assure enablement and color for valid formula '''
@@ -58,45 +58,45 @@ class MultiConstraintTest(unittest.TestCase):
         self.widget.validateConstraint = MagicMock(return_value=False)
         self.widget.validateFormula()
         style_sheet = "QLineEdit {background-color: red;}"
-        self.assertFalse(self.widget.cmdOK.isEnabled())
-        self.assertEqual(self.widget.txtConstraint.styleSheet(),style_sheet)
+        assert not self.widget.cmdOK.isEnabled()
+        assert self.widget.txtConstraint.styleSheet() == style_sheet
 
         # Valid string
         self.widget.validateConstraint = MagicMock(return_value=True)
         self.widget.validateFormula()
         style_sheet = "QLineEdit {background-color: white;}"
-        self.assertTrue(self.widget.cmdOK.isEnabled())
-        self.assertEqual(self.widget.txtConstraint.styleSheet(),style_sheet)
+        assert self.widget.cmdOK.isEnabled()
+        assert self.widget.txtConstraint.styleSheet() == style_sheet
 
     def testValidateConstraint(self):
         ''' constraint validator test'''
         #### BAD
         # none
-        self.assertFalse(self.widget.validateConstraint(None))
+        assert not self.widget.validateConstraint(None)
         # inf
-        self.assertFalse(self.widget.validateConstraint(np.inf))
+        assert not self.widget.validateConstraint(np.inf)
         # 0
-        self.assertFalse(self.widget.validateConstraint(0))
+        assert not self.widget.validateConstraint(0)
         # ""
-        self.assertFalse(self.widget.validateConstraint(""))
+        assert not self.widget.validateConstraint("")
         # p2_
-        self.assertFalse(self.widget.validateConstraint("p2_"))
+        assert not self.widget.validateConstraint("p2_")
         # p1
-        self.assertFalse(self.widget.validateConstraint("p1"))
+        assert not self.widget.validateConstraint("p1")
 
         ### GOOD
         # p2
-        self.assertTrue(self.widget.validateConstraint("p2"))
+        assert self.widget.validateConstraint("p2")
         # " p2    "
-        self.assertTrue(self.widget.validateConstraint(" p2    "))
+        assert self.widget.validateConstraint(" p2    ")
         # sqrt(p2)
-        self.assertTrue(self.widget.validateConstraint("sqrt(p2)"))
+        assert self.widget.validateConstraint("sqrt(p2)")
         # -p2
-        self.assertTrue(self.widget.validateConstraint("-p2"))
+        assert self.widget.validateConstraint("-p2")
         # log10(p2) - sqrt(p2) + p2
-        self.assertTrue(self.widget.validateConstraint("log10(p2) - sqrt(p2) + p2"))
+        assert self.widget.validateConstraint("log10(p2) - sqrt(p2) + p2")
         # log10(    p2    ) +  p2
-        self.assertTrue(self.widget.validateConstraint("log10(    p2    ) +  p2  "))
+        assert self.widget.validateConstraint("log10(    p2    ) +  p2  ")
 
     def testOnHelp(self):
         """
