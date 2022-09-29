6edf2bc14d6b5d08fe3b689e0742e4e721c7fb7e
Stuart Prescott <stuart@debian.org>
2022-09-29 09:02:08 +0000

    Change tests to asserts in InvariantDetailsTest.py

--- src/sas/qtgui/Perspectives/Invariant/UnitTesting/InvariantDetailsTest.py
+++ src/sas/qtgui/Perspectives/Invariant/UnitTesting/InvariantDetailsTest.py
@@ -51,23 +51,23 @@ class InvariantDetailsTest(unittest.TestCase):
 
         self.widget.warning_msg = "No Details on calculations available...\n"
 
-        self.assertIsInstance(self.widget, QtWidgets.QDialog)
+        assert isinstance(self.widget, QtWidgets.QDialog)
 
-        self.assertEqual(self.widget.progressBarLowQ.minimum(), 0)
-        self.assertEqual(self.widget.progressBarLowQ.maximum(), 100)
-        self.assertEqual(self.widget.progressBarData.minimum(), 0)
-        self.assertEqual(self.widget.progressBarData.maximum(), 100)
-        self.assertEqual(self.widget.progressBarHighQ.minimum(), 0)
-        self.assertEqual(self.widget.progressBarHighQ.maximum(), 100)
+        assert self.widget.progressBarLowQ.minimum() == 0
+        assert self.widget.progressBarLowQ.maximum() == 100
+        assert self.widget.progressBarData.minimum() == 0
+        assert self.widget.progressBarData.maximum() == 100
+        assert self.widget.progressBarHighQ.minimum() == 0
+        assert self.widget.progressBarHighQ.maximum() == 100
 
 
         # Tooltips
-        self.assertEqual(self.widget.txtQData.toolTip(), "Invariant in the data set's Q range.")
-        self.assertEqual(self.widget.txtQDataErr.toolTip(), "Uncertainty on the invariant from data's range.")
-        self.assertEqual(self.widget.txtQLowQ.toolTip(), "Extrapolated invariant from low-Q range.")
-        self.assertEqual(self.widget.txtQLowQErr.toolTip(), "Uncertainty on the invariant from low-Q range.")
-        self.assertEqual(self.widget.txtQHighQ.toolTip(), "Extrapolated invariant from high-Q range.")
-        self.assertEqual(self.widget.txtQHighQErr.toolTip(), "Uncertainty on the invariant from high-Q range.")
+        assert self.widget.txtQData.toolTip() == "Invariant in the data set's Q range."
+        assert self.widget.txtQDataErr.toolTip() == "Uncertainty on the invariant from data's range."
+        assert self.widget.txtQLowQ.toolTip() == "Extrapolated invariant from low-Q range."
+        assert self.widget.txtQLowQErr.toolTip() == "Uncertainty on the invariant from low-Q range."
+        assert self.widget.txtQHighQ.toolTip() == "Extrapolated invariant from high-Q range."
+        assert self.widget.txtQHighQErr.toolTip() == "Uncertainty on the invariant from high-Q range."
 
     def testOnOK(self):
         """ Test closing dialog"""
@@ -78,44 +78,44 @@ class InvariantDetailsTest(unittest.TestCase):
         """ """
         self.widget.showDialog()
         # Low Q true
-        self.assertEqual(self.widget.qlow, 9.0)
-        self.assertEqual(self.widget.txtQLowQ.text(), '9.0')
-        self.assertEqual(self.widget.progress_low_qstar, 90.0)
-        self.assertEqual(self.widget.qstar_total, 10.0)
-        self.assertEqual(self.widget.txtQData.text(), '10.0')
-        self.assertEqual(self.widget.txtQDataErr.text(), '0.1')
+        assert self.widget.qlow == 9.0
+        assert self.widget.txtQLowQ.text() == '9.0'
+        assert self.widget.progress_low_qstar == 90.0
+        assert self.widget.qstar_total == 10.0
+        assert self.widget.txtQData.text() == '10.0'
+        assert self.widget.txtQDataErr.text() == '0.1'
 
         # High Q false
-        self.assertEqual(self.widget.txtQHighQ.text(), '')
-        self.assertEqual(self.widget.txtQHighQErr.text(), '')
-        self.assertEqual(self.widget.progress_high_qstar, 0.0)
+        assert self.widget.txtQHighQ.text() == ''
+        assert self.widget.txtQHighQErr.text() == ''
+        assert self.widget.progress_high_qstar == 0.0
 
         # Progressbars
-        self.assertEqual(self.widget.progressBarLowQ.value(), self.widget.progress_low_qstar)
-        self.assertEqual(self.widget.progressBarLowQ.value(), self.widget.progress_low_qstar)
-        self.assertEqual(self.widget.progressBarHighQ.value(), self.widget.progress_high_qstar)
+        assert self.widget.progressBarLowQ.value() == self.widget.progress_low_qstar
+        assert self.widget.progressBarLowQ.value() == self.widget.progress_low_qstar
+        assert self.widget.progressBarHighQ.value() == self.widget.progress_high_qstar
 
     def testCheckValues(self):
         """ """
         self.widget.qstart_total = None
-        self.assertEqual(self.widget.checkValues(), "Invariant not calculated.\n")
+        assert self.widget.checkValues() == "Invariant not calculated.\n"
 
         self.widget.qstar_total = 0
         return_string = self.widget.checkValues()
-        self.assertIn("Invariant is zero.", return_string)
-        self.assertIn("The calculations are likely to be unreliable!", return_string)
+        assert "Invariant is zero." in return_string
+        assert "The calculations are likely to be unreliable!" in return_string
 
         self.widget.qstar_total = 10
-        self.assertIn("No Warnings to report", self.widget.checkValues())
+        assert "No Warnings to report" in self.widget.checkValues()
 
         self.widget.progress_qstar = 0
         self.widget.progress_low_qstar = 10
         return_string = self.widget.checkValues()
-        self.assertIn('Extrapolated contribution at Low Q is higher than 5% of the invariant.', return_string)
-        self.assertIn('The sum of all extrapolated contributions is higher than 5% of the invariant.', return_string)
+        assert 'Extrapolated contribution at Low Q is higher than 5% of the invariant.' in return_string
+        assert 'The sum of all extrapolated contributions is higher than 5% of the invariant.' in return_string
 
         self.widget.progress_low_qstar = -1
-        self.assertEqual(self.widget.checkValues(), "Extrapolated contribution at Low Q < 0.\n")
+        assert self.widget.checkValues() == "Extrapolated contribution at Low Q < 0.\n"
 
 
 if __name__ == "__main__":
