82b7d27e82fd7c1510dbfaa42e46933233558962
Stuart Prescott <stuart@debian.org>
2022-09-29 02:33:43 +0000

    Change to pytest fixtures MultiConstraintTest.py

--- src/sas/qtgui/Perspectives/Fitting/UnitTesting/MultiConstraintTest.py
+++ src/sas/qtgui/Perspectives/Fitting/UnitTesting/MultiConstraintTest.py
@@ -1,14 +1,13 @@
 import sys
-import unittest
-import numpy as np
 import webbrowser
+
+import pytest
+
+import numpy as np
 
 from unittest.mock import MagicMock
 
 from PyQt5 import QtGui, QtWidgets
-
-# set up import paths
-import sas.qtgui.path_prepare
 
 # Local
 from sas.qtgui.Perspectives.Fitting.MultiConstraint import MultiConstraint
@@ -16,96 +15,95 @@ from sas.qtgui.Perspectives.Fitting.MultiConstraint import MultiConstraint
 if not QtWidgets.QApplication.instance():
     app = QtWidgets.QApplication(sys.argv)
 
-class MultiConstraintTest(unittest.TestCase):
+class MultiConstraintTest:
     '''Test the MultiConstraint dialog'''
-    def setUp(self):
-        '''Create MultiConstraint dialog'''
+
+    @pytest.fixture(autouse=True)
+    def widget(self, qapp):
+        '''Create/Destroy the MultiConstraint'''
         params = ['p1', 'p2']
         self.p1 = params[0]
         self.p2 = params[1]
-        self.widget = MultiConstraint(parent=None, params=params)
+        w = MultiConstraint(parent=None, params=params)
+        yield w
+        w.close()
 
-    def tearDown(self):
-        '''Destroy the GUI'''
-        self.widget.close()
-        self.widget = None
-
-    def testDefaults(self):
+    def testDefaults(self, widget):
         '''Test the GUI in its default state'''
-        assert isinstance(self.widget, QtWidgets.QDialog)
+        assert isinstance(widget, QtWidgets.QDialog)
         # Default title
-        assert self.widget.windowTitle() == "2 parameter constraint"
+        assert widget.windowTitle() == "2 parameter constraint"
 
         # modal window
-        assert self.widget.isModal()
+        assert widget.isModal()
 
-    def testLabels(self):
+    def testLabels(self, widget):
         ''' various labels on the widget '''
         # params related setup
-        assert self.widget.txtParam1.text() == self.p1
-        assert self.widget.txtParam1_2.text() == self.p1
-        assert self.widget.txtParam2.text() == self.p2
+        assert widget.txtParam1.text() == self.p1
+        assert widget.txtParam1_2.text() == self.p1
+        assert widget.txtParam2.text() == self.p2
 
-    def testTooltip(self):
+    def testTooltip(self, widget):
         ''' test the tooltip'''
         tooltip = "E.g.\n%s = 2.0 * (%s)\n" %(self.p1, self.p2)
         tooltip += "%s = sqrt(%s) + 5"%(self.p1, self.p2)
-        assert self.widget.txtConstraint.toolTip() == tooltip
+        assert widget.txtConstraint.toolTip() == tooltip
 
-    def testValidateFormula(self):
+    def testValidateFormula(self, widget):
         ''' assure enablement and color for valid formula '''
         # Invalid string
-        self.widget.validateConstraint = MagicMock(return_value=False)
-        self.widget.validateFormula()
+        widget.validateConstraint = MagicMock(return_value=False)
+        widget.validateFormula()
         style_sheet = "QLineEdit {background-color: red;}"
-        assert not self.widget.cmdOK.isEnabled()
-        assert self.widget.txtConstraint.styleSheet() == style_sheet
+        assert not widget.cmdOK.isEnabled()
+        assert widget.txtConstraint.styleSheet() == style_sheet
 
         # Valid string
-        self.widget.validateConstraint = MagicMock(return_value=True)
-        self.widget.validateFormula()
+        widget.validateConstraint = MagicMock(return_value=True)
+        widget.validateFormula()
         style_sheet = "QLineEdit {background-color: white;}"
-        assert self.widget.cmdOK.isEnabled()
-        assert self.widget.txtConstraint.styleSheet() == style_sheet
+        assert widget.cmdOK.isEnabled()
+        assert widget.txtConstraint.styleSheet() == style_sheet
 
-    def testValidateConstraint(self):
+    def testValidateConstraint(self, widget):
         ''' constraint validator test'''
         #### BAD
         # none
-        assert not self.widget.validateConstraint(None)
+        assert not widget.validateConstraint(None)
         # inf
-        assert not self.widget.validateConstraint(np.inf)
+        assert not widget.validateConstraint(np.inf)
         # 0
-        assert not self.widget.validateConstraint(0)
+        assert not widget.validateConstraint(0)
         # ""
-        assert not self.widget.validateConstraint("")
+        assert not widget.validateConstraint("")
         # p2_
-        assert not self.widget.validateConstraint("p2_")
+        assert not widget.validateConstraint("p2_")
         # p1
-        assert not self.widget.validateConstraint("p1")
+        assert not widget.validateConstraint("p1")
 
         ### GOOD
         # p2
-        assert self.widget.validateConstraint("p2")
+        assert widget.validateConstraint("p2")
         # " p2    "
-        assert self.widget.validateConstraint(" p2    ")
+        assert widget.validateConstraint(" p2    ")
         # sqrt(p2)
-        assert self.widget.validateConstraint("sqrt(p2)")
+        assert widget.validateConstraint("sqrt(p2)")
         # -p2
-        assert self.widget.validateConstraint("-p2")
+        assert widget.validateConstraint("-p2")
         # log10(p2) - sqrt(p2) + p2
-        assert self.widget.validateConstraint("log10(p2) - sqrt(p2) + p2")
+        assert widget.validateConstraint("log10(p2) - sqrt(p2) + p2")
         # log10(    p2    ) +  p2
-        assert self.widget.validateConstraint("log10(    p2    ) +  p2  ")
+        assert widget.validateConstraint("log10(    p2    ) +  p2  ")
 
-    def testOnHelp(self):
+    def testOnHelp(self, widget):
         """
         Test the default help renderer
         """
         webbrowser.open = MagicMock()
 
         # invoke the tested method
-        self.widget.onHelp()
+        widget.onHelp()
 
         # see that webbrowser open was attempted
         webbrowser.open.assert_called_once()
