eacbb3abed155d340d0573c981624c03b17c2c43
Stuart Prescott <stuart@debian.org>
2022-09-29 02:20:27 +0000

    Change tests to asserts in FittingOptionsTest.py

--- src/sas/qtgui/Perspectives/Fitting/UnitTesting/FittingOptionsTest.py
+++ src/sas/qtgui/Perspectives/Fitting/UnitTesting/FittingOptionsTest.py
@@ -34,16 +34,16 @@ class FittingOptionsTest(unittest.TestCase):
 
     def testDefaults(self):
         '''Test the GUI in its default state'''
-        self.assertIsInstance(self.widget, QtWidgets.QDialog)
+        assert isinstance(self.widget, QtWidgets.QDialog)
         # Default title
-        self.assertEqual(self.widget.windowTitle(), "Fit Algorithms")
+        assert self.widget.windowTitle() == "Fit Algorithms"
 
         # The combo box
-        self.assertIsInstance(self.widget.cbAlgorithm, QtWidgets.QComboBox)
-        self.assertEqual(self.widget.cbAlgorithm.count(), 6)
-        self.assertEqual(self.widget.cbAlgorithm.itemText(0), 'Nelder-Mead Simplex')
-        self.assertEqual(self.widget.cbAlgorithm.itemText(4), 'Levenberg-Marquardt (scipy.leastsq)')
-        self.assertEqual(self.widget.cbAlgorithm.currentIndex(), 5)
+        assert isinstance(self.widget.cbAlgorithm, QtWidgets.QComboBox)
+        assert self.widget.cbAlgorithm.count() == 6
+        assert self.widget.cbAlgorithm.itemText(0) == 'Nelder-Mead Simplex'
+        assert self.widget.cbAlgorithm.itemText(4) == 'Levenberg-Marquardt (scipy.leastsq)'
+        assert self.widget.cbAlgorithm.currentIndex() == 5
 
     def testAssignValidators(self):
         """
@@ -52,22 +52,22 @@ class FittingOptionsTest(unittest.TestCase):
         # Can't reliably test the method in action, but can easily check the results
         
         # DREAM
-        self.assertIsInstance(self.widget.samples_dream.validator(), QtGui.QIntValidator)
-        self.assertIsInstance(self.widget.burn_dream.validator(), QtGui.QIntValidator)
-        self.assertIsInstance(self.widget.pop_dream.validator(), QtGui.QDoubleValidator)
-        self.assertIsInstance(self.widget.thin_dream.validator(), QtGui.QIntValidator)
-        self.assertIsInstance(self.widget.steps_dream.validator(), QtGui.QIntValidator)
+        assert isinstance(self.widget.samples_dream.validator(), QtGui.QIntValidator)
+        assert isinstance(self.widget.burn_dream.validator(), QtGui.QIntValidator)
+        assert isinstance(self.widget.pop_dream.validator(), QtGui.QDoubleValidator)
+        assert isinstance(self.widget.thin_dream.validator(), QtGui.QIntValidator)
+        assert isinstance(self.widget.steps_dream.validator(), QtGui.QIntValidator)
         # DE
-        self.assertIsInstance(self.widget.steps_de.validator(), QtGui.QIntValidator)
-        self.assertIsInstance(self.widget.CR_de.validator(), QtGui.QDoubleValidator)
-        self.assertIsInstance(self.widget.pop_de.validator(), QtGui.QDoubleValidator)
-        self.assertIsInstance(self.widget.F_de.validator(), QtGui.QDoubleValidator)
-        self.assertIsInstance(self.widget.ftol_de.validator(), QtGui.QDoubleValidator)
-        self.assertIsInstance(self.widget.xtol_de.validator(), QtGui.QDoubleValidator)
+        assert isinstance(self.widget.steps_de.validator(), QtGui.QIntValidator)
+        assert isinstance(self.widget.CR_de.validator(), QtGui.QDoubleValidator)
+        assert isinstance(self.widget.pop_de.validator(), QtGui.QDoubleValidator)
+        assert isinstance(self.widget.F_de.validator(), QtGui.QDoubleValidator)
+        assert isinstance(self.widget.ftol_de.validator(), QtGui.QDoubleValidator)
+        assert isinstance(self.widget.xtol_de.validator(), QtGui.QDoubleValidator)
 
         # bottom value for floats and ints
-        self.assertEqual(self.widget.steps_de.validator().bottom(), 0)
-        self.assertEqual(self.widget.CR_de.validator().bottom(), 0)
+        assert self.widget.steps_de.validator().bottom() == 0
+        assert self.widget.CR_de.validator().bottom() == 0
 
         # Behaviour on empty cell
         self.widget.onAlgorithmChange(3)
@@ -77,23 +77,23 @@ class FittingOptionsTest(unittest.TestCase):
         # Let's put some valid value in lineedit
         self.widget.steps_de.setText("1")
         # This should enable the OK button
-        self.assertTrue(self.widget.buttonBox.button(QtWidgets.QDialogButtonBox.Ok).isEnabled())
+        assert self.widget.buttonBox.button(QtWidgets.QDialogButtonBox.Ok).isEnabled()
 
     def testOnAlgorithmChange(self):
         '''Test the combo box change callback'''
         # Current ID
-        self.assertEqual(self.widget.current_fitter_id, 'lm')
+        assert self.widget.current_fitter_id == 'lm'
         # index = 0
         self.widget.onAlgorithmChange(0)
         # Check Nelder-Mead
-        self.assertEqual(self.widget.stackedWidget.currentIndex(), 1)
-        self.assertEqual(self.widget.current_fitter_id, 'lm')
+        assert self.widget.stackedWidget.currentIndex() == 1
+        assert self.widget.current_fitter_id == 'lm'
 
         # index = 4
         self.widget.onAlgorithmChange(4)
         # Check Levenberg-Marquad
-        self.assertEqual(self.widget.stackedWidget.currentIndex(), 1)
-        self.assertEqual(self.widget.current_fitter_id, 'lm')
+        assert self.widget.stackedWidget.currentIndex() == 1
+        assert self.widget.current_fitter_id == 'lm'
 
     def testOnApply(self):
         '''Test bumps update'''
@@ -108,12 +108,12 @@ class FittingOptionsTest(unittest.TestCase):
         # Apply the new values
         self.widget.onApply()
 
-        self.assertEqual(spy_apply.count(), 1)
-        self.assertIn('DREAM', spy_apply.called()[0]['args'][0])
+        assert spy_apply.count() == 1
+        assert 'DREAM' in spy_apply.called()[0]['args'][0]
 
         # Check the parameters
-        self.assertEqual(options.FIT_CONFIG.values['dream']['steps'], 50.0)
-        self.assertEqual(options.FIT_CONFIG.values['dream']['init'], 'cov')
+        assert options.FIT_CONFIG.values['dream']['steps'] == 50.0
+        assert options.FIT_CONFIG.values['dream']['init'] == 'cov'
 
     # test disabled until pyQt5 works well
     @pytest.mark.skip(reason="2022-09 already broken - causes test suite hang")
@@ -124,39 +124,39 @@ class FittingOptionsTest(unittest.TestCase):
         # Invoke the action on default tab
         self.widget.onHelp()
         # Check if show() got called
-        self.assertTrue(webbrowser.open.called)
+        assert webbrowser.open.called
         # Assure the filename is correct
-        self.assertIn("optimizer.html", webbrowser.open.call_args[0][0])
+        assert "optimizer.html" in webbrowser.open.call_args[0][0]
 
         # Change the combo index
         self.widget.cbAlgorithm.setCurrentIndex(2)
         self.widget.onHelp()
         # Check if show() got called
-        self.assertEqual(webbrowser.open.call_count, 2)
+        assert webbrowser.open.call_count == 2
         # Assure the filename is correct
-        self.assertIn("fit-dream", webbrowser.open.call_args[0][0])
+        assert "fit-dream" in webbrowser.open.call_args[0][0]
 
         # Change the index again
         self.widget.cbAlgorithm.setCurrentIndex(4)
         self.widget.onHelp()
         # Check if show() got called
-        self.assertEqual(webbrowser.open.call_count, 3)
+        assert webbrowser.open.call_count == 3
         # Assure the filename is correct
-        self.assertIn("fit-lm", webbrowser.open.call_args[0][0])
+        assert "fit-lm" in webbrowser.open.call_args[0][0]
 
     def testWidgetFromOptions(self):
         '''Test the helper function'''
         # test empty call
-        self.assertIsNone(self.widget.widgetFromOption(None))
+        assert self.widget.widgetFromOption(None) is None
         # test silly call
-        self.assertIsNone(self.widget.widgetFromOption('poop'))
-        self.assertIsNone(self.widget.widgetFromOption(QtWidgets.QMainWindow()))
+        assert self.widget.widgetFromOption('poop') is None
+        assert self.widget.widgetFromOption(QtWidgets.QMainWindow()) is None
 
         # Switch to DREAM
         self.widget.cbAlgorithm.setCurrentIndex(2)
         # test smart call
-        self.assertIsInstance(self.widget.widgetFromOption('samples'), QtWidgets.QLineEdit)
-        self.assertIsInstance(self.widget.widgetFromOption('init'), QtWidgets.QComboBox)
+        assert isinstance(self.widget.widgetFromOption('samples'), QtWidgets.QLineEdit)
+        assert isinstance(self.widget.widgetFromOption('init'), QtWidgets.QComboBox)
 
     def testUpdateWidgetFromBumps(self):
         '''Test the widget update'''
@@ -169,10 +169,10 @@ class FittingOptionsTest(unittest.TestCase):
         self.widget.updateWidgetFromBumps('newton')
 
         # See that the widget picked up the right values
-        self.assertEqual(self.widget.steps_newton.text(), '1234')
-        self.assertEqual(self.widget.starts_newton.text(), '666')
-        self.assertEqual(self.widget.ftol_newton.text(), '1e-06') # default
-        self.assertEqual(self.widget.xtol_newton.text(), '0.01')
+        assert self.widget.steps_newton.text() == '1234'
+        assert self.widget.starts_newton.text() == '666'
+        assert self.widget.ftol_newton.text() == '1e-06' # default
+        assert self.widget.xtol_newton.text() == '0.01'
 
 if __name__ == "__main__":
     unittest.main()
