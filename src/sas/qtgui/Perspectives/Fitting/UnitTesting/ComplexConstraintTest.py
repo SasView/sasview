db5fcefdb5f4f3f4a2bf9b7c265657add555093a
Stuart Prescott <stuart@debian.org>
2022-09-29 00:47:39 +0000

    Change tests to asserts in ComplexConstraintTest.py

--- src/sas/qtgui/Perspectives/Fitting/UnitTesting/ComplexConstraintTest.py
+++ src/sas/qtgui/Perspectives/Fitting/UnitTesting/ComplexConstraintTest.py
@@ -7,9 +7,6 @@ from unittest.mock import MagicMock, patch
 
 from PyQt5 import QtGui, QtWidgets, QtCore, QtTest
 
-# set up import paths
-import path_prepare
-
 from sas.qtgui.Perspectives.Fitting import FittingUtilities
 from sas.qtgui.Perspectives.Fitting.Constraint import Constraint
 from sas.qtgui.Perspectives.Fitting.ConstraintWidget import ConstraintWidget
@@ -74,38 +71,38 @@ class ComplexConstraintTest(unittest.TestCase):
 
     def testDefaults(self):
         '''Test the GUI in its default state'''
-        self.assertIsInstance(self.widget, QtWidgets.QDialog)
+        assert isinstance(self.widget, QtWidgets.QDialog)
         # Default title
-        self.assertEqual(self.widget.windowTitle(), "Complex Constraint")
+        assert self.widget.windowTitle() == "Complex Constraint"
 
         # Modal window
-        self.assertTrue(self.widget.isModal())
+        assert self.widget.isModal()
 
         # initial tab names
-        self.assertEqual(self.widget.tab_names, ['M1', 'M2', 'M3'])
-        self.assertIn('scale', self.widget.params[0])
-        self.assertIn('background', self.widget.params[1])
+        assert self.widget.tab_names == ['M1', 'M2', 'M3']
+        assert 'scale' in self.widget.params[0]
+        assert 'background' in self.widget.params[1]
 
     def testLabels(self):
         ''' various labels on the widget '''
         # params related setup
-        self.assertEqual(self.widget.txtConstraint.text(), 'M1.scale')
-        self.assertEqual(self.widget.txtOperator.text(), '=')
-        self.assertEqual(self.widget.cbModel1.currentText(), 'M1')
-        self.assertEqual(self.widget.cbModel2.currentText(), 'M1')
+        assert self.widget.txtConstraint.text() == 'M1.scale'
+        assert self.widget.txtOperator.text() == '='
+        assert self.widget.cbModel1.currentText() == 'M1'
+        assert self.widget.cbModel2.currentText() == 'M1'
         # no parameter has been selected for fitting, so left combobox should contain empty text
-        self.assertEqual(self.widget.cbParam1.currentText(), 'scale')
-        self.assertEqual(self.widget.cbParam2.currentText(), 'scale')
+        assert self.widget.cbParam1.currentText() == 'scale'
+        assert self.widget.cbParam2.currentText() == 'scale'
         # now select a parameter for fitting (M1.scale)
         self.tab1._model_model.item(0, 0).setCheckState(QtCore.Qt.Checked)
         # reload widget comboboxes
         self.widget.setupParamWidgets()
         # M1.scale has been selected for fit, should now appear in left combobox
-        self.assertEqual(self.widget.cbParam1.currentText(), 'scale')
+        assert self.widget.cbParam1.currentText() == 'scale'
         # change model in right combobox
         index = self.widget.cbModel2.findText('M2')
         self.widget.cbModel2.setCurrentIndex(index)
-        self.assertEqual(self.widget.cbModel2.currentText(), 'M2')
+        assert self.widget.cbModel2.currentText() == 'M2'
         # add a constraint (M1:scale = M2.scale)
         model, constraint = self.widget.constraint()
         self.tab1.addConstraintToRow(constraint, 0)
@@ -113,13 +110,13 @@ class ComplexConstraintTest(unittest.TestCase):
         index = self.widget.cbModel2.findText('M1')
         self.widget.cbModel2.setCurrentIndex(index)
         self.widget.setupParamWidgets()
-        self.assertEqual(self.widget.cbParam2.currentText(), 'background')
+        assert self.widget.cbParam2.currentText() == 'background'
 
     def testTooltip(self):
         ''' test the tooltip'''
         tooltip = "E.g. M1:scale = 2.0 * M2.scale\n"
         tooltip += "M1:scale = sqrt(M2.scale) + 5"
-        self.assertEqual(self.widget.txtConstraint.toolTip(), tooltip)
+        assert self.widget.txtConstraint.toolTip() == tooltip
 
     def notestValidateFormula(self):
         ''' assure enablement and color for valid formula '''
@@ -127,45 +124,45 @@ class ComplexConstraintTest(unittest.TestCase):
         self.widget.validateConstraint = MagicMock(return_value=False)
         self.widget.validateFormula()
         style_sheet = "QLineEdit {background-color: red;}"
-        self.assertFalse(self.widget.cmdOK.isEnabled())
-        self.assertEqual(self.widget.txtConstraint.styleSheet(),style_sheet)
+        assert not self.widget.cmdOK.isEnabled()
+        assert self.widget.txtConstraint.styleSheet() == style_sheet
 
         # Valid string
         self.widget.validateConstraint = MagicMock(return_value=True)
         self.widget.validateFormula()
         style_sheet = "QLineEdit {background-color: white;}"
-        self.assertTrue(self.widget.cmdOK.isEnabled())
-        self.assertEqual(self.widget.txtConstraint.styleSheet(),style_sheet)
+        assert self.widget.cmdOK.isEnabled()
+        assert self.widget.txtConstraint.styleSheet() == style_sheet
 
     def testValidateConstraint(self):
         ''' constraint validator test'''
         #### BAD
         # none
-        self.assertFalse(self.widget.validateConstraint(None))
+        assert not self.widget.validateConstraint(None)
         # inf
-        self.assertFalse(self.widget.validateConstraint(np.inf))
+        assert not self.widget.validateConstraint(np.inf)
         # 0
-        self.assertFalse(self.widget.validateConstraint(0))
+        assert not self.widget.validateConstraint(0)
         # ""
-        self.assertFalse(self.widget.validateConstraint(""))
+        assert not self.widget.validateConstraint("")
         # p2_
-        self.assertFalse(self.widget.validateConstraint("M2.scale_"))
+        assert not self.widget.validateConstraint("M2.scale_")
         # p1
-        self.assertFalse(self.widget.validateConstraint("M2.scale"))
+        assert not self.widget.validateConstraint("M2.scale")
 
         ### GOOD
         # p2
-        self.assertTrue(self.widget.validateConstraint("scale"))
+        assert self.widget.validateConstraint("scale")
         # " p2    "
-        self.assertTrue(self.widget.validateConstraint(" scale    "))
+        assert self.widget.validateConstraint(" scale    ")
         # sqrt(p2)
-        self.assertTrue(self.widget.validateConstraint("sqrt(scale)"))
+        assert self.widget.validateConstraint("sqrt(scale)")
         # -p2
-        self.assertTrue(self.widget.validateConstraint("-scale"))
+        assert self.widget.validateConstraint("-scale")
         # log10(p2) - sqrt(p2) + p2
-        self.assertTrue(self.widget.validateConstraint("log10(scale) - sqrt(scale) + scale"))
+        assert self.widget.validateConstraint("log10(scale) - sqrt(scale) + scale")
         # log10(    p2    ) +  p2
-        self.assertTrue(self.widget.validateConstraint("log10(    scale    ) +  scale  "))
+        assert self.widget.validateConstraint("log10(    scale    ) +  scale  ")
 
     def testConstraint(self):
         """
@@ -173,15 +170,15 @@ class ComplexConstraintTest(unittest.TestCase):
         """
         # default data
         c = self.widget.constraint()
-        self.assertEqual(c[0], 'M1')
-        self.assertEqual(c[1].func, 'M1.scale')
+        assert c[0] == 'M1'
+        assert c[1].func == 'M1.scale'
 
         # Change parameter and operand
         #self.widget.cbOperator.setCurrentIndex(3)
         self.widget.cbParam2.setCurrentIndex(3)
         c = self.widget.constraint()
-        self.assertEqual(c[0], 'M1')
-        self.assertEqual(c[1].func, 'M1.bjerrum_length')
+        assert c[0] == 'M1'
+        assert c[1].func == 'M1.bjerrum_length'
         #self.assertEqual(c[1].operator, '>=')
 
     def notestOnHelp(self):
@@ -206,7 +203,7 @@ class ComplexConstraintTest(unittest.TestCase):
                           self.widget.constraintReadySignal)
         QtTest.QTest.mouseClick(self.widget.cmdAddAll, QtCore.Qt.LeftButton)
         # Only two constraints should've been added: scale and background
-        self.assertEqual(spy.count(), 2)
+        assert spy.count() == 2
 
 
     def testOnApply(self):
@@ -220,9 +217,9 @@ class ComplexConstraintTest(unittest.TestCase):
         spy = QtSignalSpy(self.widget,
                           self.widget.constraintReadySignal)
         QtTest.QTest.mouseClick(self.widget.cmdOK, QtCore.Qt.LeftButton)
-        self.assertEqual(spy.count(), 1)
-        self.assertEqual(spy.signal(0)[0][0], "M1")
-        self.assertTrue(isinstance(spy.signal(0)[0][1], Constraint))
+        assert spy.count() == 1
+        assert spy.signal(0)[0][0] == "M1"
+        assert isinstance(spy.signal(0)[0][1], Constraint)
 
         # Test the `All` option in the combobox
         self.widget.applyAcrossTabs = MagicMock()
@@ -246,4 +243,4 @@ class ComplexConstraintTest(unittest.TestCase):
         expr = "M3.scale"
         self.widget.applyAcrossTabs(tabs, param, expr)
         # We should have two calls
-        self.assertEqual(spy.count(), 2)
+        assert spy.count() == 2
