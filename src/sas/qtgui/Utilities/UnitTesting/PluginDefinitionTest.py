8bdda4fc9d637c1551454a53bc705fc64b79f5b3
Stuart Prescott <stuart@debian.org>
2022-09-29 14:49:08 +0000

    Change to pytest fixtures PluginDefinitionTest.py

--- src/sas/qtgui/Utilities/UnitTesting/PluginDefinitionTest.py
+++ src/sas/qtgui/Utilities/UnitTesting/PluginDefinitionTest.py
@@ -1,13 +1,11 @@
 import sys
-import unittest
 import logging
+
+import pytest
 
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
-
-# set up import paths
-import sas.qtgui.path_prepare
 
 from sas.qtgui.UnitTesting.TestUtils import QtSignalSpy
 
@@ -15,160 +13,153 @@ from sas.qtgui.UnitTesting.TestUtils import QtSignalSpy
 from sas.qtgui.Utilities.PluginDefinition import PluginDefinition
 from sas.qtgui.Utilities.PythonSyntax import PythonHighlighter
 
-if not QApplication.instance():
-    app = QApplication(sys.argv)
 
-class PluginDefinitionTest(unittest.TestCase):
-    def setUp(self):
-        """
-        Prepare the editor
-        """
-        self.widget = PluginDefinition(None)
+class PluginDefinitionTest:
+    @pytest.fixture(autouse=True)
+    def widget(self, qapp):
+        '''Create/Destroy the PluginDefinition'''
+        w = PluginDefinition(None)
+        yield w
+        w.close()
 
-    def tearDown(self):
-        """Destroy the DataOperationUtility"""
-        self.widget.close()
-        self.widget = None
-
-    def testDefaults(self):
+    def testDefaults(self, widget):
         """Test the GUI in its default state"""
-        assert isinstance(self.widget.highlight, PythonHighlighter)
-        assert isinstance(self.widget.parameter_dict, dict)
-        assert isinstance(self.widget.pd_parameter_dict, dict)
+        assert isinstance(widget.highlight, PythonHighlighter)
+        assert isinstance(widget.parameter_dict, dict)
+        assert isinstance(widget.pd_parameter_dict, dict)
 
-        assert len(self.widget.model) == 6
-        assert "filename" in self.widget.model.keys()
-        assert "overwrite" in self.widget.model.keys()
-        assert "description" in self.widget.model.keys()
-        assert "parameters" in self.widget.model.keys()
-        assert "pd_parameters" in self.widget.model.keys()
-        assert "text" in self.widget.model.keys()
+        assert len(widget.model) == 6
+        assert "filename" in widget.model.keys()
+        assert "overwrite" in widget.model.keys()
+        assert "description" in widget.model.keys()
+        assert "parameters" in widget.model.keys()
+        assert "pd_parameters" in widget.model.keys()
+        assert "text" in widget.model.keys()
 
-    def testOnOverwrite(self):
+    def testOnOverwrite(self, widget):
         """See what happens when the overwrite checkbox is selected"""
-        spy_signal = QtSignalSpy(self.widget, self.widget.modelModified)
+        spy_signal = QtSignalSpy(widget, widget.modelModified)
 
         # check the default
-        assert not self.widget.chkOverwrite.isChecked()
+        assert not widget.chkOverwrite.isChecked()
 
         # Change the state
-        self.widget.chkOverwrite.setChecked(True)
+        widget.chkOverwrite.setChecked(True)
 
         # Check the signal
         assert spy_signal.count() == 1
 
         # model dict updated
-        assert self.widget.model['overwrite']
+        assert widget.model['overwrite']
 
-    def testOnPluginNameChanged(self):
+    def testOnPluginNameChanged(self, widget):
         """See what happens when the name of the plugin changes"""
-        spy_signal = QtSignalSpy(self.widget, self.widget.modelModified)
+        spy_signal = QtSignalSpy(widget, widget.modelModified)
 
         # Change the name
         new_text = "Duck season"
-        self.widget.txtName.setText(new_text)
-        self.widget.txtName.editingFinished.emit()
+        widget.txtName.setText(new_text)
+        widget.txtName.editingFinished.emit()
 
         # Check the signal
         assert spy_signal.count() == 1
 
         # model dict updated
-        assert self.widget.model['filename'] == new_text
+        assert widget.model['filename'] == new_text
 
-    def testOnDescriptionChanged(self):
+    def testOnDescriptionChanged(self, widget):
         """See what happens when the description of the plugin changes"""
-        spy_signal = QtSignalSpy(self.widget, self.widget.modelModified)
+        spy_signal = QtSignalSpy(widget, widget.modelModified)
 
         # Change the name
         new_text = "Rabbit season"
-        self.widget.txtDescription.setText(new_text)
-        self.widget.txtDescription.editingFinished.emit()
+        widget.txtDescription.setText(new_text)
+        widget.txtDescription.editingFinished.emit()
 
         # Check the signal
         assert spy_signal.count() == 1
 
         # model dict updated
-        assert self.widget.model['description'] == new_text
+        assert widget.model['description'] == new_text
 
-    def testOnParamsChanged(self):
+    def testOnParamsChanged(self, widget):
         """See what happens when parameters change"""
-        spy_signal = QtSignalSpy(self.widget, self.widget.modelModified)
+        spy_signal = QtSignalSpy(widget, widget.modelModified)
 
         # number of rows. default=1
-        assert self.widget.tblParams.rowCount() == 1
+        assert widget.tblParams.rowCount() == 1
 
         # number of columns. default=2
-        assert self.widget.tblParams.columnCount() == 2
+        assert widget.tblParams.columnCount() == 2
 
         # Change the param
         new_param = "shoot"
-        self.widget.tblParams.setItem(0,0,QTableWidgetItem(new_param))
-        #self.widget.tblParams.editingFinished.emit()
+        widget.tblParams.setItem(0,0,QTableWidgetItem(new_param))
+        #widget.tblParams.editingFinished.emit()
 
 
         # Check the signal
         assert spy_signal.count() == 1
 
         # model dict updated
-        assert self.widget.model['parameters'] == {0: (new_param, None)}
+        assert widget.model['parameters'] == {0: (new_param, None)}
 
         # Change the value
         new_value = "BOOM"
-        self.widget.tblParams.setItem(0,1,QTableWidgetItem(new_value))
+        widget.tblParams.setItem(0,1,QTableWidgetItem(new_value))
 
         # Check the signal
         assert spy_signal.count() == 2
 
         # model dict updated
-        assert self.widget.model['parameters'] == {0: (new_param, new_value)}
+        assert widget.model['parameters'] == {0: (new_param, new_value)}
 
         # See that the number of rows increased
-        assert self.widget.tblParams.rowCount() == 2
+        assert widget.tblParams.rowCount() == 2
 
-    def testOnPDParamsChanged(self):
+    def testOnPDParamsChanged(self, widget):
         """See what happens when pd parameters change"""
-        spy_signal = QtSignalSpy(self.widget, self.widget.modelModified)
+        spy_signal = QtSignalSpy(widget, widget.modelModified)
 
         # number of rows. default=1
-        assert self.widget.tblParamsPD.rowCount() == 1
+        assert widget.tblParamsPD.rowCount() == 1
 
         # number of columns. default=2
-        assert self.widget.tblParamsPD.columnCount() == 2
+        assert widget.tblParamsPD.columnCount() == 2
 
         # Change the param
         new_param = "shoot"
-        self.widget.tblParamsPD.setItem(0,0,QTableWidgetItem(new_param))
+        widget.tblParamsPD.setItem(0,0,QTableWidgetItem(new_param))
 
         # Check the signal
         assert spy_signal.count() == 1
 
         # model dict updated
-        assert self.widget.model['pd_parameters'] == {0: (new_param, None)}
+        assert widget.model['pd_parameters'] == {0: (new_param, None)}
 
         # Change the value
         new_value = "BOOM"
-        self.widget.tblParamsPD.setItem(0,1,QTableWidgetItem(new_value))
+        widget.tblParamsPD.setItem(0,1,QTableWidgetItem(new_value))
 
         # Check the signal
         assert spy_signal.count() == 2
 
         # model dict updated
-        assert self.widget.model['pd_parameters'] == {0: (new_param, new_value)}
+        assert widget.model['pd_parameters'] == {0: (new_param, new_value)}
 
         # See that the number of rows increased
-        assert self.widget.tblParamsPD.rowCount() == 2
+        assert widget.tblParamsPD.rowCount() == 2
 
-    def testOnFunctionChanged(self):
+    def testOnFunctionChanged(self, widget):
         """See what happens when the model text changes"""
-        spy_signal = QtSignalSpy(self.widget, self.widget.modelModified)
+        spy_signal = QtSignalSpy(widget, widget.modelModified)
 
         # Change the text
         new_model = "1\n2\n"
-        self.widget.txtFunction.setPlainText(new_model)
+        widget.txtFunction.setPlainText(new_model)
 
         # Check the signal
         assert spy_signal.count() == 1
 
         # model dict updated
-        assert self.widget.model['text'] == new_model.rstrip()
-
+        assert widget.model['text'] == new_model.rstrip()
