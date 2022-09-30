49604cea217170d2b098a6f5911c1e9f546269bd
Stuart Prescott <stuart@debian.org>
2022-09-30 02:08:50 +0000

    Change to pytest fixtures AddTextTest.py

--- src/sas/qtgui/Plotting/UnitTesting/AddTextTest.py
+++ src/sas/qtgui/Plotting/UnitTesting/AddTextTest.py
@@ -1,5 +1,4 @@
 import sys
-import unittest
 
 import pytest
 
@@ -13,54 +12,48 @@ import sas.qtgui.path_prepare
 # Local
 from sas.qtgui.Plotting.AddText import AddText
 
-if not QtWidgets.QApplication.instance():
-    app = QtWidgets.QApplication(sys.argv)
 
-class AddTextTest(unittest.TestCase):
+class AddTextTest:
     '''Test the AddText'''
-    def setUp(self):
-        '''Create the AddText'''
-        self.widget = AddText(None)
 
-    def tearDown(self):
-        '''Destroy the GUI'''
-        self.widget.close()
-        self.widget = None
+    @pytest.fixture(autouse=True)
+    def widget(self, qapp):
+        '''Create/Destroy the AddText'''
+        w = AddText(None)
+        yield w
+        w.close()
 
-    def testDefaults(self):
+    def testDefaults(self, widget):
         '''Test the GUI in its default state'''
-        assert isinstance(self.widget, QtWidgets.QDialog)
-        assert isinstance(self.widget._font, QtGui.QFont)
-        assert self.widget._color == "black"
+        assert isinstance(widget, QtWidgets.QDialog)
+        assert isinstance(widget._font, QtGui.QFont)
+        assert widget._color == "black"
         
     @pytest.mark.xfail(reason="2022-09 already broken")
-    def testOnFontChange(self):
+    def testOnFontChange(self, widget):
         '''Test the QFontDialog output'''
         font_1 = QtGui.QFont("Helvetica", 15)
         QtWidgets.QFontDialog.getFont = MagicMock(return_value=(font_1, True))
         # Call the method
-        self.widget.onFontChange(None)
+        widget.onFontChange(None)
         # Check that the text field got the new font info
-        assert self.widget.textEdit.currentFont() == font_1
+        assert widget.textEdit.currentFont() == font_1
 
         # See that rejecting the dialog doesn't modify the font
         font_2 = QtGui.QFont("Arial", 9)
         QtWidgets.QFontDialog.getFont = MagicMock(return_value=(font_2, False))
         # Call the method
-        self.widget.onFontChange(None)
+        widget.onFontChange(None)
         # Check that the text field retained the previous font info
-        assert self.widget.textEdit.currentFont() == font_1
+        assert widget.textEdit.currentFont() == font_1
 
-    def testOnColorChange(self):
+    def testOnColorChange(self, widget):
         ''' Test the QColorDialog output'''
         new_color = QtGui.QColor("red")
         QtWidgets.QColorDialog.getColor = MagicMock(return_value=new_color)
         # Call the method
-        self.widget.onColorChange(None)
+        widget.onColorChange(None)
         # Check that the text field got the new color info for text
-        assert self.widget.textEdit.palette().color(QtGui.QPalette.Text) == new_color
+        assert widget.textEdit.palette().color(QtGui.QPalette.Text) == new_color
         # ... and the hex value of this color is correct
-        assert self.widget.color() == "#ff0000"
-        
-if __name__ == "__main__":
-    unittest.main()
+        assert widget.color() == "#ff0000"
