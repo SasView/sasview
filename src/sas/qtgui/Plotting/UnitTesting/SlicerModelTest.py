d20a036ff657c27d8d7e843b7c8830a3219897ec
Stuart Prescott <stuart@debian.org>
2022-09-30 07:13:54 +0000

    Change to pytest fixtures SlicerModelTest.py

--- src/sas/qtgui/Plotting/UnitTesting/SlicerModelTest.py
+++ src/sas/qtgui/Plotting/UnitTesting/SlicerModelTest.py
@@ -1,5 +1,4 @@
 import sys
-import unittest
 
 import pytest
 
@@ -8,19 +7,16 @@ from unittest.mock import MagicMock
 from PyQt5 import QtGui, QtWidgets
 from PyQt5 import QtCore
 
-# set up import paths
-import sas.qtgui.path_prepare
-
 # Local
 from sas.qtgui.Plotting.SlicerModel import SlicerModel
 
-if not QtWidgets.QApplication.instance():
-    app = QtWidgets.QApplication(sys.argv)
 
-class SlicerModelTest(unittest.TestCase):
+class SlicerModelTest:
     '''Test the SlicerModel'''
-    def setUp(self):
-        '''Create the SlicerModel'''
+
+    @pytest.fixture(autouse=True)
+    def model(self, qapp):
+        '''Create/Destroy the SlicerModel'''
         class SModel(SlicerModel):
             params = {"a":1, "b":2}
             def __init__(self):
@@ -29,13 +25,11 @@ class SlicerModelTest(unittest.TestCase):
                 return self.params
             def setParams(self, par):
                 self.params = par
-        self.model = SModel()
+        m = SModel()
 
-    def tearDown(self):
-        '''Destroy the model'''
-        self.model = None
+        yield m
 
-    def testBaseClass(self):
+    def testBaseClass(self, qapp):
         '''Assure that SlicerModel contains pure virtuals'''
         model = SlicerModel()
         with pytest.raises(NotImplementedError):
@@ -43,39 +37,35 @@ class SlicerModelTest(unittest.TestCase):
         with pytest.raises(NotImplementedError):
             model.setModelFromParams()
 
-    def testDefaults(self):
+    def testDefaults(self, model):
         '''Test the GUI in its default state'''
-        assert isinstance(self.model.model(), QtGui.QStandardItemModel)
+        assert isinstance(model.model(), QtGui.QStandardItemModel)
 
-    def testSetModelFromParams(self):
+    def testSetModelFromParams(self, model):
         '''Test the model update'''
         # Add a row to params
-        new_dict = self.model.getParams()
+        new_dict = model.getParams()
         new_dict["c"] = 3
-        self.model.setParams(new_dict)
+        model.setParams(new_dict)
 
         # Call the update
-        self.model.setModelFromParams()
+        model.setModelFromParams()
 
         # Check the new model.
-        assert self.model.model().rowCount() == 3
-        assert self.model.model().columnCount() == 2
+        assert model.model().rowCount() == 3
+        assert model.model().columnCount() == 2
 
-    def testSetParamsFromModel(self):
+    def testSetParamsFromModel(self, model):
         ''' Test the parameters update'''
         # First - the default model
-        self.model.setModelFromParams()
-        assert self.model.model().rowCount() == 2
-        assert self.model.model().columnCount() == 2
+        model.setModelFromParams()
+        assert model.model().rowCount() == 2
+        assert model.model().columnCount() == 2
 
         # Add a row
         item1 = QtGui.QStandardItem("c")
         item2 = QtGui.QStandardItem(3)
-        self.model.model().appendRow([item1, item2])
+        model.model().appendRow([item1, item2])
         # Check the new model. The update should be automatic
-        assert self.model.model().rowCount() == 3
-        assert self.model.model().columnCount() == 2
-
-
-if __name__ == "__main__":
-    unittest.main()
+        assert model.model().rowCount() == 3
+        assert model.model().columnCount() == 2
