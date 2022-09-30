56e20f9b686646f066a36103f8fc005b90b75172
Stuart Prescott <stuart@debian.org>
2022-09-30 03:00:03 +0000

    Change tests to asserts in PlotHelperTest.py

--- src/sas/qtgui/Plotting/UnitTesting/PlotHelperTest.py
+++ src/sas/qtgui/Plotting/UnitTesting/PlotHelperTest.py
@@ -15,8 +15,8 @@ class PlotHelperTest(unittest.TestCase):
 
     def testDefaults(self):
         """ default method variables values """
-        self.assertIsInstance(PlotHelper._plots, dict)
-        self.assertEqual(PlotHelper._plots, {})
+        assert isinstance(PlotHelper._plots, dict)
+        assert PlotHelper._plots == {}
         # could have leftovers from previous tests
         #self.assertEqual(PlotHelper._plot_id, 0)
 
@@ -34,17 +34,17 @@ class PlotHelperTest(unittest.TestCase):
         plot_id_2 = PlotHelper.idOfPlot(plot2)
         id1 = int(plot_id[-1])
         id2 = int(plot_id_2[-1])
-        self.assertEqual(id2 - id1, 1)
+        assert id2 - id1 == 1
 
         # Other properties
         #self.assertEqual(PlotHelper.currentPlots(), [plot_id, plot_id_2])
-        self.assertTrue(set(PlotHelper.currentPlotIds()).issubset([plot_id, plot_id_2]))
-        self.assertEqual(PlotHelper.plotById(plot_id), plot)
-        self.assertEqual(PlotHelper.plotById(plot_id_2), plot2)
+        assert set(PlotHelper.currentPlotIds()).issubset([plot_id, plot_id_2])
+        assert PlotHelper.plotById(plot_id) == plot
+        assert PlotHelper.plotById(plot_id_2) == plot2
 
         # Delete a graph
         PlotHelper.deletePlot(plot_id)
-        self.assertEqual(PlotHelper.currentPlotIds(), [plot_id_2])
+        assert PlotHelper.currentPlotIds() == [plot_id_2]
 
         # Add another graph to see the counter
         plot3 = "Just another plot. Move along."
