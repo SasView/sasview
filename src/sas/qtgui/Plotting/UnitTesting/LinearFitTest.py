fbde2f5a695945793509d4bd623acd122fca9a20
Stuart Prescott <stuart@debian.org>
2022-09-30 02:59:16 +0000

    Change to pytest fixtures LinearFitTest.py

--- src/sas/qtgui/Plotting/UnitTesting/LinearFitTest.py
+++ src/sas/qtgui/Plotting/UnitTesting/LinearFitTest.py
@@ -1,6 +1,7 @@
 import sys
-import unittest
 import numpy
+
+import pytest
 
 import os
 os.environ["MPLBACKEND"] = "qtagg"
@@ -10,90 +11,87 @@ from unittest.mock import MagicMock
 
 from sas.qtgui.UnitTesting.TestUtils import QtSignalSpy
 
-# set up import paths
-import sas.qtgui.path_prepare
-
 from sas.qtgui.Plotting.PlotterData import Data1D
 import sas.qtgui.Plotting.Plotter as Plotter
 
 # Local
 from sas.qtgui.Plotting.LinearFit import LinearFit
 
-if not QtWidgets.QApplication.instance():
-    app = QtWidgets.QApplication(sys.argv)
 
-class LinearFitTest(unittest.TestCase):
+class LinearFitTest:
     '''Test the LinearFit'''
-    def setUp(self):
-        '''Create the LinearFit'''
-        self.data = Data1D(x=[1.0, 2.0, 3.0],
+
+    @pytest.fixture(autouse=True)
+    def widget(self, qapp):
+        '''Create/Destroy the LinearFit'''
+        data = Data1D(x=[1.0, 2.0, 3.0],
                            y=[10.0, 11.0, 12.0],
                            dx=[0.1, 0.2, 0.3],
                            dy=[0.1, 0.2, 0.3])
         plotter = Plotter.Plotter(None, quickplot=True)
-        plotter.plot(self.data)
-        self.widget = LinearFit(parent=plotter, data=self.data, xlabel="log10(x^2)", ylabel="log10(y)")
+        plotter.plot(data)
+        w = LinearFit(parent=plotter, data=data, xlabel="log10(x^2)", ylabel="log10(y)")
 
-    def tearDown(self):
+        yield w
+
         '''Destroy the GUI'''
-        self.widget.close()
-        self.widget = None
+        w.close()
 
-    def testDefaults(self):
+    def testDefaults(self, widget):
         '''Test the GUI in its default state'''
-        assert isinstance(self.widget, QtWidgets.QDialog)
-        assert self.widget.windowTitle() == "Linear Fit"
-        assert self.widget.txtA.text() == "1"
-        assert self.widget.txtB.text() == "1"
-        assert self.widget.txtAerr.text() == "0"
-        assert self.widget.txtBerr.text() == "0"
+        assert isinstance(widget, QtWidgets.QDialog)
+        assert widget.windowTitle() == "Linear Fit"
+        assert widget.txtA.text() == "1"
+        assert widget.txtB.text() == "1"
+        assert widget.txtAerr.text() == "0"
+        assert widget.txtBerr.text() == "0"
 
-        assert self.widget.lblRange.text() == "Fit range of log10(x^2)"
+        assert widget.lblRange.text() == "Fit range of log10(x^2)"
 
-    def testFit(self):
+    def testFit(self, widget):
         '''Test the fitting wrapper '''
         # Catch the update signal
-        #self.widget.updatePlot.emit = MagicMock()
-        #self.widget.updatePlot.emit = MagicMock()
-        spy_update = QtSignalSpy(self.widget, self.widget.updatePlot)
+        #widget.updatePlot.emit = MagicMock()
+        #widget.updatePlot.emit = MagicMock()
+        spy_update = QtSignalSpy(widget, widget.updatePlot)
 
         # Set some initial values
-        self.widget.txtRangeMin.setText("1.0")
-        self.widget.txtRangeMax.setText("3.0")
-        self.widget.txtFitRangeMin.setText("1.0")
-        self.widget.txtFitRangeMax.setText("3.0")
+        widget.txtRangeMin.setText("1.0")
+        widget.txtRangeMax.setText("3.0")
+        widget.txtFitRangeMin.setText("1.0")
+        widget.txtFitRangeMax.setText("3.0")
         # Run the fitting
-        self.widget.fit(None)
+        widget.fit(None)
 
         # Expected one spy instance
         assert spy_update.count() == 1
 
         return_values = spy_update.called()[0]['args'][0]
         # Compare
-        self.assertCountEqual(return_values[0], [1.0, 3.0])
+        assert sorted(return_values[0]) == [1.0, 3.0]
         assert round(abs(return_values[1][0]-10.004054329), 6) == 0
         assert round(abs(return_values[1][1]-12.030439848), 6) == 0
 
         # Set the log scale
-        self.widget.x_is_log = True
-        self.widget.fit(None)
+        widget.x_is_log = True
+        widget.fit(None)
         assert spy_update.count() == 2
         return_values = spy_update.called()[1]['args'][0]
         # Compare
-        self.assertCountEqual(return_values[0], [1.0, 3.0])
+        assert sorted(return_values[0]) == [1.0, 3.0]
         assert round(abs(return_values[1][0]-9.987732937), 6) == 0
         assert round(abs(return_values[1][1]-11.84365082), 6) == 0
 
-    def testOrigData(self):
+    def testOrigData(self, widget):
         ''' Assure the un-logged data is returned'''
         # log(x), log(y)
-        self.widget.xminFit, self.widget.xmaxFit = self.widget.range()
+        widget.xminFit, widget.xmaxFit = widget.range()
         orig_x = [ 1.,  2.,  3.]
         orig_y = [1.0, 1.0413926851582251, 1.0791812460476249]
         orig_dy = [0.01, 0.018181818181818184, 0.024999999999999998]
-        x, y, dy = self.widget.origData()
+        x, y, dy = widget.origData()
 
-        self.assertCountEqual(x, orig_x)
+        assert sorted(x) == sorted(orig_x)
         assert y[0] == orig_y[0]
         assert round(abs(y[1]-orig_y[1]), 8) == 0
         assert round(abs(y[2]-orig_y[2]), 8) == 0
@@ -102,28 +100,28 @@ class LinearFitTest(unittest.TestCase):
         assert round(abs(dy[2]-orig_dy[2]), 8) == 0
 
         # x, y
-        self.widget.x_is_log = False
-        self.widget.y_is_log = False
-        self.widget.xminFit, self.widget.xmaxFit = self.widget.range()
+        widget.x_is_log = False
+        widget.y_is_log = False
+        widget.xminFit, widget.xmaxFit = widget.range()
         orig_x = [ 1.,  2.,  3.]
         orig_y = [10., 11., 12.]
         orig_dy = [0.1, 0.2, 0.3]
-        x, y, dy = self.widget.origData()
+        x, y, dy = widget.origData()
 
-        self.assertCountEqual(x, orig_x)
-        self.assertCountEqual(y, orig_y)
-        self.assertCountEqual(dy, orig_dy)
+        assert sorted(x) == sorted(orig_x)
+        assert sorted(y) == sorted(orig_y)
+        assert sorted(dy) == sorted(orig_dy)
 
         # x, log(y)
-        self.widget.x_is_log = False
-        self.widget.y_is_log = True
-        self.widget.xminFit, self.widget.xmaxFit = self.widget.range()
+        widget.x_is_log = False
+        widget.y_is_log = True
+        widget.xminFit, widget.xmaxFit = widget.range()
         orig_x = [ 1.,  2.,  3.]
         orig_y = [1.0, 1.0413926851582251, 1.0791812460476249]
         orig_dy = [0.01, 0.018181818181818184, 0.024999999999999998]
-        x, y, dy = self.widget.origData()
+        x, y, dy = widget.origData()
 
-        self.assertCountEqual(x, orig_x)
+        assert sorted(x) == sorted(orig_x)
         assert y[0] == orig_y[0]
         assert round(abs(y[1]-orig_y[1]), 8) == 0
         assert round(abs(y[2]-orig_y[2]), 8) == 0
@@ -131,32 +129,29 @@ class LinearFitTest(unittest.TestCase):
         assert round(abs(dy[1]-orig_dy[1]), 8) == 0
         assert round(abs(dy[2]-orig_dy[2]), 8) == 0
 
-    def testCheckFitValues(self):
+    def testCheckFitValues(self, widget):
         '''Assure fit values are correct'''
         # Good values
-        assert self.widget.checkFitValues(self.widget.txtFitRangeMin)
+        assert widget.checkFitValues(widget.txtFitRangeMin)
         # Colors platform dependent
-        #self.assertEqual(self.widget.txtFitRangeMin.palette().color(10).name(), "#f0f0f0")
+        #self.assertEqual(widget.txtFitRangeMin.palette().color(10).name(), "#f0f0f0")
         # Bad values
-        self.widget.x_is_log = True
-        self.widget.txtFitRangeMin.setText("-1.0")
-        assert not self.widget.checkFitValues(self.widget.txtFitRangeMin)
+        widget.x_is_log = True
+        widget.txtFitRangeMin.setText("-1.0")
+        assert not widget.checkFitValues(widget.txtFitRangeMin)
        
 
-    def testFloatInvTransform(self):
+    def testFloatInvTransform(self, widget):
         '''Test the helper method for providing conversion function'''
-        self.widget.xLabel="x"
-        assert self.widget.floatInvTransform(5.0) == 5.0
-        self.widget.xLabel="x^(2)"
-        assert self.widget.floatInvTransform(25.0) == 5.0
-        self.widget.xLabel="x^(4)"
-        assert self.widget.floatInvTransform(81.0) == 3.0
-        self.widget.xLabel="log10(x)"
-        assert self.widget.floatInvTransform(2.0) == 100.0
-        self.widget.xLabel="ln(x)"
-        assert self.widget.floatInvTransform(1.0) == numpy.exp(1)
-        self.widget.xLabel="log10(x^(4))"
-        assert self.widget.floatInvTransform(4.0) == 10.0
-      
-if __name__ == "__main__":
-    unittest.main()
+        widget.xLabel="x"
+        assert widget.floatInvTransform(5.0) == 5.0
+        widget.xLabel="x^(2)"
+        assert widget.floatInvTransform(25.0) == 5.0
+        widget.xLabel="x^(4)"
+        assert widget.floatInvTransform(81.0) == 3.0
+        widget.xLabel="log10(x)"
+        assert widget.floatInvTransform(2.0) == 100.0
+        widget.xLabel="ln(x)"
+        assert widget.floatInvTransform(1.0) == numpy.exp(1)
+        widget.xLabel="log10(x^(4))"
+        assert widget.floatInvTransform(4.0) == 10.0
