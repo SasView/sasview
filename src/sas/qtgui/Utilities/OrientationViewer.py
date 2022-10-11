5d30f62ed409618796cb5b4e704c18af9877e071
lucas-wilkins <lucas.wilkins@stfc.ac.uk>
2022-10-11 14:48:48 +0100

    Qt stuff

--- src/sas/qtgui/Utilities/OrientationViewer.py
+++ src/sas/qtgui/Utilities/OrientationViewer.py
@@ -6,14 +6,20 @@ from PyQt5 import QtWidgets
 class OrientationViewer(QtWidgets.QMainWindow, Ui_OrientationViewer):
 
     def __init__(self, parent=None):
-        super().__init__(parent._parent)
+        super().__init__(parent)
 
         self.parent = parent
         self.setupUi(self)
 
 
 def main():
-    pass
+    """ Show a demo of the slider """
+    app = QtWidgets.QApplication([])
+    viewer = OrientationViewer()
+
+    viewer.show()
+    app.exec_()
+
 
 if __name__ == "__main__":
     main()
