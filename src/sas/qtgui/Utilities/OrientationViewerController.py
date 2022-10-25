8ad4d1746c6578e9d58a4c88a38c0bc5337b7f01
lucas-wilkins <lucas.wilkins@stfc.ac.uk>
2022-10-25 14:21:28 +0100

    Test window

--- src/sas/qtgui/Utilities/OrientationViewerController.py
+++ src/sas/qtgui/Utilities/OrientationViewerController.py
@@ -3,7 +3,29 @@ from PyQt5 import QtWidgets
 from sas.qtgui.Utilities.UI.OrientationViewerControllerUI import Ui_OrientationViewierControllerUI
 
 
-class AddMultEditor(QtWidgets.QDialog, Ui_OrientationViewierControllerUI):
+class OrientationViewierController(QtWidgets.QDialog, Ui_OrientationViewierControllerUI):
     def __init__(self, parent=None):
         super().__init__()
 
+        self.setupUi(self)
+
+
+
+
+def main():
+    """ Show a demo of the slider """
+    app = QtWidgets.QApplication([])
+
+    mainWindow = QtWidgets.QMainWindow()
+    viewer = OrientationViewierController(mainWindow)
+
+    mainWindow.setCentralWidget(viewer)
+
+    mainWindow.show()
+
+    mainWindow.resize(700, 100)
+    app.exec_()
+
+
+if __name__ == "__main__":
+    main()
