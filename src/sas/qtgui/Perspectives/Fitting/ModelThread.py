3c077421d0a456efc35f748b19391289212a6eae
Peter Beaucage <peter.beaucage@nist.gov>
2022-10-29 11:57:04 +0200

    Add flag for experimental data and gate plotting on it

--- src/sas/qtgui/Perspectives/Fitting/ModelThread.py
+++ src/sas/qtgui/Perspectives/Fitting/ModelThread.py
@@ -100,6 +100,8 @@ class Calc2D(CalcThread):
         output[index_model] = value
         elapsed = time.time() - self.starttime
 
+        setattr(self.data,'isExpData',True)
+
         res = dict(image = output, data = self.data, page_id = self.page_id,
             model = self.model, state = self.state,
             toggle_mode_on = self.toggle_mode_on, elapsed = elapsed,
@@ -253,6 +255,8 @@ class Calc1D(CalcThread):
                 intermediate_results = {}
 
         elapsed = time.time() - self.starttime
+
+        setattr(self.data,'isExpData',True)
 
         res = dict(x = self.data.x[index], y = output[index],
             page_id = self.page_id, state = self.state, weight = self.weight,
