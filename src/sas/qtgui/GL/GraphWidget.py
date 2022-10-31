57d2620907d3cc11c1837b7451b718ca7ed3757d
lucas-wilkins <lucas.wilkins@stfc.ac.uk>
2022-10-31 15:53:22 +0000

    This is why working with opengl sucks

--- src/sas/qtgui/GL/GraphWidget.py
+++ src/sas/qtgui/GL/GraphWidget.py
@@ -1,7 +1,7 @@
 from typing import Optional, Tuple
 import numpy as np
 
-from PyQt5 import QtWidgets, Qt, QtGui, QtOpenGL
+from PyQt5 import QtWidgets, Qt, QtGui, QtOpenGL, QtCore
 
 from OpenGL.GL import *
 from OpenGL.GLU import *
@@ -20,6 +20,18 @@ class GraphWidget(QtOpenGL.QGLWidget):
 
         self.background_color = (0, 0, 0, 0)
 
+        # Mouse control settings
+        self.mouse_sensitivity_azimuth = 0.01
+        self.mouse_sensitivity_elevation = 0.01
+        self.mouse_sensitivity_distance = 1.0
+        self.mouse_sensitivity_position = 1.0
+
+        # Mouse control variables
+        self.mouse_position = None
+        self.view_centre_difference = None
+        self.view_azimuth_difference = None
+        self.view_elevation_difference = None
+
     def test_paint(self):
         glTranslatef(-2.5, 0.5, -6.0)
         glColor3f( 1.0, 1.5, 0.0 )
@@ -36,11 +48,6 @@ class GraphWidget(QtOpenGL.QGLWidget):
         glDepthFunc(GL_LESS)
         glEnable(GL_DEPTH_TEST)
         glShadeModel(GL_SMOOTH)
-        glMatrixMode(GL_PROJECTION)
-        glLoadIdentity()
-        gluPerspective(45.0,1.33,0.1, 100.0)
-        glMatrixMode(GL_MODELVIEW)
-
 
     def default_viewport(self):
         return 0, 0, int(self.width() * self.devicePixelRatioF()), int(self.height() * self.devicePixelRatioF())
@@ -49,17 +56,15 @@ class GraphWidget(QtOpenGL.QGLWidget):
         """
         Paint the GL viewport
         """
-        # glViewport(*self.default_viewport())
 
         self.set_projection()
-        # self.set_model_view()
+        self.set_model_view()
 
         glClearColor(*self.background_color)
         glClear( GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT )
 
         self.test_paint()
 
-        # self.drawItemTree(useItemNames=useItemNames)
 
     def projection_matrix(self):
         x0, y0, w, h = self.default_viewport()
@@ -75,20 +80,62 @@ class GraphWidget(QtOpenGL.QGLWidget):
         return tr
 
     def set_projection(self):
+
         glMatrixMode(GL_PROJECTION)
-        glLoadMatrixf(np.array(self.projection_matrix().data(), dtype=np.float32))
+        glLoadIdentity()
+        gluPerspective(45.0,1.33,0.1, 100.0)
+        # glLoadMatrixf(np.array(self.projection_matrix().data(), dtype=np.float32))
 
     def view_matrix(self):
         tr = QtGui.QMatrix4x4()
-        tr.translate( 0.0, 0.0, -self.view_distance)
-        tr.rotate(self.view_elevation-90, 1, 0, 0)
-        tr.rotate(self.view_azimuth+90, 0, 0, -1)
+        tr.translate(0.0, 0.0, -self.view_distance)
+
+        azimuth = self.view_azimuth + 90
+        elevation = self.view_elevation - 90
+
+        if self.view_azimuth_difference is not None:
+            azimuth += self.view_azimuth_difference
+
+        if self.view_elevation_difference is not None:
+            elevation += self.view_elevation_difference
+
+        tr.rotate(elevation, 1, 0, 0)
+        tr.rotate(azimuth, 0, 0, -1)
         tr.translate(*self.view_centre)
         return tr
 
     def set_model_view(self):
         glMatrixMode(GL_MODELVIEW)
         glLoadMatrixf(np.array(self.view_matrix().data(), dtype=np.float32))
+
+    def mousePressEvent(self, ev):
+        self.mouse_position = ev.localPos()
+
+    def mouseMoveEvent(self, ev):
+        new_mouse_position = ev.localPos()
+        diff = new_mouse_position - self.mouse_position
+
+        if ev.buttons() == QtCore.Qt.MouseButton.LeftButton:
+            self.view_azimuth_difference = self.mouse_sensitivity_azimuth * diff.x()
+            self.view_elevation_difference = self.mouse_sensitivity_elevation * diff.y()
+
+        elif ev.buttons() == QtCore.Qt.MouseButton.RightButton:
+            self.view_centre_difference = [self.mouse_sensitivity_position * diff.x(), 0,
+                                           self.mouse_sensitivity_position * diff.y()]
+
+        self.update()
+    def mouseReleaseEvent(self, ev):
+        # Mouse released, add dragging offset the view variables
+
+        # self.view_centre += np.array(self.view_centre_difference)
+        # self.view_elevation += self.view_elevation_difference
+        # self.view_azimuth += self.view_azimuth_difference
+
+        self.view_centre_difference = None
+        self.view_azimuth_difference = None
+        self.view_elevation_difference = None
+
+        self.update()
 
 def main():
     """ Show a demo of the opengl window """
