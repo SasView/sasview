b36296183205b05ff72016903a441efeb7b69890
krzywon <krzywon@yahoo.com>
2022-11-02 09:09:26 -0400

    Change dataDeleted to onDataDeleted for semantic reasons, and remove data from results panel when data deleted

--- src/sas/qtgui/Utilities/ResultPanel.py
+++ src/sas/qtgui/Utilities/ResultPanel.py
@@ -56,13 +56,7 @@ class ResultPanel(QtWidgets.QTabWidget):
     def onPlotResults(self, results, optimizer="Unknown"):
         # import moved here due to its cost
         from bumps.dream.stats import var_stats, format_vars
-        # Clear up previous results
-        for view in (self.convergenceView, self.correlationView,
-                     self.uncertaintyView, self.traceView):
-            view.close()
-        # close all tabs. REMEMBER TO USE REVERSED RANGE!!!
-        for index in reversed(range(self.count())):
-            self.removeTab(index)
+        self.clearAnyData()
 
         result = results[0][0]
         name = result.data.sas_data.name
@@ -97,11 +91,25 @@ class ResultPanel(QtWidgets.QTabWidget):
         if self.count()==0:
             self.close()
 
-    def dataDeleted(self, data):
+    def onDataDeleted(self, data):
+        """ Check if the data set is shown in the window and close tabs as needed. """
         if not data or not self.isVisible():
             return
         if data.id == self.data_id:
+            self.setWindowTitle(self.window_name)
+            self.clearAnyData()
             self.close()
+
+    def clearAnyData(self):
+        """ Clear any previous results and reset window to its base state. """
+        self.data_id = None
+        # Clear up previous results
+        for view in (self.convergenceView, self.correlationView,
+                     self.uncertaintyView, self.traceView):
+            view.close()
+        # close all tabs. REMEMBER TO USE REVERSED RANGE!!!
+        for index in reversed(range(self.count())):
+            self.removeTab(index)
 
     def closeEvent(self, event):
         """
