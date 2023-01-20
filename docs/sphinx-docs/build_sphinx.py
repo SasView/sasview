edfa419b45421d384f02bc7315eb0ddfebd56814
lucas-wilkins <lucas.wilkins@stfc.ac.uk>
2023-01-20 14:25:18 +0000

    Is abspath the issue?

--- docs/sphinx-docs/build_sphinx.py
+++ docs/sphinx-docs/build_sphinx.py
@@ -39,12 +39,12 @@ SPHINX_SOURCE = joinpath(SPHINX_ROOT, "source-temp")
 SPHINX_PERSPECTIVES = joinpath(SPHINX_SOURCE, "user", "qtgui", "Perspectives")
 
 # sasview paths
-SASVIEW_ROOT = joinpath(SPHINX_ROOT, '..', '..')
+SASVIEW_ROOT = abspath(joinpath(SPHINX_ROOT, '..', '..'))
 SASVIEW_DOCS = joinpath(SPHINX_ROOT, "source")
 # 'platform' descriptor vanished from the main build,
 # so we need to update the location.
 #SASVIEW_BUILD = abspath(joinpath(SASVIEW_ROOT, "build", "lib"+platform))
-SASVIEW_BUILD = abspath(joinpath(SASVIEW_ROOT, "build", "lib"))
+SASVIEW_BUILD = joinpath(SASVIEW_ROOT, "build", "lib")
 SASVIEW_MEDIA_SOURCE = joinpath(SASVIEW_ROOT, "src", "sas")
 SASVIEW_DOC_TARGET = joinpath(SASVIEW_BUILD, "doc")
 SASVIEW_API_TARGET = joinpath(SPHINX_SOURCE, "dev", "sasview-api")
