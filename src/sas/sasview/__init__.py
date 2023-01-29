e32e2bcb2ba0cf24627c07e3ec95684c8ba890f9
Wojciech Potrzebowski <Wojciech.Potrzebowski@esss.se>
2023-01-29 13:32:13 +0100

    Updated zenodo version

--- src/sas/sasview/__init__.py
+++ src/sas/sasview/__init__.py
@@ -3,6 +3,6 @@ from distutils.version import StrictVersion
 # in http://epydoc.sourceforge.net/stdlib/distutils.version.StrictVersion-class.html
 __version__ = "5.0.6"
 StrictVersion(__version__)
-__DOI__ = "Zenodo, "
+__DOI__ = "Zenodo, 10.5281/zenodo.7581379"
 __release_date__ = "2023"
 __build__ = "GIT_COMMIT"
