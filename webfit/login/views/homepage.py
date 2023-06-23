bab728d49c7164c0db2154346c77c3dcbc64858e
XaelShan <tns14@nist.gov>
2023-06-23 15:24:28 -0400

    created login app, only formatted no real code

---
+++ webfit/login/views/homepage.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# test function
+def placeholder(request):
+    return HttpResponse("Hi")
+
+#turn this file into homepage later
