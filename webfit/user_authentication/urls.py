e7692e4225b9659465f3541e89d9c55e7cce2b9d
XaelShan <tns14@nist.gov>
2023-06-26 13:15:19 -0400

    minor formatting+urlpathversioning setting changes

--- webfit/user_authentication/urls.py
+++ webfit/user_authentication/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path, include
+from django.urls import path, re_path, include
 from .views import homepage, authentication
 
+app_name = 'user_authentication'
 urlpatterns = [
     path("", homepage.placeholder),
-    path("login/", authentication.placeholder_login_screen),
+
+    re_path(r'^(?P<version>[v1]+)/login/$', authentication.placeholder_login_screen, name='login placeholder'),
 ]
--- webfit/webfit/settings.py
+++ webfit/webfit/settings.py
@@ -11,6 +11,15 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from rest_framework import permissions, versioning
+from django.core.signals import setting_changed
+
+from django.conf import settings
+# Import from `django.core.signals` instead of the official location
+# `django.test.signals` to avoid importing the test module unnecessarily.
+from django.core.signals import setting_changed
+from django.utils.module_loading import import_string
+from rest_framework import ISO_8601
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,7 +46,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "analyze",
-    "user_authentication"
+    "user_authentication",
 ]
 
 MIDDLEWARE = [
@@ -70,9 +79,45 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "webfit.wsgi.application"
 
+#rest framework
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"
+    ],
+    "DEFAULT_VERSIONING_CLASS": "rest_framwork.versioning.QueryParameterVersioning",
+    "DEFAULT_VERSION" : "v1",
+    "ALLOWED_VERSIONS" : "v1",
+    #version_param is default set to "version"
+}
+
+# List of settings that may be in string import notation.
+IMPORT_STRINGS = [
+    'DEFAULT_RENDERER_CLASSES',
+    'DEFAULT_PARSER_CLASSES',
+    'DEFAULT_AUTHENTICATION_CLASSES',
+    'DEFAULT_PERMISSION_CLASSES',
+    'DEFAULT_THROTTLE_CLASSES',
+    'DEFAULT_CONTENT_NEGOTIATION_CLASS',
+    'DEFAULT_METADATA_CLASS',
+    'DEFAULT_VERSIONING_CLASS',
+    'DEFAULT_PAGINATION_CLASS',
+    'DEFAULT_FILTER_BACKENDS',
+    'DEFAULT_SCHEMA_CLASS',
+    'EXCEPTION_HANDLER',
+    'TEST_REQUEST_RENDERER_CLASSES',
+    'UNAUTHENTICATED_USER',
+    'UNAUTHENTICATED_TOKEN',
+    'VIEW_NAME_FUNCTION',
+    'VIEW_DESCRIPTION_FUNCTION'
+]
+
+# List of settings that have been removed
+REMOVED_SETTINGS = [
+    'PAGINATE_BY', 'PAGINATE_BY_PARAM', 'MAX_PAGINATE_BY',
+]
 
 # Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databaes
 
 DATABASES = {
     "default": {
@@ -123,7 +168,101 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-#rest framework
-REST_FRAMEWORK = {
-    "DEFAULT_VERSIONING_CLASS": "rest_framwork.versioning.NamespaceVersioning"
-}
+def perform_import(val, setting_name):
+    """
+    If the given setting is a string import notation,
+    then perform the necessary import or imports.
+    """
+    if val is None:
+        return None
+    elif isinstance(val, str):
+        return import_from_string(val, setting_name)
+    elif isinstance(val, (list, tuple)):
+        return [import_from_string(item, setting_name) for item in val]
+    return val
+
+
+def import_from_string(val, setting_name):
+    """
+    Attempt to import a class from a string representation.
+    """
+    try:
+        return import_string(val)
+    except ImportError as e:
+        msg = "Could not import '%s' for API setting '%s'. %s: %s." % (val, setting_name, e.__class__.__name__, e)
+        raise ImportError(msg)
+
+class APISettings:
+    """
+    A settings object that allows REST Framework settings to be accessed as
+    properties. For example:
+
+        from rest_framework.settings import api_settings
+        print(api_settings.DEFAULT_RENDERER_CLASSES)
+
+    Any setting with string import paths will be automatically resolved
+    and return the class, rather than the string literal.
+
+    Note:
+    This is an internal class that is only compatible with settings namespaced
+    under the REST_FRAMEWORK name. It is not intended to be used by 3rd-party
+    apps, and test helpers like `override_settings` may not work as expected.
+    """
+    def __init__(self, user_settings=None, defaults=None, import_strings=None):
+        if user_settings:
+            self._user_settings = self.__check_user_settings(user_settings)
+        self.defaults = defaults or REST_FRAMEWORK
+        self.import_strings = import_strings or IMPORT_STRINGS
+        self._cached_attrs = set()
+
+    @property
+    def user_settings(self):
+        if not hasattr(self, '_user_settings'):
+            self._user_settings = getattr(settings, 'REST_FRAMEWORK', {})
+        return self._user_settings
+
+    def __getattr__(self, attr):
+        if attr not in self.defaults:
+            raise AttributeError("Invalid API setting: '%s'" % attr)
+
+        try:
+            # Check if present in user settings
+            val = self.user_settings[attr]
+        except KeyError:
+            # Fall back to defaults
+            val = self.defaults[attr]
+
+        # Coerce import strings into classes
+        if attr in self.import_strings:
+            val = perform_import(val, attr)
+
+        # Cache the result
+        self._cached_attrs.add(attr)
+        setattr(self, attr, val)
+        return val
+
+    def __check_user_settings(self, user_settings):
+        SETTINGS_DOC = "https://www.django-rest-framework.org/api-guide/settings/"
+        for setting in REMOVED_SETTINGS:
+            if setting in user_settings:
+                raise RuntimeError("The '%s' setting has been removed. Please refer to '%s' for available settings." % (setting, SETTINGS_DOC))
+        return user_settings
+
+    def reload(self):
+        for attr in self._cached_attrs:
+            delattr(self, attr)
+        self._cached_attrs.clear()
+        if hasattr(self, '_user_settings'):
+            delattr(self, '_user_settings')
+
+
+api_settings = APISettings(None, REST_FRAMEWORK, IMPORT_STRINGS)
+
+
+def reload_api_settings(*args, **kwargs):
+    setting = kwargs['setting']
+    if setting == 'REST_FRAMEWORK':
+        api_settings.reload()
+
+
+setting_changed.connect(reload_api_settings)
