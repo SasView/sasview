a4ac07df997f7139f362c22222149668edfe37bf
XaelShan <tns14@nist.gov>
2023-06-27 11:40:48 -0400

    cleaned up + added comments

--- webfit/analyze/fitting/models.py
+++ webfit/analyze/fitting/models.py
@@ -15,33 +15,44 @@ from django.core.exceptions import (
     ObjectDoesNotExist,
     ValidationError,
 )
+#models.CharField(max_length=100, blank=False, null=False)
+#models.CharField(max_length=200, help_text="")
+#models.ForeignKey("other model class name", on_delete=models.CASCADE) <--- used for refering to other models in other apps
+"""CHOICES= [
+   "choice",
+    (
+        ("choice1", "choice2),
+        ("group choice","2nd choice in group"),
+    ),
+ ]"""
 
 models_logger = getLogger(__name__)
 
-# do we want individual dbs for each perspective
-# for each 
+# do we want individual dbs for each perspective?
 
 loader = Loader()
-#    username = models.CharField(max_length=100, blank=False, null=False)
-# password = models.CharField(max_length=100, blank=False, null=False)
-   #fin later : category = models.ForeignKey()
 
 class fits(models.Model):
 
-    #create uploade files
-        #create option to add to open list of data
-        #can opt in to save data into example data pool
-
-    #1 api, takes data and creates unique Hash (algorithm and calculates unique 260 char string)
-    #importing 
-
-    #import sasmodels or through models.py in fit (modelmanagerbase)<---- create choice list
-    #view saswebcalc
+    #import
+    #takes files string and turn it into DataInfo
     file_string = models.CharField(max_length=200, null = False, help_text="The name for all the fit data")
     data = Loader.load(file_string)
 
     #export
+    #takes DataInfo and saves it into to specified file location
     saved_file_string = models.CharField(max_length=200, null = False, help_text="file name to data saved")
     Loader.save(saved_file_string, data)
 
-    #pagestate has a list of data_attributes
+    """create uploade files
+        create option to add to open list of data
+        can opt in to save data into example data pool
+
+    1 api, takes data and creates unique Hash (algorithm and calculates unique 260 char string)
+    importing 
+    """
+
+    """
+    import sasmodels or through models.py in fit (modelmanagerbase)<---- create choice list
+    view saswebcalc
+    """
