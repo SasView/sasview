0953fb35992edf3aba2348fef7d554b66df5e0b1
XaelShan <tns14@nist.gov>
2023-06-29 15:30:22 -0400

    added back views to data, not working yet

--- webfit/data/views.py
+++ webfit/data/views.py
@@ -18,3 +18,18 @@ def ImportData(request, pk, version= None):
 
 
     #takes files string and turn it into DataInfo
+    if Data.file_string != False:
+        Data.data = loader.load(Data.file_string)
+#opt in to uploading to example data pool
+    if Data.opt_in == True:
+        #upload data to example data pool
+        loader.save(loader, file = "PUT FILE STRING HERE LATER", data = Data.data)
+
+def ExportData(request, version = None):
+    #takes DataInfo and saves it into to specified file location
+    file_locaton = Data.
+    loader.save(loader, file = Data.saved_file_string, data = Data.data)
+    
+    if Data.saved_file == True:
+        return(AlreadySavedError)
+
