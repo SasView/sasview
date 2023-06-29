dc4f18c20c994fe9f4a9dae516d003ee6b7a1e9f
XaelShan <tns14@nist.gov>
2023-06-29 15:28:01 -0400

    added back views to data, not working yet

--- webfit/data/urls.py
+++ webfit/data/urls.py
@@ -6,5 +6,5 @@ logger = logging.getLogger(__name__)
 
 urlpatterns = [
     path("import/", views.ImportData, name = "import data"),
-    path("export/", views.ExportData, name = "export/save data")
+    #path("export/", views.ExportData, name = "export/save data")
 ]
--- webfit/data/views.py
+++ webfit/data/views.py
@@ -14,22 +14,7 @@ loader = Loader()
 @api_view(['POST', 'PUT'])
 def ImportData(request, pk, version= None):
     if request.method == 'POST':
-        
+        x=1
 
 
     #takes files string and turn it into DataInfo
-    if Data.file_string != False:
-        Data.data = loader.load(Data.file_string)
-#opt in to uploading to example data pool
-    if Data.opt_in == True:
-        #upload data to example data pool
-        loader.save(loader, file = "PUT FILE STRING HERE LATER", data = Data.data)
-
-def ExportData(request, version = None):
-    #takes DataInfo and saves it into to specified file location
-    file_locaton = Data.
-    loader.save(loader, file = Data.saved_file_string, data = Data.data)
-    
-    if Data.saved_file == True:
-        return(AlreadySavedError)
-
