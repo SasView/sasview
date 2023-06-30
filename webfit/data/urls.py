f97a165ddece1e7ce985ec1192c1bafb21aa0ec0
XaelShan <tns14@nist.gov>
2023-06-30 11:02:25 -0400

    fixed urls

--- webfit/data/urls.py
+++ webfit/data/urls.py
@@ -5,7 +5,6 @@ from . import views
 logger = logging.getLogger(__name__)
 
 urlpatterns = [
-    re_path(r"import/(?P<file_id>)/$", views.import_file_string, name = "import data"),
-    re_path(r"import/(?P<file_id>)/(?P<opt_in>)/$", views.import_file_string, name = "import data and opt in"),
+    path("import/", views.import_file_string, name = "import data"),
     path("export/", views.export_data, name = "export/save data")
 ]
