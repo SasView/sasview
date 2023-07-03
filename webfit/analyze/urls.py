3de754eca08f0ef5fc6b39b350a951eef8b807ef
XaelShan <tns14@nist.gov>
2023-07-03 15:18:52 -0400

    started fit serializer + views + updated models/urls

--- webfit/analyze/urls.py
+++ webfit/analyze/urls.py
@@ -6,11 +6,11 @@ logger = logging.getLogger(__name__)
 
 #urls to fit
 fit_patterns = [
-    path("bumps/", views.run, name = "bumps"),   
+    path("", views.start, name = "starting a fit"),  
 ]
 
 urlpatterns = [
-    path("fit/", include(fit_patterns), name = "fitter"),
+    path("fit/", include(fit_patterns), name = "fit patterns"),
     #path("Inversion/", include()),   <- where is this in the main script? 
     #path("Invariant/", include()),
     #path("Corfunc/", include()),
