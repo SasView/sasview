16eb67a69ae087b3f4bef4a846b8ec59b33eea36
XaelShan <tns14@nist.gov>
2023-07-06 14:11:57 -0400

    comment about loader

--- webfit/data/views.py
+++ webfit/data/views.py
@@ -27,8 +27,14 @@ def list_data(request, db_id = None, version = None):
 @api_view(['GET'])
 def data_info(request, db_id, version = None):
     if request.method == 'Get':
+        public_data = Data.objects.filter(opt_in=True)
+        file = get_object_or_404(Data, id = db_id)
+        #TODO check if this actually checks the id is public/properly logged in
+        if (not file in public_data) and (not request.username is file.username):
+            return HttpResponseBadRequest("Database ID not public")
         file = get_object_or_404(Data, id = db_id)
         #TODO ^^ how to check if file_id is in user_file_ids
+        #TODO doesn't actually load file, fix this (changed file to actual file, not file string) <- possibly get rid of loader, just file
         loader = Loader.load(file.file)
         return_data = {"info" : loader.__str__()}
         return return_data
@@ -42,7 +48,7 @@ def upload(request, version = None):
           
     #saves file
     if request.method == 'POST':
-        serializer(file = request.file, opt_in = request.opt_in)
+        serializer(file = request.file, opt_in = request.data.opt_in)
     
     #saves or updates file
     elif request.method == 'PUT':
