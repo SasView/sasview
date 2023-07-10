58a11b4dd436bace2ccd9d3d1e8c7374c5d1ade5
XaelShan <tns14@nist.gov>
2023-07-10 18:24:57 -0400

    changed migration names to version_0001 and added bookmark link to figure out foreignkey migrations later

--- webfit/migrations/0001_initial.py
+++ webfit/migrations/0001_initial.py
@@ -1,8 +1,8 @@
 from django.db import migrations, models
 
-from data.migrations.first import Migration as DataMigrations
-from analyze.migrations.first import Migration as AnalysisMigrations
-from analyze.fitting.migrations.first import Migration as FitMigrations
+from data.migrations.version_0001 import Migration as DataMigrations
+from analyze.migrations.version_0001 import Migration as AnalysisMigrations
+from analyze.fitting.migrations.version_0001 import Migration as FitMigrations
 
 
 class MigrationAll(migrations.Migration):
