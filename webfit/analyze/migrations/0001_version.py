aee67dfef7eae58eefb8ff48176783c523415add
XaelShan <tns14@nist.gov>
2023-07-11 14:36:53 -0400

    UHHH think this should work???

--- webfit/analyze/migrations/0001_version.py
+++ webfit/analyze/migrations/0001_version.py
@@ -12,33 +12,40 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
+            name='AnalysisParameterBase',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(max_length=300, help_text="Parameter Name")),
+                ('value', models.FloatField(blank=False, help_text="the value of the parameter")),
+                ('data_type', models.CharField(max_length=100, help_text="parameter type (int/double)")),
+                ('unit', models.CharField(blank=False, help_text = "string for what unit the parameter is")),
+            ]
+        ),
+
+        migrations.CreateModel(
+            name='AnalysisModelBase',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(max_length=100, help_text="name of analysis model")),
+                ('parameters', models.ForeignKey(default = None, to="analyze.analysisparameterbase", on_delete=models.CASCADE))
+            ],
+        ),
+        
+        migrations.CreateModel(
             name='AnalysisBase',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('current_user', models.ForeignKey(default = None, primary_key=False, to='user_app.UserProfile', on_delete=models.CASCADE)),
-                ('data_id', models.ForeignKey(default = None, primary_key=False, on_delete=models.CASCADE, to='data.Data')),
+                ('data_id', models.ForeignKey(default = None, primary_key=False, on_delete=models.CASCADE, to='data.Datafile')),
                 ('model_id', models.ForeignKey(default = None, primary_key=False, on_delete=models.CASCADE, to='analyze.AnalysisModelBase')),
-                ('gpu_requested', models.BooleanField(default = False)),
-                ('time_recieved', models.DateTimeField(auto_now_add=True)),
-                ('time_started', models.DateTimeField(auto_now=False, blank=True, null=True)),
-                ('time_complete', models.DateTimeField(auto_now=False, blank=True, null=True)),
-                ('analysis_success', models.BooleanField(default = False)),
-                ('is_public', models.BooleanField(default = False)),
+                
+                ('gpu_requested', models.BooleanField(default = False, help_text= "use GPU rather than CPU")),
+                ('time_recieved', models.DateTimeField(auto_now_add=True, help_text="analysis requested")),
+                ('time_started', models.DateTimeField(auto_now=False, blank=True, null=True, help_text="analysis initiated")),
+                ('time_complete', models.DateTimeField(auto_now=False, blank=True, null=True, help_text="analysis stopped")),
+                ('analysis_success', models.BooleanField(default = False, help_text="Successful completion of analysis")),
+                
+                ('is_public', models.BooleanField(default = False, help_text="does the user want their data to be public")),
             ],
-        ),
-        migrations.CreateModel(
-            name='AnalysisModelBase',
-            fields=[
-                ('name', models.CharField(max_length=100)),
-            ],
-        ),
-        migrations.CreateModel(
-            name='AnalysisParameterBase',
-            fields=[
-                ('name', models.CharField(max_length=300)),
-                ('value', models.FloatField(blank=False)),
-                ('data_type', models.CharField(max_length=100)),
-                ('unit', models.CharField(blank=False)),
-            ]
         ),
     ]
--- webfit/data/migrations/0001_version.py
+++ webfit/data/migrations/0001_version.py
@@ -11,13 +11,13 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Data',
+            name='Datafile',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('current_user', models.ForeignKey(default = None, primary_key=False, to='user_app.UserProfile', on_delete=models.CASCADE)),
-                ('file', models.FileField(null = False)),
-                ('save_file_string', models.CharField(max_length=200, null = False)),
-                ('is_public', models.BooleanField(default = False)),
+                ('file', models.FileField(null = False, help_text="This is a file", upload_to="")),
+                ('save_file_string', models.CharField(max_length=200, null = False, help_text="File location to save data")),
+                ('is_public', models.BooleanField(default = False, help_text= "opt in to submit your data into example pool")),
             ],
         ),
     ]
