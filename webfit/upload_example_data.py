588e766bcc0c34cdf4e550179224de462434b35c
XaelShan <tns14@nist.gov>
2023-07-11 17:57:28 -0400

    creating fixtures + uploading example data + uploading parameters

---
+++ webfit/upload_example_data.py
@@ -0,0 +1,12 @@
+import os
+import re
+import logging
+from glob import glob
+
+#should this be moved outside the webfit/django directory?
+#^model marketplace does this for somereason? is it so that they can import withotu worrying 
+#about django's code interfering?
+
+"""
+
+"""
