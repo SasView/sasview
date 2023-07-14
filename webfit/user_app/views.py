327f336d2a412602d9b19a4db7f451daeac3ece4
XaelShan <tns14@nist.gov>
2023-07-14 12:18:06 -0400

    working on user views stuff

---
+++ webfit/user_app/views.py
@@ -0,0 +1,46 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from knox.auth import AuthToken, TokenAuthentication
+from serializers import UserSerializer, RegisterSerializer
+
+def serialize_user(user):
+    return {
+        "username": user.username,
+        "email": user.email,
+        "first_name": user.first_name,
+        "last_name": user.last_name
+    }
+
+@api_view(['POST'])
+def login(request):
+    serializer = AuthTokenSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    user = serializer.validated_data['user']
+    _, token = AuthToken.objects.create(user)
+    return Response({
+        'user_data': serialize_user(user),
+        'token': token
+    })
+        
+
+@api_view(['POST'])
+def register(request):
+    serializer = RegisterSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        user = serializer.save()
+        _, token = AuthToken.objects.create(user)
+        return Response({
+            "user_info": serialize_user(user),
+            "token": token
+        })
+
+
+@api_view(['GET'])
+def get_user(request):
+    user = request.user
+    if user.is_authenticated:
+        return Response({
+            'user_data': serialize_user(user)
+        })
+    return Response({})
