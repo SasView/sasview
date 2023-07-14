d18d02b82d4d80c77fa37a0bd351e453c021f070
Piotr Rozyczko <piotr.rozyczko@gmail.com>
2023-07-14 11:50:57 +0200

    removed 3.8 from releases, added 3.10 and 3.11. unpinned pyinstaller

--- .github/workflows/matrix.py
+++ .github/workflows/matrix.py
@@ -38,13 +38,17 @@ os_test_list = os_release_list + [
 
 # List of python versions to use for release builds
 python_release_list = [
-    '3.8',
+    '3.9',
+    '3.10',
+    '3.11',
 ]
 
 # List of python versions to use for tests
 python_test_list = python_release_list + [
+    '3.8',
     '3.9',
-    '3.10'
+    '3.10',
+    '3.11',
 ]
 
 
