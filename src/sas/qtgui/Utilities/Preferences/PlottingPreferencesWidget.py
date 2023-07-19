a627248444d985d91c81481f4e8ac6d025cc0b6e
Dorian Lozano <lozano@ill.fr>
2023-07-19 12:54:13 +0200

    Buttons moved from Plotting Preferences to Display Preferences

--- src/sas/qtgui/Utilities/Preferences/PlottingPreferencesWidget.py
+++ src/sas/qtgui/Utilities/Preferences/PlottingPreferencesWidget.py
@@ -8,9 +8,7 @@ class PlottingPreferencesWidget(PreferencesWidget):
         super(PlottingPreferencesWidget, self).__init__("Plotting Options")
         self.config_params = ['FITTING_PLOT_FULL_WIDTH_LEGENDS',
                               'FITTING_PLOT_LEGEND_TRUNCATE',
-                              'FITTING_PLOT_LEGEND_MAX_LINE_LENGTH',
-                              'DISABLE_RESIDUALS',
-                              'DISABLE_POLYDISPERSITY_PLOT']
+                              'FITTING_PLOT_LEGEND_MAX_LINE_LENGTH']
 
     def _addAllWidgets(self):
         self.legendFullWidth = self.addCheckBox(
@@ -28,28 +26,14 @@ class PlottingPreferencesWidget(PreferencesWidget):
             default_number=config.FITTING_PLOT_LEGEND_MAX_LINE_LENGTH)
         self.legendLineLength.textChanged.connect(
             lambda: self._validate_input_and_stage(self.legendLineLength, 'FITTING_PLOT_LEGEND_MAX_LINE_LENGTH'))
-        self.disableResiduals = self.addCheckBox(
-            title="Disable Residuals Display",
-            checked=config.DISABLE_RESIDUALS)
-        self.disableResiduals.clicked.connect(
-            lambda: self._stageChange('DISABLE_RESIDUALS', self.disableResiduals.isChecked()))
-        self.disablePolydispersityPlot = self.addCheckBox(
-            title="Disable Polydispersity Plot Display",
-            checked=config.DISABLE_POLYDISPERSITY_PLOT)
-        self.disablePolydispersityPlot.clicked.connect(
-            lambda: self._stageChange('DISABLE_POLYDISPERSITY_PLOT', self.disablePolydispersityPlot.isChecked()))
 
     def _toggleBlockAllSignaling(self, toggle):
         self.legendFullWidth.blockSignals(toggle)
         self.legendTruncate.blockSignals(toggle)
         self.legendLineLength.blockSignals(toggle)
-        self.disableResiduals.blockSignals(toggle)
-        self.disablePolydispersityPlot.blockSignals(toggle)
 
     def _restoreFromConfig(self):
         self.legendFullWidth.setChecked(bool(config.FITTING_PLOT_FULL_WIDTH_LEGENDS))
         self.legendTruncate.setChecked(bool(config.FITTING_PLOT_LEGEND_TRUNCATE))
         self.legendLineLength.setText(str(config.FITTING_PLOT_LEGEND_MAX_LINE_LENGTH))
         self.legendLineLength.setStyleSheet("background-color: white")
-        self.disableResiduals.setChecked(config.DISABLE_RESIDUALS)
-        self.disablePolydispersityPlot.setChecked(config.DISABLE_POLYDISPERSITY_PLOT)
