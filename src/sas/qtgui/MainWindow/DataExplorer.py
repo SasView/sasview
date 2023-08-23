04185ee505c979ef90dd3428b1b08c9f9ac33c87
rozyczko <piotr.rozyczko@gmail.com>
2023-08-23 11:13:23 +0200

    corrected signature of getExistingDirectory

--- src/sas/qtgui/MainWindow/DataExplorer.py
+++ src/sas/qtgui/MainWindow/DataExplorer.py
@@ -232,7 +232,7 @@ class DataExplorerWindow(DroppableDataLoadWidget):
         caption = 'Choose a directory'
         options = QtWidgets.QFileDialog.ShowDirsOnly | QtWidgets.QFileDialog.DontUseNativeDialog
         directory = self.default_load_location
-        folder = QtWidgets.QFileDialog.getExistingDirectory(parent, caption, directory, "", options)
+        folder = QtWidgets.QFileDialog.getExistingDirectory(parent, caption, directory, options)
 
         if folder is None:
             return
