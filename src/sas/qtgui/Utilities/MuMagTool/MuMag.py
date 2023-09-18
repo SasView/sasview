ca14f1d5d80ee87572683dc857742ae1d64ec965
Funkmich008 <87016586+Funkmich008@users.noreply.github.com>
2023-09-18 10:31:33 +0200

    next update of the UI of MuMag adding buttons and text edit fields, change of button names

--- src/sas/qtgui/Utilities/MuMagTool/MuMag.py
+++ src/sas/qtgui/Utilities/MuMagTool/MuMag.py
@@ -4,17 +4,13 @@ from PySide6 import QtWidgets
 from sas.qtgui.Utilities.MuMagTool import MuMagLib
 
 
-def on_press_2():
-    print("HELLO")
-
 class MuMag(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         super().__init__()
 
         self.setupUi(self)
-        self.pushButton.clicked.connect(self.import_data_button_callback)
-        self.pushButton_2.clicked.connect(self.plot_experimental_data_button_callback)
-        self.pushButton.clicked.connect(on_press_2)
+        self.ImportDataButton.clicked.connect(self.import_data_button_callback)
+        self.PlotDataButton.clicked.connect(self.plot_experimental_data_button_callback)
 
     def import_data_button_callback(self):
         MuMagLib.import_data_button_callback_sub()
