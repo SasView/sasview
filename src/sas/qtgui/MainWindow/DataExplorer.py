c7f7ad2869d8f958ccf82343232d42c8c1629d34
rozyczko <piotr.rozyczko@gmail.com>
2023-09-21 17:55:59 +0200

    Qt6-ize the setFilterRegularExpression syntax

--- src/sas/qtgui/MainWindow/DataExplorer.py
+++ src/sas/qtgui/MainWindow/DataExplorer.py
@@ -128,7 +128,7 @@ class DataExplorerWindow(DroppableDataLoadWidget):
         self.theory_model.itemChanged.connect(self.onFileListChanged)
 
         # Don't show "empty" rows with data objects
-        self.data_proxy.filterRegularExpression = "[^()]" 
+        self.data_proxy.setFilterRegularExpression(QtCore.QRegularExpression(".+"))
 
         # Create a window to allow the display name to change
         self.nameChangeBox = ChangeName(self)
