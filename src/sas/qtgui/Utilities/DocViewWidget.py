cce82839b49a884d0a4f50dd138059c8a2dbfc58
krzywon <krzywon@yahoo.com>
2023-12-20 13:11:27 -0500

    Lint and code cleanup

--- src/sas/qtgui/Utilities/DocViewWidget.py
+++ src/sas/qtgui/Utilities/DocViewWidget.py
@@ -1,16 +1,16 @@
 import sys
 import os
-from typing import Optional
 import logging
 
 
-from PySide6 import QtGui, QtWebEngineWidgets, QtCore, QtWidgets, QtWebEngineCore
+from PySide6 import QtCore, QtWidgets, QtWebEngineCore
 from twisted.internet import threads
 
 from .UI.DocViewWidgetUI import Ui_DocViewerWindow
 from sas.qtgui.Utilities.TabbedModelEditor import TabbedModelEditor
 from sas.qtgui.Utilities import GuiUtils
 from sas.sascalc.fit import models
+
 
 class DocViewWindow(QtWidgets.QDialog, Ui_DocViewerWindow):
     """
@@ -27,7 +27,7 @@ class DocViewWindow(QtWidgets.QDialog, Ui_DocViewerWindow):
         self.source = source
         self.regen_in_progress = False
 
-        self.initializeSignals() # Connect signals
+        self.initializeSignals()  # Connect signals
 
         self.regenerateIfNeeded()
 
@@ -51,32 +51,34 @@ class DocViewWindow(QtWidgets.QDialog, Ui_DocViewerWindow):
         # Test to see if we're dealing with a model html file or other html file
         if "models" in path[0]:
             py_base_file_name = os.path.splitext(os.path.basename(path[0]))[0]
-            self.editorWindow =  TabbedModelEditor(parent=self.parent,
-                                                   edit_only=True,
-                                                   load_file=py_base_file_name,
-                                                   model=True)
+            self.editorWindow = TabbedModelEditor(parent=self.parent,
+                                                  edit_only=True,
+                                                  load_file=py_base_file_name,
+                                                  model=True)
         else:
             # Remove everything before /user/ (or \user\)
             file = sub(r"^.*?(?=[\/\\]user)", "", path[0])
 
-            # index.html is the only rst file outside of /user/ folder-- set it manually
+            # index.html is the only rst file outside the /user/ folder-- set it manually
             if "index.html" in file:
                 file = "/index.html"
-            self.editorWindow =  TabbedModelEditor(parent=self.parent,
-                                                   edit_only=True,
-                                                   load_file=file,
-                                                   model=False)
+            self.editorWindow = TabbedModelEditor(parent=self.parent,
+                                                  edit_only=True,
+                                                  load_file=file,
+                                                  model=False)
         self.editorWindow.show()
 
     def onClose(self):
         """
         Close window
+        Keep as a separate method to allow for additional functionality when closing
         """
         self.close()
 
     def onShow(self):
         """
         Show window
+        Keep as a separate method to allow for additional functionality when opening
         """
         self.show()
     
@@ -128,11 +130,12 @@ class DocViewWindow(QtWidgets.QDialog, Ui_DocViewerWindow):
         
         if self.regen_in_progress is False:
             self.loadHtml() #loads the html file specified in the source url to the QWebViewer
-    
+
+    @staticmethod
     def newer(self, src, html):
         try:
             return not os.path.exists(html) or os.path.getmtime(src) > os.path.getmtime(html)
-        except:
+        except Exception:
             return True
 
     def loadHtml(self):
@@ -158,7 +161,7 @@ class DocViewWindow(QtWidgets.QDialog, Ui_DocViewerWindow):
         Process path into proper QUrl for use in QWebViewer
         """
         url = self.source
-        # Check to see if path is absolute or relative, accomodating urls from many different places
+        # Check to see if path is absolute or relative, accommodating urls from many different places
         if not os.path.exists(url):
             from sas.qtgui.Utilities.GuiUtils import HELP_DIRECTORY_LOCATION
             location = HELP_DIRECTORY_LOCATION + url
@@ -188,7 +191,8 @@ class DocViewWindow(QtWidgets.QDialog, Ui_DocViewerWindow):
         d = threads.deferToThread(self.regenerateDocs, regen_docs, target=file_name) # Regenerate specific documentation file
         d.addCallback(self.docRegenComplete, self.source)
         self.regen_in_progress = True
-    
+
+    @staticmethod
     def regenerateDocs(self, regen_docs, target=None):
         """
         Regenerates documentation for a specific file (target) in a subprocess
@@ -200,7 +204,7 @@ class DocViewWindow(QtWidgets.QDialog, Ui_DocViewerWindow):
             target,
         ]
         doc_regen_dir = os.path.dirname(regen_docs)
-        subprocess.run(command, cwd=doc_regen_dir) # cwd parameter tells subprocess to open from a specific directory
+        subprocess.run(command, cwd=doc_regen_dir)  # cwd parameter tells subprocess to open from a specific directory
     
     def docRegenComplete(self, d, help_location):
         """
