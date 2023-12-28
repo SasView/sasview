458f4322ebd4d81f96dfa8cf445a77a1cdbb64c3
Wojciech Potrzebowski <wojciech.potrzebowski@ess.eu>
2023-12-28 22:20:11 +0100

    adding dist-info to explicit signing

--- build_tools/code_sign_osx.py
+++ build_tools/code_sign_osx.py
@@ -15,12 +15,17 @@ zmq_dylib_list_resources = glob.glob(
     "SasView*.app/Contents/Resources/zmq/.dylibs/*.dylib", recursive=True
 )
 
+dist_info_list_resources = glob.glob(
+    "SasView*.app/Contents/MacOS/*.dist-info", recursive=True
+)
+
 sign_command = ['codesign', '--timestamp', '--options=runtime', '--verify', '--verbose=4', '--force',
                 '--sign', 'Developer ID Application: European Spallation Source Eric (W2AG9MPZ43)']
 
 
 #TODO: Check if it is necesarry to do it per file (one long list maybe enough)
-for sfile in itertools.chain(so_list, dylib_list, dylib_list_resources, zmq_dylib_list_resources):
+for sfile in itertools.chain(so_list, dylib_list, dylib_list_resources,
+                             zmq_dylib_list_resources, dist_info_list_resources):
     sign_command.append(sfile)
     subprocess.check_call(sign_command)
     sign_command.pop()
