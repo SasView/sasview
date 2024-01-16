234567238db958e067f54f43390c15fb7eaf4181
Wojciech Potrzebowski <wojciech.potrzebowski@ess.eu>
2024-01-16 22:11:34 +0100

    Sigining explicetly pyside libs

--- build_tools/code_sign_osx.py
+++ build_tools/code_sign_osx.py
@@ -15,12 +15,19 @@ zmq_dylib_list_resources = glob.glob(
     "SasView*.app/Contents/Resources/zmq/.dylibs/*.dylib", recursive=True
 )
 
+pyside_libs = glob.glob(
+    "SasView*.app/Contents/Resources/PySide6/Qt/lib/*", recursive=True
+)
+
 sign_command = ['codesign', '--timestamp', '--options=runtime', '--verify', '--verbose=4', '--force',
                 '--sign', 'Developer ID Application: European Spallation Source Eric (W2AG9MPZ43)']
 
 
 #TODO: Check if it is necesarry to do it per file (one long list maybe enough)
-for sfile in itertools.chain(so_list, dylib_list, dylib_list_resources, zmq_dylib_list_resources):
+for sfile in itertools.chain(so_list, dylib_list,
+                             dylib_list_resources,
+                             zmq_dylib_list_resources,
+                             pyside_libs):
     sign_command.append(sfile)
     subprocess.check_call(sign_command)
     sign_command.pop()
