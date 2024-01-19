74fd05408421100175b533647c3aec85898315cc
Jack <jrooks@udel.edu>
2024-01-19 15:56:30 -0500

    Revert change to setting the angle with a center angle of alpha (which did not exist?) to theta

--- src/sas/qtgui/Plotting/Slicers/SectorSlicer.py
+++ src/sas/qtgui/Plotting/Slicers/SectorSlicer.py
@@ -385,13 +385,13 @@ class SideInteractor(BaseInteractor):
         else:
             self.phi = numpy.fabs(self.phi)
         if side:
-            self.theta = mline.alpha + self.phi
+            self.theta = mline.theta + self.phi
 
         if mline is not None:
             if delta != 0:
                 self.theta2 = mline + delta
             else:
-                self.theta2 = mline.alpha
+                self.theta2 = mline.theta
         if delta == 0:
             theta3 = self.theta + delta
         else:
