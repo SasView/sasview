ceb97b5e654c74064ca6a0f7580d33d95a4837f0
krzywon <krzywon@yahoo.com>
2024-02-22 16:30:23 -0500

    Remove explicit QtWebEngine code signing in an attempt to fix macOS help window not loading

--- build_tools/code_sign_osx.py
+++ build_tools/code_sign_osx.py
@@ -15,20 +15,14 @@ zmq_dylib_list_resources = glob.glob(
     "SasView*.app/Contents/Resources/zmq/.dylibs/*.dylib", recursive=True
 )
 
-pyside_QtWebEngineProcessApp = glob.glob(
-    "SasView*.app/Contents/Resources/PySide6/Qt/lib/QtWebEngineCore.framework/Versions/A/Helpers/QtWebEngineProcess.app", recursive=True
+pyside_Qt_Helpers = glob.glob(
+    "SasView*.app/Contents/Resources/PySide6/Qt/lib/Qt*.framework/Helpers/Qt*.app", recursive=True
 )
-
-pyside_QtWebEngineCore = glob.glob(
-    "SasView*.app/Contents/Resources/PySide6/Qt/lib/QtWebEngineCore.framework/Versions/A/QtWebEngineCore", recursive=True
-)
-
-pyside_QtWebEngineProcess_Helpers = glob.glob(
-    "SasView*.app/Contents/Resources/PySide6/Qt/lib/QtWebEngineCore.framework/Helpers/QtWebEngineProcess.app/Contents/MacOS/QtWebEngineProcess", recursive=True
-)
-
 pyside_Qtlibs = glob.glob(
     "SasView*.app/Contents/Resources/PySide6/Qt/lib/Qt*.framework/Versions/A/Qt*", recursive=True
+)
+pyside_QtApp_Helpers = glob.glob(
+    "SasView*.app/Contents/Resources/PySide6/Qt/lib/Qt*.framework/Versions/A/Helpers/Qt*.app", recursive=True
 )
 
 #pyside_libs = pyside_QtWebEngineCore + pyside_QtWebEngineProcess
@@ -40,7 +34,7 @@ sign_deep_command = ['codesign', '--timestamp', '--deep', '--options=runtime', '
                  '--sign',  'Developer ID Application: European Spallation Source Eric (W2AG9MPZ43)']
 
 #Signing QtWebEngineProcess.app first as it is a helper app
-for sfile in itertools.chain(pyside_QtWebEngineProcessApp):
+for sfile in itertools.chain(pyside_QtApp_Helpers):
     sign_deep_command.append(sfile)
     subprocess.check_call(sign_deep_command)
     sign_deep_command.pop()
@@ -48,8 +42,7 @@ for sfile in itertools.chain(pyside_QtWebEngineProcessApp):
 for sfile in itertools.chain(so_list, dylib_list,
                              dylib_list_resources,
                              zmq_dylib_list_resources,
-                             pyside_QtWebEngineCore,
-                             pyside_QtWebEngineProcess_Helpers,
+                             pyside_Qt_Helpers,
                              pyside_Qtlibs):
     sign_command.append(sfile)
     subprocess.check_call(sign_command)
