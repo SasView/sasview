bb1cfd1921ba063391a91a8dc7dfe6b843181bda
klytje <k.relle@live.dk>
2024-02-29 12:29:46 +0100

    added apt-get update to github runners

--- .github/workflows/matrix.py
+++ .github/workflows/matrix.py
@@ -26,9 +26,9 @@ jobs = []
 #   dynamically linked by pyinstaller.
 #   https://pyinstaller.readthedocs.io/en/stable/usage.html#making-gnu-linux-apps-forward-compatible
 os_release_list = [
-#    'ubuntu-20.04',
-#    'windows-latest',
-#    'macos-latest', 
+    'ubuntu-20.04',
+    'windows-latest',
+    'macos-latest', 
 ]
 
 # List of OS images to use for release tests
