ec3a066f98e223a5f41e63a4169e25c0176606c8
krellemeister <k.relle@live.dk>
2024-05-10 17:12:49 +0200

    re-added tests to macos

--- test/sascalculator/utest_sas_gen.py
+++ test/sascalculator/utest_sas_gen.py
@@ -230,11 +230,6 @@ class sas_gen_test(unittest.TestCase):
 
         rng = np.random.default_rng(1984)
 
-        from sas.sascalc.calculator.ausaxs.architecture import get_os, OS
-        if get_os() is OS.MAC:
-            self.assertTrue(True, "AUSAXS library is currently not available for MacOS. Skipping test.")
-            return
-
         if not ausaxs_sans_debye.ausaxs_available():
             self.assertTrue(False, "AUSAXS library not found, test cannot be run.")
             return
