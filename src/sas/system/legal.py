2cadc55ea9c60b924117901278a45f013996b193
krzywon <krzywon@yahoo.com>
2024-05-24 13:25:34 -0400

    Automatically grab the year in sas.system.legal

--- src/sas/system/legal.py
+++ src/sas/system/legal.py
@@ -1,6 +1,10 @@
+import datetime
+
+
 class Legal:
     def __init__(self):
-        self.copyright = "Copyright (c) 2009-2023 UTK, UMD, ESS, NIST, ORNL, ISIS, ILL, DLS, TUD, BAM and ANSTO"
+        year = datetime.datetime.now().year
+        self.copyright = f"Copyright (c) 2009-{year}, SasView Developers"
 
 
 legal = Legal()
