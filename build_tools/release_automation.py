cae812c2427ac6a0a675f70cc5fa8cb62f8d8379
krzywon <krzywon@yahoo.com>
2024-06-04 17:10:18 -0400

    Link to correct tag in release_automation.py

--- build_tools/release_automation.py
+++ build_tools/release_automation.py
@@ -38,7 +38,7 @@ sasview_data = {
 'metadata': {
     'title': f'SasView version 6.0.0',
     'description': f'6.0.0 release',
-    'related_identifiers': [{'identifier': 'https://github.com/SasView/sasview/releases/tag/v5.0.6',
+    'related_identifiers': [{'identifier': 'https://github.com/SasView/sasview/releases/tag/v6.0.0-beta-1',
                         'relation': 'isAlternateIdentifier', 'scheme': 'url'}],
     'contributors': [
         {'name': 'Anuchitanukul, Atijit', 'affiliation': 'STFC - Rutherford Appleton Laboratory', 'type':'Researcher'},
