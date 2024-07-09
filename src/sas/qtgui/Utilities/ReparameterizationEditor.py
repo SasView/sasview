f0f4fade071be76cc5850fe81b75214926afcce0
tsole0 <locke.apogee@gmail.com>
2024-07-09 15:40:36 -0400

    add `ReparameterEditDialog.py` and logic for creating parameters and transmitting to main editor window.

--- src/sas/qtgui/Utilities/ReparameterizationEditor.py
+++ src/sas/qtgui/Utilities/ReparameterizationEditor.py
@@ -2,6 +2,7 @@ from PySide6 import QtWidgets, QtCore, QtGui
 
 from sas.qtgui.Utilities.UI.ReparameterizationEditorUI import Ui_ReparameterizationEditor
 from sas.qtgui.Utilities.ModelSelector import ModelSelector
+from sas.qtgui.Utilities.ParameterEditDialog import ParameterEditDialog
 
 class ReparameterizationEditor(QtWidgets.QDialog, Ui_ReparameterizationEditor):
 
@@ -17,23 +18,26 @@ class ReparameterizationEditor(QtWidgets.QDialog, Ui_ReparameterizationEditor):
     def addSignals(self):
         self.selectModelButton.clicked.connect(self.onSelectModel)
         self.cmdCancel.clicked.connect(self.close)
+        self.cmdAddParam.clicked.connect(self.onAddParam)
 
     def onSelectModel(self):
         """
         Launch model selection dialog
         """
         self.model_selector = ModelSelector(self)
-        self.model_selector.returnModelParamsSignal.connect(self.loadOldParams)
+        self.model_selector.returnModelParamsSignal.connect(lambda params: self.loadParams(params, self.oldParamTree))
         self.model_selector.show()
 
-    def loadOldParams(self, params):
+    def loadParams(self, params, tree):
         """
         Load parameters from the selected model into the oldParamTree
+        :param param:
+        :param tree: the tree widget to load the parameters into
         """
         for param in params:
-            item = QtWidgets.QTreeWidgetItem(self.oldParamTree)
+            item = QtWidgets.QTreeWidgetItem(tree)
             item.setText(0, param.name)
-            self.oldParamTree.addTopLevelItem(item)
+            tree.addTopLevelItem(item)
 
             # Create list of specs: (display name, property name)
             specs = [ ("default", "default"),
@@ -53,7 +57,14 @@ class ReparameterizationEditor(QtWidgets.QDialog, Ui_ReparameterizationEditor):
                     sub_item.setText(1, str(getattr(param, prop_name)[index]))
                 else:
                     sub_item.setText(1, str(getattr(param, spec[1])))
-                    
+    
+    def onAddParam(self):
+        """
+        Add parameter to "New parameters box" by invoking parameter editor dialog
+        """
+        self.param_editor = ParameterEditDialog(self)
+        self.param_editor.returnNewParamsSignal.connect(lambda params: self.loadParams(params, self.newParamTree))
+        self.param_editor.show()
 
     def closeEvent(self, event):
         self.close()
