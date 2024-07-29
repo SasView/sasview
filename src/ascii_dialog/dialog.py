d16ad34a83d19eb1a424b5c9d2f39c5ca9f0ae10
James Crake-Merani <James.Crake-Merani@stfc.ac.uk>
2024-07-29 10:12:41 +0100

    Loading file dialog.

--- src/ascii_dialog/dialog.py
+++ src/ascii_dialog/dialog.py
@@ -1,9 +1,20 @@
-from PySide6.QtWidgets import QPushButton, QWidget, QApplication
+from PySide6.QtWidgets import QFileDialog, QLabel, QPushButton, QVBoxLayout, QWidget, QApplication
+from PySide6.QtCore import Slot
 
 class AsciiDialog(QWidget):
     def __init__(self):
         super().__init__()
 
+        self.load_button = QPushButton("Load File")
+        self.load_button.clicked.connect(self.load)
+
+        self.layout = QVBoxLayout(self)
+        self.layout.addWidget(self.load_button)
+
+    @Slot()
+    def load(self):
+        filename = QFileDialog.getOpenFileName(self)
+        print(filename)
 
 
 if __name__ == "__main__":
