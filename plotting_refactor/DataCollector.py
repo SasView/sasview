45c7979248f9f34f4c0bf6f50b755a43739aa6a2
Julius Karliczek <karliczek@ill.fr>
2024-07-29 11:58:10 +0200

    Non logic changing changes for DataCollector.py. Docstrings, changing for-loops in favor of for-each loops and other styling stuff

--- plotting_refactor/DataCollector.py
+++ plotting_refactor/DataCollector.py
@@ -1,35 +1,50 @@
-#class to keep track of all datasets of fitpages and more
+from PySide6 import QtWidgets
 import RandomDatasetCreator
 from Dataset import Dataset
-from typing import List
 
 class DataCollector:
+    """
+    This class keeps track of all generated datasets. When the update_dataset is called through onCalculate
+    from MainWindow, either a new dataset can be created or the existing dataset is adjusted with respect to
+    the currently displayed SpinBox Values from the Fitpage.
+    """
     def __init__(self):
-        self.datasets: List[Dataset] = []
+        self.datasets: list[Dataset] = []
         self.datasetcreator = RandomDatasetCreator.DatasetCreator()
 
-    def update_dataset(self, main_window, fitpage_index, create_fit, checked_2d):
+    def update_dataset(self, main_window: QtWidgets.QMainWindow, fitpage_index: int,
+                       create_fit: bool, checked_2d: bool):
+        """
+        Search for an existing dataset saved in here. If no dataset for the corresponding fitpage exists: create new
+        data.
+        """
+
         # search for an existing dataset with the right fitpage_index
         existing_dataset_index = -1
-        for i in range(len(self.datasets)):
-            if self.datasets[i].get_fitpage_index() == fitpage_index:
+        for i, dataset in enumerate(self.datasets):
+            if dataset.get_fitpage_index() == fitpage_index:
                 existing_dataset_index = i
+
         if existing_dataset_index == -1:
             # create new dataset in case it does not exist
-            x_data, y_data, y_fit = self.simulate_data(main_window, fitpage_index, create_fit, checked_2d)
+            x_data, y_data, y_fit = self.simulate_data(main_window, create_fit, checked_2d)
             plotpage_index = -1
 
             dataset = Dataset(fitpage_index, x_data, y_data, y_fit, checked_2d, plotpage_index)
             self.datasets.append(dataset)
         else:
             # update values for existing dataset with respect to the number boxes in the fitpage
-            x_data, y_data, y_fit = self.simulate_data(main_window, fitpage_index, create_fit, checked_2d)
+            x_data, y_data, y_fit = self.simulate_data(main_window, create_fit, checked_2d)
             self.datasets[existing_dataset_index].set_x_data(x_data)
             self.datasets[existing_dataset_index].set_y_data(y_data)
             self.datasets[existing_dataset_index].set_y_fit(y_fit)
             self.datasets[existing_dataset_index].set_2d(checked_2d)
 
-    def simulate_data(self, main_window, fitpage_index, create_fit, checked_2d):
+    def simulate_data(self, main_window: QtWidgets.QMainWindow, create_fit: bool, checked_2d: bool):
+        """
+        Collect all information from the FitPage from the MainWindow hat is needed to calculate the test data.
+        Feed this information to the DatasetCreator and return the values.
+        """
         combobox_index = main_window.fittingTabs.currentWidget().get_combobox_index()
         param_scale = main_window.fittingTabs.currentWidget().doubleSpinBox_scale.value()
         param_radius = main_window.fittingTabs.currentWidget().doubleSpinBox_radius.value()
@@ -40,41 +55,64 @@ class DataCollector:
 
         return x_data, y_data, y_fit
 
-    def get_datasets(self) -> List:
+    def get_datasets(self) -> list:
         return self.datasets
 
-    def get_data_fp(self, fitpage_index) -> Dataset:
-        for i in range(len(self.datasets)):
-            if fitpage_index == self.datasets[i].get_fitpage_index():
-                return self.datasets[i]
+    def get_data_fp(self, fitpage_index: int) -> Dataset:
+        """
+        Get the dataset for a certain fitpage
+        """
+        for dataset in self.datasets:
+            if fitpage_index == dataset.get_fitpage_index():
+                return dataset
 
-    def get_data_id(self, data_id) -> Dataset:
-        for i in range(len(self.datasets)):
-            if data_id == self.datasets[i].get_data_id():
-                return self.datasets[i]
+    def get_data_id(self, data_id: int) -> Dataset:
+        """
+        Get the dataset for certain id
+        """
+        for dataset in self.datasets:
+            if data_id == dataset.get_data_id():
+                return dataset
 
-    def get_x_data(self, fitpage_index) -> List:
-        for i in range(len(self.datasets)):
-            if fitpage_index == self.datasets[i].get_fitpage_index():
-                return self.datasets[i].get_x_data()
+    def get_x_data(self, fitpage_index: int) -> list:
+        """
+        Get x data for certain fitpage index
+        """
+        for dataset in self.datasets:
+            if fitpage_index == dataset.get_fitpage_index():
+                return dataset.get_x_data()
 
-    def get_y_data(self, fitpage_index) -> List:
-        for i in range(len(self.datasets)):
-            if fitpage_index == self.datasets[i].get_fitpage_index():
-                return self.datasets[i].get_y_data()
+    def get_y_data(self, fitpage_index: int) -> list:
+        """
+        Get y data for certain fitpage index
+        """
+        for dataset in self.datasets:
+            if fitpage_index == dataset.get_fitpage_index():
+                return dataset.get_y_data()
 
-    def get_y_fit_data(self, fitpage_index) -> List:
-        for i in range(len(self.datasets)):
-            if fitpage_index == self.datasets[i].get_fitpage_index():
-                return self.datasets[i].get_y_fit()
+    def get_y_fit_data(self, fitpage_index: int) -> list:
+        """
+        Get y fit data for certain fitpage index
+        """
+        for dataset in self.datasets:
+            if fitpage_index == dataset.get_fitpage_index():
+                return dataset.get_y_fit()
 
-    def get_plotpage_index(self, fitpage_index) -> int:
-        for i in range(len(self.datasets)):
-            if fitpage_index == self.datasets[i].get_fitpage_index():
-                return self.datasets[i].get_plotpage_index()
+    def get_plotpage_index(self, fitpage_index: int) -> int:
+        """
+        Get the plotpage index for a certain fitpage index: Plotpage index refers to the index of the major tabs in
+        the plotting widget in which the data is displayed.
+        """
+        for dataset in self.datasets:
+            if fitpage_index == dataset.get_fitpage_index():
+                return dataset.get_plotpage_index()
 
-    def set_plot_index(self, fitpage_index, plot_index):
-        for i in range(len(self.datasets)):
-            if fitpage_index == self.datasets[i].get_fitpage_index():
-                self.datasets[i].set_plotpage_index(plot_index)
+    def set_plot_index(self, fitpage_index: int, plot_index: int):
+        """
+        Set the plotpage index for the dataset for a certain fitpage index. Plotpage index refers to the index of the
+        major tabs in the plotting widget in which the data is displayed.
+        """
+        for dataset in self.datasets:
+            if fitpage_index == dataset.get_fitpage_index():
+                dataset.set_plotpage_index(plot_index)
 
