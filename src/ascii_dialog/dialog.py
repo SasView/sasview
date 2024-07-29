3b333948be8d9334ae6405396d1e9ba12c0f8cb3
James Crake-Merani <James.Crake-Merani@stfc.ac.uk>
2024-07-29 10:55:09 +0100

    Added param setting for seperator.

--- src/ascii_dialog/dialog.py
+++ src/ascii_dialog/dialog.py
@@ -1,5 +1,6 @@
-from PySide6.QtWidgets import QFileDialog, QLabel, QPushButton, QVBoxLayout, QWidget, QApplication
+from PySide6.QtWidgets import QFileDialog, QHBoxLayout, QLabel, QLineEdit, QPushButton, QVBoxLayout, QWidget, QApplication
 from PySide6.QtCore import Slot
+from guess import guess_seperator
 from os import path
 
 class AsciiDialog(QWidget):
@@ -13,10 +14,26 @@ class AsciiDialog(QWidget):
         self.load_button = QPushButton("Load File")
         self.load_button.clicked.connect(self.load)
 
+        # Data parameters
+        self.sep_layout = QHBoxLayout()
+        self.sep_label = QLabel('Seperator')
+        self.sep_entry = QLineEdit()
+        self.sep_layout.addWidget(self.sep_label)
+        self.sep_layout.addWidget(self.sep_entry)
+
         self.layout = QVBoxLayout(self)
 
         self.layout.addWidget(self.filename_label)
         self.layout.addWidget(self.load_button)
+        self.layout.addLayout(self.sep_layout)
+
+    def attempt_guesses(self):
+        guessed_seperator = guess_seperator(self.raw_csv)
+        if guessed_seperator == None:
+            # Seperator couldn't be guessed; just let the user fill that in.
+            guessed_seperator = ''
+
+        self.sep_entry.setText(guessed_seperator)
 
     @Slot()
     def load(self):
@@ -26,6 +43,8 @@ class AsciiDialog(QWidget):
         # TODO: Add error handling
         with open(filename) as file:
             self.raw_csv = file.readlines()
+
+        self.attempt_guesses()
 
 
 if __name__ == "__main__":
