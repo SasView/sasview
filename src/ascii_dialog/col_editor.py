16fd04c434093387a5b2154167129a08217a1c7c
James Crake-Merani <James.Crake-Merani@stfc.ac.uk>
2024-07-30 13:25:49 +0100

    Use the options for the dataset type.

--- src/ascii_dialog/col_editor.py
+++ src/ascii_dialog/col_editor.py
@@ -1,22 +1,22 @@
 from PySide6.QtWidgets import QComboBox, QHBoxLayout, QWidget
 
-def create_col_combo_box() -> QComboBox:
-    placeholder_options = ["First", "Second", "Third"]
-    new_combo_box = QComboBox()
-    for option in placeholder_options:
-        new_combo_box.addItem(option)
-    return new_combo_box
 
 class ColEditor(QWidget):
-    def __init__(self, cols: int):
+    def create_col_combo_box(self) -> QComboBox:
+        new_combo_box = QComboBox()
+        for option in self.options:
+            new_combo_box.addItem(option)
+        return new_combo_box
+    def __init__(self, cols: int, options: list[str]):
         super().__init__()
 
         self.cols = cols
+        self.options = options
         self.layout = QHBoxLayout(self)
         self.option_widgets = []
         for _ in range(cols):
             # TODO: This is placeholder data.
-            new_combo_box = create_col_combo_box()
+            new_combo_box = self.create_col_combo_box()
             self.option_widgets.append(new_combo_box)
             self.layout.addWidget(new_combo_box)
 
@@ -25,7 +25,7 @@ class ColEditor(QWidget):
         # remove some.
         if self.cols < new_cols:
             for _ in range(new_cols - self.cols):
-                new_combo_box = create_col_combo_box()
+                new_combo_box = self.create_col_combo_box()
                 self.option_widgets.append(new_combo_box)
                 self.layout.addWidget(new_combo_box)
             self.cols = new_cols
