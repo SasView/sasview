a94ca77d9058172f8e8f611283255f5e420b8cee
James Crake-Merani <James.Crake-Merani@stfc.ac.uk>
2024-07-30 11:46:56 +0100

    Correct expression.

--- src/ascii_dialog/dialog.py
+++ src/ascii_dialog/dialog.py
@@ -100,7 +100,7 @@ class AsciiDialog(QWidget):
                     case 'Comma':
                         seperator_text = r','
                     case 'Whitespace':
-                        seperator_text = r'␣+'
+                        seperator_text = r'\s+'
                     case 'Tab':
                         seperator_text = r'\t'
                 expr += seperator_text
