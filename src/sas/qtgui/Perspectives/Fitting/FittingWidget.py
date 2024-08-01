4c8d3e04ada98ee6805ff53f9039c01dc0357256
rozyczko <piotr.rozyczko@gmail.com>
2024-08-01 10:45:48 +0200

    try setting the default values to -1

--- src/sas/qtgui/Perspectives/Fitting/FittingWidget.py
+++ src/sas/qtgui/Perspectives/Fitting/FittingWidget.py
@@ -263,8 +263,8 @@ class FittingWidget(QtWidgets.QWidget, Ui_FittingWidgetUI):
         # Utility variable to enable unselectable option in category combobox
         self._previous_category_index = 0
         # Utility variables for multishell display
-        self._n_shells_row = 0
-        self._num_shell_params = 0
+        self._n_shells_row = -1
+        self._num_shell_params = -1
         # Dictionary of {model name: model class} for the current category
         self.models = {}
         # Dictionary of QModels
@@ -4467,9 +4467,6 @@ class FittingWidget(QtWidgets.QWidget, Ui_FittingWidgetUI):
             if self.kernel_module.is_multiplicity_model:
                 self.kernel_module.multiplicity=multip
                 self.updateMultiplicityCombo(multip)
-        else:
-            self._n_shells_row = -1
-            self._num_shell_params = -1
 
         if 'tab_name' in line_dict.keys() and self.kernel_module is not None:
             self.kernel_module.name = line_dict['tab_name'][0]
