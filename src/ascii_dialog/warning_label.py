0dca37412399c1ba9e64300aa17d0d4868f6ca63
James Crake-Merani <James.Crake-Merani@stfc.ac.uk>
2024-08-02 12:17:09 +0100

    Abstract the font setting.

--- src/ascii_dialog/warning_label.py
+++ src/ascii_dialog/warning_label.py
@@ -4,17 +4,22 @@ from PySide6.QtWidgets import QLabel
 
 
 class WarningLabel(QLabel):
+    def set_font_red(self):
+        self.setStyleSheet("QLabel { color: red}")
+
+    def set_font_normal(self):
+        self.setStyleSheet('')
+
     def update(self, missing_columns, duplicate_classes):
         if len(missing_columns) != 0:
             self.setText(f'The following columns are missing: {missing_columns}')
-            self.setStyleSheet("QLabel { color: red}")
+            self.set_font_red()
         elif len(duplicate_classes) > 0:
             self.setText(f'There are duplicate columns.')
-            self.setStyleSheet("QLabel { color: red}")
+            self.set_font_red()
         else:
             self.setText('All is fine') # TODO: Probably want to find a more appropriate message.
-            self.setStyleSheet('')
-
+            self.set_font_normal()
 
     def __init__(self, initial_missing_columns, initial_duplicate_classes):
         super().__init__()
