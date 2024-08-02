44d5ce3db1378073221c9d68ff31eab235112cc6
James Crake-Merani <James.Crake-Merani@stfc.ac.uk>
2024-08-02 11:05:35 +0100

    If no missing, all is fine.

--- src/ascii_dialog/warning_label.py
+++ src/ascii_dialog/warning_label.py
@@ -6,7 +6,8 @@ from PySide6.QtWidgets import QLabel
 class WarningLabel(QLabel):
     def update(self, missing_columns):
         if len(missing_columns) == 0:
-            pass # TODO: Set normal message
+            self.setText('All is fine') # TODO: Probably want to find a more appropriate message.
+            self.setStyleSheet('')
         else:
             self.setText(f'The following columns are missing: {missing_columns}')
             self.setStyleSheet("QLabel { color: red}")
