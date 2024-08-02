a8b59cc95759915a2a37431db56ae9284ed32ed1
James Crake-Merani <James.Crake-Merani@stfc.ac.uk>
2024-08-02 12:01:31 +0100

    Changed orderings to fix bug.

--- src/ascii_dialog/warning_label.py
+++ src/ascii_dialog/warning_label.py
@@ -5,15 +5,15 @@ from PySide6.QtWidgets import QLabel
 
 class WarningLabel(QLabel):
     def update(self, missing_columns, duplicate_classes):
-        if len(missing_columns) == 0:
-            self.setText('All is fine') # TODO: Probably want to find a more appropriate message.
-            self.setStyleSheet('')
+        if len(missing_columns) != 0:
+            self.setText(f'The following columns are missing: {missing_columns}')
+            self.setStyleSheet("QLabel { color: red}")
         elif len(duplicate_classes) > 0:
             self.setText(f'There are duplicate columns.')
             self.setStyleSheet("QLabel { color: red}")
         else:
-            self.setText(f'The following columns are missing: {missing_columns}')
-            self.setStyleSheet("QLabel { color: red}")
+            self.setText('All is fine') # TODO: Probably want to find a more appropriate message.
+            self.setStyleSheet('')
 
 
     def __init__(self, initial_missing_columns, initial_duplicate_classes):
