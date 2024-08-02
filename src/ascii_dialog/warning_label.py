7f9bdf3853b385d854cfa5cd1570434f3d41253e
James Crake-Merani <James.Crake-Merani@stfc.ac.uk>
2024-08-02 11:26:10 +0100

    Add functionality for duplicate columns.

--- src/ascii_dialog/warning_label.py
+++ src/ascii_dialog/warning_label.py
@@ -4,15 +4,18 @@ from PySide6.QtWidgets import QLabel
 
 
 class WarningLabel(QLabel):
-    def update(self, missing_columns):
+    def update(self, missing_columns, duplicate_classes):
         if len(missing_columns) == 0:
             self.setText('All is fine') # TODO: Probably want to find a more appropriate message.
             self.setStyleSheet('')
+        elif len(duplicate_classes) > 0:
+            self.setText(f'There are duplicate columns.')
+            self.setStyleSheet("QLabel { color: red}")
         else:
             self.setText(f'The following columns are missing: {missing_columns}')
             self.setStyleSheet("QLabel { color: red}")
 
 
-    def __init__(self, initial_missing_columns):
+    def __init__(self, initial_missing_columns, initial_duplicate_classes):
         super().__init__()
-        self.update(initial_missing_columns)
+        self.update(initial_missing_columns, initial_duplicate_classes)
