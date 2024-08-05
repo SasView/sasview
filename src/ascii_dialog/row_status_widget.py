b1baca964eabc35df1965c391154e507dabb3408
James Crake-Merani <James.Crake-Merani@stfc.ac.uk>
2024-08-05 09:54:35 +0100

    Update label based on state.

--- src/ascii_dialog/row_status_widget.py
+++ src/ascii_dialog/row_status_widget.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python3
 
+from PySide6.QtGui import Qt
 from PySide6.QtWidgets import QCheckBox
 
 
 class RowStatusWidget(QCheckBox):
+    def update_label(self):
+        current_status = self.checkState()
+        match current_status:
+            case Qt.CheckState.Unchecked:
+                self.setText('Not included')
+            case Qt.CheckState.PartiallyChecked:
+                self.setText('Included as metadata')
+            case Qt.CheckState.Checked:
+                self.setText('Included as data')
+
     def __init__(self):
         super().__init__()
         self.setTristate(True)
