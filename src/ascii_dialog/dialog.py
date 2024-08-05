92e38ab29d43649e3d4d94c676a3f2c67c20535e
James Crake-Merani <James.Crake-Merani@stfc.ac.uk>
2024-08-05 12:14:03 +0100

    Change font and colour depending on check state.

--- src/ascii_dialog/dialog.py
+++ src/ascii_dialog/dialog.py
@@ -183,8 +183,14 @@ class AsciiDialog(QWidget):
                 if j >= col_count:
                     continue # Ignore rows that have extra columns.
                 item = QTableWidgetItem(col_value)
-                if i < starting_pos:
+                if initial_state == Qt.CheckState.PartiallyChecked:
                     item.setForeground(QColor.fromString('grey'))
+                elif initial_state == Qt.CheckState.Unchecked:
+                    item.setForeground(QColor.fromString('grey'))
+                    item_font = item.font()
+                    item_font.setStrikeOut(True)
+                    item.setFont(item_font)
+                    item.font().setStrikeOut(True)
                 self.table.setItem(i, j + 1, item)
             if i == TABLE_MAX_ROWS:
                 break
