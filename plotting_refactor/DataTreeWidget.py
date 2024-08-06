f72b66a135b5a15b4a7c8ae525f379d9aff8b3c9
Julius Karliczek <karliczek@ill.fr>
2024-08-06 17:06:40 +0200

    DataTreeWidget: add docstrings and type specifics for method headers

--- plotting_refactor/DataTreeWidget.py
+++ plotting_refactor/DataTreeWidget.py
@@ -1,18 +1,29 @@
 from PySide6.QtWidgets import QTreeWidget, QTreeWidgetItem, QAbstractItemView
-from PySide6.QtCore import QMimeData, QRect, QByteArray, QDataStream, QIODevice
+from PySide6.QtCore import QMimeData, QRect, QByteArray, QDataStream, QIODevice, Qt
 from PySide6.QtGui import QDrag
 from DataTreeItems import DataItem
+from DataViewer import DataViewer
+from DataCollector import DataCollector
 
 class DataTreeWidget(QTreeWidget):
-    def __init__(self, DataViewer, datacollector):
-        super().__init__(parent=DataViewer)
+    """
+    Tree widget that is appearing in the DataViewer. It represents data stored in the DataCollector by objects from
+    DataTreeItems. Instantiating of these DataTreeItems happens in the DataViewer.
+    """
+    def __init__(self, dataviewer: DataViewer, datacollector: DataCollector):
+        super().__init__(parent=dataviewer)
         self.datacollector = datacollector
         self.setGeometry(QRect(10, 10, 391, 312))
         self.setDragEnabled(True)
         self.setColumnCount(1)
         self.setHeaderLabels(["Data Name"])
 
-    def startDrag(self, supportedActions):
+    def startDrag(self, supportedActions: Qt.DropAction):
+        """
+        Overwriting the startDrag from the normal QTreeWidget. When dragging the QTreeWidgetItem to another plot,
+        mimetypes ID and Type are used to store the dataset.data_id and the type_num. Type_num represents if the
+        item is a data, fit or residuals item.
+        """
         item = self.currentItem()
         if item:
             if isinstance(item.data(0, 1), DataItem):
