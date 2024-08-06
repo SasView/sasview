98a1b264e654401bf39889ca5962526b06a04967
James Crake-Merani <James.Crake-Merani@stfc.ac.uk>
2024-08-06 11:44:18 +0100

    Rename parameter.

--- src/ascii_dialog/warning_label.py
+++ src/ascii_dialog/warning_label.py
@@ -14,13 +14,13 @@ class WarningLabel(QLabel):
     def set_font_normal(self):
         self.setStyleSheet('')
 
-    def update(self, missing_columns, duplicate_classes):
+    def update(self, missing_columns, duplicate_columns):
         """Determine, and set the appropriate warning messages given how many
         columns are missing, and how many columns are duplicated."""
         if len(missing_columns) != 0:
             self.setText(f'The following columns are missing: {missing_columns}')
             self.set_font_red()
-        elif len(duplicate_classes) > 0:
+        elif len(duplicate_columns) > 0:
             self.setText(f'There are duplicate columns.')
             self.set_font_red()
         else:
