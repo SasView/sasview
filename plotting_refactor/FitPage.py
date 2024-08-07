a234e0e0b828d59db0c6ee438d3b119534f2448b
Julius Karliczek <karliczek@ill.fr>
2024-08-07 16:48:57 +0200

    change method parameter type specification and return type specification

--- plotting_refactor/FitPage.py
+++ plotting_refactor/FitPage.py
@@ -7,7 +7,7 @@ class FitPage(QtWidgets.QWidget, Ui_fitPageWidget):
     Widget that is shown in the tabs from the Mainwindow. Is a subclass of a widget to directly store fitpage indexes
     in it.
     """
-    def __init__(self, identifier):
+    def __init__(self, identifier: int):
         super(FitPage, self).__init__()
         self.setupUi(self)
 
@@ -19,19 +19,19 @@ class FitPage(QtWidgets.QWidget, Ui_fitPageWidget):
         self.comboBoxFormFactor.currentIndexChanged.connect(self.index_changed)
 
     @property
-    def identifier(self):
+    def identifier(self) -> int:
         return self._identifier
 
-    def get_combobox_index(self):
+    def get_combobox_index(self) -> int:
         return self.comboBoxFormFactor.currentIndex()
 
-    def get_checkbox_fit(self):
+    def get_checkbox_fit(self) -> bool:
         return self.checkBoxCreateFit.isChecked()
 
-    def get_checkbox_2d(self):
+    def get_checkbox_2d(self) -> bool:
         return self.checkBox2dData.isChecked()
 
-    def index_changed(self, selected_item):
+    def index_changed(self, selected_item: int):
         if selected_item == 0:
             self.doubleSpinBox_height.setDisabled(True)
         elif selected_item == 1:
