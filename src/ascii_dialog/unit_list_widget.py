ce8d6904afc6f84a936dd15f94d14bd1870b16b1
James Crake-Merani <James.Crake-Merani@stfc.ac.uk>
2024-08-09 15:21:40 +0100

    Have a way of getting the selected unit.

--- src/ascii_dialog/unit_list_widget.py
+++ src/ascii_dialog/unit_list_widget.py
@@ -10,9 +10,15 @@ class UnitListWidget(QListWidget):
 
     def populate_list(self, units: list[NamedUnit]) -> None:
         self.clear()
+        self.units = units
         for unit in units:
             item = QListWidgetItem(self.repr_unit(unit))
             self.addItem(item)
 
+    @property
+    def selected_unit(self) -> NamedUnit | None:
+        return self.units[self.currentRow()]
+
     def __init__(self):
         super().__init__()
+        self.units: list[NamedUnit] = []
