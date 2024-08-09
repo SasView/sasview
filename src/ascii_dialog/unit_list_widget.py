4f3fb36ca55983ce07b240b9520105a6134c4089
James Crake-Merani <James.Crake-Merani@stfc.ac.uk>
2024-08-09 11:27:20 +0100

    Include symbol in the representation on the list.

--- src/ascii_dialog/unit_list_widget.py
+++ src/ascii_dialog/unit_list_widget.py
@@ -5,10 +5,13 @@ from sasdata.quantities.units import NamedUnit
 
 
 class UnitListWidget(QListWidget):
+    def repr_unit(self, unit: NamedUnit) -> str:
+        return f"{unit.symbol} ({unit.name})"
+
     def populate_list(self, units: list[NamedUnit]) -> None:
         self.clear()
         for unit in units:
-            item = QListWidgetItem(unit.name)
+            item = QListWidgetItem(self.repr_unit(unit))
             self.addItem(item)
 
     def __init__(self):
