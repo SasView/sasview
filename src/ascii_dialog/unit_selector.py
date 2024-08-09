1396808827a1c98df3a54be0ea14e706a38cede0
James Crake-Merani <James.Crake-Merani@stfc.ac.uk>
2024-08-09 11:15:11 +0100

    Integrate the new list widget.

--- src/ascii_dialog/unit_selector.py
+++ src/ascii_dialog/unit_selector.py
@@ -1,5 +1,7 @@
-from PySide6.QtWidgets import QApplication, QComboBox, QVBoxLayout, QWidget
-from sasdata.quantities.units import length, area, volume, inverse_length, inverse_area, inverse_volume, time, rate, speed, density, force, pressure, energy, power, charge, potential, resistance
+from PySide6.QtWidgets import QApplication, QComboBox, QListWidget, QVBoxLayout, QWidget
+from sasdata.quantities.units import UnitGroup, length, area, volume, inverse_length, inverse_area, inverse_volume, time, rate, speed, density, force, pressure, energy, power, charge, potential, resistance
+
+from unit_list_widget import UnitListWidget
 
 # TODO: Ask Lucas if this list can be in his code (or if it already is and I
 # can't find it). I am lazy so only doing a subsection for now.
@@ -9,6 +11,10 @@ all_unit_groups = [
 ]
 
 class UnitSelector(QWidget):
+    def current_unit_group(self) -> UnitGroup:
+        index = self.unit_type_selector.currentIndex()
+        return all_unit_groups[index]
+
     def __init__(self):
         super().__init__()
 
@@ -16,8 +22,13 @@ class UnitSelector(QWidget):
         unit_group_names = [group.name for group in all_unit_groups]
         self.unit_type_selector.addItems(unit_group_names)
 
+        self.unit_list_widget = UnitListWidget()
+        # TODO: Are they all named units?
+        self.unit_list_widget.populate_list(self.current_unit_group().units)
+
         self.layout = QVBoxLayout(self)
         self.layout.addWidget(self.unit_type_selector)
+        self.layout.addWidget(self.unit_list_widget)
 
 if __name__ == "__main__":
     app = QApplication([])
