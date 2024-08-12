afc101fd1f2d78e164faebcbe4401aaac5c0f9f8
James Crake-Merani <James.Crake-Merani@stfc.ac.uk>
2024-08-12 10:57:58 +0100

    Add a line for each column.

--- src/ascii_dialog/unit_preferences.py
+++ src/ascii_dialog/unit_preferences.py
@@ -1,10 +1,28 @@
 #!/usr/bin/env python3
 
-from PySide6.QtWidgets import QApplication, QWidget
+from PySide6.QtWidgets import QApplication, QVBoxLayout, QWidget
+from sasdata.quantities.units import NamedUnit
+from sasdata.dataset_types import unit_kinds
+from unit_preference_line import UnitPreferenceLine
+import random
 
 class UnitPreferences(QWidget):
     def __init__(self):
         super().__init__()
+
+        # TODO: Presumably this will be loaded from some config from somewhere.
+        # For now just fill it with some placeholder values.
+        column_names = unit_kinds.keys()
+        self.columns: dict[str, NamedUnit] = {}
+        for name in column_names:
+            self.columns[name] = random.choice(unit_kinds[name].units)
+
+        self.layout = QVBoxLayout()
+        for column_name, unit in self.columns.items():
+            line = UnitPreferenceLine(column_name, unit, unit_kinds[column_name])
+            self.layout.addWidget(line)
+
+
 
 if __name__ == "__main__":
     app = QApplication([])
