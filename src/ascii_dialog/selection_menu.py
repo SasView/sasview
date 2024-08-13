5fd75e079b3a66d92eac0b6d0dd268b3155a19c2
James Crake-Merani <James.Crake-Merani@stfc.ac.uk>
2024-08-13 08:34:11 +0100

    Got the context menu to work.

--- src/ascii_dialog/selection_menu.py
+++ src/ascii_dialog/selection_menu.py
@@ -3,19 +3,19 @@
 
 from PySide6.QtCore import Signal
 from PySide6.QtGui import QAction
-from PySide6.QtWidgets import QMenu
+from PySide6.QtWidgets import QMenu, QWidget
 
 class SelectionMenu(QMenu):
     select_all_event = Signal()
     deselect_all_event = Signal()
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self, parent: QWidget):
+        super().__init__(parent)
 
-        select_all = QAction("Select All")
+        select_all = QAction("Select All", parent)
         select_all.triggered.connect(self.select_all_event)
 
-        deselect_all = QAction("Deselect All")
+        deselect_all = QAction("Deselect All", parent)
         deselect_all.triggered.connect(self.deselect_all_event)
 
         self.addAction(select_all)
