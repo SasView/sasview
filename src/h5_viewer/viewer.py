2aaa2c9c79f7eda768e4ebac4bf2ae30d18a31c0
James Crake-Merani <James.Crake-Merani@stfc.ac.uk>
2024-08-14 16:14:13 +0100

    Take an h5 file from command line to load.

--- src/h5_viewer/viewer.py
+++ src/h5_viewer/viewer.py
@@ -1,16 +1,30 @@
 #!/usr/bin/env python3
 
 import h5py
-from PySide6.QtWidgets import QApplication, QWidget
+from PySide6.QtWidgets import QApplication, QHBoxLayout, QWidget
+from h5_tree import Hd5TreeWidget
+from h5py import File as H5File
+from sys import argv
 
 class Hd5Viewer(QWidget):
-    def __init__(self):
+    def __init__(self, hd5_file: H5File):
         super().__init__()
+        self.hd5_file = hd5_file
+
+        # Tree widget
+        self.tree = Hd5TreeWidget(self.hd5_file)
+        self.tree.update_tree()
+
+        self.layout = QHBoxLayout()
+        self.layout.addWidget(self.tree)
+
 
 if __name__ == "__main__":
-    app = QApplication([])
-
-    widget = Hd5Viewer()
-    widget.show()
-
-    exit(app.exec())
+    if len(argv) < 2:
+        print('Please enter a file to load as a command line argument.')
+    else:
+        app = QApplication([])
+        to_load = H5File(argv[1], 'r')
+        widget = Hd5Viewer(to_load)
+        widget.show()
+        exit(app.exec())
