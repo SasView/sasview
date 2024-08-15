53d19c296eef252decae6058b8fc515159f73d82
James Crake-Merani <James.Crake-Merani@stfc.ac.uk>
2024-08-15 10:11:11 +0100

    Update view on new dataset.

--- src/h5_viewer/dataset_view.py
+++ src/h5_viewer/dataset_view.py
@@ -7,12 +7,16 @@ from h5py import Dataset
 class DatasetViewWidget(QWidget):
     def __init__(self, initial_dataset: Dataset | None):
         super().__init__()
-        self._current_dataset = initial_dataset
+        self._current_dataset: Dataset | None = initial_dataset
 
         self.dataset_label = QLabel('Select a dataset on the left to view it.')
 
         self.layout = QVBoxLayout(self)
         self.layout.addWidget(self.dataset_label)
+
+    def update_view(self):
+        if not self._current_dataset is None:
+            self.dataset_label.setText(self._current_dataset.name)
 
     @property
     def current_dataset(self):
@@ -21,3 +25,4 @@ class DatasetViewWidget(QWidget):
     @current_dataset.setter
     def current_dataset(self, value: Dataset):
         self._current_dataset = value
+        self.update_view()
