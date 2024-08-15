78b346eb3aa42de96f0b5d20f3b5202c033ac9a8
James Crake-Merani <James.Crake-Merani@stfc.ac.uk>
2024-08-15 11:26:28 +0100

    Code for drawing the table.

--- src/h5_viewer/dataset_view.py
+++ src/h5_viewer/dataset_view.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
-from PySide6.QtWidgets import QLabel, QVBoxLayout, QWidget
+from PySide6.QtWidgets import QLabel, QTableWidget, QTableWidgetItem, QVBoxLayout, QWidget
 from h5py import Dataset
+from numpy import ndarray
 
 
 class DatasetViewWidget(QWidget):
@@ -10,9 +11,13 @@ class DatasetViewWidget(QWidget):
         self._current_dataset: Dataset | None = initial_dataset
 
         self.dataset_label = QLabel()
+        self.table = QTableWidget()
+        # Make the table readonly
+        self.table.setEditTriggers(QTableWidget.EditTrigger.NoEditTriggers)
 
         self.layout = QVBoxLayout(self)
         self.layout.addWidget(self.dataset_label)
+        self.layout.addWidget(self.table)
 
         self.update_view()
 
@@ -21,6 +26,28 @@ class DatasetViewWidget(QWidget):
             self.dataset_label.setText('Select a dataset on the left to view it.')
         else:
             self.dataset_label.setText(self._current_dataset.name)
+        self.fill_table()
+
+    def fill_table(self):
+        self.table.clear()
+        if self._current_dataset is None:
+            self.table.setDisabled(True)
+        else:
+            self.table.setDisabled(False)
+            self.table.setColumnCount(self._current_dataset.ndim)
+            self.table.setRowCount(self._current_dataset.shape[0])
+            for i, row in enumerate(self._current_dataset):
+                # If the dataset is 1 dimensional then 'row' is just the value
+                # itself but otherwise it'll be an array of values.
+                if isinstance(row, ndarray):
+                    columns = row
+                else:
+                    columns = [row]
+                for j, col in enumerate(columns):
+                    item = QTableWidgetItem(str(col))
+                    self.table.setItem(i, j, item)
+        self.table.show()
+
 
     @property
     def current_dataset(self):
