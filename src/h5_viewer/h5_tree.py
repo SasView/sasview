7c180eaff994f048f629eb48f3b3e34576929600
James Crake-Merani <James.Crake-Merani@stfc.ac.uk>
2024-08-15 08:23:59 +0100

    Fixed type checking.

--- src/h5_viewer/h5_tree.py
+++ src/h5_viewer/h5_tree.py
@@ -4,6 +4,7 @@ from PySide6.QtWidgets import QTreeWidget, QTreeWidgetItem
 from h5py import File as H5File
 from h5py import Group as H5Group
 from h5py import Dataset
+from h5py._hl.group import Group
 
 class Hd5TreeWidget(QTreeWidget):
     def __init__(self, hd5_file: H5File):
@@ -11,11 +12,11 @@ class Hd5TreeWidget(QTreeWidget):
         self.hd5_file: H5File = hd5_file
 
     def __add_to_tree__(self, root: QTreeWidgetItem, group: H5Group):
-        for group_item in group.items():
-            if group_item is H5Group:
+        for name, group_item in group.items():
+            if isinstance(group_item, Group):
                 new_tree_item = QTreeWidgetItem(root, [group_item.name])
                 self.__add_to_tree__(new_tree_item, group_item)
-            elif group_item is Dataset:
+            elif isinstance(group_item, Dataset):
                 # TODO: Might be able to reduce code duplication here.
                 new_tree_item = QTreeWidgetItem(root, [group_item.name])
 
