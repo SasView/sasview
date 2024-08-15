8dc39810e6e45c64076d0fdab45356740041a54a
James Crake-Merani <James.Crake-Merani@stfc.ac.uk>
2024-08-15 09:07:30 +0100

    Use the name from the destructuring.

--- src/h5_viewer/h5_tree.py
+++ src/h5_viewer/h5_tree.py
@@ -15,11 +15,11 @@ class Hd5TreeWidget(QTreeWidget):
     def __add_to_tree__(self, root: QTreeWidgetItem, group: H5Group):
         for name, group_item in group.items():
             if isinstance(group_item, Group):
-                new_tree_item = QTreeWidgetItem(root, [group_item.name])
+                new_tree_item = QTreeWidgetItem(root, [name])
                 self.__add_to_tree__(new_tree_item, group_item)
             elif isinstance(group_item, Dataset):
                 # TODO: Might be able to reduce code duplication here.
-                new_tree_item = QTreeWidgetItem(root, [group_item.name])
+                new_tree_item = QTreeWidgetItem(root, [name])
 
 
     def update_tree(self):
