43a0a61e2da3361bec3cd0861a22afc13f55a736
James Crake-Merani <James.Crake-Merani@stfc.ac.uk>
2024-08-16 13:22:27 +0100

    Added some widgets.

--- src/h5_viewer/json_view.py
+++ src/h5_viewer/json_view.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from PySide6.QtWidgets import QWidget
+from PySide6.QtWidgets import QLabel, QTextEdit, QVBoxLayout, QWidget
 from json import dumps
 from pprint import pformat
 
@@ -8,6 +8,13 @@ class JsonViewWidget(QWidget):
     def __init__(self, initial_json_dict: dict[str, object]):
         super().__init__()
         self._json_dict: dict[str, object ] = initial_json_dict
+
+        self.label = QLabel('JSON Data')
+        self.text_box = QTextEdit()
+
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.label)
+        self.layout.addWidget(self.text_box)
 
     @property
     def current_json_dict(self):
