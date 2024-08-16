9f208e593c3bf00df30842a0dcb3fa465548a490
James Crake-Merani <James.Crake-Merani@stfc.ac.uk>
2024-08-16 09:56:22 +0100

    Use the str viewer on a str.

--- src/h5_viewer/viewer.py
+++ src/h5_viewer/viewer.py
@@ -9,6 +9,7 @@ from h5py import File as H5File
 from sys import argv
 
 from dataset_view import DatasetViewWidget
+from str_view import StrViewWidget
 
 class Hd5Viewer(QWidget):
     def __init__(self, hd5_file: H5File):
@@ -23,8 +24,10 @@ class Hd5Viewer(QWidget):
 
         # Viewer widget
         self.dataset_viewer = DatasetViewWidget(None)
+        self.str_viewer = StrViewWidget(None)
         self.stacked_viewers = QStackedWidget()
         self.stacked_viewers.addWidget(self.dataset_viewer)
+        self.stacked_viewers.addWidget(self.str_viewer)
 
         self.layout = QHBoxLayout(self)
         self.layout.addWidget(self.tree)
@@ -35,8 +38,13 @@ class Hd5Viewer(QWidget):
         new_selection = self.tree.selected_item
         if isinstance(new_selection, Dataset):
             self.dataset_viewer.current_dataset = new_selection
+            self.stacked_viewers.setCurrentIndex(0)
+        elif isinstance(new_selection, str):
+            self.str_viewer.current_str = new_selection
+            self.stacked_viewers.setCurrentIndex(1)
         else:
             self.dataset_viewer.current_dataset = None
+            self.stacked_viewers.setCurrentIndex(0)
 
 if __name__ == "__main__":
     if len(argv) < 2:
