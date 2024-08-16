d2ae8d5a1510138abc6d4b6ac92ec9821041ca9e
James Crake-Merani <James.Crake-Merani@stfc.ac.uk>
2024-08-16 09:56:11 +0100

    Show the current str value.

--- src/h5_viewer/str_view.py
+++ src/h5_viewer/str_view.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python3
 
-from PySide6.QtWidgets import QWidget
+from PySide6.QtWidgets import QLabel, QVBoxLayout, QWidget
 
 
-class StrView(QWidget):
-    def __init__(self, initial_str = str | None):
+class StrViewWidget(QWidget):
+    def __init__(self, initial_str: str | None):
         super().__init__()
         self._current_str: str | None = initial_str
+
+        self.value_label = QLabel()
+
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.value_label)
+
+    def update_view(self):
+        self.value_label.setText(f'Value: {self._current_str}')
 
     @property
     def current_str(self):
@@ -15,3 +23,4 @@ class StrView(QWidget):
     @current_str.setter
     def current_str(self, value: str | None):
         self._current_str = value
+        self.update_view()
