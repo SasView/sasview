211b06cd7c39277491a63bd363080cfea615d3e1
Julius Karliczek <karliczek@ill.fr>
2024-08-20 15:42:59 +0200

    Set TabbedPlotWidget icon and window name

--- src/sas/qtgui/Plotting/TabbedPlotWidget.py
+++ src/sas/qtgui/Plotting/TabbedPlotWidget.py
@@ -1,14 +1,23 @@
 from PySide6 import QtWidgets
+from PySide6 import QtCore
+from PySide6 import QtGui
 
 class TabbedPlotWidget(QtWidgets.QTabWidget):
     """
     Central plot widget that holds tabs and subtabs for all existing plots
     """
-    name = 'TabbedPlotWidget'
     def __init__(self, parent=None):
         super(TabbedPlotWidget, self).__init__()
 
         self.manager = parent
-        self.setObjectName('TabbedPlotWidget')
+
+        self._set_icon()
+        self.setWindowTitle('TabbedPlotWidget')
+
         self.setMinimumSize(500, 500)
-        self.hide()
+        self.show()
+
+    def _set_icon(self):
+        icon = QtGui.QIcon()
+        icon.addFile(u":/res/ball.ico", QtCore.QSize(), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.setWindowIcon(icon)
