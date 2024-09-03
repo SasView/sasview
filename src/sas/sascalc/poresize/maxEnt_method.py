be0e995612c21faccb3327041c07975bfb4d3e9e
butlerpd <butlerpd@udel.edu>
2024-09-03 23:23:07 +0000

    Start of new class to use sasmodels in porsize distribution

--- src/sas/sascalc/poresize/maxEnt_method.py
+++ src/sas/sascalc/poresize/maxEnt_method.py
@@ -1,6 +1,9 @@
 import numpy as np
 import math
-from sasdata.dataloader import data_info
+from sasmodels.core import load_model
+from sasmodels.direct_model import call_kernel
+from sasmodels.direct_model import DirectModel
+#from sasdata.dataloader import data_info
 
 # Constants (comments mostly copied from the original GSASIIsasd.py)
 TEST_LIMIT        = 0.05                    # for convergence
@@ -53,6 +56,59 @@ def SphereVol(Bins):
     Vol = (4./3.)*np.pi*Bins**3
     return Vol
 
+class dist_model():
+    """
+    This class is used to construct the matrix of I(q) curves at each value of
+    the dimension whose distribution is being sought. For example the Radius of
+    a sphere for the classic pore size distribution calculation. It uses the
+    full sasmodels infrastructre including volume and resolution corrections.
+
+    For this to work, Q needs to be a data1D type object with y=none and dy=none.
+    see sasda or sasmodels.data.data1D
+
+    Q is a data1D object with q and simga q taken from the loaded data (y and
+    dy should be set to NONE)
+    model is a string cotaining the name of the model (e.g. 'sphere')
+    pars is a dictionary of all parameters and their value for the given model
+    dimension is a string containing the name of the parameter that is being
+    varied (to find the distribution)
+    bins is an array of type float containing the various values of the
+    dimension variable that forms the distribution. Only the weights will be
+    changed.
+    QUESTION: is it required that the values in bins be sorted?
+
+    ..NOTE: this code is not yet ready to be used (clearly) but is meant as
+       a base structure
+    """
+    def __init__(self,Q, model, pars,dimension,bins):
+        self.q=Q
+        self.model = load_model(model)
+        self.params=pars
+        self.dim_distr=dimension
+        self.bins=bins
+
+
+    def FormFactor(self,value):
+        full_params = self.params
+        full_params[self.dim_distr]=value
+        # VERSION 1
+        # Iq = f(radius_polar=100) - this is from the example in script documentation
+        # This is probably the best choice?
+        f = DirectModel(self.q, self.model)
+        Iq = f(full_params)
+        # VERSION 2
+        # not sure this allows for passing the resolution function
+        # in other words the parameter is probably a q vector not a data1D object?
+        kernel = self.model.make_kernel([self.q])
+        Iq = call_kernel(kernel, full_params)
+        return Iq
+
+    def base_matrix(self):
+        for i in self.bins:
+            intensity[i] = self.FormFactor(i)
+        return np.dstack(intensity,self.bins)
+
+
 class matrix_operation():
     # Transformation matrix
     # TODO: Use sasmodels kernel to make this function more generalized
@@ -73,6 +129,8 @@ class matrix_operation():
         Gmat = Gmat.reshape((len(Bins),len(Q)))
         return Gmat
     
+    # this just looks like a matrix product. Why do we need a function to wrap
+    # a function? would not x=np.dot(m1,m2) be the same as x=matrix_transform(m1,m2)?
     def matrix_transform(self, m1, m2):
         '''
         Transform data-space -> solution-space or transform solution-space -> data-space
