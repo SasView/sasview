2ff082bc16ddc8599e7ccde18f7ea29dc989d01d
Julius Karliczek <karliczek@ill.fr>
2024-09-19 14:58:15 +0200

    Multiple plots can be plotted in one window on the first try. Therefore i introduced some way to reorganize the already existing axes in a subtab window.

--- src/sas/qtgui/Perspectives/Fitting/FittingWidget.py
+++ src/sas/qtgui/Perspectives/Fitting/FittingWidget.py
@@ -2449,10 +2449,8 @@ class FittingWidget(QtWidgets.QWidget, Ui_FittingWidgetUI):
         self.cmdPlot.setText("Compute/Plot")
         # Force data recalculation so existing charts are updated
         if not self.data_is_loaded:
-            print("showTheoryPlot from FittingWidget")
             self.showTheoryPlot()
         else:
-            print("showPlot from FittingWidget")
             self.showPlot()
         # This is an important processEvent.
         # This allows charts to be properly updated in order
@@ -2512,10 +2510,10 @@ class FittingWidget(QtWidgets.QWidget, Ui_FittingWidgetUI):
         # send this information to the TabbedPlotWidget so that it can unpack and show the plots as well
         self.parent.tabbedPlotWidget.add_tab(item_name, item_model, self.tab_id)
 
-        tab_index = self.parent.tabbedPlotWidget.tab_fitpage_dict[self.tab_id]
-        tpw_axes = self.parent.tabbedPlotWidget.widget(tab_index).ax
+        # tab_index = self.parent.tabbedPlotWidget.tab_fitpage_dict[self.tab_id]
+        # self.parent.tabbedPlotWidget.widget(tab_index).add_more_axes
+        # tpw_axes = self.parent.tabbedPlotWidget.widget(tab_index).ax
 
-        print("axes received in FittingWidget:", tpw_axes)
 
         fitpage_name = self.kernel_module.name
         plots = GuiUtils.plotsFromDisplayName(item_name, item_model)
@@ -2526,8 +2524,10 @@ class FittingWidget(QtWidgets.QWidget, Ui_FittingWidgetUI):
             item, plot = item_plot
             if plot.plot_role != DataRole.ROLE_DATA and fitpage_name in plot.name:
                 data_shown = True
-                self.communicate.plotRequestedSignal.emit([item, tpw_axes[i], plot], self.tab_id)
+                self.communicate.plotRequestedSignal.emit([item, plot], self.tab_id)
         # return the last data item seen, if nothing was plotted; supposed to be just data)
+        tab_index = self.parent.tabbedPlotWidget.tab_fitpage_dict[self.tab_id]
+        self.parent.tabbedPlotWidget.widget(tab_index).rearrange_plots()
         return None if data_shown else item
 
     def onOptionsUpdate(self):
