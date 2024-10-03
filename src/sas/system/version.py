2d1ad6c0454bf5bdf8ca41a57c8c66f54884a88d
krzywon <krzywon@yahoo.com>
2024-10-03 08:54:47 -0400

    Reset version to match the expect form of 'X.Y.Z' and add a note for future developers

--- src/sas/system/version.py
+++ src/sas/system/version.py
@@ -1,3 +1,4 @@
-__version__ = "6.0.0rc1"
+# Version must be of the form 'X.Y.Z'. No alpha/beta/rc versioning should be set here
+__version__ = "6.0.0"
 __release_date__ = "2024"
 __build__ = "GIT_COMMIT"
