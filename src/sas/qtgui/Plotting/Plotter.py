4534e469ab513fe2dffb60c12e7499a7c6379ae2
James Crake-Merani <James.Crake-Merani@stfc.ac.uk>
2024-10-17 11:07:33 +0100

    Posistion should be updated even on right clicks.

--- src/sas/qtgui/Plotting/Plotter.py
+++ src/sas/qtgui/Plotting/Plotter.py
@@ -916,6 +916,11 @@ class PlotterWidget(PlotterBase):
         """
         Left button down and ready to drag
         """
+        try:
+            self.x_click = float(event.xdata)  # / size_x
+            self.y_click = float(event.ydata)  # / size_y
+        except:
+            self.position = None
         # Check that the LEFT button was pressed
         if event.button != 1:
             return
@@ -927,11 +932,6 @@ class PlotterWidget(PlotterBase):
                 return
         if event.inaxes is None:
             return
-        try:
-            self.x_click = float(event.xdata)  # / size_x
-            self.y_click = float(event.ydata)  # / size_y
-        except:
-            self.position = None
 
         x_str = GuiUtils.formatNumber(self.x_click)
         y_str = GuiUtils.formatNumber(self.y_click)
