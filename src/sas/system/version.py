e1c535767aaa022051300af11596c955e2ca1aa5
krzywon <krzywon@yahoo.com>
2024-10-24 16:19:06 -0400

    Update the version number throughout sasview and create new release_6.0.1 branch

--- src/sas/system/version.py
+++ src/sas/system/version.py
@@ -1,4 +1,4 @@
 # Version must be of the form 'X.Y.Z'. No alpha/beta/rc versioning should be set here
-__version__ = "6.0.0"
+__version__ = "6.0.1"
 __release_date__ = "2024"
 __build__ = "GIT_COMMIT"
