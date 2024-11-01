0d1304b1d20efa3301ba070dde33d218e9dcd861
James Crake-Merani <James.Crake-Merani@stfc.ac.uk>
2024-11-01 13:52:31 +0000

    Use a signal for when the custom button is pressed

--- src/metadata_filename_gui/metadata_component_selector.py
+++ src/metadata_filename_gui/metadata_component_selector.py
@@ -1,6 +1,12 @@
 from PySide6.QtWidgets import QWidget, QPushButton, QHBoxLayout
+from PySide6.QtCore import Signal, Qt
 
 class MetadataComponentSelector(QWidget):
+    # Creating a separate signal for this because the custom button may be destroyed/recreated whenever the options are
+    # redrawn.
+
+    custom_button_pressed = Signal(Qt.MouseButton())
+
     def __init__(self, metadatum: str, metadata_dict: dict[str, str]):
         super().__init__()
         self.options: list[str]
@@ -26,6 +32,7 @@ class MetadataComponentSelector(QWidget):
             self.option_buttons.append(option_button)
         # This final button is to convert to use custom entry instead of this.
         self.custom_entry_button = QPushButton('Custom')
+        self.custom_entry_button.clicked.connect(self.custom_entry_button)
         self.layout.addWidget(self.custom_entry_button)
 
     def selection_changed(self):
