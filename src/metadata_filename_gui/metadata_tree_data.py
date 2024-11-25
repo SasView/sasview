908718994152a2697de233fe78f51841e0bad04e
James Crake-Merani <James.Crake-Merani@stfc.ac.uk>
2024-11-25 11:46:42 +0000

    Gonna try a different approach.

---
+++ src/metadata_filename_gui/metadata_tree_data.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+
+metadata = {
+    'source': ['name', 'radiation', 'type', 'probe_particle', 'beam_size_name', 'beam_size', 'beam_shape', 'wavelength', 'wavelength_min', 'wavelength_max', 'wavelength_spread'],
+    'detector': ['name', 'distance', 'offset', 'orientation', 'beam_center', 'pixel_size', 'slit_length'],
+    'aperture': ['name', 'type', 'size_name', 'size', 'distance'],
+    'collimation': ['name', 'lengths'],
+    'process': ['name', 'date', 'description', 'term', 'notes'],
+    'sample': ['name', 'sample_id', 'thickness', 'transmission', 'temperature', 'position', 'orientation', 'details'],
+    'transmission_spectrum': ['name', 'timestamp', 'transmission', 'transmission_deviation'],
+    'other': ['title', 'run', 'definition']
+}
+
+initial_metadata_dict = {key: {} for key, _ in metadata.items()}
