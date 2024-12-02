2a8dea33abcfb316f80a89869cd455dcc32ce0c7
James Crake-Merani <James.Crake-Merani@stfc.ac.uk>
2024-12-02 15:05:11 +0000

    Start using the new internal metadata class.

--- src/metadata_filename_gui/metadata_tree_widget.py
+++ src/metadata_filename_gui/metadata_tree_widget.py
@@ -2,34 +2,23 @@ from PySide6.QtWidgets import QTreeWidget, QTreeWidgetItem, QLabel
 from PySide6.QtCore import QAbstractItemModel
 from metadata_filename_gui.metadata_component_selector import MetadataComponentSelector
 from metadata_filename_gui.metadata_selector import MetadataSelector
+from metadata_filename_gui.internal_metadata import InternalMetadata
+from metadata_filename_gui.metadata_tree_data import metadata as metadata_categories
 
 class MetadataTreeWidget(QTreeWidget):
-    def __init__(self, metadata_dict: dict[str, dict[str, str]], master_metadata: dict[str, dict[str, int]]):
+    def __init__(self, metadata: InternalMetadata):
         super().__init__()
         self.setColumnCount(2)
         self.setHeaderLabels(['Name', 'Filename Components'])
-        self.metadata_dict = metadata_dict
-        self.master_metadata = master_metadata
+        self.metadata: InternalMetadata = metadata
 
-
-    def draw_tree(self, options: list[str]):
+    def draw_tree(self, options: list[str], full_filename: str):
         self.clear()
-        # TODO: I'm not sure whether I like this approach. Maybe use some reflection from the Metadata class instead.
-        metadata = {
-            'source': ['name', 'radiation', 'type', 'probe_particle', 'beam_size_name', 'beam_size', 'beam_shape', 'wavelength', 'wavelength_min', 'wavelength_max', 'wavelength_spread'],
-            'detector': ['name', 'distance', 'offset', 'orientation', 'beam_center', 'pixel_size', 'slit_length'],
-            'aperture': ['name', 'type', 'size_name', 'size', 'distance'],
-            'collimation': ['name', 'lengths'],
-            'process': ['name', 'date', 'description', 'term', 'notes'],
-            'sample': ['name', 'sample_id', 'thickness', 'transmission', 'temperature', 'position', 'orientation', 'details'],
-            'transmission_spectrum': ['name', 'timestamp', 'transmission', 'transmission_deviation'],
-            'other': ['title', 'run', 'definition']
-        }
-        for top_level, items in metadata.items():
+        for top_level, items in metadata_categories.items():
             top_level_item = QTreeWidgetItem([top_level])
             for metadatum in items:
                 # selector = MetadataComponentSelector(metadatum, self.metadata_dict)
-                selector = MetadataSelector(metadatum, options, self.metadata_dict[top_level], self.master_metadata[top_level])
+                selector = MetadataSelector(top_level, metadatum, options, self.metadata, full_filename, self.separator_pattern)
                 metadatum_item = QTreeWidgetItem([metadatum])
                 # selector.draw_options(options, metadata_dict.get(metadatum))
                 top_level_item.addChild(metadatum_item)
