c115a165ef51a82ec2647714c98a05ec728c860e
James Crake-Merani <james@jamescm.co.uk>
2025-01-23 10:53:59 +0000

    Add another error message.

--- src/sas/data_manager.py
+++ src/sas/data_manager.py
@@ -5,6 +5,12 @@ from src.sas.refactored import Perspective
 # TODO: Add plots to this type.
 TrackedData = SasData | Perspective
 
+# TODO: Probably want to handle order, if that is even relevant.
+valid_associations = [
+    (Perspective, SasData)
+    # TODO: Include plots
+]
+
 
 class DataManager():
     # Don't mutate these directly, or scary bad stuff will happen.
@@ -22,3 +28,6 @@ class DataManager():
     def make_association(self, data_1: TrackedData, data_2: TrackedData):
         if not (data_1 in self._all_data_entries or data_2 in self._all_data_entries):
             raise ValueError('Both data_1, and data_2 need to be tracked in the data manager.')
+        if not any([isinstance(data_1, x) and isinstance(data_2, y) for x, y in valid_associations]):
+            # TODO: Clearer error message.
+            raise ValueError('Invalid association.')
