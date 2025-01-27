b0339227e8172bb9c568519f81e110ba567a3e86
James Crake-Merani <james@jamescm.co.uk>
2025-01-27 10:31:27 +0000

    Added widgets to the new data explorer.

--- src/sas/refactored_data_explorer.py
+++ src/sas/refactored_data_explorer.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import QLabel, QVBoxLayout, QWidget
+from PySide6.QtWidgets import QHBoxLayout, QLabel, QPushButton, QTreeView, QVBoxLayout, QWidget
 
 # TODO: Just using the word 'new' to avoid conflicts. The final version
 # shouldn't have that name.
@@ -8,5 +8,37 @@ class NewDataExplorer(QWidget):
 
         self.layout = QVBoxLayout(self)
 
-        test_label = QLabel('Test')
-        self.layout.addWidget(test_label)
+        self.add_data_row = QHBoxLayout(self)
+
+        self.load_data_row = QPushButton("Load Data", self)
+        self.add_perspective = QPushButton("+ New Perspective", self)
+
+        self.add_data_row.addWidget(self.load_data_row)
+        self.add_data_row.addWidget(self.add_perspective)
+
+        self.filter_row = QHBoxLayout(self)
+        filter_names = ['Data', 'Perspective', 'Theory', 'Plot']
+        self.filter_buttons: dict[str, QPushButton] = {name: QPushButton(name, self) for name in filter_names}
+        for widget in self.filter_buttons.values():
+            widget.setCheckable(True)
+            self.filter_row.addWidget(widget)
+
+        # TODO: I suspect this may end up being a separate widget. For now, just
+        # use an empty tree view.
+        self.tree_view = QTreeView()
+
+        # TODO: Is there a better name for this?
+        self.final_row = QHBoxLayout(self)
+
+        self.remove_button = QPushButton('Remove', self)
+        self.remove_button.setToolTip('Remove the selected data from SasView')
+        self.plot_button = QPushButton('Plot', self)
+
+        self.final_row.addWidget(self.remove_button)
+        self.final_row.addWidget(self.plot_button)
+
+
+        self.layout.addLayout(self.add_data_row)
+        self.layout.addLayout(self.filter_row)
+        self.layout.addWidget(self.tree_view)
+        self.layout.addLayout(self.final_row)
