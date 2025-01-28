3912688a27ec5d679b4ac9eeb3ad0157de70ac9e
James Crake-Merani <james@jamescm.co.uk>
2025-01-28 10:26:40 +0000

    Centre the checkbox and remove label.

--- src/ascii_dialog/row_status_widget.py
+++ src/ascii_dialog/row_status_widget.py
@@ -1,29 +1,32 @@
 #!/usr/bin/env python3
 
-from PySide6.QtCore import Signal, Slot
-from PySide6.QtWidgets import QCheckBox
+from PySide6.QtCore import Signal, Slot, Qt
+from PySide6.QtWidgets import QCheckBox, QHBoxLayout, QWidget
 
 
-class RowStatusWidget(QCheckBox):
+class RowStatusWidget(QWidget):
     """Widget to toggle whether the row is to be included as part of the data."""
     def __init__(self, initial_value: bool, row: int):
         super().__init__()
         self.row = row
-        self.setChecked(initial_value)
+        self.checkbox = QCheckBox()
+        self.checkbox.setChecked(initial_value)
         self.updateLabel()
-        self.stateChanged.connect(self.onStateChange)
+        self.checkbox.stateChanged.connect(self.onStateChange)
+        self.layout = QHBoxLayout(self)
+        self.layout.addWidget(self.checkbox, alignment=Qt.AlignmentFlag.AlignCenter)
 
     status_changed = Signal(int)
     def updateLabel(self):
         """Update the label of the check box depending on whether it is checked,
         or not."""
-        if self.isChecked():
-            self.setText('Included')
-        else:
-            self.setText('Not Included')
+        pass
 
 
     @Slot()
     def onStateChange(self):
         self.updateLabel()
         self.status_changed.emit(self.row)
+
+    def isChecked(self):
+        return self.checkbox.isChecked
