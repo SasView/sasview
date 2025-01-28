b97adbb6fd35c0b54168690026797c35376515df
summerhenson <summerh@gmail.com>
2025-01-28 10:29:30 -0500

    Remove trailing whitespace

--- src/sas/webfit/data/views.py
+++ src/sas/webfit/data/views.py
@@ -2,7 +2,7 @@ import os
 
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponseBadRequest, HttpResponseForbidden, Http404
-from django.core.files.storage import FileSystemStorage 
+from django.core.files.storage import FileSystemStorage
 
 #TODO go over to see if token is needed for is_authenticated
 from rest_framework.authtoken.models import Token
