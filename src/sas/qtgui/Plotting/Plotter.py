b7bb99e04d02d7c85bcb170097863602b01eefd1
krzywon <krzywon@yahoo.com>
2025-01-30 10:56:49 -0500

    Move the plot role check to the data explorer so the data objects are all assigned proper initial transforms

--- src/sas/qtgui/Plotting/Plotter.py
+++ src/sas/qtgui/Plotting/Plotter.py
@@ -178,14 +178,6 @@ class PlotterWidget(PlotterBase):
 
         markersize = data.markersize
 
-        # Use the plot role to define the x and y scales
-        if data.plot_role in [DataRole.ROLE_POLYDISPERSITY, DataRole.ROLE_RESIDUAL_SESANS, DataRole.ROLE_STAND_ALONE]:
-            self.xscale = 'linear'
-        # X and Y are separate to accommodate future plot roles that might only need one axis on a linear scale
-        if data.plot_role in [DataRole.ROLE_POLYDISPERSITY, DataRole.ROLE_RESIDUAL, DataRole.ROLE_RESIDUAL_SESANS,
-                              DataRole.ROLE_STAND_ALONE]:
-            self.yscale = 'linear'
-
         # Include scaling (log vs. linear)
         ax.set_xscale(self.xscale, nonpositive='clip') if self.xscale != 'linear' else self.ax.set_xscale(self.xscale)
         ax.set_yscale(self.yscale, nonpositive='clip') if self.yscale != 'linear' else self.ax.set_yscale(self.yscale)
