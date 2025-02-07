1d65f089fd812cfe696c2cbe3a1a42e4d7004016
James Crake-Merani <James.Crake-Merani@stfc.ac.uk>
2025-02-07 11:26:25 +0000

    Skeleton for dummy perspective.

--- src/sas/dummy_perspective.py
+++ src/sas/dummy_perspective.py
@@ -1,27 +1,17 @@
 from typing import override
-from PySide6.QtWidgets import QDialog, QListWidget, QVBoxLayout, QWidget
-from qtpy.QtWidgets import QLabel
+from PySide6.QtWidgets import QDialog, QWidget
 from sas.refactored import Perspective
-from sas.data_manager import NewDataManager as DataManager
 
-class DummyPerspective(Perspective):
-    def __init__(self, data_manager: DataManager, parent: QWidget | None = None) -> None:
-        super().__init__(data_manager, parent)
+class DummyPerspective(QDialog, Perspective):
+    def __init__(self, parent: QWidget | None = None) -> None:
+        super().__init__(parent)
 
-        self.data_list_label = QLabel('Current Loaded Data')
-        self.data_list = QListWidget()
-
-        self.layout = QVBoxLayout(self)
-        self.layout.addWidget(self.data_list_label)
-        self.layout.addWidget(self.data_list)
+    @property
+    @override
+    def title(self) -> str:
+        return "Dummy"
 
     @property
     @override
     def title(self) -> str:
         return "Dummy Perspective"
-
-    @override
-    def newAssocation(self):
-        self.data_list.clear()
-        for datum in self.associatedData:
-            self.data_list.addItem(datum.name)
