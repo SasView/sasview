9f61d66d3b17d0bfa972feb0777008a92f18cc6f
James Crake-Merani <James.Crake-Merani@stfc.ac.uk>
2025-02-10 11:43:59 +0000

    Method to build the table.

--- src/sas/data_explorer_tree.py
+++ src/sas/data_explorer_tree.py
@@ -1,8 +1,21 @@
-from PySide6.QtWidgets import QTreeWidget, QWidget
+from PySide6.QtWidgets import QTreeWidget, QTreeWidgetItem, QWidget
+from sasdata.data import SasData
 from sas.data_manager import NewDataManager as DataManager
+from sas.refactored import Perspective
 
 
 class DataExplorerTree(QTreeWidget):
     def __init__(self, data_manager: DataManager, parent: QWidget | None = None) -> None:
         super().__init__(parent)
         self._data_manager = data_manager
+
+    def buildTable(self):
+        # TODO: Right now we are ignoring associations.
+        self.clear()
+        for datum in  self._data_manager.all_data:
+            if isinstance(datum, SasData):
+                name = datum.name
+            else: # If perspective
+                name = datum.title
+            item = QTreeWidgetItem(name)
+            self.addTopLevelItem(item)
