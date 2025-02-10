cdd64736269f19ef92a5308772d078ee367bf065
James Crake-Merani <James.Crake-Merani@stfc.ac.uk>
2025-02-10 08:41:10 +0000

    Remove this comment.

--- src/ascii_dialog/default_units.py
+++ src/ascii_dialog/default_units.py
@@ -2,7 +2,6 @@
 
 import sasdata.quantities.units as unit
 
-# Based on the email Jeff sent me./
 default_units = {
     'Q': [unit.per_nanometer, unit.per_angstrom, unit.per_meter],
     'I': [unit.per_centimeter, unit.per_meter]
