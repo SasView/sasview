bd01aa128ef3f4196a0253aafb2738a8d6bd8e0d
James Crake-Merani <James.Crake-Merani@stfc.ac.uk>
2025-02-12 10:33:12 +0000

    Move this logic into SasData.

--- src/ascii_dialog/default_units.py
+++
@@ -1,8 +0,0 @@
-# NOTE: This module will probably be a lot more involved once how this is getting into the configuration will be sorted.
-
-import sasdata.quantities.units as unit
-
-default_units = {
-    'Q': [unit.per_nanometer, unit.per_angstrom, unit.per_meter],
-    'I': [unit.per_centimeter, unit.per_meter]
-}
