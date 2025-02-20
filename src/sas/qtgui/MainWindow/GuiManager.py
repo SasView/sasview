02f2d24cec261e451c026628f45e985eb1c8f3f4
James Crake-Merani <James.Crake-Merani@stfc.ac.uk>
2025-02-20 11:47:10 +0000

    Break if we've already found the right window.

--- src/sas/qtgui/MainWindow/GuiManager.py
+++ src/sas/qtgui/MainWindow/GuiManager.py
@@ -290,6 +290,7 @@ class GuiManager:
             for sub_window in sub_window_list:
                 if sub_window.widget() == new_selected:
                     sub_window.activateWindow()
+                    break
 
     @staticmethod
     def addCategories():
