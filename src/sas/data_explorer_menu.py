6425fc2eed95c64f22d87cb10dfabe49a3401beb
James Crake-Merani <james@jamescm.co.uk>
2025-02-25 11:00:24 +0000

    Only enable send to functionality for sasdata.

--- src/sas/data_explorer_menu.py
+++ src/sas/data_explorer_menu.py
@@ -5,16 +5,16 @@ from sas.data_manager import NewDataManager
 from src.sas.refactored import Perspective
 
 class DataExplorerMenu(QMenu):
-    def __init__(self, parent: QWidget, data_manager: NewDataManager):
+    def __init__(self, parent: QWidget, data_manager: NewDataManager, send_to: bool):
         super().__init__(parent)
 
         remove_data = QAction("Remove", parent)
         # TODO: There will be loads more
 
-        send_to_menu = self.addMenu("Send To")
+        if send_to:
+            send_to_menu = self.addMenu("Send To")
 
-        for perspective in data_manager.all_perspectives:
-            new_action = QAction(perspective.formatName, parent)
-            send_to_menu.addAction(new_action)
+            for perspective in data_manager.all_perspectives:
+                new_action = QAction(perspective.formatName, parent)
+                send_to_menu.addAction(new_action)
         self.addAction(remove_data)
-
