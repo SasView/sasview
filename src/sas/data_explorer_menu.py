2b062923ab7eff31d9aa941753f63f3b4cb04df2
James Crake-Merani <james@jamescm.co.uk>
2025-02-25 08:57:20 +0000

    Have the send to menu in the same class.

--- src/sas/data_explorer_menu.py
+++ src/sas/data_explorer_menu.py
@@ -11,18 +11,10 @@ class DataExplorerMenu(QMenu):
         remove_data = QAction("Remove", parent)
         # TODO: There will be loads more
 
-        send_to_menu = SendToMenu(self, data_manager.all_perspectives)
+        send_to_menu = self.addMenu("Send To")
 
-        self.addAction(remove_data)
-        self.addMenu(send_to_menu)
-
-class SendToMenu(QMenu):
-    def __init__(self, parent: QMenu, perspectives: list[Perspective]):
-        super().__init__(parent)
-
-        # TODO: May need a way of identifying the perspective other than the name.
-        self.menu_items: list[QAction] = []
-
-        for perspective in perspectives:
+        for perspective in data_manager.all_perspectives:
             new_action = QAction(perspective.formatName, parent)
-            self.menu_items.append(new_action)
+            send_to_menu.addAction(new_action)
+        self.addAction(remove_data)
+
