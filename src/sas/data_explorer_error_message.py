ad4d5e65c684c730f94e8165ce71512005d3e1d5
James Crake-Merani <James.Crake-Merani@stfc.ac.uk>
2025-03-10 10:02:37 +0000

    Handle having multiple error messages.

--- src/sas/data_explorer_error_message.py
+++ src/sas/data_explorer_error_message.py
@@ -2,8 +2,15 @@ from PySide6.QtWidgets import QMessageBox, QWidget
 
 
 class DataExplorerErrorMessage(QMessageBox):
-    def __init__(self, parent: QWidget, error: ValueError):
+    def __init__(self, parent: QWidget, errors: list[ValueError]):
         super().__init__(parent)
         self.setIcon(QMessageBox.Icon.Critical)
-        self.setText(str(error))
         self.setWindowTitle('Data Error')
+        if len(errors) == 0:
+            error = errors[0]
+            self.setText(str(error))
+        else:
+            full_error_msg = "Some data could not be removed due to the following errors"
+            error_msg_set = set([str(err) for err in errors])
+            error_msg_str = '\n'.join(error_msg_set)
+            full_error_msg += f"\n{error_msg_str}"
