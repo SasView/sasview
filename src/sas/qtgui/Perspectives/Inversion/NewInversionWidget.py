0e5a5233a3281bec8d470e0bb9166ef54951e14b
James Crake-Merani <james@jamescm.co.uk>
2025-03-27 09:57:16 +0000

    Blank skeleton of refactored class.

---
+++ src/sas/qtgui/Perspectives/Inversion/NewInversionWidget.py
@@ -0,0 +1,8 @@
+
+from PySide6.QtWidgets import QWidget
+
+from src.sas.qtgui.Perspectives.Inversion.UI.TabbedInversionUI import Ui_PrInversion
+
+
+class NewInversionWidget(QWidget, Ui_PrInversion):
+    pass
