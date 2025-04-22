f6db830a23cfaa34b3240f036f64de8a50070729
butlerpd <butlerpd@udel.edu>
2025-04-22 00:31:35 +0000

    Working on poresize tests
    uploading today's work. Now failing within SizeDistribution.py? which it does not normally?

--- test/poresize/MaxEntTest.py
+++ test/poresize/MaxEntTest.py
@@ -3,31 +3,37 @@ import pytest
 import csv
 import numpy as np
 from sasmodels import resolution as rst
-from sasdata.dataloader import data_info
-from sas.sascalc.poresize.SizeDistribution import sizeDistribution
+from sasdata.dataloader.data_info import Data1D
+from sasdata.dataloader.loader import Loader
+from src.sas.sascalc.poresize.SizeDistribution import sizeDistribution
 
 
 def find(filename):
     return os.path.join(os.path.dirname(__file__), "data", filename)
 
 
-class SizeDistributionTest:
-    @pytest.fixture(autouse=True)
-    def data1(self):
-        class StartTest:
-            def __init__(self, filename, answerFile):
-                self.fileName = filename
-                self.answer = answerFile
+#class SizeDistributionTest:
+@pytest.fixture(autouse=True)
+def data1():
+#        class StartTest:
+#        def __init__(self, filename, answerFile):
+#            self.fileName = filename
+#            self.answer = answerFile
+    data_from_loader = Loader().load(find("Alumina_usaxs_irena_Iq_fit.csv"))
+    data = data_from_loader[0]
+    size_distribution = sizeDistribution(data)
+    size_distribution.aspectRatio = 1.0
+    size_distribution.diamMin = 25
+    size_distribution.diamMax = 10000
+    size_distribution.nbins = 100
+    size_distribution.logbin = True
+    size_distribution.skyBackground = 1e-3
+    size_distribution.weightType = 'dI'
+    size_distribution.weightFactor = 1.0
 
-            def file_to_input(
-                self,
-                diamRange,
-                logBin=True,
-                contrast=1,
-                sky=1e-3,
-                weightsOpt="I",
-                resOpt=None,
-            ):
+    return size_distribution
+#            def file_to_input(self):
+"""
                 Q = np.array([])
                 I = np.array([])
                 dI = np.array([])
@@ -82,29 +88,34 @@ class SizeDistributionTest:
                         )
                 input_dict["Resolution"] = perfect1D
                 return input_dict, slit
+                """
+def fetch_answer(self):
+    bins = np.array([])
+    mags = np.array([])
 
-            def fetch_answer(self):
-                bins = np.array([])
-                mags = np.array([])
+    with open(find("Alumina_usaxs_irena_diameter_fit.csv")) as fp:
+        spamreader = csv.reader(fp, delimiter=",")
 
-                with open(self.answer) as fp:
-                    spamreader = csv.reader(fp, delimiter=",")
+        for row in spamreader:
+            try:
+                bins = np.append(bins, float(row[0]))
+                mags = np.append(mags, float(row[2]))
+            except:
+                pass
+    return bins, mags
 
-                    for row in spamreader:
-                        try:
-                            bins = np.append(bins, float(row[0]))
-                            mags = np.append(mags, float(row[2]))
-                        except:
-                            pass
-                return bins, mags
+#        filename1 = find("Alumina_usaxs_irena_Iq_fit.csv")
+#        answer1 = find("Alumina_usaxs_irena_diameter_fit.csv")
+#        return StartTest(filename1, answer1)
 
-        filename1 = find("Alumina_usaxs_irena_Iq_fit.csv")
-        answer1 = find("Alumina_usaxs_irena_diameter_fit.csv")
-        return StartTest(filename1, answer1)
-
-    def test_noRes(self, data1):
-        input_dict, slit = data1.file_to_input(diamRange=[25, 10000, 100])
-        _, Bins, _, BinMag, _, _ = sizeDistribution(input_dict)
-        answerBins, answerMags = data1.fetch_answer()
-        assert Bins == pytest.approx(answerBins, rel=100)
-        assert BinMag == pytest.approx(answerMags, rel=100)
+def test_noRes(data1):
+#        input_dict, slit = data1.file_to_input(diamRange=[25, 10000, 100])
+#    _, Bins, _, BinMag, _, _ = sizeDistribution(input_dict)
+#   Do we need to subtract background here?
+    print (type(data1))
+    subtracted_data = data1.data
+    trim_data, intensities, init_binsBack, sigma = data1.prep_maxEnt(subtracted_data, full_fit=True)
+    _, Bins, _, BinMag, _, _ = data1.run_maxEnt(trim_data, intensities,init_binsBack, sigma)
+    answerBins, answerMags = fetch_answer()
+    assert Bins == pytest.approx(answerBins, rel=100)
+    assert BinMag == pytest.approx(answerMags, rel=100)
