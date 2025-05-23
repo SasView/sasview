Fix for second problem in log scaling of data
This now should also fix problems where the data itself even before subtracting a fixed margin is negative for log scale. Whether this is the best solution is an open question.

src/sas/qtgui/Plotting/Plotter.py
@@ -332,24 +332,29 @@ class PlotterWidget(PlotterBase):
         return False
 
     def _plot_bounds(self, offset=0.05) -> tuple[tuple[float, float], tuple[float, float]]:
-        """ Get the appropriate bounds for the plots
+        """
+        Get the appropriate bounds for the plots. This is a workaround for a
+        longstanding but edge case bug in matplotlib's autoscale. Basically,
+        data whose range is tiny on a log scale (e.g. 0.9 to 1.1 on a log
+        scale).
 
-        This should be superfluous. However, matplotlib seems to have some
-        glitches for edge cases. Ideally this can be removed if matplotlib
-        glitches are fixed. In the meantime this private method ensures that
-        all plots are scaled such that there is a small "white space" between
-        the data extremes and the plot edges.
+        This should be removed when matplotlib fixes the bug. In the meantime
+        this private method ensures that all plots are scaled such that there
+        is a small "white space" between the data extremes and the plot edges.
 
-        In order to achieve this in a plot scale agnostic fashion (e.g. log
-        vs linear etc.) the smallest and largest x and y in all the data sets
-        are converted from data coordinates to axes coordinates before
-        computing the range of the data (max-min). A fraction of that range
-        (default is 5%) is then added and subtracted from the smallest and
-        largest values of x and y, still in axes space. This ensures that
-        regardless of the plot scaling 10% of the plot space (5% on either
-        end) will be empty.
+        In order to achieve this, the min and max of all the data on the plot
+        are computed, taking into account that will include the top, or bottom,
+        of any error bars present with the data. For log axes data that first
+        requires removing any negative values from the min max computation.
 
-        Finally those adjusted min and max values are converted back to data
+        The min and max values for each scale are then converted from data
+        coordinates to axes coordinates before computing the range of the data
+        (max-min). A fraction of that range (default is 5%) is then added and
+        subtracted from the smallest (min) and largest (max) values of x and y,
+        still in axes space. This ensures that regardless of the plot scaling
+        10% of the plot space (5% on either end) will be empty.
+
+        Finally, those adjusted min and max values are converted back to data
         coordinates to be used in setting the plot bounds in the usual fashion
 
         :param offset: The fraction of the absolute value of the full
@@ -362,28 +367,43 @@ class PlotterWidget(PlotterBase):
         y_min, y_max = np.inf, -np.inf
 
         # First let's find the smallest xmin and ymin and largest xmax and ymax
-        # in all the data sets
+        # in all the data sets. if the data have error bars then we need to
+        # keep track of the data poing + (or -) the error bar height to make
+        # sure the error bars stay within the plot.
         for key in self.plot_dict:
 
             plot_data = self.plot_dict[key].view
 
+            # NOTE: we need to trim any negative values if the scale of the
+            # particular axis is log scale before looking for the minimum
+
+            # First the x axis
+            plot_data.x = [i for i in plot_data.x if i > 0] if self.ax.xaxis.get_scale() == "log" else plot_data.x
             if len(plot_data.x) > 0:
                 x_min = min(np.min(plot_data.x), x_min)
                 x_max = max(np.max(plot_data.x), x_max)
 
+            # and now the y axis. Note: here we need to ensure not only that y
+            # value is in bounds we also need to make sure that the top (or
+            # bottom) of the error bar on that point is also within the plot
+            # bounds.
             if len(plot_data.y) > 0:
-
                 dy = plot_data.dy
                 if dy is None:
+                    plot_data.y = [i for i in plot_data.y if i > 0] if self.ax.yaxes.get_scale() == "log" else plot_data.y
                     y_min = min(np.min(plot_data.y), y_min)
                     y_max = max(np.max(plot_data.y), y_max)
                 else:
                     try:
-                        y_min = min(np.min(np.array(plot_data.y) - np.array(dy)), y_min)
-                        y_max = max(np.max(np.array(plot_data.y) + np.array(dy)), y_max)
+                        if self.ax.yaxis.get_scale() == "log":
+                            y_min = min(np.min([i for i in (np.array(plot_data.y) - np.array(dy)) if i > 0]), y_min)
+                            y_max = max(np.max([i for i in (np.array(plot_data.y) + np.array(dy))if i > 0]), y_max)
+                        else:
+                            y_min = min(np.min(np.array(plot_data.y) - np.array(dy)), y_min)
+                            y_max = max(np.max(np.array(plot_data.y) + np.array(dy)), y_max)
                     except ValueError:
                         # Ignore error term if it doesn't match y scale and causes an error
-                        
+                        plot_data.y = [i for i in plot_data.y if i > 0] if self.ax.yaxis.get_scale() == "log" else plot_data.y
                         y_min = min(np.min(plot_data.y), y_min)
                         y_max = max(np.max(plot_data.y), y_max)
 