8bf5c88f5a38cd31305262aa85fa82a8fbe14989
James Crake-Merani <James.Crake-Merani@stfc.ac.uk>
2025-06-03 12:14:17 +0100

    Ruff format.

--- src/sas/qtgui/MainWindow/MetadataExplorer.py
+++ src/sas/qtgui/MainWindow/MetadataExplorer.py
@@ -1,10 +1,12 @@
 from PySide6.QtWidgets import QApplication, QDialog
 
+
 class MetadataExplorer(QDialog):
     def __init__(self):
         super().__init__()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app = QApplication([])
 
     dialog = MetadataExplorer()
