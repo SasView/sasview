d99ad324c2b16a3e312cd373b261d0434f428f0f
James Crake-Merani <James.Crake-Merani@stfc.ac.uk>
2025-06-04 10:11:49 +0100

    Custom implementation of metadata as dict.

--- src/sas/qtgui/MainWindow/MetadataExplorer.py
+++ src/sas/qtgui/MainWindow/MetadataExplorer.py
@@ -1,10 +1,19 @@
 from sys import argv
-from dataclasses import asdict
 from pprint import pp
 
 from PySide6.QtWidgets import QApplication, QDialog
-from sasdata.metadata import Metadata
+from sasdata.metadata import MetaNode, Metadata
 from sasdata.temp_xml_reader import load_data
+
+
+def metadata_as_dict(to_convert: object):
+    """This is a custom implementation of asdict from dataclasses. The key difference is that MetaNode class objects are
+    preserved, but everything else is converted into a dict. This makes it easier to iterate over."""
+    converted_dict = to_convert.__dict__
+    for key, value in converted_dict.items():
+        if not (value is dict or isinstance(value, MetaNode)):
+            converted_dict[key] = metadata_as_dict(value)
+    return converted_dict
 
 
 class MetadataExplorer(QDialog):
