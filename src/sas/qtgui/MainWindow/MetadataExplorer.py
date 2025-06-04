1a235a6758caa79ebc3f40299b75bf9c2b264b0c
James Crake-Merani <James.Crake-Merani@stfc.ac.uk>
2025-06-04 08:16:08 +0100

    Store metadata in the explorer class.

--- src/sas/qtgui/MainWindow/MetadataExplorer.py
+++ src/sas/qtgui/MainWindow/MetadataExplorer.py
@@ -1,9 +1,12 @@
 from PySide6.QtWidgets import QApplication, QDialog
 
+from sasdata.metadata import Metadata
+
 
 class MetadataExplorer(QDialog):
-    def __init__(self):
+    def __init__(self, metadata: Metadata):
         super().__init__()
+        self.metadata = metadata
 
 
 if __name__ == "__main__":
