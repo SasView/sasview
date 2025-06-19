eeeb86c2d76b6c7b5d28398b7fd4540dbd554e63
krellemeister <k.relle@live.dk>
2025-06-19 13:09:23 +0200

    separated out DataClasses from Shape2SAS main script; should be the end of refactoring for now

--- src/sas/sascalc/shape2sas/Typing.py
+++ src/sas/sascalc/shape2sas/Typing.py
@@ -1,6 +1,7 @@
 import numpy as np
-from typing import Optional, Tuple, List, Any
+from typing import Tuple, List
 
+Vectors = List[List[float]]
 Vector2D = Tuple[np.ndarray, np.ndarray]
 Vector3D = Tuple[np.ndarray, np.ndarray, np.ndarray]
 Vector4D = Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]
