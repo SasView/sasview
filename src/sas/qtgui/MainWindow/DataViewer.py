331d6880dc8d15887ca9cfa700808046bd2719ac
James Crake-Merani <james@jamescm.co.uk>
2025-07-04 12:13:33 +0100

    This is the right way to resize properly.

--- src/sas/qtgui/MainWindow/DataViewer.py
+++ src/sas/qtgui/MainWindow/DataViewer.py
@@ -4,6 +4,7 @@ from PySide6.QtWidgets import (
     QTableWidget,
     QTableWidgetItem,
     QAbstractScrollArea,
+    QHeaderView,
 )
 
 from sasdata.data import SasData
@@ -34,10 +35,6 @@ class DataViewer(QDialog):
     def buildTable(self):
         # Make the table readonly
         self.dataTable.setEditTriggers(QTableWidget.EditTrigger.NoEditTriggers)
-        # The table's width will always resize to fit the amount of space it has.
-        self.dataTable.setSizeAdjustPolicy(
-            QAbstractScrollArea.SizeAdjustPolicy.AdjustToContents
-        )
         columns = self.to_view._data_contents.keys()
         self.dataTable.setColumnCount(len(columns))
         # NOTE: Assumes each column has the same amount of rows, which should be
@@ -46,6 +43,9 @@ class DataViewer(QDialog):
             len(next(iter(self.to_view._data_contents.values())).value)
         )
         self.dataTable.setHorizontalHeaderLabels(columns)
+        self.dataTable.horizontalHeader().setSectionResizeMode(
+            QHeaderView.ResizeMode.Stretch
+        )
         for i, data in enumerate(self.to_view._data_contents.values()):
             for j, datum in enumerate(data.value):
                 self.dataTable.setItem(j, i, QTableWidgetItem(str(datum)))
