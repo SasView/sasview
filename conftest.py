e8800a65eacdae59d914cf9c4b10fafd7cd83f9d
PaulSharp <44529197+DrPaulSharp@users.noreply.github.com>
2025-07-10 12:36:38 +0100

    Fixes fatal access violation in Windows tests

--- conftest.py
+++ conftest.py
@@ -1,8 +1,18 @@
+import os
+import platform
+
+import pytest
+
 from run import prepare
+from src.sas.system import config
 
 prepare()
 
-from src.sas.system import config
-
 config.override_with_defaults()
 
+
+@pytest.fixture(scope="session", autouse=True)
+def set_env():
+    # pytest and numba do not play nicely together on windows, see: https://github.com/numba/numba/issues/8223#issuecomment-1175213066
+    if platform.system() == "Windows":
+        os.environ["SAS_NUMBA"] = "0"
