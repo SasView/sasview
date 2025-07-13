17bc110ea2b760fc3cdc49a25b6a9f29d8a71364
krellemeister <k.relle@live.dk>
2025-07-13 17:15:54 +0200

    constraint window now opens on top

--- src/sas/qtgui/Calculators/Shape2SAS/DesignWindow.py
+++ src/sas/qtgui/Calculators/Shape2SAS/DesignWindow.py
@@ -166,7 +166,8 @@ class DesignWindow(QDialog, Ui_Shape2SAS, Perspective):
         #TODO: implement in a future project
 
         ###Building Constraint window
-        self.constraint = Constraints()
+        self.constraint = Constraints(parent=self)
+        self.constraint.setWindowFlags(Qt.Window | Qt.Tool)
         self.subunitTable.add.clicked.connect(self.addToVariableTable)
         self.subunitTable.deleteButton.clicked.connect(self.deleteFromVariableTable)
         self.subunitTable.table.clicked.connect(self.updateDeleteButton)
@@ -183,6 +184,8 @@ class DesignWindow(QDialog, Ui_Shape2SAS, Perspective):
     def showConstraintWindow(self):
         """Get the Constraint window"""
 
+        self.constraint.setScreen(self.screen())
+        self.constraint.move(self.pos().x()+50, self.pos().y()+50)
         self.constraint.show()
 
     def checkedVariables(self):
