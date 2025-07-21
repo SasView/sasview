4d38aa097ccf46a380aa09c88ccca0d733a66725
James Crake-Merani <James.Crake-Merani@stfc.ac.uk>
2025-07-21 11:49:06 +0100

    Loop is redundant.

--- src/ascii_dialog/dialog.py
+++ src/ascii_dialog/dialog.py
@@ -1,6 +1,7 @@
 
 
 
+
 from os import path
 
 from PySide6.QtCore import QModelIndex, QPoint, Slot
@@ -100,8 +101,7 @@ class AsciiDialog(QDialog):
         # TODO: Temporarily exclude SESANS until that's been fixed.
         with suppress(ValueError):
             reduced_dataset_types = dataset_types.remove('SESANS')
-        for name in reduced_dataset_types:
-            self.dataset_combobox.addItem(name)
+        self.dataset_combobox.addItems(reduced_dataset_types)
         self.dataset_layout.addWidget(self.dataset_label)
         self.dataset_layout.addWidget(self.dataset_combobox)
 
