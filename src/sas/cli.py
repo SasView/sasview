e6e9ca2bbb7c47d024f22ff66b094ff96cb9e981
Paul Kienzle <paul.kienzle@nist.gov>
2025-10-21 11:15:52 -0400

    move qt gui build to cli.py and remove run.py (#3596)
    * move qt gui build to cli.py and remove run.py
    
    * [pre-commit.ci lite] apply automatic fixes for ruff linting errors
    
    * tweak install docs; improve dev mode test; try conftest
    
    * Don't warn about doctree copy if file already exists
    
    * tweak code comments
    
    ---------
    
    Co-authored-by: Paul Kienzle <pkienzle@nist.gov>
    Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>

--- src/sas/cli.py
+++ src/sas/cli.py
@@ -39,14 +39,15 @@ open a console to show the output with the *-o* flag or redirect output to
 a file using something like *sasview ... > output.txt*.
 """
 # TODO: Support dropping datafiles onto .exe?
-# TODO: Maybe use the bumps cli with project file as model?
 import argparse
+import logging
 import sys
+from pathlib import Path
 
 
-def parse_cli(argv):
+def parse_cli(argv: list[str]) -> argparse.Namespace:
     """
-    Parse the command argv returning an argparse.Namespace.
+    Parse the argv arguments from the command line.
 
     * version: bool - print version
     * command: str - string to exec
@@ -102,60 +103,79 @@ def parse_cli(argv):
         opts.args = rest
     return opts
 
-def main(logging="production"):
-    # Copy files before loading config
-    from sas.system.user import copy_old_files_to_new_location
-    copy_old_files_to_new_location()
+def main(dev_mode: bool|None=None) -> int:
+    """
+    Run the main program.
 
-    from sas.system import console, lib, log
+    If *dev_mode* is True then rebuild the UI before running. If *dev_mode*
+    is not provided, then look into the filesystem to guess whether we are
+    running from the source tree or from an installed version.
+    """
+    # Guess if we are in development mode if not provided on the command line.
+    if dev_mode is None:
+        # Check that we are in ".../src/sas/cli.py" and ".../pyproject.toml" exists.
+        src = Path(__file__).resolve().parent.parent
+        dev_mode = src.name == "src" and (src.parent / "pyproject.toml").exists()
 
     # I/O redirection for the windows console. Need to do this early so that
     # output will be displayed on the console. Presently not working for
     # production (it always opens the console even if it is not needed)
-    # so require "sasview con ..." to open the console. Not an infamous
+    # so require "sasview -o ..." to open the console. Not an infamous
     # "temporary fix" I hope...
     if "-i" in sys.argv[1:] or "-o" in sys.argv[1:]:
+        from sas.system import console
         console.setup_console()
 
     # Eventually argument processing might affect logger or config, so do it first
-    cli = parse_cli(sys.argv)
+    opts = parse_cli(sys.argv)
+
+    # Move config files from .sasview to the platform specific user config directory
+    from sas.system.user import copy_old_files_to_new_location
+    copy_old_files_to_new_location()
+
+    # Now we can load the config files and setup the sasview environment
+    from sas.system import lib, log
 
     # Setup logger and sasmodels
-    if cli.loglevel:
-        logging = cli.loglevel
-    logging = logging.upper()
-    if logging == "PRODUCTION":
+    log_mode = opts.loglevel if opts.loglevel else "development" if dev_mode else "production"
+    log_mode = log_mode.upper()
+    if log_mode == "PRODUCTION":
         log.production()
-    elif logging == "DEVELOPMENT":
+    elif log_mode == "DEVELOPMENT":
         log.development()
-    elif logging.upper() in {'DEBUG', 'INFO', 'WARN', 'ERROR', 'CRITICAL'}:
-        log.setup_logging(logging)
+    elif log_mode.upper() in {'DEBUG', 'INFO', 'WARN', 'ERROR', 'CRITICAL'}:
+        log.setup_logging(log_mode)
     else:
-        raise ValueError(f"Unknown logging mode \"{logging}\"")
+        raise ValueError(f"Unknown logging mode \"{log_mode}\"")
 
     lib.setup_sasmodels()
     lib.setup_qt_env() # Note: does not import any gui libraries
 
-    if cli.version: # -V
+    if opts.version: # -V
         import sas
         print(f"SasView {sas.__version__}")
         # Exit immediately after -V.
         return 0
 
     context = {'exit': sys.exit}
-    if cli.module: # -m module [arg...]
+    if opts.module: # -m module [arg...]
         import runpy
         # TODO: argv[0] should be the path to the module file not the dotted name
-        sys.argv = [cli.module, *cli.args]
-        context = runpy.run_module(cli.module, run_name="__main__")
-    elif cli.command: # -c "command"
-        sys.argv = ["-c", *cli.args]
-        exec(cli.command, context)
-    elif cli.args: # script [arg...]
+        sys.argv = [opts.module, *opts.args]
+        context = runpy.run_module(opts.module, run_name="__main__")
+    elif opts.command: # -c "command"
+        sys.argv = ["-c", *opts.args]
+        exec(opts.command, context)
+    elif opts.args: # script [arg...]
         import runpy
-        sys.argv = cli.args
-        context = runpy.run_path(cli.args[0], run_name="__main__")
-    elif not cli.interactive: # no arguments so start the GUI
+        sys.argv = opts.args
+        context = runpy.run_path(opts.args[0], run_name="__main__")
+    elif not opts.interactive: # no arguments so start the GUI
+        if dev_mode:
+            logging.info("rebuilding UI")
+            from sas.qtgui.convertUI import rebuild_new_ui
+            rebuild_new_ui()
+
         from sas.qtgui.MainWindow.MainWindow import run_sasview
         # sys.argv is unchanged
         # Maybe hand cli.quiet to run_sasview?
@@ -167,13 +187,13 @@ def main(logging="production"):
     #     from IPython import start_ipython
     #     sys.argv = ["ipython", *args]
     #     sys.exit(start_ipython())
-    if cli.interactive:
+    if opts.interactive:
         import code
         # The python banner is something like
         #     f"Python {sys.version} on {platform.system().lower()}"
         # where sys.version contains "VERSION (HGTAG, DATE)\n[COMPILER]"
         # We are replacing it with something that includes the sasview version.
-        if cli.quiet:
+        if opts.quiet:
             exitmsg = banner = ""
         else:
             import platform
--- src/sas/system/user.py
+++ src/sas/system/user.py
@@ -171,10 +171,11 @@ def module_copytree(module: str, src: PATH_LIKE, dest: PATH_LIKE) -> None:
         if resource.is_dir():
             # recurse into the directory
             module_copytree(module, s_name, f_name)
-        elif resource.is_file() and not f_name.exists():
-            logger.debug("Copied: %s", s_name)
-            with open(f_name, "wb") as dh:
-                dh.write(resource.read_bytes())
+        elif resource.is_file():
+            if not f_name.exists():
+                logger.debug("Copied: %s", s_name)
+                with open(f_name, "wb") as dh:
+                    dh.write(resource.read_bytes())
         else:
             logger.warning("Skipping %s (unknown type)", str(s_name))
 
