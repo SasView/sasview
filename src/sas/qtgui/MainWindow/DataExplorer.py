4c080ff3de714ac08dd4bd37d8fbe83ea6da3a64
rozyczko <piotr.rozyczko@gmail.com>
2025-11-12 15:27:49 +0100

    we need the unused import - force ruff

--- src/sas/qtgui/MainWindow/DataExplorer.py
+++ src/sas/qtgui/MainWindow/DataExplorer.py
@@ -24,7 +24,10 @@ from sas.qtgui.MainWindow.NameChanger import ChangeName
 from sas.qtgui.Plotting.MaskEditor import MaskEditor
 
 # DO NOT REMOVE THE IMPORT BELOW - used in dynamic plotter creation
-from sas.qtgui.Plotting.Plotter import PlotterWidget
+from sas.qtgui.Plotting.Plotter import (
+    Plotter,  # noqa: F401
+    PlotterWidget,
+)
 from sas.qtgui.Plotting.Plotter2D import Plotter2D, Plotter2DWidget
 from sas.qtgui.Plotting.PlotterData import Data1D, Data2D, DataRole
 
