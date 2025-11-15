9c7b56e7b262b73a2fa1cb0c87e2623d1289c112
Christian <christian.woegerbauer@tum.de>
2025-11-15 16:17:49 +0100

    update requested code structure

--- src/sas/qtgui/MainWindow/DataExplorer.py
+++ src/sas/qtgui/MainWindow/DataExplorer.py
@@ -815,7 +815,7 @@ class DataExplorerWindow(DroppableDataLoadWidget):
             _ = msgbox.exec_()
 
     def freezeFromName(self, search_name = None):
-        def find_name(model, target_name: str, column: int=0)-> tuple:
+        def findName(model, target_name: str, column: int=0)-> tuple:
             for row in range(model.rowCount()):
                 for row2 in range(model.item(row, column).rowCount()):
                     tmp = model.item(row, column).child(row2, column)
@@ -824,7 +824,7 @@ class DataExplorerWindow(DroppableDataLoadWidget):
             return -1, -1
         
         model = self.model
-        row_index_parent, row_index_child = find_name(model, search_name)
+        row_index_parent, row_index_child = findName(model, search_name)
         data_dir = model.item(row_index_parent)
         data = data_dir.child(row_index_child)
         new_item = self.cloneTheory(data)
@@ -832,7 +832,7 @@ class DataExplorerWindow(DroppableDataLoadWidget):
         model.appendRow(new_item)
         model.endResetModel()
         self.sendData(None, [new_item])
-        
+
     def sendData(self, event=None, selected_items = None):
         """
         Send selected item data to the current perspective and set the relevant notifiers
