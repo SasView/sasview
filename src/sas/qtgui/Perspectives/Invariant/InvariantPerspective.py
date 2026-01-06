0b73b6e41c6599ce7029ca5829e2c1b259662184
jellybean2004 <sujayashr18@gmail.com>
2026-01-06 16:36:48 +0000

    Changes plot line colors

--- src/sas/qtgui/Perspectives/Invariant/InvariantPerspective.py
+++ src/sas/qtgui/Perspectives/Invariant/InvariantPerspective.py
@@ -327,6 +327,7 @@ class InvariantWindow(QtWidgets.QDialog, Ui_tabbedInvariantUI, Perspective):
         if self.high_extrapolation_plot:
             self.high_extrapolation_plot.plot_role = DataRole.ROLE_DEFAULT
             self.high_extrapolation_plot.symbol = "Line"
+            self.high_extrapolation_plot.custom_color = "#2ca02c"
             self.high_extrapolation_plot.show_errors = False
             self.high_extrapolation_plot.show_q_range_sliders = True
             self.high_extrapolation_plot.slider_update_on_move = False
@@ -342,6 +343,7 @@ class InvariantWindow(QtWidgets.QDialog, Ui_tabbedInvariantUI, Perspective):
         if self.low_extrapolation_plot:
             self.low_extrapolation_plot.plot_role = DataRole.ROLE_DEFAULT
             self.low_extrapolation_plot.symbol = "Line"
+            self.low_extrapolation_plot.custom_color = "#ff7f0e"
             self.low_extrapolation_plot.show_errors = False
             self.low_extrapolation_plot.show_q_range_sliders = True
             self.low_extrapolation_plot.slider_update_on_move = False
