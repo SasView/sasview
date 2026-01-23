a068362c02fc227ae57a3acf5a904f3b5659ea61
krzywon <jkrzywon@nist.gov>
2026-01-23 11:32:38 -0500

    Update release date in version.py

--- src/sas/system/version.py
+++ src/sas/system/version.py
@@ -3,7 +3,7 @@ try:
 except ImportError:
     __version__ = "6.1.3"
 
-__release_date__ = "2025"
+__release_date__ = "2026"
 __build__ = "GIT_COMMIT"
 
 __all__ = ["__build__", "__release_date__", "__version__"]
