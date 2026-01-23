07090ef23a1c98b79c114cc4a1ea45e2d34f4c9e
krzywon <jkrzywon@nist.gov>
2026-01-23 10:35:10 -0500

    Move the about text from the About box to sas.system.legal so multiple versions of the same text aren't in the code.

--- src/sas/system/legal.py
+++ src/sas/system/legal.py
@@ -5,6 +5,22 @@ class Legal:
     def __init__(self):
         year = datetime.datetime.now().year
         self.copyright = f"Copyright (c) 2009-{year}, SasView Developers"
+        self.about = """
+                <html>
+                    <head/>
+                    <body>
+                        <p>
+                            This work originally developed as part of the DANSE project funded by the NSF under
+                             grant DMR-0520547, and currently maintained by UTK, NIST, UMD, ORNL, ISIS, ESS, ILL,
+                             ANSTO, TU Delft, DLS, SciLifeLab, and the scattering community.
+                        </p>
+                        <p>
+                            SasView also contains code developed with funding from the EU Horizon 2020 programme under
+                             the SINE2020 project (Grant No 654000).
+                        </p>
+                    </body>
+                </html>"""
+
 
 
 legal = Legal()
