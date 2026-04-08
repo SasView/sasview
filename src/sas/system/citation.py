673c0f963815e2e7351621918cb7eab5fb797b6b
krzywon <jkrzywon@nist.gov>
2026-04-08 12:02:57 -0400

    Rename zenodo.py to citation.py and add the release manager and acknowledgement text to it.

---
+++ src/sas/system/citation.py
@@ -0,0 +1,5 @@
+__DOI__ = "10.5281/zenodo.17795298"
+__RELEASE_MANAGER__ = "{}"
+__ACKNOWLEDGEMENT__ = "This work benefited from the use of the SasView application, originally developed " \
+                      "under NSF Award DMR - 0520547. SasView also contains code developed with funding" \
+                      " from the EU Horizon 2020 programme under the SINE2020 project Grant No 654000."
