3230efcb5bcbb6e19e3c6895dd24a082b38da3b1
Wojtek Potrzebowski <wojtek.potrzebowski@scilifelab.se>
2026-07-13 15:25:59 +0200

    Cleaned up and separated code from GuiManager

--- src/sas/qtgui/Utilities/SASBDB/__init__.py
+++ src/sas/qtgui/Utilities/SASBDB/__init__.py
@@ -5,3 +5,5 @@ This package provides functionality for interacting with SASBDB,
 including downloading datasets and exporting data to SASBDB format.
 """
 
+from .sasbdb_loader import load_downloaded_dataset, metadata_summary, populate_metadata
+
