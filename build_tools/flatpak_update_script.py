52be7bb83a4650ed4e181dcd3370104076b293f8
James Crake-Merani <james.crake-merani@stfc.ac.uk>
2026-08-13 14:08:14 +0100

    Open for writing.

--- build_tools/flatpak_update_script.py
+++ build_tools/flatpak_update_script.py
@@ -54,7 +54,7 @@ def process_requirements_file(file_path: Path, output_directory: Path) -> Path:
     new_lines = [l for l in lines if "-r" not in l]
     new_path = output_directory / Path("processed_requirements") / file_path.name
     new_path.parent.mkdir(parents=True, exist_ok=True)
-    with open(new_path) as f:
+    with open(new_path, "w") as f:
         f.writelines(new_lines)
     return new_path
 
