edbc4f472ddf74837ed176553e52e9e7ee0c053e
James Crake-Merani <james.crake-merani@stfc.ac.uk>
2026-08-27 14:50:23 +0100

    The data explorer method now merely registers trends.

--- src/sas/data_explorer_tree.py
+++ src/sas/data_explorer_tree.py
@@ -141,7 +141,7 @@ class DataExplorerTree(QTreeWidget):
                 creation_result = trend_creation_dialog.exec()
                 if creation_result == QDialog.DialogCode.Accepted:
                     # TODO: Need to pass in axes
-                    self._data_manager.make_trend(trend_data)
+                    self._data_manager.register_trend(trend_creation_dialog.trend)
         if len(errors):
             box = DataExplorerErrorMessage(self, errors)
             box.show()
