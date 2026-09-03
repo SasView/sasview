8788fb95a3733c3cc558f73a7f530ac5704f183c
James Crake-Merani <james.crake-merani@stfc.ac.uk>
2026-09-03 08:59:40 +0100

    Don't need Path here.

--- build_tools/flatpak_update_script.py
+++ build_tools/flatpak_update_script.py
@@ -77,7 +77,7 @@ def process_requirements_file(file_path: Path, output_directory: Path) -> Path:
     with open(file_path) as f:
         lines = f.readlines()
     new_lines = [l for l in lines if "-r " not in l and "-e " not in l]
-    new_path = output_directory / Path("processed_requirements") / file_path.name
+    new_path = output_directory / "processed_requirements" / file_path.name
     new_path.parent.mkdir(parents=True, exist_ok=True)
     with open(new_path, "w") as f:
         f.writelines(new_lines)
