b3c580225aee5764e4d317551eb2fde0bd21bd7b
James Crake-Merani <james.crake-merani@stfc.ac.uk>
2026-09-07 09:54:16 +0100

    Stop circular import.

--- src/sas/refactored.py
+++ src/sas/refactored.py
@@ -1,4 +1,3 @@
-from sas.data_manager import TrackedData
 from sasdata.trend import Trend
 import logging
 from abc import abstractmethod
@@ -13,6 +12,7 @@ from sasdata.trend import Trend
 
 if TYPE_CHECKING:
     from sas.data_manager import NewDataManager as DataManager
+    from sas.data_manager import TrackedData
 
 # TODO: None of these classes belong in here. This is a temporary location of
 # them so I can sketch out what they should look like.
@@ -38,7 +38,7 @@ class Perspective(QDialog):
 
     @property
     @abstractmethod
-    def supported_data(self) -> set[type[TrackedData]]:
+    def supported_data(self) -> set[type["TrackedData"]]:
         """The types of data that can be sent to the perspective"""
 
     @property
