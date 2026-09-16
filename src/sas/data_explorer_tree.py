3e1eb5dafa31b252b29b6691d2c39d891e54bdae
James Crake-Merani <james.crake-merani@stfc.ac.uk>
2026-09-16 15:21:16 +0100

    Movedc NamedTrend to SasData.

--- src/sas/data_explorer_tree.py
+++ src/sas/data_explorer_tree.py
@@ -6,7 +6,7 @@ from PySide6.QtGui import QCursor
 from PySide6.QtWidgets import QAbstractItemView, QDialog, QTreeWidget, QTreeWidgetItem, QWidget
 
 from sasdata.data import SasData
-from sasdata.trend import Trend
+from sasdata.trend import Trend, NamedTrend
 
 from sas.data_explorer_error_message import DataExplorerErrorMessage
 from sas.data_explorer_menu import DataExplorerMenu, DataExplorerMenuAction
@@ -14,7 +14,7 @@ from sas.data_manager import NewDataManager as DataManager
 from sas.data_manager import TrackedData, isinstance_fix
 from sas.qtgui.MainWindow.DataViewer import DataViewer
 from sas.qtgui.MainWindow.TrendCreation import TrendCreation
-from sas.refactored import NamedTrend, Perspective
+from sas.refactored import Perspective
 
 
 # TODO: Is this the right place for this?
