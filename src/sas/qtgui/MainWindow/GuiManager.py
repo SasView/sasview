c088dca2c459fd8aa255c74b38c18d432bb51acf
rozyczko <piotr.rozyczko@gmail.com>
2026-09-24 14:10:26 +0200

    modified the conditional to a fancy new walrus thingamajig

--- src/sas/qtgui/MainWindow/GuiManager.py
+++ src/sas/qtgui/MainWindow/GuiManager.py
@@ -1390,7 +1390,7 @@ class GuiManager:
         Detach the active window from the workspace, or attach it back
         """
         workspace_manager = self.workspace_manager
-        if widget := workspace_manager.active_widget() is None:
+        if (widget := workspace_manager.active_widget()) is None:
             return
         workspace_manager.toggle(widget)
         self.updateDetachWindowAction()
